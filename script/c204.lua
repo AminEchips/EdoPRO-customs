a5f1367a337c8313053280608e2db189563940f3
Chips <aminekhoury0411@gmail.com>
2025-04-14 19:19:36 -0400

    Add files via upload

---
+++ script/c204.lua
@@ -0,0 +1,85 @@
+-- Neo Space Contact
+local s,id=GetID()
+
+function s.initial_effect(c)
+    -- Activate + effect
+    local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+    e1:SetCost(s.cost)
+    e1:SetTarget(s.target)
+    e1:SetOperation(s.activate)
+    c:RegisterEffect(e1)
+end
+
+s.listed_names={89943723} -- Elemental HERO Neos
+
+-- Filter for cards that are Neos or mention Neos
+function s.filter(c)
+    return (c:IsCode(89943723) or (c:ListsCode and c:ListsCode(89943723)))
+        and not c:IsCode(id) and c:IsAbleToGrave()
+end
+
+function s.neospacian_filter(c,e,tp)
+    return c:IsSetCard(0x1f) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+
+function s.neos_mention_search(c)
+    return ((c:IsCode(89943723) or (c:ListsCode and c:ListsCode(89943723))) and not c:IsCode(id))
+        and c:IsAbleToHand()
+end
+
+-- Store what card was sent
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+    local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil)
+    local tc=g:GetFirst()
+    if tc then
+        Duel.SendtoGrave(tc,REASON_COST)
+        e:SetLabelObject(tc)
+    end
+end
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    local tc=e:GetLabelObject()
+    if not tc then return false end
+    if chk==0 then return true end
+    if tc:IsType(TYPE_MONSTER) then
+        Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_GRAVE)
+    else
+        Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+    end
+end
+
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+    -- Lock to only Fusion Summons from Extra Deck
+    local e1=Effect.CreateEffect(e:GetHandler())
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+    e1:SetTargetRange(1,0)
+    e1:SetTarget(function(_,c) return c:IsLocation(LOCATION_EXTRA) and not c:IsType(TYPE_FUSION) end)
+    e1:SetReset(RESET_PHASE+PHASE_END)
+    Duel.RegisterEffect(e1,tp)
+
+    local tc=e:GetLabelObject()
+    if not tc then return end
+    if tc:IsType(TYPE_MONSTER) then
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+        local g=Duel.SelectMatchingCard(tp,s.neospacian_filter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,e,tp)
+        if #g>0 then
+            Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+        end
+    else
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+        local g=Duel.SelectMatchingCard(tp,s.neos_mention_search,tp,LOCATION_DECK,0,1,1,nil)
+        if #g>0 then
+            Duel.SendtoHand(g,nil,REASON_EFFECT)
+            Duel.ConfirmCards(1-tp,g)
+        end
+    end
+end
