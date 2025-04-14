83eefeeb25e936b31c91064e8b8a0d9739ef6400
Chips <aminekhoury0411@gmail.com>
2025-04-14 17:40:56 -0400

    Update c203.lua

--- script/c203.lua
+++ script/c203.lua
@@ -23,13 +23,13 @@ function s.initial_effect(c)
     e2:SetOperation(s.operation2)
     c:RegisterEffect(e2)
 
-    -- Effect 3: During the End Phase, place "Neo Space" from your hand to the Field Zone (Field Spell)
+    -- Effect 3: During the End Phase, place "Neo Space" from your hand to the Spell/Trap Zone
     local e3=Effect.CreateEffect(c)
     e3:SetDescription(aux.Stringid(id,2))
     e3:SetCategory(CATEGORY_TOFIELD)
-    e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-    e3:SetCode(EVENT_PHASE+PHASE_END)
-    e3:SetRange(LOCATION_GRAVE)
+    e3:SetType(EFFECT_TYPE_ACTIVATE)  -- Correcting the effect type to activate it directly
+    e3:SetCode(EVENT_FREE_CHAIN)  -- Allow activation from hand
+    e3:SetRange(LOCATION_HAND)  -- Activate from hand
     e3:SetCountLimit(1)
     e3:SetTarget(s.target3)
     e3:SetOperation(s.operation3)
@@ -72,7 +72,7 @@ function s.operation2(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
--- Effect 3: Place "Neo Space" from hand to Field Zone
+-- Effect 3: Place "Neo Space" from hand to Spell/Trap Zone
 function s.target3(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then
         return Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_HAND,0,1,nil,91427878) -- Neo Space's code
@@ -83,7 +83,7 @@ end
 function s.operation3(e,tp,eg,ep,ev,re,r,rp)
     local g=Duel.SelectMatchingCard(tp,Card.IsCode,tp,LOCATION_HAND,0,1,1,nil,91427878) -- Neo Space's code
     if #g>0 then
-        -- Move Neo Space to the Field Zone as a Field Spell
-        Duel.MoveToField(g:GetFirst(),tp,tp,LOCATION_FZONE,POS_FACEUP,true)
+        -- Move Neo Space to the Spell/Trap Zone as a Field Spell
+        Duel.MoveToField(g:GetFirst(),tp,tp,LOCATION_SZONE,POS_FACEUP,true)
     end
 end
