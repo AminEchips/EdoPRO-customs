e012f68389f56a12bccbb13609543f9b7d5b9d1b
BrandedChips <aminekhoury0411@gmail.com>
2025-04-14 10:05:41 -0400

    Add files via upload

--- script/c200.lua
+++ script/c200.lua
@@ -1,12 +1,7 @@
 --Elemental HERO Cosmic Soldier
 local s,id=GetID()
 function s.initial_effect(c)
-    c:EnableReviveLimit()
-    local e0=Effect.CreateEffect(c)
-    e0:SetType(EFFECT_TYPE_SINGLE)
-    e0:SetCode(EFFECT_ADD_SETCODE)
-    e0:SetValue(0x1f)
-    c:RegisterEffect(e0)
+        -- Special Summon from hand
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -14,8 +9,11 @@ function s.initial_effect(c)
     e1:SetRange(LOCATION_HAND)
     e1:SetCountLimit(1,id)
     e1:SetCost(s.spcost)
+    e1:SetTarget(s.sptg)
     e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
+
+    -- Add Polymerization
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
     e2:SetCategory(CATEGORY_TOHAND)
@@ -28,48 +26,71 @@ function s.initial_effect(c)
     c:RegisterEffect(e2)
 end
 
+-- Filter for Neo-Spacian or Elemental HERO Neos
 function s.spcfilter(c)
-    return c:IsAbleToGrave() and (c:IsSetCard(0x1f) or c:IsCode(89943723))
+    return c:IsAbleToGraveAsCost() and (c:IsSetCard(0x1f) or c:IsCode(89943723))
 end
+
+-- Send a Neo-Spacian or Neos to GY as cost
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.IsExistingMatchingCard(s.spcfilter,tp,LOCATION_DECK,0,1,nil) end
-end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
     local g=Duel.SelectMatchingCard(tp,s.spcfilter,tp,LOCATION_DECK,0,1,1,nil)
-    if #g>0 and Duel.SendtoGrave(g,REASON_EFFECT)>0 and c:IsRelateToEffect(e) then
-        if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
-            local tc=g:GetFirst()
-            local e1=Effect.CreateEffect(c)
-            e1:SetType(EFFECT_TYPE_SINGLE)
-            e1:SetCode(EFFECT_CHANGE_CODE)
-            e1:SetValue(tc:GetCode())
-            e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-            c:RegisterEffect(e1)
-            local e2=e1:Clone()
-            e2:SetCode(EFFECT_CHANGE_ATTRIBUTE)
-            e2:SetValue(tc:GetAttribute())
-            c:RegisterEffect(e2)
-        end
+    e:SetLabel(g:GetFirst():GetCode()) -- store the sent card's code
+    e:SetLabelObject(g:GetFirst())     -- store the sent card object
+    Duel.SendtoGrave(g,REASON_COST)
+end
+
+-- Target itself for Special Summon
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+
+-- Special Summon itself and copy name/attribute of sent card
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local tc=e:GetLabelObject()
+    if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 and tc then
+        local code=tc:GetCode()
+        local attr=tc:GetAttribute()
+        -- Change name
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_CHANGE_CODE)
+        e1:SetValue(code)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+        c:RegisterEffect(e1)
+        -- Change attribute
+        local e2=e1:Clone()
+        e2:SetCode(EFFECT_CHANGE_ATTRIBUTE)
+        e2:SetValue(attr)
+        c:RegisterEffect(e2)
     end
 end
 
+-- Cost for tributing 1 other HERO or Neo-Spacian
 function s.cfilter(c)
     return c:IsFaceup() and (c:IsSetCard(0x8) or c:IsSetCard(0x1f))
 end
+
 function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.CheckReleaseGroup(tp,s.cfilter,1,nil) end
     local g=Duel.SelectReleaseGroup(tp,s.cfilter,1,1,nil)
     Duel.Release(g,REASON_COST)
 end
+
+-- Search Polymerization
 function s.thfilter(c)
     return c:IsCode(24094653) and c:IsAbleToHand()
 end
+
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
     Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
 end
+
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
     local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
@@ -79,5 +100,5 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
-s.listed_names={89943723}
+s.listed_names={89943723,24094653}
 s.listed_series={0x8, 0x1f}
