e38702eb36584f2ced0a05efe801dff304160713
Chips <aminekhoury0411@gmail.com>
2025-04-14 19:39:52 -0400

    Update c204.lua

--- script/c204.lua
+++ script/c204.lua
@@ -9,14 +9,23 @@ function s.initial_effect(c)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_FREE_CHAIN)
     e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+    e1:SetCost(s.cost)
     e1:SetTarget(s.target)
     e1:SetOperation(s.activate)
     c:RegisterEffect(e1)
+
+    -- Register custom activity counter for summon restrictions
+    Duel.AddCustomActivityCounter(id,ACTIVITY_SPSUMMON,s.counterfilter)
 end
 
 s.listed_names={89943723} -- Elemental HERO Neos
 
--- Filter for cards that are Neos or mention Neos
+-- Allow only Fusion from Extra Deck
+function s.counterfilter(c)
+    return not c:IsSummonLocation(LOCATION_EXTRA) or c:IsType(TYPE_FUSION)
+end
+
+-- Filter for Neos or cards that mention it
 function s.filter(c)
     return (c:IsCode(89943723) or (c.ListsCode and c:ListsCode(89943723)))
         and not c:IsCode(id) and c:IsAbleToGrave()
@@ -26,34 +35,35 @@ function s.neospacian_filter(c,e,tp)
     return c:IsSetCard(0x1f) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 
--- ONLY allows Neos or Spell/Trap that mentions Neos (not monsters)
 function s.neos_mention_search(c)
     return ((c:IsCode(89943723)) or
         (not c:IsType(TYPE_MONSTER) and c.ListsCode and c:ListsCode(89943723)))
         and not c:IsCode(id) and c:IsAbleToHand()
 end
 
--- Target check for activation (valid card to send)
+-- Activation restriction: no non-Fusion Extra Deck summon before activation
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetCustomActivityCount(id,tp,ACTIVITY_SPSUMMON)==0 end
+
+    -- Apply lock after activation
+    local e1=Effect.CreateEffect(e:GetHandler())
+    e1:SetDescription(aux.Stringid(id,1))
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH+EFFECT_FLAG_CLIENT_HINT)
+    e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+    e1:SetTargetRange(1,0)
+    e1:SetTarget(function(_,c) return c:IsLocation(LOCATION_EXTRA) and not c:IsType(TYPE_FUSION) end)
+    e1:SetReset(RESET_PHASE+PHASE_END)
+    Duel.RegisterEffect(e1,tp)
+end
+
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then
         return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil)
     end
 end
 
--- Main operation: mill + branch + Fusion lock
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-    -- Lock to only Fusion Summons from Extra Deck
-    local e1=Effect.CreateEffect(e:GetHandler())
-    e1:SetType(EFFECT_TYPE_FIELD)
-    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-    e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-    e1:SetTargetRange(1,0)
-    e1:SetTarget(function(_,c)
-        return c:IsLocation(LOCATION_EXTRA) and not c:IsType(TYPE_FUSION)
-    end)
-    e1:SetReset(RESET_PHASE+PHASE_END)
-    Duel.RegisterEffect(e1,tp)
-
     -- Select card to send
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
     local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil)
@@ -76,4 +86,5 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
         end
     end
 end
+
 
