dac3c146cc4cf605efe16cf84619d993f3e66f4c
BrandedChips <aminekhoury0411@gmail.com>
2025-04-14 10:37:50 -0400

    Update c200.lua

--- script/c200.lua
+++ script/c200.lua
@@ -1,7 +1,10 @@
---Elemental HERO Cosmic Soldier
+--Elemental HERO Cosmic Soldier (Debug Version)
 local s,id=GetID()
 function s.initial_effect(c)
     c:EnableReviveLimit()
+
+    -- Debug log
+    Debug.Message("Cosmic Soldier script loaded")
 
     -- Special Summon from hand
     local e1=Effect.CreateEffect(c)
@@ -15,7 +18,7 @@ function s.initial_effect(c)
     e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
 
-    -- Add Polymerization
+    -- Search Polymerization
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
     e2:SetCategory(CATEGORY_TOHAND)
@@ -28,68 +31,63 @@ function s.initial_effect(c)
     c:RegisterEffect(e2)
 end
 
--- Prevent the card itself from being sent to GY for cost
+-- First effect: send Neos or Neo-Spacian from Deck to GY (excluding self)
 function s.spcfilter(c)
     return c:IsAbleToGraveAsCost() and (c:IsSetCard(0x1f) or c:IsCode(89943723)) and not c:IsCode(200)
 end
-
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.IsExistingMatchingCard(s.spcfilter,tp,LOCATION_DECK,0,1,nil) end
+    Debug.Message("Paying Special Summon cost...")
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
     local g=Duel.SelectMatchingCard(tp,s.spcfilter,tp,LOCATION_DECK,0,1,1,nil)
-    e:SetLabel(g:GetFirst():GetCode())
     e:SetLabelObject(g:GetFirst())
     Duel.SendtoGrave(g,REASON_COST)
 end
-
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
         and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
-
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     local tc=e:GetLabelObject()
+    Debug.Message("Special Summon executing...")
     if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 and tc then
         local code=tc:GetCode()
         local attr=tc:GetAttribute()
-        -- Change name
         local e1=Effect.CreateEffect(c)
         e1:SetType(EFFECT_TYPE_SINGLE)
         e1:SetCode(EFFECT_CHANGE_CODE)
         e1:SetValue(code)
         e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
         c:RegisterEffect(e1)
-        -- Change attribute
         local e2=e1:Clone()
         e2:SetCode(EFFECT_CHANGE_ATTRIBUTE)
         e2:SetValue(attr)
         c:RegisterEffect(e2)
+        Debug.Message("Copied code: "..code.." and attribute: "..attr)
     end
 end
 
--- Prevent the card from tributing itself
+-- Second effect: tribute another HERO or Neo-Spacian (not self)
 function s.cfilter(c,e)
     return c:IsFaceup() and (c:IsSetCard(0x8) or c:IsSetCard(0x1f)) and c~=e:GetHandler()
 end
-
 function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    Debug.Message("Checking tribute cost...")
     if chk==0 then return Duel.CheckReleaseGroup(tp,function(c) return s.cfilter(c,e) end,1,nil) end
     local g=Duel.SelectReleaseGroup(tp,function(c) return s.cfilter(c,e) end,1,1,nil)
     Duel.Release(g,REASON_COST)
 end
-
 function s.thfilter(c)
     return c:IsCode(24094653) and c:IsAbleToHand()
 end
-
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
     Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
 end
-
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
+    Debug.Message("Adding Polymerization to hand...")
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
     local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
     if #g>0 then
