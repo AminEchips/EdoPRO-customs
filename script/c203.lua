77a580d3dd25d7c2afe4d0572f6b43699213bb72
Chips <aminekhoury0411@gmail.com>
2025-04-14 18:35:59 -0400

    Update c203.lua

--- script/c203.lua
+++ script/c203.lua
@@ -1,104 +1,110 @@
 -- EN - Evolution Neo Space
 local s,id=GetID()
-
 function s.initial_effect(c)
-    -- Effect 1: Activate from hand and place in Spell/Trap Zone as a Continuous Spell
+    -- Activate (Continuous Spell)
+    local e0=Effect.CreateEffect(c)
+    e0:SetType(EFFECT_TYPE_ACTIVATE)
+    e0:SetCode(EVENT_FREE_CHAIN)
+    c:RegisterEffect(e0)
+
+    -- Effect 1: Send Fusion → Summon Neo-Spacian with same Attribute from GY
     local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_ACTIVATE)
-    e1:SetCode(EVENT_FREE_CHAIN)
-    e1:SetTarget(s.activate_target)
-    e1:SetOperation(s.activate_operation)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetRange(LOCATION_SZONE)
+    e1:SetCountLimit(1,id)
+    e1:SetTarget(s.tg1)
+    e1:SetOperation(s.op1)
     c:RegisterEffect(e1)
 
-    -- Effect 2: Continuous effect while on the field
+    -- Effect 2: When Neos-related Fusion sent to GY → Shuffle back into Extra Deck
     local e2=Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_FIELD)
-    e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_TODECK)
+    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_TO_GRAVE)
     e2:SetRange(LOCATION_SZONE)
-    e2:SetTargetRange(LOCATION_MZONE,0)
-    e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x8))  -- Targets "Neo-Spacian" monsters
-    e2:SetValue(aux.tgoval)
+    e2:SetCountLimit(1,{id,1})
+    e2:SetCondition(s.tdcon)
+    e2:SetTarget(s.tdtg)
+    e2:SetOperation(s.tdop)
     c:RegisterEffect(e2)
 
-    -- Effect 3: If Fusion Monster(s) that mentions "Elemental HERO Neos" is sent to the GY, shuffle them into the Extra Deck
+    -- Effect 3: During End Phase → Place Neo Space in Field Zone
     local e3=Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(id,1))
-    e3:SetCategory(CATEGORY_TODECK)
-    e3:SetType(EFFECT_TYPE_TRIGGER_O)
-    e3:SetCode(EVENT_SEND_TO_GRAVE)
+    e3:SetDescription(aux.Stringid(id,2))
+    e3:SetCategory(CATEGORY_TOFIELD)
+    e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e3:SetCode(EVENT_PHASE+PHASE_END)
     e3:SetRange(LOCATION_SZONE)
-    e3:SetCondition(s.condition2)
-    e3:SetTarget(s.target2)
-    e3:SetOperation(s.operation2)
+    e3:SetCountLimit(1,{id,2})
+    e3:SetTarget(s.fztg)
+    e3:SetOperation(s.fzop)
     c:RegisterEffect(e3)
-
-    -- Effect 4: During End Phase, place "Neo Space" from Deck or GY into the Field Zone
-    local e4=Effect.CreateEffect(c)
-    e4:SetDescription(aux.Stringid(id,2))
-    e4:SetCategory(CATEGORY_TOFIELD)
-    e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-    e4:SetCode(EVENT_PHASE+PHASE_END)
-    e4:SetRange(LOCATION_SZONE)  -- Only activates when in Spell/Trap Zone
-    e4:SetCountLimit(1)
-    e4:SetTarget(s.target3)
-    e4:SetOperation(s.operation3)
-    c:RegisterEffect(e4)
 end
 
--- Effect 1: Activate from hand and place in Spell/Trap Zone as a Continuous Spell
-function s.activate_target(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then
-        -- Can always activate from hand
-        return true
+-- Effect 1: Send Fusion → Special Summon Neo-Spacian with same Attribute
+function s.fusionfilter(c,tp)
+    return c:IsType(TYPE_FUSION) and c:IsAbleToGrave() and c:IsFaceup()
+        and Duel.IsExistingMatchingCard(s.neofilter,tp,LOCATION_GRAVE,0,1,nil,c:GetAttribute())
+end
+function s.neofilter(c,attr)
+    return c:IsSetCard(0x1f) and c:IsCanBeSpecialSummoned(nil,0,tp,false,false) and c:IsAttribute(attr)
+end
+function s.tg1(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.fusionfilter,tp,LOCATION_MZONE,0,1,nil,tp) end
+    Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_MZONE)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
+function s.op1(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+    local tc=Duel.SelectMatchingCard(tp,s.fusionfilter,tp,LOCATION_MZONE,0,1,1,nil,tp):GetFirst()
+    if tc and Duel.SendtoGrave(tc,REASON_EFFECT)>0 then
+        Duel.BreakEffect()
+        local g=Duel.SelectMatchingCard(tp,s.neofilter,tp,LOCATION_GRAVE,0,1,1,nil,tc:GetAttribute())
+        if #g>0 then
+            Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+        end
     end
-    Duel.SetOperationInfo(0,CATEGORY_TOFIELD,nil,1,tp,LOCATION_HAND)  -- Move from hand to Spell/Trap Zone
 end
 
-function s.operation_activate(e,tp,eg,ep,ev,re,r,rp)
-    -- Move the card to the Spell/Trap Zone as a Continuous Spell
-    local c=e:GetHandler()
-    Duel.MoveToField(c,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+-- Effect 2: If Neos Fusion is sent to GY → shuffle into Extra Deck
+function s.neosfusionfilter(c)
+    return c:IsType(TYPE_FUSION) and c:IsLocation(LOCATION_GRAVE)
+        and c:IsAbleToExtra() and c:ListsCode(89943723)
 end
-
--- Effect 2: Continuous effect while on the field
--- This effect ensures "Neo-Spacian" monsters cannot be targeted
-function s.condition2(e,tp,eg,ep,ev,re,r,rp)
-    -- Condition: Check if Fusion Monsters related to "Elemental HERO Neos" are sent to the GY
-    return eg:IsExists(function(c)
-        return c:IsType(TYPE_FUSION) and c:IsSetCard(0x8) and c:IsCode(89943723)  -- Check for "Elemental HERO Neos"
-    end,1,nil)
+function s.tdcon(e,tp,eg,ep,ev,re,r,rp)
+    return eg:IsExists(s.neosfusionfilter,1,nil)
 end
-
-function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then
-        -- Check if there are Fusion Monsters related to "Elemental HERO Neos"
-        return Duel.IsExistingMatchingCard(aux.FilterBoolFunction(Card.IsFusionType,TYPE_FUSION),tp,LOCATION_GRAVE,0,1,nil)
-    end
-    Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_GRAVE)
+function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return eg:IsExists(s.neosfusionfilter,1,nil) end
+    local g=eg:Filter(s.neosfusionfilter,nil)
+    Duel.SetTargetCard(g)
+    Duel.SetOperationInfo(0,CATEGORY_TODECK,g,#g,0,0)
 end
-
-function s.operation2(e,tp,eg,ep,ev,re,r,rp)
-    -- Shuffle Fusion Monsters related to "Elemental HERO Neos" into the Extra Deck
-    local g=Duel.GetMatchingGroup(aux.FilterBoolFunction(Card.IsFusionType,TYPE_FUSION),tp,LOCATION_GRAVE,0,nil)
+function s.tdop(e,tp,eg,ep,ev,re,r,rp)
+    local g=Duel.GetTargetCards(e)
     if #g>0 then
-        Duel.SendtoDeck(g,nil,2,REASON_EFFECT)
+        Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
     end
 end
 
--- Effect 3: During End Phase, place "Neo Space" from Deck or GY into the Field Zone
-function s.target3(e,tp,eg,ep,ev,re,r,rp,chk)
+-- Effect 3: During End Phase → place Neo Space in Field Zone
+function s.fzfilter(c)
+    return c:IsCode(42015635) and not c:IsForbidden()
+end
+function s.fztg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then
-        -- Check if "Neo Space" (code: 42015635) is in the Deck or GY
-        return Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,42015635)  -- Neo Space's code
+        return Duel.IsExistingMatchingCard(s.fzfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil)
+            and Duel.GetLocationCount(tp,LOCATION_FZONE)>0
     end
-    Duel.SetOperationInfo(0,CATEGORY_TOFIELD,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
 end
-
-function s.operation3(e,tp,eg,ep,ev,re,r,rp)
-    -- Place "Neo Space" into the Field Zone from Deck or GY
-    local g=Duel.SelectMatchingCard(tp,Card.IsCode,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,42015635) -- Neo Space code
+function s.fzop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetLocationCount(tp,LOCATION_FZONE)<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+    local g=Duel.SelectMatchingCard(tp,s.fzfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
     if #g>0 then
-        -- Move Neo Space to the Field Zone as a Field Spell
         Duel.MoveToField(g:GetFirst(),tp,tp,LOCATION_FZONE,POS_FACEUP,true)
     end
 end
