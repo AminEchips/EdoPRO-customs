8cd259b30a8869849dd4f4b030bd5e08d528bd49
BrandedChips <aminekhoury0411@gmail.com>
2025-04-14 10:34:02 -0400

    Update c200.lua

--- script/c200.lua
+++ script/c200.lua
@@ -1,7 +1,9 @@
 --Elemental HERO Cosmic Soldier
 local s,id=GetID()
 function s.initial_effect(c)
-        -- Special Summon from hand
+    c:EnableReviveLimit()
+
+    -- Special Summon from hand
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -26,30 +28,26 @@ function s.initial_effect(c)
     c:RegisterEffect(e2)
 end
 
--- Filter for Neo-Spacian or Elemental HERO Neos
+-- Prevent the card itself from being sent to GY for cost
 function s.spcfilter(c)
-    local id=200
-    return c:IsAbleToGraveAsCost() and (c:IsSetCard(0x1f) or c:IsCode(89943723)) and not c:IsCode(id)
+    return c:IsAbleToGraveAsCost() and (c:IsSetCard(0x1f) or c:IsCode(89943723)) and not c:IsCode(200)
 end
 
--- Send a Neo-Spacian or Neos to GY as cost
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.IsExistingMatchingCard(s.spcfilter,tp,LOCATION_DECK,0,1,nil) end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
     local g=Duel.SelectMatchingCard(tp,s.spcfilter,tp,LOCATION_DECK,0,1,1,nil)
-    e:SetLabel(g:GetFirst():GetCode()) -- store the sent card's code
-    e:SetLabelObject(g:GetFirst())     -- store the sent card object
+    e:SetLabel(g:GetFirst():GetCode())
+    e:SetLabelObject(g:GetFirst())
     Duel.SendtoGrave(g,REASON_COST)
 end
 
--- Target itself for Special Summon
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
         and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
 
--- Special Summon itself and copy name/attribute of sent card
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     local tc=e:GetLabelObject()
@@ -71,20 +69,17 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
--- Cost for tributing 1 other HERO or Neo-Spacian
-function s.cfilter(c)
-    return c:IsFaceup() and (c:IsSetCard(0x8) or c:IsSetCard(0x1f))
+-- Prevent the card from tributing itself
+function s.cfilter(c,e)
+    return c:IsFaceup() and (c:IsSetCard(0x8) or c:IsSetCard(0x1f)) and c~=e:GetHandler()
 end
 
 function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
-    local c=e:GetHandler()
-    if chk==0 then return Duel.CheckReleaseGroup(tp,function(tc) return s.cfilter(tc) and tc~=c end,1,nil) end
-    local g=Duel.SelectReleaseGroup(tp,function(tc) return s.cfilter(tc) and tc~=c end,1,1,nil)
+    if chk==0 then return Duel.CheckReleaseGroup(tp,function(c) return s.cfilter(c,e) end,1,nil) end
+    local g=Duel.SelectReleaseGroup(tp,function(c) return s.cfilter(c,e) end,1,1,nil)
     Duel.Release(g,REASON_COST)
 end
-end
 
--- Search Polymerization
 function s.thfilter(c)
     return c:IsCode(24094653) and c:IsAbleToHand()
 end
@@ -105,4 +100,3 @@ end
 
 s.listed_names={89943723,24094653}
 s.listed_series={0x8, 0x1f}
-
