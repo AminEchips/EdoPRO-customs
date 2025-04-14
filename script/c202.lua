66e2e58b754a36e056590b60e76259db2e1f7d29
BrandedChips <aminekhoury0411@gmail.com>
2025-04-14 13:25:39 -0400

    Update c202.lua

--- script/c202.lua
+++ script/c202.lua
@@ -1,4 +1,86 @@
 --Favorite Fusion
-function c202.initial_effect(c)
-	
+local s,id=GetID()
+function s.initial_effect(c)
+    -- Fusion Summon 1 "Elemental HERO" Fusion Monster
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetCountLimit(1,id)
+    e1:SetTarget(s.fustg)
+    e1:SetOperation(s.fusop)
+    c:RegisterEffect(e1)
+
+    -- GY effect: recycle 3 "HERO" Fusion Monsters and draw 1
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetRange(LOCATION_GRAVE)
+    e2:SetCountLimit(1,id+100)
+    e2:SetCost(aux.bfgcost) -- banish this card from GY
+    e2:SetTarget(s.tdtg)
+    e2:SetOperation(s.tdop)
+    c:RegisterEffect(e2)
+end
+
+-- Effect 1: Fusion Summon an "Elemental HERO" Fusion Monster
+function s.filter(c,e,tp,m,f,chkf)
+    return c:IsSetCard(0x8) and c:IsType(TYPE_FUSION) and c:IsLevelAbove(1)
+        and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
+        and Duel.GetLocationCountFromEx(tp,tp,nil,c)>0
+        and c:CheckFusionMaterial(m,nil,chkf)
+end
+
+function s.fustg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then
+        local chkf=tp
+        local mg1=Duel.GetFusionMaterial(tp)
+        local mg2=Duel.GetMatchingGroup(Card.IsAbleToGrave,tp,LOCATION_DECK,0,nil)
+        mg1:Merge(mg2)
+        return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg1,nil,chkf)
+    end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+
+function s.fusop(e,tp,eg,ep,ev,re,r,rp)
+    local chkf=tp
+    local mg1=Duel.GetFusionMaterial(tp)
+    local mg2=Duel.GetMatchingGroup(Card.IsAbleToGrave,tp,LOCATION_DECK,0,nil)
+    mg1:Merge(mg2)
+    local sg=Duel.GetMatchingGroup(s.filter,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,chkf)
+    if #sg>0 then
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+        local tc=sg:Select(tp,1,1,nil):GetFirst()
+        if tc then
+            local mat=Fusion.SelectFusionMaterial(tp,tc,mg1,nil,chkf)
+            tc:SetMaterial(mat)
+            Duel.SendtoGrave(mat,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
+            Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
+            tc:CompleteProcedure()
+        end
+    end
+end
+
+-- Effect 2: Banish, recycle 3 "HERO" Fusion Monsters, draw 1
+function s.tdfilter(c)
+    return c:IsType(TYPE_FUSION) and c:IsSetCard(0x8) and c:IsAbleToDeck()
+end
+
+function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsPlayerCanDraw(tp,1)
+        and Duel.IsExistingMatchingCard(s.tdfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,3,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,3,tp,LOCATION_GRAVE+LOCATION_REMOVED)
+    Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+
+function s.tdop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+    local g=Duel.SelectMatchingCard(tp,s.tdfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,3,3,nil)
+    if #g==3 then
+        Duel.SendtoDeck(g,nil,2,REASON_EFFECT)
+        Duel.ShuffleDeck(tp)
+        Duel.Draw(tp,1,REASON_EFFECT)
+    end
 end
