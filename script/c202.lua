2b7b76de8974495ec54247ef46fc11aa875e0849
Chips <aminekhoury0411@gmail.com>
2025-04-14 16:44:09 -0400

    Update c202.lua

--- script/c202.lua
+++ script/c202.lua
@@ -1,81 +1,102 @@
---Favorite Fusion (ID: 202)
+--Favorite Fusion
 local s,id=GetID()
 function s.initial_effect(c)
-	-- Fusion Summon an "Elemental HERO" Fusion Monster
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)
-	c:RegisterEffect(e1)
-	-- GY effect: shuffle 3 HERO Fusions, draw 1
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
-	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCountLimit(1,id)
-	e2:SetCost(aux.bfgcost)
-	e2:SetTarget(s.drtg)
-	e2:SetOperation(s.drop)
-	c:RegisterEffect(e2)
+    -- Activate and Fusion Summon
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetCountLimit(1,id)
+    e1:SetTarget(s.fustg)
+    e1:SetOperation(s.fusop)
+    c:RegisterEffect(e1)
+
+    -- GY effect: Banish to recycle 3 "HERO" Fusion Monsters and draw 1
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetRange(LOCATION_GRAVE)
+    e2:SetCountLimit(1,id+100)
+    e2:SetCost(aux.bfgcost)
+    e2:SetTarget(s.tdtg)
+    e2:SetOperation(s.tdop)
+    c:RegisterEffect(e2)
 end
 
-function s.filter0(c)
-	return c:IsSetCard(0x8) and c:IsAbleToDeck() and c:IsType(TYPE_FUSION)
-end
-function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsPlayerCanDraw(tp,1)
-		and Duel.IsExistingMatchingCard(s.filter0,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,3,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,3,tp,LOCATION_GRAVE+LOCATION_REMOVED)
-	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
-end
-function s.drop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.SelectMatchingCard(tp,s.filter0,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,3,3,nil)
-	if #g==3 then
-		Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
-		local og=Duel.GetOperatedGroup()
-		if og:IsExists(Card.IsLocation,1,nil,LOCATION_EXTRA+LOCATION_DECK) then
-			Duel.BreakEffect()
-			Duel.Draw(tp,1,REASON_EFFECT)
-		end
-	end
+-- Fusion Summon
+function s.filter(c,e,tp,m,chkf)
+    return c:IsSetCard(0x8) and c:IsType(TYPE_FUSION)
+        and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
+        and c:CheckFusionMaterial(m,nil,chkf)
 end
 
-function s.filter1(c,e,tp)
-	return c:IsSetCard(0x3008) and c:IsType(TYPE_FUSION)
-		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
+function s.fustg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then
+        local chkf=tp
+        local mg1=Duel.GetFusionMaterial(tp)
+        local mg2=Duel.GetMatchingGroup(Card.IsAbleToGrave,tp,LOCATION_DECK,0,nil)
+        mg1:Merge(mg2)
+        return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg1,chkf)
+    end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 
-function s.deckmatfilter(c)
-	return c:IsAbleToGrave() and c:IsSetCard(0x8)
+function s.fusop(e,tp,eg,ep,ev,re,r,rp)
+    local chkf=tp
+    local mg1=Duel.GetFusionMaterial(tp)
+
+    -- Check if a "Wingman" is being summoned for Deck access
+    local mg2=Duel.GetMatchingGroup(Card.IsAbleToGrave,tp,LOCATION_DECK,0,nil)
+    mg1:Merge(mg2)
+
+    local sg=Duel.GetMatchingGroup(function(c)
+        return c:IsSetCard(0x8) and c:IsType(TYPE_FUSION) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
+            and c:CheckFusionMaterial(mg1,nil,chkf)
+    end,tp,LOCATION_EXTRA,0,nil)
+
+    if #sg==0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local tc=sg:Select(tp,1,1,nil):GetFirst()
+    if not tc then return end
+
+    local deckFusion = tc:IsName("Elemental HERO Flame Wingman") or tc:IsCode(35809262) -- example: Flame Wingman
+    local matGroup = Duel.GetFusionMaterial(tp)
+
+    if deckFusion then
+        matGroup:Merge(Duel.GetMatchingGroup(Card.IsAbleToGrave,tp,LOCATION_DECK,0,nil))
+    end
+
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FMATERIAL)
+    local mat=Duel.SelectFusionMaterial(tp,tc,matGroup,nil,chkf)
+    if not mat or #mat==0 then return end
+
+    tc:SetMaterial(mat)
+    Duel.SendtoGrave(mat,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
+    Duel.BreakEffect()
+    Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
+    tc:CompleteProcedure()
 end
 
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+-- GY effect: Recycle and draw
+function s.tdfilter(c)
+    return c:IsType(TYPE_FUSION) and c:IsSetCard(0x8) and c:IsAbleToDeck()
 end
 
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local chkf=tp
-	local mg=Duel.GetFusionMaterial(tp):Filter(Card.IsCanBeFusionMaterial,nil)
-	local sg=Duel.GetMatchingGroup(s.filter1,tp,LOCATION_EXTRA,0,nil,e,tp)
-	if #sg==0 then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local tc=sg:Select(tp,1,1,nil):GetFirst()
-	if not tc then return end
-	local mat=Duel.SelectFusionMaterial(tp,tc,mg,nil,chkf)
-	if tc:GetText() and tc:GetText():lower():find("wingman") and Duel.IsExistingMatchingCard(s.deckmatfilter,tp,LOCATION_DECK,0,1,nil) then
-		if Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-			local dg=Duel.SelectMatchingCard(tp,s.deckmatfilter,tp,LOCATION_DECK,0,1,1,nil)
-			mat:Merge(dg)
-		end
-	end
-	tc:SetMaterial(mat)
-	Duel.SendtoGrave(mat,REASON_MATERIAL+REASON_FUSION)
-	Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
-	tc:CompleteProcedure()
+function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsPlayerCanDraw(tp,1)
+        and Duel.IsExistingMatchingCard(s.tdfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,3,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,3,tp,LOCATION_GRAVE+LOCATION_REMOVED)
+    Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+
+function s.tdop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+    local g=Duel.SelectMatchingCard(tp,s.tdfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,3,3,nil)
+    if #g==3 then
+        Duel.SendtoDeck(g,nil,2,REASON_EFFECT)
+        Duel.ShuffleDeck(tp)
+        Duel.Draw(tp,1,REASON_EFFECT)
+    end
 end
