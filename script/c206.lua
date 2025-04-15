bf797e7de692fc2e07266a3fe0efb01140424cc6
Chips <aminekhoury0411@gmail.com>
2025-04-14 21:30:50 -0400

    Update c206.lua

--- script/c206.lua
+++ script/c206.lua
@@ -72,27 +72,25 @@ end
 
 -- HERO destroyed trigger
 function s.spfilter(c,tp)
-    return c:IsReason(REASON_BATTLE+REASON_EFFECT)
-        and c:IsPreviousControler(tp) and c:IsPreviousLocation(LOCATION_MZONE)
-        and c:IsSetCard(0x8) and c:IsType(TYPE_MONSTER)
+    return c:IsReason(REASON_BATTLE+REASON_EFFECT) and c:IsPreviousControler(tp)
+        and c:IsPreviousLocation(LOCATION_MZONE) and c:IsSetCard(0x8) and c:IsType(TYPE_MONSTER)
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
     return eg:IsExists(s.spfilter,1,nil,tp)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
     local g=eg:Filter(s.spfilter,nil,tp)
-    if chk==0 then
-        return #g>0 and Duel.IsExistingMatchingCard(function(c)
-            return c:IsSetCard(0x8) and c:IsLevel(g:GetFirst():GetLevel())
-                and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-        end,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,nil)
-    end
+    if chk==0 then return #g>0 and g:GetFirst():GetLevel()>0 and Duel.IsExistingMatchingCard(function(c)
+        return c:IsSetCard(0x8) and c:IsLevel(g:GetFirst():GetLevel())
+            and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+    end,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
     local g=eg:Filter(s.spfilter,nil,tp)
     if #g==0 then return end
     local lv=g:GetFirst():GetLevel()
+    if lv==0 then return end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
     local sg=Duel.SelectMatchingCard(tp,function(c)
         return c:IsSetCard(0x8) and c:IsLevel(lv)
@@ -106,17 +104,21 @@ end
 -- GY: set if any monster destroyed by battle
 function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return e:GetHandler():IsSSetable() end
+    Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,e:GetHandler(),1,0,0)
 end
 function s.setop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     if c:IsRelateToEffect(e) then
         Duel.SSet(tp,c)
+        -- Banish when leaves field
         local e1=Effect.CreateEffect(c)
+        e1:SetDescription(3300)
         e1:SetType(EFFECT_TYPE_SINGLE)
         e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
-        e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+        e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
         e1:SetReset(RESET_EVENT+RESETS_REDIRECT)
         e1:SetValue(LOCATION_REMOVED)
-        c:RegisterEffect(e1)
+        c:RegisterEffect(e1,true)
     end
 end
+
