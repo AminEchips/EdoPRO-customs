d5db35c6746d0ef9e090766a4ae84ece50476a02
Chips <aminekhoury0411@gmail.com>
2025-04-15 10:23:05 -0400

    Update c206.lua

--- script/c206.lua
+++ script/c206.lua
@@ -1,41 +1,73 @@
--- HERO Second Signal
+-- Elemental HERO Gaseous Soldier
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Effect 1: Fusion Summon
+    -- Special Summon itself from hand
     local e1=Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
-    e1:SetType(EFFECT_TYPE_ACTIVATE)
-    e1:SetCode(EVENT_FREE_CHAIN)
-    e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
-    e1:SetTarget(s.fustg)
-    e1:SetOperation(s.fusop)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCountLimit(1,id)
+    e1:SetCondition(s.spcon)
+    e1:SetTarget(s.sptg)
+    e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
 
-    -- Effect 2: Summon same-level HERO when one is destroyed
+    -- Quick Effect: Fusion Summon during Main Phase
     local e2=Effect.CreateEffect(c)
-    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e2:SetType(EFFECT_TYPE_ACTIVATE)
-    e2:SetCode(EVENT_DESTROYED)
-    e2:SetProperty(EFFECT_FLAG_DELAY)
-    e2:SetCondition(s.condition)
-    e2:SetTarget(s.target)
-    e2:SetOperation(s.operation)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+    e2:SetType(EFFECT_TYPE_QUICK_O)
+    e2:SetCode(EVENT_FREE_CHAIN)
+    e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1,{id,1})
+    e2:SetCondition(function(e,tp) return Duel.IsMainPhase() end)
+    e2:SetTarget(s.fustg)
+    e2:SetOperation(s.fusop)
     c:RegisterEffect(e2)
 
-    -- Effect 3: Set from GY if any monster is destroyed by battle; banish on leave
+    -- Return itself from GY if HERO Fusion is destroyed
     local e3=Effect.CreateEffect(c)
     e3:SetDescription(aux.Stringid(id,2))
+    e3:SetCategory(CATEGORY_TOHAND)
     e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-    e3:SetCode(EVENT_BATTLE_DESTROYED)
     e3:SetProperty(EFFECT_FLAG_DELAY)
+    e3:SetCode(EVENT_DESTROYED)
     e3:SetRange(LOCATION_GRAVE)
     e3:SetCountLimit(1,{id,2})
-    e3:SetTarget(s.settg)
-    e3:SetOperation(s.setop)
+    e3:SetCondition(s.thcon)
+    e3:SetTarget(s.thtg)
+    e3:SetOperation(s.thop)
     c:RegisterEffect(e3)
 end
 
--- ===== Fusion Summon Logic =====
+-- Special Summon condition
+function s.cfilter(c)
+    return c:IsSetCard(0x8) and not c:IsAttribute(ATTRIBUTE_WIND)
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+        e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+        e1:SetReset(RESET_EVENT+RESETS_REDIRECT)
+        e1:SetValue(LOCATION_REMOVED)
+        c:RegisterEffect(e1,true)
+    end
+end
+
+-- Fusion Summon logic
 function s.fusfilter(c,e,tp,mg,chkf)
     return c:IsType(TYPE_FUSION) and c:IsSetCard(0x8)
         and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
@@ -43,15 +75,13 @@ function s.fusfilter(c,e,tp,mg,chkf)
 end
 function s.fustg(e,tp,eg,ep,ev,re,r,rp,chk)
     local chkf=tp
-    local mg=Duel.GetFusionMaterial(tp):Filter(Card.IsAbleToDeck,nil)
-    if chk==0 then
-        return Duel.IsExistingMatchingCard(s.fusfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg,chkf)
-    end
+    local mg=Duel.GetFusionMaterial(tp)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.fusfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg,chkf) end
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 function s.fusop(e,tp,eg,ep,ev,re,r,rp)
     local chkf=tp
-    local mg=Duel.GetFusionMaterial(tp):Filter(Card.IsAbleToDeck,nil)
+    local mg=Duel.GetFusionMaterial(tp)
     local sg=Duel.GetMatchingGroup(s.fusfilter,tp,LOCATION_EXTRA,0,nil,e,tp,mg,chkf)
     if #sg==0 then return end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
@@ -59,79 +89,30 @@ function s.fusop(e,tp,eg,ep,ev,re,r,rp)
     if not tc then return end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FMATERIAL)
     local mat=Duel.SelectFusionMaterial(tp,tc,mg,nil,chkf)
-    if #mat==0 then return end
+    if not mat or #mat==0 then return end
     tc:SetMaterial(mat)
-    Duel.SendtoDeck(mat,nil,SEQ_DECKSHUFFLE,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
+    Duel.SendtoGrave(mat,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
     Duel.BreakEffect()
     Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
     tc:CompleteProcedure()
 end
 
--- ===== HERO destroyed summon logic =====
-function s.cfilter(c,tp)
-    return c:IsReason(REASON_BATTLE+REASON_EFFECT)
-        and c:IsPreviousControler(tp)
-        and c:IsPreviousLocation(LOCATION_MZONE)
-        and c:IsPreviousPosition(POS_FACEUP)
-        and c:IsPreviousSetCard(0x8) -- HERO
-        and c:IsType(TYPE_MONSTER)
+-- Return if HERO Fusion destroyed
+function s.thfilter(c,tp)
+    return c:IsPreviousControler(tp) and c:IsSetCard(0x8) and c:IsType(TYPE_FUSION)
+        and c:IsPreviousLocation(LOCATION_MZONE) and c:IsPreviousPosition(POS_FACEUP)
+        and bit.band(c:GetReason(),REASON_BATTLE+REASON_EFFECT)~=0
 end
-
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-    return eg:IsExists(s.cfilter,1,nil,tp)
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+    return eg:IsExists(s.thfilter,1,nil,tp)
 end
-
-function s.spfilter(c,lv,e,tp)
-    return c:IsSetCard(0x8) and c:IsLevel(lv)
-        and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return e:GetHandler():IsAbleToHand() end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
 end
-
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-    local g=eg:Filter(s.cfilter,nil,tp)
-    if chk==0 then
-        return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-            and g:GetFirst()
-            and g:GetFirst():GetLevel()>0
-            and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,nil,g:GetFirst():GetLevel(),e,tp)
-    end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE)
-end
-
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-    local g=eg:Filter(s.cfilter,nil,tp)
-    if #g==0 then return end
-    local lv=g:GetFirst():GetLevel()
-    if lv<=0 then return end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-    local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,lv,e,tp)
-    if #sg>0 then
-        Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
-    end
-end
--- ===== GY: Set this card if any monster destroyed by battle =====
-function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return e:GetHandler():IsSSetable() end
-    Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,e:GetHandler(),1,0,0)
-end
-function s.setop(e,tp,eg,ep,ev,re,r,rp)
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     if c:IsRelateToEffect(e) then
-        Duel.SSet(tp,c)
-        -- Banish when leaves field
-        local e1=Effect.CreateEffect(c)
-        e1:SetDescription(3300)
-        e1:SetType(EFFECT_TYPE_SINGLE)
-        e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
-        e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
-        e1:SetReset(RESET_EVENT+RESETS_REDIRECT)
-        e1:SetValue(LOCATION_REMOVED)
-        c:RegisterEffect(e1,true)
+        Duel.SendtoHand(c,nil,REASON_EFFECT)
     end
 end
-
-s.listed_series={0x3008}
-
-
-
-
