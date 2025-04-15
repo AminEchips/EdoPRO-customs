3d6af458e612f6bed3d02d80fa3bb32d41e6c17d
Chips <aminekhoury0411@gmail.com>
2025-04-14 22:06:30 -0400

    Update c206.lua

--- script/c206.lua
+++ script/c206.lua
@@ -1,7 +1,7 @@
 -- HERO Second Signal
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Effect 1: Activate to Fusion Summon
+    -- Effect 1: Fusion Summon
     local e1=Effect.CreateEffect(c)
     e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -11,18 +11,18 @@ function s.initial_effect(c)
     e1:SetOperation(s.fusop)
     c:RegisterEffect(e1)
 
-    -- Effect 2: Activate when HERO is destroyed to summon same-level HERO
+    -- Effect 2: Summon same-level HERO when one is destroyed
     local e2=Effect.CreateEffect(c)
     e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e2:SetType(EFFECT_TYPE_ACTIVATE)
     e2:SetCode(EVENT_DESTROYED)
     e2:SetProperty(EFFECT_FLAG_DELAY)
-    e2:SetCondition(s.spcon)
-    e2:SetTarget(s.sptg)
-    e2:SetOperation(s.spop)
+    e2:SetCondition(s.condition)
+    e2:SetTarget(s.target)
+    e2:SetOperation(s.operation)
     c:RegisterEffect(e2)
 
-    -- Effect 3: Set this from GY if any monster is destroyed by battle; banish on leave
+    -- Effect 3: Set from GY if any monster is destroyed by battle; banish on leave
     local e3=Effect.CreateEffect(c)
     e3:SetDescription(aux.Stringid(id,2))
     e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
@@ -35,7 +35,7 @@ function s.initial_effect(c)
     c:RegisterEffect(e3)
 end
 
--- Fusion Summon logic
+-- ===== Fusion Summon Logic =====
 function s.fusfilter(c,e,tp,mg,chkf)
     return c:IsType(TYPE_FUSION) and c:IsSetCard(0x8)
         and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
@@ -67,23 +67,21 @@ function s.fusop(e,tp,eg,ep,ev,re,r,rp)
     tc:CompleteProcedure()
 end
 
--- Trigger if HERO you controlled is destroyed (battle or effect)
+-- ===== HERO destroyed summon logic =====
 function s.cfilter(c,tp)
     return c:IsReason(REASON_BATTLE+REASON_EFFECT)
-        and c:IsPreviousLocation(LOCATION_MZONE)
         and c:IsPreviousControler(tp)
-        and c:IsPreviousPosition(POS_FACEUP)
+        and c:IsPreviousLocation(LOCATION_MZONE)
         and c:IsPreviousSetCard(0x8)
-        and c:IsType(TYPE_MONSTER)
 end
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
     return eg:IsExists(s.cfilter,1,nil,tp)
 end
 function s.spfilter(c,lv,e,tp)
     return c:IsSetCard(0x8) and c:IsLevel(lv)
         and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
     local g=eg:Filter(s.cfilter,nil,tp)
     if chk==0 then
         return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
@@ -92,7 +90,7 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
     end
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE)
 end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
     if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
     local g=eg:Filter(s.cfilter,nil,tp)
     if #g==0 then return end
@@ -105,7 +103,7 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
--- Set from GY if any monster destroyed by battle
+-- ===== GY: Set this card if any monster destroyed by battle =====
 function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return e:GetHandler():IsSSetable() end
     Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,e:GetHandler(),1,0,0)
@@ -125,6 +123,7 @@ function s.setop(e,tp,eg,ep,ev,re,r,rp)
         c:RegisterEffect(e1,true)
     end
 end
+
 
 
 
