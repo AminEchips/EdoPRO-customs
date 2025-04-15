d47798335f180d90bd65dcb1d64d2efbafc6f946
Chips <aminekhoury0411@gmail.com>
2025-04-15 09:51:03 -0400

    Update c208.lua

--- script/c208.lua
+++ script/c208.lua
@@ -27,13 +27,14 @@ function s.initial_effect(c)
     e2:SetOperation(s.fusop)
     c:RegisterEffect(e2)
 
-    -- Add to hand if sent from field to GY
+    -- Add itself from GY to hand if HERO Fusion is destroyed
     local e3=Effect.CreateEffect(c)
     e3:SetDescription(aux.Stringid(id,2))
     e3:SetCategory(CATEGORY_TOHAND)
-    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
     e3:SetProperty(EFFECT_FLAG_DELAY)
-    e3:SetCode(EVENT_TO_GRAVE)
+    e3:SetCode(EVENT_DESTROYED)
+    e3:SetRange(LOCATION_GRAVE)
     e3:SetCountLimit(1,{id,2})
     e3:SetCondition(s.thcon)
     e3:SetTarget(s.thtg)
@@ -95,23 +96,23 @@ function s.fusop(e,tp,eg,ep,ev,re,r,rp)
     tc:CompleteProcedure()
 end
 
--- Add to hand if sent from field to GY
-function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    return c:IsPreviousLocation(LOCATION_ONFIELD)
+-- Add itself from GY to hand when a HERO Fusion is destroyed
+function s.thfilter(c,tp)
+    return c:IsPreviousControler(tp)
+        and c:IsSetCard(0x8)
+        and c:IsType(TYPE_FUSION)
+        and c:IsReason(REASON_BATTLE+REASON_EFFECT)
 end
-function s.thfilter(c)
-    return c:IsSetCard(0x8) and c:IsAbleToHand()
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+    return eg:IsExists(s.thfilter,1,nil,tp)
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+    if chk==0 then return e:GetHandler():IsAbleToHand() end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-    local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
-    if #g>0 then
-        Duel.SendtoHand(g,nil,REASON_EFFECT)
-        Duel.ConfirmCards(1-tp,g)
+    local c=e:GetHandler()
+    if c:IsRelateToEffect(e) then
+        Duel.SendtoHand(c,nil,REASON_EFFECT)
     end
 end
