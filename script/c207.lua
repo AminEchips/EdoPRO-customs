4fb6b1ef646d0332316aaef3056890531d197642
Chips <aminekhoury0411@gmail.com>
2025-04-14 22:22:23 -0400

    Add files via upload

---
+++ script/c207.lua
@@ -0,0 +1,80 @@
+-- HERO Enforcement
+local s,id=GetID()
+function s.initial_effect(c)
+    -- Target and destroy monsters
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_DESTROY)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e1:SetTarget(s.target)
+    e1:SetOperation(s.activate)
+    c:RegisterEffect(e1)
+
+    -- GY effect: Add Polymerization or Miracle Fusion
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_TOHAND)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetRange(LOCATION_GRAVE)
+    e2:SetCountLimit(1,{id,1})
+    e2:SetCost(aux.bfgcost)
+    e2:SetTarget(s.gytg)
+    e2:SetOperation(s.gyop)
+    c:RegisterEffect(e2)
+end
+
+s.listed_names={24094653, 45906428}
+s.listed_series={0x8}
+
+function s.attrfilter(c)
+    return c:IsFaceup() and c:IsSetCard(0x8) and c:IsType(TYPE_MONSTER)
+end
+
+function s.countAttributes(tp)
+    local g=Duel.GetMatchingGroup(s.attrfilter,tp,LOCATION_MZONE,0,nil)
+    local attrs={}
+    local count=0
+    for tc in aux.Next(g) do
+        local attr=tc:GetAttribute()
+        if not attrs[attr] then
+            attrs[attr]=true
+            count=count+1
+        end
+    end
+    return count
+end
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    local ct=s.countAttributes(tp)
+    if chkc then return chkc:IsOnField() end
+    if chk==0 then return ct>0 and Duel.IsExistingTarget(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+    local g=Duel.SelectTarget(tp,nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,ct,nil)
+    Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+end
+
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+    local g=Duel.GetTargetCards(e)
+    if #g>0 then
+        Duel.Destroy(g,REASON_EFFECT)
+    end
+end
+
+function s.thfilter(c)
+    return c:IsAbleToHand() and (c:IsCode(24094653) or c:IsCode(45906428))
+end
+
+function s.gytg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+end
+
+function s.gyop(e,tp,eg,ep,ev,re,r,rp)
+    local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
+    end
+end
