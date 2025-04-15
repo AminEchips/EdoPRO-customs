4e51dad80706af33e1d335bcc0f4673e1740f733
Chips <aminekhoury0411@gmail.com>
2025-04-15 18:11:58 -0400

    Update c214.lua

--- script/c214.lua
+++ script/c214.lua
@@ -2,10 +2,9 @@
 local s,id=GetID()
 function s.initial_effect(c)
     c:EnableReviveLimit()
-    --Fusion Materials: Burstinatrix + Necroshade
-    Fusion.AddProcMix(c,true,true,58932615,89252153)
-
-    --Add H - Heated Heart on Special Summon
+    --Fusion Materials
+    Fusion.AddProcMix(c,true,true,58932615,511023014) -- Burstinatrix + Necroshade
+    -- Add "H - Heated Heart" on Special Summon
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
@@ -13,28 +12,29 @@ function s.initial_effect(c)
     e1:SetCode(EVENT_SPSUMMON_SUCCESS)
     e1:SetProperty(EFFECT_FLAG_DELAY)
     e1:SetCountLimit(1,id)
+    e1:SetCondition(s.thcon)
     e1:SetTarget(s.thtg)
     e1:SetOperation(s.thop)
     c:RegisterEffect(e1)
-
-    --On attack declare: banish from GY, gain 500 ATK, double damage
+    -- Banish from GY, gain ATK, and double battle damage on attack declaration
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
-    e2:SetCategory(CATEGORY_REMOVE)
+    e2:SetCategory(CATEGORY_REMOVE+CATEGORY_ATKCHANGE)
     e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e2:SetCode(EVENT_ATTACK_ANNOUNCE)
-    e2:SetProperty(EFFECT_FLAG_DELAY)
     e2:SetCountLimit(1,{id,1})
     e2:SetTarget(s.btg)
     e2:SetOperation(s.bop)
     c:RegisterEffect(e2)
 end
-s.listed_names={74825788} -- H - Heated Heart
-s.material_setcode={0x3008}
+s.listed_names={40418351} -- H - Heated Heart
 
--- Search H - Heated Heart
+-- Effect 1: Add H - Heated Heart
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION)
+end
 function s.thfilter(c)
-    return c:IsCode(74825788) and c:IsAbleToHand()
+    return c:IsCode(40418351) and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
@@ -49,18 +49,15 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
--- On attack declare
-function s.rmfilter(c)
-    return c:IsAbleToRemove()
-end
+-- Effect 2: Attack boost and damage double
 function s.btg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.rmfilter,tp,LOCATION_GRAVE,0,1,nil) end
+    if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_GRAVE,0,1,nil) end
     Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_GRAVE)
 end
 function s.bop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-    local g=Duel.SelectMatchingCard(tp,s.rmfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+    local g=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_GRAVE,0,1,1,nil)
     if #g>0 and Duel.Remove(g,POS_FACEUP,REASON_EFFECT)>0 and c:IsRelateToEffect(e) and c:IsFaceup() then
         -- Gain 500 ATK
         local e1=Effect.CreateEffect(c)
@@ -69,18 +66,17 @@ function s.bop(e,tp,eg,ep,ev,re,r,rp)
         e1:SetValue(500)
         e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
         c:RegisterEffect(e1)
-        -- Double battle damage
+        -- Double battle damage to opponent this turn
         local e2=Effect.CreateEffect(c)
-        e2:SetType(EFFECT_TYPE_SINGLE)
-        e2:SetCode(EFFECT_CHANGE_BATTLE_DAMAGE)
-        e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-        e2:SetCondition(s.damcon)
-        e2:SetValue(AUX_DOUBLE_DAMAGE)
-        e2:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
+        e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+        e2:SetCode(EVENT_PRE_BATTLE_DAMAGE)
+        e2:SetRange(LOCATION_MZONE)
+        e2:SetOperation(function(e,tp,eg,ep,ev,re,r,rp)
+            if ep~=tp and Duel.GetAttacker()==e:GetHandler() then
+                Duel.ChangeBattleDamage(ep,ev*2)
+            end
+        end)
+        e2:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_DAMAGE_CAL)
         c:RegisterEffect(e2)
     end
 end
-function s.damcon(e)
-    return e:GetHandler():GetBattleTarget()~=nil
-end
-
