8613e32164309309a587c1071cfe39b86b2bc01e
Chips <aminekhoury0411@gmail.com>
2025-04-15 10:34:53 -0400

    Update c209.lua

--- script/c209.lua
+++ script/c209.lua
@@ -1,65 +1,79 @@
 -- Elemental HERO Titantron
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Set 1 "Skyscraper" Field Spell directly from Deck
+    -- e1: Set 1 Spell that lists "Elemental HERO" in text from GY
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
-    e1:SetType(EFFECT_TYPE_IGNITION)
-    e1:SetRange(LOCATION_HAND)
+    e1:SetCategory(CATEGORY_LEAVE_GRAVE)
+    e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e1:SetProperty(EFFECT_FLAG_DELAY)
+    e1:SetCode(EVENT_REMOVE)
+    e1:SetRange(LOCATION_REMOVED)
     e1:SetCountLimit(1,id)
+    e1:SetCondition(s.setcon)
     e1:SetTarget(s.settg)
     e1:SetOperation(s.setop)
     c:RegisterEffect(e1)
 
-    -- If this card is banished for a Fusion Summon: add 1 of your banished "Elemental HERO" monsters to your hand
+    -- e2: Special Summon itself, then optionally add 1 banished HERO if used for Fusion Summon of a HERO
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
-    e2:SetCategory(CATEGORY_TOHAND)
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND)
     e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-    e2:SetCode(EVENT_REMOVE)
     e2:SetProperty(EFFECT_FLAG_DELAY)
+    e2:SetCode(EVENT_REMOVE)
     e2:SetRange(LOCATION_REMOVED)
     e2:SetCountLimit(1,{id,1})
-    e2:SetCondition(s.thcon)
-    e2:SetTarget(s.thtg)
-    e2:SetOperation(s.thop)
+    e2:SetCondition(s.spcon)
+    e2:SetTarget(s.sptg)
+    e2:SetOperation(s.spop)
     c:RegisterEffect(e2)
 end
 s.listed_series={0x8}
-s.listed_names={45906428}
+s.listed_names={CARD_ELEMENTAL_HERO_NEOS}
 
--- Set "Skyscraper" from Deck
+-- ========== Effect 1: Set Spell with "Elemental HERO" in text ==========
+function s.setcon(e,tp,eg,ep,ev,re,r,rp)
+    return eg:IsContains(e:GetHandler())
+end
 function s.setfilter(c)
-    return c:IsCode(45906428) and c:IsSSetable()
+    return c:IsType(TYPE_SPELL) and c:IsSSetable() and aux.HasListedSetCard(c,0x8) and c:IsInGY()
 end
 function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-        and Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_DECK,0,1,nil) end
+    if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_GRAVE,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,nil,1,tp,LOCATION_GRAVE)
 end
 function s.setop(e,tp,eg,ep,ev,re,r,rp)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
-    local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,1,1,nil)
+    local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_GRAVE,0,1,1,nil)
     if #g>0 then
         Duel.SSet(tp,g:GetFirst())
     end
 end
 
--- Triggered when banished for Fusion
-function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-    return eg:IsContains(e:GetHandler()) and bit.band(r,REASON_FUSION)~=0
+-- ========== Effect 2: Special Summon this, then optionally add HERO ==========
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+    return eg:IsContains(e:GetHandler())
 end
-function s.thfilter(c)
-    return c:IsSetCard(0x8) and c:IsAbleToHand() and c:IsFaceup()
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_REMOVED,0,1,nil) end
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_REMOVED)
+function s.addfilter(c)
+    return c:IsSetCard(0x8) and c:IsFaceup() and c:IsAbleToHand()
 end
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-    local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_REMOVED,0,1,1,nil)
-    if #g>0 then
-        Duel.SendtoHand(g,nil,REASON_EFFECT)
-        Duel.ConfirmCards(1-tp,g)
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if not c:IsRelateToEffect(e) then return end
+    if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 and bit.band(r,REASON_FUSION)~=0 and re and re:GetHandler():IsSetCard(0x8) then
+        local g=Duel.GetMatchingGroup(s.addfilter,tp,LOCATION_REMOVED,0,nil)
+        if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+            local tg=g:Select(tp,1,1,nil)
+            Duel.SendtoHand(tg,nil,REASON_EFFECT)
+            Duel.ConfirmCards(1-tp,tg)
+        end
     end
 end
+
