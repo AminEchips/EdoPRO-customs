7f94ebd14ffb4e4d84d3d44dcdf546f1904b75bf
Chips <aminekhoury0411@gmail.com>
2025-04-15 18:33:40 -0400

    Update c215.lua

--- script/c215.lua
+++ script/c215.lua
@@ -29,7 +29,7 @@ function s.initial_effect(c)
 end
 
 function s.thfilter(c)
-    return c:IsCode(38033532) and c:IsAbleToHand() -- E - Emergency Call
+    return c:IsCode(00213326) and c:IsAbleToHand() -- E - Emergency Call
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
