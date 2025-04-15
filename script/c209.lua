2b19bae953a7c601078954fa776e3f60f09f251c
Chips <aminekhoury0411@gmail.com>
2025-04-15 10:30:02 -0400

    Update c209.lua

--- script/c209.lua
+++ script/c209.lua
@@ -1,4 +1,3 @@
-
 -- Elemental HERO Titantron
 local s,id=GetID()
 function s.initial_effect(c)
@@ -12,13 +11,14 @@ function s.initial_effect(c)
     e1:SetOperation(s.setop)
     c:RegisterEffect(e1)
 
-    -- If used as Fusion Material: add 1 of your banished "Elemental HERO" monsters to your hand
+    -- If this card is banished for a Fusion Summon: add 1 of your banished "Elemental HERO" monsters to your hand
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
     e2:SetCategory(CATEGORY_TOHAND)
-    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_REMOVE)
     e2:SetProperty(EFFECT_FLAG_DELAY)
-    e2:SetCode(EVENT_BE_MATERIAL)
+    e2:SetRange(LOCATION_REMOVED)
     e2:SetCountLimit(1,{id,1})
     e2:SetCondition(s.thcon)
     e2:SetTarget(s.thtg)
@@ -26,7 +26,7 @@ function s.initial_effect(c)
     c:RegisterEffect(e2)
 end
 s.listed_series={0x8}
-s.listed_names={45906428} -- Skyscraper
+s.listed_names={45906428}
 
 -- Set "Skyscraper" from Deck
 function s.setfilter(c)
@@ -44,12 +44,12 @@ function s.setop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
--- Add banished "Elemental HERO" to hand
+-- Triggered when banished for Fusion
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-    return r==REASON_FUSION
+    return eg:IsContains(e:GetHandler()) and bit.band(r,REASON_FUSION)~=0
 end
 function s.thfilter(c)
-    return c:IsSetCard(0x8) and c:IsFaceup() and c:IsAbleToHand()
+    return c:IsSetCard(0x8) and c:IsAbleToHand() and c:IsFaceup()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_REMOVED,0,1,nil) end
