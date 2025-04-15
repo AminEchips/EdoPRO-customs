57a52f9104f1e2d62d77557e27e6cf4f21137f21
Chips <aminekhoury0411@gmail.com>
2025-04-15 11:53:50 -0400

    Update c201.lua

--- script/c201.lua
+++ script/c201.lua
@@ -1,74 +1,78 @@
---Elemental HERO Titantron
+--Elemental HERO Plasma Soldier
 local s,id=GetID()
 function s.initial_effect(c)
-	-- Trigger on being banished
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND+CATEGORY_LEAVE_GRAVE)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_REMOVE)
-	e1:SetProperty(EFFECT_FLAG_DELAY)
-	e1:SetCountLimit(1,id)
-	e1:SetTarget(s.tg)
-	e1:SetOperation(s.op)
-	c:RegisterEffect(e1)
+    -- Effect 1: Set 1 Trap with "HERO" in text on summon
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_SUMMON_SUCCESS)
+    e1:SetProperty(EFFECT_FLAG_DELAY)
+    e1:SetCountLimit(1,id)
+    e1:SetTarget(s.settg)
+    e1:SetOperation(s.setop)
+    c:RegisterEffect(e1)
+    local e2=e1:Clone()
+    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+    c:RegisterEffect(e2)
+
+    -- Effect 2: GY Attribute change
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetCategory(CATEGORY_ANNOUNCE)
+    e3:SetType(EFFECT_TYPE_IGNITION)
+    e3:SetRange(LOCATION_GRAVE)
+    e3:SetCountLimit(1,id+100)
+    e3:SetCost(s.attrcost)
+    e3:SetTarget(s.attrtg)
+    e3:SetOperation(s.attrop)
+    c:RegisterEffect(e3)
 end
 
--- Filter: "Elemental HERO"-mentioning Spell in GY
+-- Effect 1: Set 1 Trap that mentions "HERO"
 function s.setfilter(c)
-	return c:IsType(TYPE_SPELL) and c:IsSSetable() and c:CheckText("Elemental HERO")
+    return c:IsType(TYPE_TRAP) and c:IsSSetable() and c:ListsArchetype(0x3008)
 end
 
--- Filter: banished "Elemental HERO" monsters
-function s.hero_filter(c)
-	return c:IsSetCard(0x8) and c:IsType(TYPE_MONSTER) and c:IsFaceup() and c:IsAbleToHand()
+
+function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,0,nil,1,tp,LOCATION_DECK)
 end
 
-function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	local b1=Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_GRAVE,0,1,nil)
-	local b2=c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-	if chk==0 then return b1 or b2 end
-	local op=0
-	if b1 and b2 then
-		op=Duel.SelectOption(tp,aux.Stringid(id,1),aux.Stringid(id,2))
-	elseif b1 then
-		op=0
-		Duel.Hint(HINT_OPSELECTED,1-tp,aux.Stringid(id,1))
-	else
-		op=1
-		Duel.Hint(HINT_OPSELECTED,1-tp,aux.Stringid(id,2))
-	end
-	e:SetLabel(op)
-	if op==0 then
-		Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,nil,1,tp,LOCATION_GRAVE)
-	else
-		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,LOCATION_REMOVED)
-	end
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+    local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,1,1,nil)
+    if #g>0 then
+        Duel.SSet(tp,g:GetFirst())
+    end
 end
 
-function s.op(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local op=e:GetLabel()
-	if op==0 then
-		-- Set 1 Spell that mentions "Elemental HERO" from GY
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
-		local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_GRAVE,0,1,1,nil)
-		if #g>0 then
-			Duel.SSet(tp,g:GetFirst())
-		end
-	else
-		-- Special Summon this card, then add 1 banished "Elemental HERO" if valid
-		if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
-			local rc=c:GetReasonCard()
-			if rc and rc:IsType(TYPE_FUSION) and rc:IsSetCard(0x8) then
-				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-				local g=Duel.SelectMatchingCard(tp,s.hero_filter,tp,LOCATION_REMOVED,0,1,1,nil)
-				if #g>0 then
-					Duel.SendtoHand(g,nil,REASON_EFFECT)
-					Duel.ConfirmCards(1-tp,g)
-				end
-			end
-		end
-	end
+
+-- Effect 2: Attribute Change from GY
+function s.attrcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
+    Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
 end
+
+function s.attrtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+    Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,nil)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATTRIBUTE)
+    local attr=Duel.AnnounceAttribute(tp,1,ATTRIBUTE_ALL)
+    e:SetLabel(attr)
+end
+
+function s.attrop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_CHANGE_ATTRIBUTE)
+        e1:SetValue(e:GetLabel())
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+        tc:RegisterEffect(e1)
+    end
+end
+
+s.listed_series={0x3008}
