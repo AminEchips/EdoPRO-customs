7ce9d4363186fb0ae2b8257b6ad8d1776a71d846
Chips <aminekhoury0411@gmail.com>
2025-04-15 12:36:08 -0400

    Update c210.lua

--- script/c210.lua
+++ script/c210.lua
@@ -1,125 +1,127 @@
 --Elemental HERO Electaser
 local s,id=GetID()
 function s.initial_effect(c)
-	--Effect 1: If added to hand (except draw), Special Summon it
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetProperty(EFFECT_FLAG_DELAY)
-	e1:SetCode(EVENT_TO_HAND)
-	e1:SetCountLimit(1,id)
-	e1:SetCondition(s.spcon1)
-	e1:SetTarget(s.sptg1)
-	e1:SetOperation(s.spop1)
-	c:RegisterEffect(e1)
-	--Effect 2: Tribute to Special Summon banished non-LIGHT Elemental HERO
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1,{id,1})
-	e2:SetCost(s.spcost2)
-	e2:SetTarget(s.sptg2)
-	e2:SetOperation(s.spop2)
-	c:RegisterEffect(e2)
-	--Effect 3: Boost ATK and grant destruction protection
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,2))
-	e3:SetCategory(CATEGORY_ATKCHANGE)
-	e3:SetType(EFFECT_TYPE_IGNITION)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetCountLimit(1,{id,2})
-	e3:SetCondition(s.atkcon)
-	e3:SetOperation(s.atkop)
-	c:RegisterEffect(e3)
+    -- Effect 1: Special Summon if added to hand (except by draw)
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1:SetProperty(EFFECT_FLAG_DELAY)
+    e1:SetCode(EVENT_TO_HAND)
+    e1:SetCountLimit(1,id)
+    e1:SetCondition(s.spcon1)
+    e1:SetTarget(s.sptg1)
+    e1:SetOperation(s.spop1)
+    c:RegisterEffect(e1)
+
+    -- Effect 2: Tribute to revive a banished non-LIGHT Elemental HERO (targeting)
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1,{id,1})
+    e2:SetCost(s.spcost2)
+    e2:SetTarget(s.sptg2)
+    e2:SetOperation(s.spop2)
+    c:RegisterEffect(e2)
+
+    -- Effect 3: ATK boost and protection
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,2))
+    e3:SetCategory(CATEGORY_ATKCHANGE)
+    e3:SetType(EFFECT_TYPE_IGNITION)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCountLimit(1,{id,2})
+    e3:SetTarget(s.atktg)
+    e3:SetOperation(s.atkop)
+    c:RegisterEffect(e3)
 end
 
--- Effect 1
+-- EFFECT 1
 function s.spcon1(e,tp,eg,ep,ev,re,r,rp)
-	return not e:GetHandler():IsReason(REASON_DRAW)
+    return not e:GetHandler():IsReason(REASON_DRAW)
 end
 function s.sptg1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
 function s.spop1(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) then
-		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
-	end
+    local c=e:GetHandler()
+    if c:IsRelateToEffect(e) then
+        Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+    end
 end
 
--- Effect 2: Tribute to revive a banished non-LIGHT Elemental HERO
-function s.spfilter2(c,e,tp)
-	return c:IsSetCard(0x3008) and not c:IsAttribute(ATTRIBUTE_LIGHT)
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
+-- EFFECT 2: Tribute to summon banished non-LIGHT Elemental HERO
+function s.spcost2(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return e:GetHandler():IsReleasable() end
+    Duel.Release(e:GetHandler(),REASON_COST)
+end
+function s.filter2(c,e,tp)
+    return c:IsSetCard(0x3008) and not c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_REMOVED) and chkc:IsControler(tp) and s.spfilter2(chkc,e,tp) end
-	if chk==0 then return Duel.IsExistingTarget(s.spfilter2,tp,LOCATION_REMOVED,0,1,nil,e,tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,s.spfilter2,tp,LOCATION_REMOVED,0,1,1,nil,e,tp)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+    if chkc then return chkc:IsLocation(LOCATION_REMOVED) and chkc:IsControler(tp) and s.filter2(chkc,e,tp) end
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.IsExistingTarget(s.filter2,tp,LOCATION_REMOVED,0,1,nil,e,tp) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectTarget(tp,s.filter2,tp,LOCATION_REMOVED,0,1,1,nil,e,tp)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
 function s.spop2(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) then
-		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
-	end
+    local tc=Duel.GetFirstTarget()
+    if tc and tc:IsRelateToEffect(e) then
+        Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+    end
 end
 
--- Effect 3 (fixed): Boost ATK and grant destruction protection
-function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingTarget(s.condfilter,tp,LOCATION_MZONE,0,1,nil)
-end
+-- EFFECT 3: Target Level 10 LIGHT Elemental HERO, gain ATK, then banish this & protect
 function s.condfilter(c)
-	return c:IsSetCard(0x3008) and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsLevel(10) and c:IsFaceup()
+    return c:IsSetCard(0x3008) and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsLevel(10) and c:IsFaceup()
 end
 function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.condfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.condfilter,tp,LOCATION_MZONE,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,s.condfilter,tp,LOCATION_MZONE,0,1,1,nil)
+    if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.condfilter(chkc) end
+    if chk==0 then return Duel.IsExistingTarget(s.condfilter,tp,LOCATION_MZONE,0,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+    Duel.SelectTarget(tp,s.condfilter,tp,LOCATION_MZONE,0,1,1,nil)
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local tc=Duel.GetFirstTarget()
-	if not (tc and tc:IsRelateToEffect(e) and tc:IsFaceup()) then return end
+    local c=e:GetHandler()
+    local tc=Duel.GetFirstTarget()
+    if not (tc and tc:IsRelateToEffect(e) and tc:IsFaceup()) then return end
 
-	-- Count unique Attributes
-	local attr={}
-	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
-	for mc in aux.Next(g) do
-		local a=mc:GetAttribute()
-		for i=0,6 do
-			local mask=1<<i
-			if a&mask>0 then attr[mask]=true end
-		end
-	end
-	local ct=0
-	for _,v in pairs(attr) do if v then ct=ct+1 end end
-	if ct==0 then return end
+    -- Count unique Attributes on field
+    local attr = {}
+    local g = Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+    for sc in aux.Next(g) do
+        local a = sc:GetAttribute()
+        for i=0,6 do
+            local mask = 1<<i
+            if a & mask > 0 then attr[mask] = true end
+        end
+    end
+    local ct = 0
+    for _,v in pairs(attr) do if v then ct = ct + 1 end end
 
-	local atk=ct*500
-	-- Apply ATK gain
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_UPDATE_ATTACK)
-	e1:SetValue(atk)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-	tc:RegisterEffect(e1)
+    -- Apply ATK
+    if ct > 0 then
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_ATTACK)
+        e1:SetValue(ct*500)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+        tc:RegisterEffect(e1)
+    end
 
-	-- Banish Electaser as cost
-	if c:IsRelateToEffect(e) and Duel.Remove(c,POS_FACEUP,REASON_EFFECT)>0 then
-		-- Apply protection
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-		e2:SetValue(1)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		tc:RegisterEffect(e2)
-	end
+    -- Banish Electaser and protect the targeted monster
+    if c:IsRelateToEffect(e) and Duel.Remove(c,POS_FACEUP,REASON_EFFECT)>0 then
+        local e2=Effect.CreateEffect(c)
+        e2:SetType(EFFECT_TYPE_SINGLE)
+        e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+        e2:SetValue(1)
+        e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+        tc:RegisterEffect(e2)
+    end
 end
