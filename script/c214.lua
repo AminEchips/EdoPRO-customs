e1eaea25cfa0250d0ef212f726439e59552d97a2
Chips <aminekhoury0411@gmail.com>
2025-04-15 17:58:11 -0400

    Add files via upload

---
+++ script/c214.lua
@@ -0,0 +1,84 @@
+--Elemental HERO Nova
+local s,id=GetID()
+function s.initial_effect(c)
+    c:EnableReviveLimit()
+    Fusion.AddProcMix(c,true,true,58932615,75434695) -- Burstinatrix + Necroshade
+
+    -- Add "H - Heated Heart" on Special Summon
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e1:SetProperty(EFFECT_FLAG_DELAY)
+    e1:SetCountLimit(1,id)
+    e1:SetTarget(s.thtg)
+    e1:SetOperation(s.thop)
+    c:RegisterEffect(e1)
+
+    -- ATK gain + double damage when attacking
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_ATKCHANGE)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e2:SetCountLimit(1,{id,1})
+    e2:SetCondition(s.atkcon)
+    e2:SetCost(s.atkcost)
+    e2:SetOperation(s.atkop)
+    c:RegisterEffect(e2)
+end
+
+-- Search "H - Heated Heart"
+function s.thfilter(c)
+    return c:IsCode(96008713) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
+    end
+end
+
+-- Battle trigger
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetAttacker()==e:GetHandler()
+end
+function s.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_GRAVE,0,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+    local g=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_GRAVE,0,1,1,nil)
+    Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if c:IsRelateToEffect(e) and c:IsFaceup() then
+        -- ATK boost
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_ATTACK)
+        e1:SetValue(500)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+        c:RegisterEffect(e1)
+
+        -- Double damage
+        local e2=Effect.CreateEffect(c)
+        e2:SetType(EFFECT_TYPE_SINGLE)
+        e2:SetCode(EFFECT_CHANGE_BATTLE_DAMAGE)
+        e2:SetValue(AUXDAM_DOUBLE)
+        e2:SetCondition(s.damcon)
+        e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE_CAL)
+        c:RegisterEffect(e2)
+    end
+end
+function s.damcon(e)
+    local c=e:GetHandler()
+    local bc=c:GetBattleTarget()
+    return bc~=nil and bc:IsControler(1-c:GetControler())
+end
