619cb442e50d34d015e6398e250ba577904087b5
Chips <aminekhoury0411@gmail.com>
2025-04-15 18:15:14 -0400

    Update c214.lua

--- script/c214.lua
+++ script/c214.lua
@@ -2,9 +2,10 @@
 local s,id=GetID()
 function s.initial_effect(c)
     c:EnableReviveLimit()
-    --Fusion Materials
-    Fusion.AddProcMix(c,true,true,58932615,89252153) -- Burstinatrix + Necroshade
-    -- Add "H - Heated Heart" on Special Summon
+    --Fusion Materials: Burstinatrix + Necroshade
+    Fusion.AddProcMix(c,true,true,58932615,89252153)
+
+    --Effect 1: Add "H - Heated Heart" on Fusion Summon
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
@@ -16,20 +17,23 @@ function s.initial_effect(c)
     e1:SetTarget(s.thtg)
     e1:SetOperation(s.thop)
     c:RegisterEffect(e1)
-    -- Banish from GY, gain ATK, and double battle damage on attack declaration
+
+    --Effect 2: ATK gain + double damage on attack declaration
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
     e2:SetCategory(CATEGORY_REMOVE+CATEGORY_ATKCHANGE)
     e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e2:SetProperty(EFFECT_FLAG_DELAY)
     e2:SetCountLimit(1,{id,1})
     e2:SetTarget(s.btg)
     e2:SetOperation(s.bop)
     c:RegisterEffect(e2)
 end
 s.listed_names={40418351} -- H - Heated Heart
+s.material_setcode={0x3008}
 
--- Effect 1: Add H - Heated Heart
+-- EFFECT 1: Search on Fusion Summon
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
     return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION)
 end
@@ -49,15 +53,18 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
--- Effect 2: Attack boost and damage double
+-- EFFECT 2: Banish monster, gain ATK, double damage
+function s.banfilter(c)
+    return c:IsMonster() and c:IsAbleToRemove()
+end
 function s.btg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_GRAVE,0,1,nil) end
+    if chk==0 then return Duel.IsExistingMatchingCard(s.banfilter,tp,LOCATION_GRAVE,0,1,nil) end
     Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_GRAVE)
 end
 function s.bop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-    local g=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_GRAVE,0,1,1,nil)
+    local g=Duel.SelectMatchingCard(tp,s.banfilter,tp,LOCATION_GRAVE,0,1,1,nil)
     if #g>0 and Duel.Remove(g,POS_FACEUP,REASON_EFFECT)>0 and c:IsRelateToEffect(e) and c:IsFaceup() then
         -- Gain 500 ATK
         local e1=Effect.CreateEffect(c)
@@ -66,7 +73,7 @@ function s.bop(e,tp,eg,ep,ev,re,r,rp)
         e1:SetValue(500)
         e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
         c:RegisterEffect(e1)
-        -- Double battle damage to opponent this turn
+        -- Double battle damage
         local e2=Effect.CreateEffect(c)
         e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
         e2:SetCode(EVENT_PRE_BATTLE_DAMAGE)
@@ -80,3 +87,4 @@ function s.bop(e,tp,eg,ep,ev,re,r,rp)
         c:RegisterEffect(e2)
     end
 end
+
