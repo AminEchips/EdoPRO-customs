f6e772e5b4d3a669e30d36bea0b390063b74fb3e
Chips <aminekhoury0411@gmail.com>
2025-04-15 10:38:57 -0400

    Update c209.lua

--- script/c209.lua
+++ script/c209.lua
@@ -1,7 +1,7 @@
 -- Elemental HERO Titantron
 local s,id=GetID()
 function s.initial_effect(c)
-    -- e1: Set 1 Spell that lists "Elemental HERO" in text from GY
+    -- Set 1 Spell that mentions "Elemental HERO" in its text
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_LEAVE_GRAVE)
@@ -15,7 +15,7 @@ function s.initial_effect(c)
     e1:SetOperation(s.setop)
     c:RegisterEffect(e1)
 
-    -- e2: Special Summon itself, then optionally add 1 banished HERO if used for Fusion Summon of a HERO
+    -- Special Summon itself, then if banished for HERO Fusion, add 1 banished HERO to hand
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
     e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND)
@@ -30,14 +30,13 @@ function s.initial_effect(c)
     c:RegisterEffect(e2)
 end
 s.listed_series={0x8}
-s.listed_names={CARD_ELEMENTAL_HERO_NEOS}
 
--- ========== Effect 1: Set Spell with "Elemental HERO" in text ==========
+-- Effect 1: Set Spell from GY
 function s.setcon(e,tp,eg,ep,ev,re,r,rp)
     return eg:IsContains(e:GetHandler())
 end
 function s.setfilter(c)
-    return c:IsType(TYPE_SPELL) and c:IsSSetable() and aux.HasListedSetCard(c,0x8) and c:IsInGY()
+    return c:IsType(TYPE_SPELL) and c:IsSSetable() and c:IsAbleToSet() and c:CheckText("Elemental HERO")
 end
 function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_GRAVE,0,1,nil) end
@@ -51,7 +50,7 @@ function s.setop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
--- ========== Effect 2: Special Summon this, then optionally add HERO ==========
+-- Effect 2: Special Summon and optional add
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
     return eg:IsContains(e:GetHandler())
 end
@@ -60,20 +59,24 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
         and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
-function s.addfilter(c)
-    return c:IsSetCard(0x8) and c:IsFaceup() and c:IsAbleToHand()
+function s.thfilter(c)
+    return c:IsSetCard(0x8) and c:IsAbleToHand() and c:IsFaceup()
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     if not c:IsRelateToEffect(e) then return end
-    if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 and bit.band(r,REASON_FUSION)~=0 and re and re:GetHandler():IsSetCard(0x8) then
-        local g=Duel.GetMatchingGroup(s.addfilter,tp,LOCATION_REMOVED,0,nil)
-        if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
-            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-            local tg=g:Select(tp,1,1,nil)
-            Duel.SendtoHand(tg,nil,REASON_EFFECT)
-            Duel.ConfirmCards(1-tp,tg)
+    if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP) > 0 then
+        -- Only check for fusion summon with HERO monster
+        if bit.band(r,REASON_FUSION)~=0 and re and re:GetHandler():IsSetCard(0x8) then
+            local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_REMOVED,0,nil)
+            if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+                Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+                local sg=g:Select(tp,1,1,nil)
+                Duel.SendtoHand(sg,nil,REASON_EFFECT)
+                Duel.ConfirmCards(1-tp,sg)
+            end
         end
     end
 end
+
 
