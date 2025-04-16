ceebe827eca92c3fba301d48168bca08523efdfc
Chips <aminekhoury0411@gmail.com>
2025-04-15 23:29:24 -0400

    Update c227.lua

--- script/c227.lua
+++ script/c227.lua
@@ -3,22 +3,21 @@ local s,id=GetID()
 function s.initial_effect(c)
     c:EnableReviveLimit()
     Fusion.AddProcMixN(c,true,true,s.matfilter,4)
-    aux.AddFusionProcCodeFunRep(c,aux.FilterBoolFunction(Card.IsFusionSetCard,0x3008),s.attfilter,4,true,true)
-    aux.AddCodeList(c,45906428) -- Miracle Fusion
 
-    -- Shuffle 1 card and burn
+    --Shuffle & Burn on Special Summon or if Attribute differs
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_TODECK+CATEGORY_DAMAGE)
-    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1:SetCategory(CATEGORY_DAMAGE+CATEGORY_TODECK)
+    e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
     e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-    e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+    e1:SetRange(LOCATION_MZONE)
     e1:SetCountLimit(1,id)
-    e1:SetTarget(s.tdtg)
-    e1:SetOperation(s.tdop)
+    e1:SetCondition(s.shufcon)
+    e1:SetTarget(s.shuftg)
+    e1:SetOperation(s.shufop)
     c:RegisterEffect(e1)
 
-    -- Special Summon from GY
+    --Special Summon 1 "Elemental HERO" from GY
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
     e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -29,51 +28,46 @@ function s.initial_effect(c)
     e2:SetOperation(s.spop)
     c:RegisterEffect(e2)
 
-    -- Gain ATK when attack declared
+    --Gain ATK when attack is declared
     local e3=Effect.CreateEffect(c)
     e3:SetDescription(aux.Stringid(id,2))
-    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
     e3:SetCode(EVENT_ATTACK_ANNOUNCE)
-    e3:SetCategory(CATEGORY_ATKCHANGE)
+    e3:SetRange(LOCATION_MZONE)
     e3:SetCountLimit(1,{id,2})
-    e3:SetTarget(s.atktg)
     e3:SetOperation(s.atkop)
     c:RegisterEffect(e3)
 end
 
-function s.matfilter(c,fc,sumtype,tp)
-    return c:IsFusionSetCard(0x3008,fc,sumtype,tp) and c:IsType(TYPE_FUSION)
+-- Must be 4 "Elemental HERO" Fusion Monsters with different Attributes (WIND, WATER, FIRE, EARTH)
+function s.matfilter(c,scard,sumtype,tp)
+    return c:IsSetCard(0x3008) and c:IsType(TYPE_FUSION)
 end
 
-function s.attfilter(c,fc,sumtype,tp)
-    return c:IsFusionSetCard(0x3008,fc,sumtype,tp) and c:IsType(TYPE_FUSION)
-        and (c:IsAttribute(ATTRIBUTE_WIND) or c:IsAttribute(ATTRIBUTE_WATER) or c:IsAttribute(ATTRIBUTE_FIRE) or c:IsAttribute(ATTRIBUTE_EARTH))
+-- Effect 1: Burn + Shuffle
+function s.shufcon(e,tp,eg,ep,ev,re,r,rp)
+    return eg:IsContains(e:GetHandler()) or eg:IsExists(s.attrdiff,e:GetHandlerPlayer())
 end
-
--- Effect 1: Bounce + Burn
-function s.tdfilter(c)
-    return c:IsAbleToDeck()
+function s.attrdiff(c)
+    return c:IsFaceup() and c:IsControler(1-c:GetOwner()) and c:IsSummonType(SUMMON_TYPE_SPECIAL)
 end
-function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return chkc:IsOnField() and s.tdfilter(chkc) end
-    if chk==0 then return Duel.IsExistingTarget(s.tdfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+function s.shuftg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,1-tp,LOCATION_ONFIELD)
+end
+function s.shufop(e,tp,eg,ep,ev,re,r,rp)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-    local g=Duel.SelectTarget(tp,s.tdfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
-    Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
-    Duel.SetPossibleOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,0)
-end
-function s.tdop(e,tp,eg,ep,ev,re,r,rp)
-    local tc=Duel.GetFirstTarget()
-    if tc:IsRelateToEffect(e) then
-        local atk=tc:GetAttack()
-        if Duel.SendtoDeck(tc,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)>0 and tc:IsPreviousLocation(LOCATION_ONFIELD) and atk>0 then
-            Duel.BreakEffect()
+    local g=Duel.SelectMatchingCard(tp,nil,tp,0,LOCATION_ONFIELD,1,1,nil)
+    local tc=g:GetFirst()
+    if tc and Duel.SendtoDeck(tc,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)>0 and tc:IsPreviousTypeOnField(TYPE_MONSTER) then
+        local atk=tc:GetBaseAttack()
+        if atk>0 then
             Duel.Damage(1-tp,atk,REASON_EFFECT)
         end
     end
 end
 
--- Effect 2: Special Summon 1 E-HERO from GY
+-- Effect 2: Special Summon an Elemental HERO from the GY
 function s.spfilter(c,e,tp)
     return c:IsSetCard(0x3008) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
@@ -90,25 +84,20 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
--- Effect 3: ATK boost on attack declaration
-function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
-    local c=e:GetHandler()
-    local g=Duel.GetMatchingGroup(Card.IsAbleToGrave,tp,LOCATION_HAND+LOCATION_ONFIELD,0,e:GetHandler())
-    if chk==0 then return #g>0 end
-    Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_HAND+LOCATION_ONFIELD)
-end
+-- Effect 3: Gain 1000 ATK per card sent from hand/field
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
     local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGrave,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,99,nil)
-    if #g>0 and Duel.SendtoGrave(g,REASON_EFFECT)>0 then
-        local atk=Duel.GetOperatedGroup():GetCount()*1000
-        if c:IsRelateToEffect(e) and c:IsFaceup() then
-            local e1=Effect.CreateEffect(c)
-            e1:SetType(EFFECT_TYPE_SINGLE)
-            e1:SetCode(EFFECT_UPDATE_ATTACK)
-            e1:SetValue(atk)
-            e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
-            c:RegisterEffect(e1)
-        end
+    if #g==0 then return end
+    if Duel.SendtoGrave(g,REASON_EFFECT)>0 and e:GetHandler():IsRelateToEffect(e) then
+        local atk=1000*#g
+        local c=e:GetHandler()
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_ATTACK)
+        e1:SetValue(atk)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+        c:RegisterEffect(e1)
     end
 end
+
