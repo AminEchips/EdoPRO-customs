66fa228266f48435fec38b637b74c7869f94223c
Chips <aminekhoury0411@gmail.com>
2025-04-16 17:09:16 -0400

    Add files via upload

---
+++ script/c240.lua
@@ -0,0 +1,88 @@
+--Evil HERO Supreme King Bane
+local s,id=GetID()
+function s.initial_effect(c)
+    -- Fusion materials: 1 "Evil HERO" Fusion Monster + 1 Level 8 or higher monster
+    c:EnableReviveLimit()
+    Fusion.AddProcMix(c,true,true,s.fusfilter,s.lv8filter)
+
+    -- Must be Special Summoned with "Dark Fusion"
+    local e0=Effect.CreateEffect(c)
+    e0:SetType(EFFECT_TYPE_SINGLE)
+    e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+    e0:SetCode(EFFECT_SPSUMMON_CONDITION)
+    e0:SetValue(aux.fuslimit)
+    c:RegisterEffect(e0)
+
+    -- Cannot be destroyed by battle or card effects
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+    e1:SetValue(1)
+    c:RegisterEffect(e1)
+    local e2=e1:Clone()
+    e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+    c:RegisterEffect(e2)
+
+    -- Destroy all opponent's cards if you control "Supreme King's Castle"
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,0))
+    e3:SetCategory(CATEGORY_DESTROY)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e3:SetCondition(s.descon)
+    e3:SetTarget(s.destg)
+    e3:SetOperation(s.desop)
+    c:RegisterEffect(e3)
+
+    -- Set 1 Fusion Spell or "Super Polymerization" from GY
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,1))
+    e4:SetType(EFFECT_TYPE_IGNITION)
+    e4:SetRange(LOCATION_MZONE)
+    e4:SetCountLimit(1)
+    e4:SetTarget(s.settg)
+    e4:SetOperation(s.setop)
+    c:RegisterEffect(e4)
+end
+
+s.listed_names={97569816} -- Super Polymerization
+s.material_setcode={0x8}
+s.dark_calling=true
+
+function s.fusfilter(c)
+    return c:IsSetCard(0x8) and c:IsType(TYPE_FUSION)
+end
+function s.lv8filter(c)
+    return c:IsLevelAbove(8)
+end
+
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_ONFIELD,0,1,nil,84330567) -- Supreme King's Castle
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(nil,tp,0,LOCATION_ONFIELD,1,nil) end
+    local g=Duel.GetMatchingGroup(nil,tp,0,LOCATION_ONFIELD,nil)
+    Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+    local g=Duel.GetMatchingGroup(nil,tp,0,LOCATION_ONFIELD,nil)
+    if #g>0 then
+        Duel.Destroy(g,REASON_EFFECT)
+    end
+end
+
+function s.setfilter(c)
+    return ((c:IsType(TYPE_SPELL) and c:IsType(TYPE_FUSION)) or c:IsCode(97569816)) and c:IsSSetable()
+end
+function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_GRAVE,0,1,nil) end
+end
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+    local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+    if #g>0 then
+        Duel.SSet(tp,g:GetFirst())
+    end
+end
