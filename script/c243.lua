f33d979c40c898a204280094827d4801c247efde
Chips <aminekhoury0411@gmail.com>
2025-04-16 18:34:34 -0400

    Add files via upload

---
+++ script/c243.lua
@@ -0,0 +1,75 @@
+--The Evil Rises
+local s,id=GetID()
+function s.initial_effect(c)
+    --Negate effect
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY+CATEGORY_RECOVER)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_CHAINING)
+    e1:SetCondition(s.negcon)
+    e1:SetTarget(s.negtg)
+    e1:SetOperation(s.negop)
+    c:RegisterEffect(e1)
+
+    --GY effect
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetRange(LOCATION_GRAVE)
+    e2:SetCountLimit(1,id)
+    e2:SetCost(aux.bfgcost)
+    e2:SetTarget(s.gytg)
+    e2:SetOperation(s.gyop)
+    c:RegisterEffect(e2)
+end
+
+s.listed_series={0x6008}
+s.listed_names={94820406} -- Dark Fusion
+
+function s.cfilter(c)
+    return c:IsSetCard(0x6008) and c:IsLevelAbove(6)
+end
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
+        and re:IsHasType(EFFECT_TYPE_ACTIVATE+EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_IGNITION)
+        and Duel.IsChainNegatable(ev)
+end
+function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+    if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+        Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+    end
+end
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) and Duel.Destroy(eg,REASON_EFFECT)>0 then
+        local rc=re:GetHandler()
+        if rc:IsType(TYPE_MONSTER) and rc:IsPreviousLocation(LOCATION_MZONE) then
+            Duel.Recover(tp,rc:GetBaseAttack(),REASON_EFFECT)
+        end
+    end
+end
+
+function s.filter(c)
+    return c:ListsCode(94820406) and (c:IsAbleToHand() or c:IsCanBeSpecialSummoned(nil,0,tp,false,false))
+end
+function s.gytg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,tp) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE+LOCATION_REMOVED)
+end
+function s.gyop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_OPERATECARD)
+    local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil,tp)
+    local tc=g:GetFirst()
+    if tc then
+        local op=Duel.SelectOption(tp,aux.Stringid(id,2),aux.Stringid(id,3)) -- Add or Special Summon
+        if op==0 and tc:IsAbleToHand() then
+            Duel.SendtoHand(tc,nil,REASON_EFFECT)
+            Duel.ConfirmCards(1-tp,tc)
+        elseif op==1 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and tc:IsCanBeSpecialSummoned(nil,0,tp,false,false) then
+            Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+        end
+    end
+end
