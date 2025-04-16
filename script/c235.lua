eec87606f422e16c9b3b948ae1db644cf0558397
Chips <aminekhoury0411@gmail.com>
2025-04-16 10:11:17 -0400

    Add files via upload

---
+++ script/c235.lua
@@ -0,0 +1,108 @@
+--Evil HERO Calamity
+local s,id=GetID()
+function s.initial_effect(c)
+    -- Special Summon from hand by sending 2 other "HERO" cards from hand or field to GY
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCountLimit(1,id)
+    e1:SetCondition(s.spcon)
+    e1:SetCost(s.spcost)
+    e1:SetTarget(s.sptg)
+    e1:SetOperation(s.spop)
+    c:RegisterEffect(e1)
+
+    -- Quick Effect: Link Summon using this card
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetType(EFFECT_TYPE_QUICK_O)
+    e2:SetCode(EVENT_FREE_CHAIN)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END+TIMING_BATTLE_PHASE)
+    e2:SetCountLimit(1,{id,1})
+    e2:SetCondition(s.lkcon)
+    e2:SetTarget(s.lktg)
+    e2:SetOperation(s.lkop)
+    c:RegisterEffect(e2)
+
+    -- Banish effect: Add 1 card that mentions "Dark Fusion"
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,2))
+    e3:SetCategory(CATEGORY_TOHAND)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e3:SetCode(EVENT_REMOVE)
+    e3:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+    e3:SetCountLimit(1,{id,2})
+    e3:SetTarget(s.thtg)
+    e3:SetOperation(s.thop)
+    c:RegisterEffect(e3)
+end
+s.listed_series={0x8}
+s.listed_names={94820406} -- Dark Fusion
+
+-- Special Summon from hand
+function s.cfilter(c)
+    return c:IsSetCard(0x8) and c:IsAbleToGraveAsCost()
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+end
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,2,e:GetHandler()) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+    local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,2,2,e:GetHandler())
+    Duel.SendtoGrave(g,REASON_COST)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    if e:GetHandler():IsRelateToEffect(e) then
+        Duel.SpecialSummon(e:GetHandler(),0,tp,tp,false,false,POS_FACEUP)
+    end
+end
+
+-- Quick Link Summon
+function s.lkcon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.IsMainPhase() or Duel.IsBattlePhase()
+end
+function s.lkfilter(c,tp,mc)
+    return c:IsType(TYPE_LINK) and c:IsSetCard(0x8) and mc:IsCanBeLinkMaterial(c)
+        and Duel.GetLocationCountFromEx(tp,tp,mc,c)>0 and c:IsLinkSummonable(nil,{mc})
+end
+function s.lktg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then
+        return Duel.IsExistingMatchingCard(s.lkfilter,tp,LOCATION_EXTRA,0,1,nil,tp,e:GetHandler())
+    end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function s.lkop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,s.lkfilter,tp,LOCATION_EXTRA,0,1,1,nil,tp,c)
+    local sc=g:GetFirst()
+    if sc then
+        Duel.LinkSummon(tp,sc,nil,{c})
+    end
+end
+
+-- Add "Dark Fusion" card when banished
+function s.thfilter(c)
+    return c:ListsCode(94820406) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.thfilter(chkc) end
+    if chk==0 then return Duel.IsExistingTarget(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectTarget(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,tp,LOCATION_GRAVE)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc:IsRelateToEffect(e) then
+        Duel.SendtoHand(tc,nil,REASON_EFFECT)
+    end
+end
