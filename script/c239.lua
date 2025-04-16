e94dee9cefe473a583722602768f7af8226f6ae8
Chips <aminekhoury0411@gmail.com>
2025-04-16 15:00:10 -0400

    Update c239.lua

--- script/c239.lua
+++ script/c239.lua
@@ -1,122 +1,121 @@
 --Evil HERO Brimstorm
 local s,id=GetID()
 function s.initial_effect(c)
-    c:EnableReviveLimit()
-    Fusion.AddProcMix(c,true,true,s.matfilter,aux.FilterBoolFunction(Card.IsType,TYPE_FUSION))
+	-- Must be Special Summoned with "Dark Fusion"
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,aux.FilterBoolFunction(s.matfilter),22908820,238)
 
-    -- Must be Special Summoned with Dark Fusion
-    local e0=Effect.CreateEffect(c)
-    e0:SetType(EFFECT_TYPE_SINGLE)
-    e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-    e0:SetCode(EFFECT_SPSUMMON_CONDITION)
-    e0:SetValue(aux.fuslimit)
-    c:RegisterEffect(e0)
+	-- Piercing and DEF change
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DEFCHANGE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_BATTLED)
+	e1:SetCondition(s.defcon)
+	e1:SetOperation(s.defop)
+	c:RegisterEffect(e1)
 
-    -- Change DEF and inflict piercing
-    local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_POSITION)
-    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e1:SetCode(EVENT_BATTLED)
-    e1:SetCondition(s.poscon)
-    e1:SetOperation(s.posop)
-    c:RegisterEffect(e1)
+	-- Battle protection
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_DAMAGE_STEP_END)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.protcon)
+	e2:SetTarget(s.prottg)
+	e2:SetOperation(s.protop)
+	c:RegisterEffect(e2)
 
-    local e2=Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_SINGLE)
-    e2:SetCode(EFFECT_PIERCE)
-    c:RegisterEffect(e2)
-
-    -- Protection from banishing
-    local e3=Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(id,1))
-    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e3:SetCode(EVENT_DAMAGE_STEP_END)
-    e3:SetCountLimit(1,id)
-    e3:SetCondition(s.bancon)
-    e3:SetTarget(s.bantg)
-    e3:SetOperation(s.banop)
-    c:RegisterEffect(e3)
-
-    -- Negate and revive
-    local e4=Effect.CreateEffect(c)
-    e4:SetDescription(aux.Stringid(id,2))
-    e4:SetCategory(CATEGORY_NEGATE+CATEGORY_SPECIAL_SUMMON)
-    e4:SetType(EFFECT_TYPE_QUICK_O)
-    e4:SetCode(EVENT_CHAINING)
-    e4:SetRange(LOCATION_MZONE)
-    e4:SetCondition(s.negcon)
-    e4:SetCost(s.negcost)
-    e4:SetTarget(s.negtg)
-    e4:SetOperation(s.negop)
-    c:RegisterEffect(e4)
+	-- Negate and revive
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_NEGATE+CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_CHAINING)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetCondition(s.negcon)
+	e3:SetCost(s.negcost)
+	e3:SetTarget(s.negtg)
+	e3:SetOperation(s.negop)
+	c:RegisterEffect(e3)
 end
-
-s.listed_names={239}
-s.listed_series={0x8}
 
 function s.matfilter(c,fc,sumtype,tp)
-    return c:IsSetCard(0x8,fc,sumtype,tp)
+	return c:IsSetCard(0x8,fc,sumtype,tp) and c:IsType(TYPE_FUSION)
 end
 
-function s.poscon(e,tp,eg,ep,ev,re,r,rp)
-    local tc=e:GetHandler():GetBattleTarget()
-    return tc and tc:IsControler(1-tp)
+function s.defcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local bc=c:GetBattleTarget()
+	return bc and bc:IsControler(1-tp)
 end
-function s.posop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    local bc=c:GetBattleTarget()
-    if bc and bc:IsFaceup() and bc:IsRelateToBattle() then
-        Duel.ChangePosition(bc,POS_FACEUP_DEFENSE)
-        local e1=Effect.CreateEffect(c)
-        e1:SetType(EFFECT_TYPE_SINGLE)
-        e1:SetCode(EFFECT_SET_DEFENSE_FINAL)
-        e1:SetValue(0)
-        e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-        bc:RegisterEffect(e1)
-    end
+function s.defop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_DEFENSE_FINAL)
+		e1:SetValue(0)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
+		-- Piercing
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_PIERCE)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e2)
+	end
 end
 
-function s.bancon(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    local bc=c:GetBattleTarget()
-    return bc and bc:IsSetCard(0x8) and bc:IsControler(tp) and bc:IsRelateToBattle()
+function s.protcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local bc=c:GetBattleTarget()
+	return bc and bc:IsSetCard(0x8)
 end
-function s.bantg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return true end
+function s.prottg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
 end
-function s.banop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_FIELD)
-    e1:SetCode(EFFECT_CANNOT_REMOVE)
-    e1:SetTargetRange(LOCATION_ONFIELD,0)
-    e1:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x8))
-    e1:SetReset(RESET_PHASE+PHASE_END+RESET_OPPO_TURN)
-    Duel.RegisterEffect(e1,tp)
+function s.protop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local bc=c:GetBattleTarget()
+	if bc and bc:IsRelateToBattle() then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CANNOT_BE_REMOVED)
+		e1:SetValue(1)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		bc:RegisterEffect(e1)
+	end
 end
 
 function s.negcon(e,tp,eg,ep,ev,re,r,rp)
-    return rp~=tp and Duel.IsChainNegatable(ev)
+	return rp~=tp and Duel.IsChainNegatable(ev)
 end
 function s.negcost(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
-    Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsAbleToRemoveAsCost() end
+	Duel.Remove(c,POS_FACEUP,REASON_COST)
 end
 function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
-    Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.fusfilter,tp,LOCATION_GRAVE,0,1,nil)
+		and Duel.IsExistingMatchingCard(s.nfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_REMOVED)
 end
-function s.spfilter(c,e,tp)
-    return c:IsSetCard(0x8) and c:IsType(TYPE_FUSION) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+function s.fusfilter(c)
+	return c:IsSetCard(0x8) and c:IsType(TYPE_FUSION)
+end
+function s.nfilter(c,e,tp)
+	return c:IsCode(22908820,238) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.negop(e,tp,eg,ep,ev,re,r,rp)
-    if Duel.NegateActivation(ev) then
-        Duel.BreakEffect()
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-        local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
-        if #g>0 then
-            Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-        end
-    end
+	if Duel.NegateActivation(ev) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,s.nfilter,tp,LOCATION_REMOVED,0,1,1,nil,e,tp)
+		if #g>0 then
+			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
 end
