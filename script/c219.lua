98580796ef307e5e18e9925dc162a6e17ccf6a11
Chips <aminekhoury0411@gmail.com>
2025-04-15 21:42:02 -0400

    Update c219.lua

--- script/c219.lua
+++ script/c219.lua
@@ -2,94 +2,102 @@
 local s,id=GetID()
 function s.initial_effect(c)
     c:EnableReviveLimit()
-    Fusion.AddProcMix(c,true,true,40044918,89312388) -- Stratos + Prisma
+    Fusion.AddProcMix(c,true,true,40044918,56647086) -- Stratos + Prisma
 
-    -- Return opponent's card to hand on Fusion Summon
+    -- Bounce 1 opponent's card on Fusion Summon
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_TOHAND)
     e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-    e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
-    e1:SetCondition(s.thcon)
-    e1:SetTarget(s.thtg)
-    e1:SetOperation(s.thop)
+    e1:SetProperty(EFFECT_FLAG_DELAY)
+    e1:SetCountLimit(1,id)
+    e1:SetCondition(s.bouncecon)
+    e1:SetTarget(s.bouncetg)
+    e1:SetOperation(s.bounceop)
     c:RegisterEffect(e1)
 
-    -- Attack directly by halving ATK
+    -- Halve ATK and direct attack
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
     e2:SetType(EFFECT_TYPE_IGNITION)
     e2:SetRange(LOCATION_MZONE)
-    e2:SetCountLimit(1)
-    e2:SetCondition(s.dircon)
-    e2:SetOperation(s.dirop)
+    e2:SetCountLimit(1,{id,1})
+    e2:SetCondition(s.atkcon)
+    e2:SetOperation(s.atkop)
     c:RegisterEffect(e2)
 
-    -- Add 1 "Elemental HERO" from GY to hand
+    -- Add 1 Elemental HERO from GY if damage inflicted or monster destroyed by battle
     local e3=Effect.CreateEffect(c)
     e3:SetDescription(aux.Stringid(id,2))
     e3:SetCategory(CATEGORY_TOHAND)
     e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e3:SetProperty(EFFECT_FLAG_DELAY)
     e3:SetCode(EVENT_BATTLE_DAMAGE)
-    e3:SetCountLimit(1,{id,1})
-    e3:SetCondition(s.addcon)
-    e3:SetTarget(s.addtg)
-    e3:SetOperation(s.addop)
+    e3:SetProperty(EFFECT_FLAG_DELAY)
+    e3:SetCountLimit(1,{id,2})
+    e3:SetCondition(s.reccon1)
+    e3:SetTarget(s.rectg)
+    e3:SetOperation(s.recop)
     c:RegisterEffect(e3)
+    local e4=e3:Clone()
+    e4:SetCode(EVENT_BATTLE_DESTROYING)
+    e4:SetCondition(s.reccon2)
+    c:RegisterEffect(e4)
 end
-s.listed_series={0x3008}
-s.listed_names={40044918,89312388}
 
-function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+s.listed_series={0x3008}
+
+-- Effect 1: Bounce
+function s.bouncecon(e,tp,eg,ep,ev,re,r,rp)
     return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION)
 end
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return chkc:IsControler(1-tp) and chkc:IsOnField() and chkc:IsAbleToHand() end
-    if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToHand,tp,0,LOCATION_ONFIELD,1,nil) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
-    local g=Duel.SelectTarget(tp,Card.IsAbleToHand,tp,0,LOCATION_ONFIELD,1,1,nil)
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+function s.bouncetg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToHand,tp,0,LOCATION_ONFIELD,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,1-tp,LOCATION_ONFIELD)
 end
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
-    local tc=Duel.GetFirstTarget()
-    if tc and tc:IsRelateToEffect(e) then
-        Duel.SendtoHand(tc,nil,REASON_EFFECT)
+function s.bounceop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+    local g=Duel.SelectMatchingCard(tp,Card.IsAbleToHand,tp,0,LOCATION_ONFIELD,1,1,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
     end
 end
 
-function s.dircon(e,tp,eg,ep,ev,re,r,rp)
+-- Effect 2: Halve ATK and direct attack
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
     return Duel.GetCurrentPhase()==PHASE_MAIN1
 end
-function s.dirop(e,tp,eg,ep,ev,re,r,rp)
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
-    if c:IsFaceup() and c:IsRelateToEffect(e) then
-        local e1=Effect.CreateEffect(c)
-        e1:SetType(EFFECT_TYPE_SINGLE)
-        e1:SetCode(EFFECT_UPDATE_ATTACK)
-        e1:SetValue(-c:GetAttack()/2)
-        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-        c:RegisterEffect(e1)
-        local e2=Effect.CreateEffect(c)
-        e2:SetType(EFFECT_TYPE_SINGLE)
-        e2:SetCode(EFFECT_DIRECT_ATTACK)
-        e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-        c:RegisterEffect(e2)
-    end
+    if not c:IsRelateToEffect(e) or not c:IsFaceup() then return end
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+    e1:SetValue(c:GetAttack()/2)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+    c:RegisterEffect(e1)
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_DIRECT_ATTACK)
+    e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+    c:RegisterEffect(e2)
 end
 
-function s.addcon(e,tp,eg,ep,ev,re,r,rp)
-    return Duel.GetAttacker()==e:GetHandler() or Duel.GetAttackTarget()==e:GetHandler()
+-- Effect 3: Recovery
+function s.reccon1(e,tp,eg,ep,ev,re,r,rp)
+    return ep~=tp -- inflict battle damage to opponent
+end
+function s.reccon2(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler():IsRelateToBattle()
 end
 function s.filter(c)
     return c:IsSetCard(0x3008) and c:IsAbleToHand()
 end
-function s.addtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.rectg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil) end
     Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
 end
-function s.addop(e,tp,eg,ep,ev,re,r,rp)
+function s.recop(e,tp,eg,ep,ev,re,r,rp)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
     local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil)
     if #g>0 then
@@ -97,3 +105,4 @@ function s.addop(e,tp,eg,ep,ev,re,r,rp)
         Duel.ConfirmCards(1-tp,g)
     end
 end
+
