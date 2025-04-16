9ff751e6de96c282d1c6ac0902086c76d22a5d6b
Chips <aminekhoury0411@gmail.com>
2025-04-15 22:19:19 -0400

    Update c222.lua

--- script/c222.lua
+++ script/c222.lua
@@ -2,9 +2,9 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	Fusion.AddProcMixN(c,true,true,s.matfilter,2)
-	
-	-- Treated as EARTH while on field
+	Fusion.AddProcMixN(c,true,true,s.fusionfilter,2)
+
+	-- Gains EARTH Attribute while on field
 	local e0=Effect.CreateEffect(c)
 	e0:SetType(EFFECT_TYPE_SINGLE)
 	e0:SetCode(EFFECT_ADD_ATTRIBUTE)
@@ -13,60 +13,57 @@ function s.initial_effect(c)
 	e0:SetValue(ATTRIBUTE_EARTH)
 	c:RegisterEffect(e0)
 
-	-- Destroy 1 Spell/Trap
+	-- Destroy 1 Spell/Trap (targeting)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCountLimit(1)
 	e1:SetTarget(s.destg)
 	e1:SetOperation(s.desop)
 	c:RegisterEffect(e1)
 
-	-- Tribute 1 monster: Add 1 non-EARTH/WIND Elemental HERO
+	-- Tribute and search
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1,id)
+	e2:SetCountLimit(1,{id,1})
 	e2:SetCost(s.thcost)
 	e2:SetTarget(s.thtg)
 	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
 
-	-- On Battle Destruction: Special Summon 1 from hand, 1 from GY
+	-- Special Summon on battle destroy
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,2))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetCode(EVENT_BATTLE_DESTROYING)
 	e3:SetProperty(EFFECT_FLAG_DELAY)
-	e3:SetCountLimit(1,{id,1})
+	e3:SetCountLimit(1,{id,2})
 	e3:SetCondition(aux.bdocon)
 	e3:SetTarget(s.sptg)
 	e3:SetOperation(s.spop)
 	c:RegisterEffect(e3)
 end
+
 s.listed_series={0x3008}
 
--- Fusion material: 2 Elemental HERO Fusion Monsters (1 EARTH, 1 WIND)
-function s.matfilter(c,fc,sumtype,tp)
+-- Fusion filter: 2 Elemental HERO Fusion Monsters
+function s.fusionfilter(c,fc,sumtype,tp)
 	return c:IsFusionSetCard(0x3008) and c:IsType(TYPE_FUSION,fc,sumtype,tp)
-		and (c:IsAttribute(ATTRIBUTE_EARTH,fc,sumtype,tp) or c:IsAttribute(ATTRIBUTE_WIND,fc,sumtype,tp))
 end
 
--- Effect 1: Destroy Spell/Trap
-function s.desfilter(c)
-	return c:IsSpellTrap() and c:IsDestructable()
-end
+-- Effect 1: Targeted Spell/Trap destruction
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsOnField() and s.desfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.desfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+	if chkc then return chkc:IsOnField() and chkc:IsSpellTrap() end
+	if chk==0 then return Duel.IsExistingTarget(aux.FilterBoolFunction(Card.IsSpellTrap),tp,0,LOCATION_ONFIELD,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,s.desfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	local g=Duel.SelectTarget(tp,aux.FilterBoolFunction(Card.IsSpellTrap),tp,0,LOCATION_ONFIELD,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
@@ -76,15 +73,14 @@ function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- Effect 2: Tribute 1, search non-EARTH/WIND Elemental HERO
+-- Effect 2: Tribute to search
 function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.CheckReleaseGroup(tp,Card.IsReleasable,1,nil) end
 	local g=Duel.SelectReleaseGroup(tp,Card.IsReleasable,1,1,nil)
 	Duel.Release(g,REASON_COST)
 end
 function s.thfilter(c)
-	return c:IsSetCard(0x3008) and c:IsType(TYPE_MONSTER)
-		and not c:IsAttribute(ATTRIBUTE_EARTH+ATTRIBUTE_WIND) and c:IsAbleToHand()
+	return c:IsSetCard(0x3008) and not c:IsAttribute(ATTRIBUTE_EARTH+ATTRIBUTE_WIND) and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
@@ -100,25 +96,19 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 end
 
 -- Effect 3: Special Summon 1 from hand, 1 from GY
-function s.spfilter1(c,e,tp)
+function s.spfilter(c,e,tp)
 	return c:IsSetCard(0x3008) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then 
-		return Duel.GetLocationCount(tp,LOCATION_MZONE)>1
-			and Duel.IsExistingMatchingCard(s.spfilter1,tp,LOCATION_HAND,0,1,nil,e,tp)
-			and Duel.IsExistingMatchingCard(s.spfilter1,tp,LOCATION_GRAVE,0,1,nil,e,tp) 
-	end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp)
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,LOCATION_HAND+LOCATION_GRAVE)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<2 then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g1=Duel.SelectMatchingCard(tp,s.spfilter1,tp,LOCATION_HAND,0,1,1,nil,e,tp)
-	if #g1==0 then return end
-	local g2=Duel.SelectMatchingCard(tp,s.spfilter1,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
-	if #g2==0 then return end
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=1 then return end
+	local g1=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	local g2=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 	g1:Merge(g2)
-	Duel.SpecialSummon(g1,0,tp,tp,false,false,POS_FACEUP)
+	if #g1>0 then Duel.SpecialSummon(g1,0,tp,tp,false,false,POS_FACEUP) end
 end
-
