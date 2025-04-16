0b7797adaea8d90942b90435ecdc8beae9efaf82
Chips <aminekhoury0411@gmail.com>
2025-04-15 21:18:13 -0400

    Add files via upload

---
+++ script/c218.lua
@@ -0,0 +1,87 @@
+--Elemental HERO Lightning Tide
+local s,id=GetID()
+function s.initial_effect(c)
+    -- Must be Fusion Summoned
+    c:EnableReviveLimit()
+    Fusion.AddProcMix(c,true,true,79979666,20721928) -- Bubbleman + Sparkman
+
+    -- Discard to negate a card’s effects until end of turn
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_DISABLE)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCountLimit(1)
+    e1:SetCost(s.negcost)
+    e1:SetTarget(s.negtg)
+    e1:SetOperation(s.negop)
+    c:RegisterEffect(e1)
+
+    -- Bounce before damage calculation if conditions met
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_TOHAND)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+    e2:SetCondition(s.thcon)
+    e2:SetTarget(s.thtg)
+    e2:SetOperation(s.thop)
+    c:RegisterEffect(e2)
+end
+
+-- Cost: Discard 1 card
+function s.negcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
+    Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
+end
+
+-- Target any face-up card on the field
+function s.negfilter(c)
+    return c:IsFaceup() and not c:IsDisabled()
+end
+function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsLocation(LOCATION_ONFIELD) and s.negfilter(chkc) end
+    if chk==0 then return Duel.IsExistingTarget(s.negfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+    local g=Duel.SelectTarget(tp,s.negfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
+end
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc and tc:IsFaceup() and tc:IsRelateToEffect(e) then
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_DISABLE)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+        tc:RegisterEffect(e1)
+        local e2=Effect.CreateEffect(e:GetHandler())
+        e2:SetType(EFFECT_TYPE_SINGLE)
+        e2:SetCode(EFFECT_DISABLE_EFFECT)
+        e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+        tc:RegisterEffect(e2)
+    end
+end
+
+-- Return to hand before damage calc
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local bc=c:GetBattleTarget()
+    if not bc then return false end
+    local ne1=bc:IsType(TYPE_EFFECT) and bc:IsDisabled()
+    local ne2=not bc:IsType(TYPE_EFFECT)
+    return ne1 or ne2
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local c=e:GetHandler()
+    local bc=c:GetBattleTarget()
+    if chk==0 then return bc and bc:IsAbleToHand() end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,bc,1,0,0)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local bc=c:GetBattleTarget()
+    if bc and bc:IsRelateToBattle() then
+        Duel.SendtoHand(bc,nil,REASON_EFFECT)
+    end
+end
