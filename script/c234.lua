37f4ff13a2597491e3530cda606a28279b472832
Chips <aminekhoury0411@gmail.com>
2025-04-16 10:06:44 -0400

    Update c234.lua

--- script/c234.lua
+++ script/c234.lua
@@ -1,15 +1,14 @@
 --Evil HERO Skullwing Soldier
 local s,id=GetID()
 function s.initial_effect(c)
-	-- Set 1 Spell/Trap that mentions "Dark Fusion"
+	-- Tribute to Set Spell/Trap on Normal or Special Summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_LEAVE_GRAVE)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TODECK)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCountLimit(1,id)
-	e1:SetCost(s.setcost)
 	e1:SetTarget(s.settg)
 	e1:SetOperation(s.setop)
 	c:RegisterEffect(e1)
@@ -17,56 +16,72 @@ function s.initial_effect(c)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e2)
 
-	-- Banish recovery or Special Summon
+	-- Recover or Special Summon when banished from hand or GY
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
 	e3:SetCode(EVENT_REMOVE)
-	e3:SetProperty(EFFECT_FLAG_DELAY)
 	e3:SetCountLimit(1,{id,1})
-	e3:SetTarget(s.revtg)
-	e3:SetOperation(s.revop)
+	e3:SetCondition(s.thcon)
+	e3:SetTarget(s.thtg)
+	e3:SetOperation(s.thop)
 	c:RegisterEffect(e3)
 end
-s.listed_series={0x6008}
-s.listed_names={id,94820406} -- "Dark Fusion"
+s.listed_series={0x6008} -- Evil HERO
+s.listed_names={94820406} -- Dark Fusion
 
--- Tribute cost
-function s.setcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckReleaseGroup(tp,aux.FilterBoolFunction(Card.IsSetCard,0x8),1,nil) end
-	local g=Duel.SelectReleaseGroup(tp,aux.FilterBoolFunction(Card.IsSetCard,0x8),1,1,nil)
-	Duel.Release(g,REASON_COST)
+-- e1/e2 functions
+function s.cfilter(c)
+	return c:IsSetCard(0x8) and c:IsReleasable()
 end
--- Settable filter
 function s.setfilter(c)
 	return c:IsSpellTrap() and c:ListsCode(94820406) and c:IsSSetable()
 end
 function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_DECK,0,1,nil) end
+	if chk==0 then return Duel.CheckReleaseGroup(tp,s.cfilter,1,nil)
+		and Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,nil,1,tp,LOCATION_DECK)
 end
 function s.setop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,1,1,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	local g=Duel.SelectReleaseGroup(tp,s.cfilter,1,1,nil)
 	if #g>0 then
-		Duel.SSet(tp,g:GetFirst())
+		Duel.Release(g,REASON_COST)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+		local tc=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,1,1,nil):GetFirst()
+		if tc then
+			Duel.SSet(tp,tc)
+		end
 	end
 end
 
--- Recover or special summon banished HERO
-function s.revtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	return true
+-- e3 functions
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_HAND+LOCATION_GRAVE)
 end
-function s.revfilter(c)
-	return c:IsSetCard(0x8) and not c:IsCode(id) and (c:IsAbleToHand() or c:IsCanBeSpecialSummoned(nil,0,tp,false,false))
+function s.thfilter(c)
+	return c:IsSetCard(0x8) and not c:IsCode(id) and c:IsAbleToHand()
 end
-function s.revop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(s.revfilter,tp,LOCATION_REMOVED,0,nil)
-	if #g==0 then return end
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(0x8) and not c:IsCode(id)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and c:IsHasEffect(EFFECT_EXTRA_FUSION_MATERIAL)
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_REMOVED) and chkc:IsControler(tp) and s.thfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.thfilter,tp,LOCATION_REMOVED,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	local tc=g:Select(tp,1,1,nil):GetFirst()
-	if not tc then return end
-	if tc:IsCanBeSpecialSummoned(nil,0,tp,false,false) and tc:IsHasEffect(94820406)
-		and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+	local g=Duel.SelectTarget(tp,s.thfilter,tp,LOCATION_REMOVED,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if not tc or not tc:IsRelateToEffect(e) then return end
+	if tc:ListsCode(94820406) and tc:IsCanBeSpecialSummoned(e,0,tp,false,false) and Duel.SelectYesNo(tp,aux.Stringid(id,2))
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	else
 		Duel.SendtoHand(tc,nil,REASON_EFFECT)
