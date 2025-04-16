82cc84d7b034e664782671d064d80f65a7f9dbfb
Chips <aminekhoury0411@gmail.com>
2025-04-16 09:06:42 -0400

    Update c232.lua

--- script/c232.lua
+++ script/c232.lua
@@ -1,79 +1,75 @@
-
 --Evil HERO Cursed Shadow
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Token summon on Normal/Special Summon
-    local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
-    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-    e1:SetProperty(EFFECT_FLAG_DELAY)
-    e1:SetCountLimit(1,id)
-    e1:SetTarget(s.tktg)
-    e1:SetOperation(s.tkop)
-    c:RegisterEffect(e1)
-    local e2=e1:Clone()
-    e2:SetCode(EVENT_SUMMON_SUCCESS)
-    c:RegisterEffect(e2)
+	-- Special Summon Token on Normal or Special Summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.tktg)
+	e1:SetOperation(s.tkop)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
 
-    -- Place "Supreme King's Castle" if banished by effect of card that mentions "Dark Fusion"
-    local e3=Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(id,1))
-    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e3:SetProperty(EFFECT_FLAG_DELAY)
-    e3:SetCode(EVENT_REMOVE)
-    e3:SetCountLimit(1,{id,1})
-    e3:SetCondition(s.placecon)
-    e3:SetTarget(s.placetg)
-    e3:SetOperation(s.placeop)
-    c:RegisterEffect(e3)
+	-- Place Supreme King's Castle if banished by effect of a card that mentions Dark Fusion
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_REMOVE)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetCondition(s.plcon)
+	e3:SetTarget(s.pltg)
+	e3:SetOperation(s.plop)
+	c:RegisterEffect(e3)
 end
 
 s.listed_names={94820406,72043279} -- Dark Fusion, Supreme King's Castle
 s.listed_series={0x8} -- HERO
 
--- Token summon
+-- Token Summon
 function s.tktg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-        and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,0,0,6,RACE_FIEND,ATTRIBUTE_DARK) end
-    Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,tp,0)
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,0,0,6,RACE_FIEND,ATTRIBUTE_DARK) end
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,tp,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0)
 end
 function s.tkop(e,tp,eg,ep,ev,re,r,rp)
-    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-    if not Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,0,0,6,RACE_FIEND,ATTRIBUTE_DARK) then return end
-    local token=Duel.CreateToken(tp,id+1)
-    Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)
-
-    -- Lock Special Summons except "HERO"
-    local e1=Effect.CreateEffect(e:GetHandler())
-    e1:SetType(EFFECT_TYPE_FIELD)
-    e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-    e1:SetTargetRange(1,0)
-    e1:SetTarget(s.splimit)
-    e1:SetReset(RESET_PHASE+PHASE_END)
-    Duel.RegisterEffect(e1,tp)
-end
-function s.splimit(e,c,sump,sumtype,sumpos,targetp,se)
-    return not c:IsSetCard(0x8)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	if not Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,0,0,6,RACE_FIEND,ATTRIBUTE_DARK) then return end
+	local token=Duel.CreateToken(tp,id+1)
+	Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)
+	-- Lock non-HERO Special Summons
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(function(e,c) return not c:IsSetCard(0x8) end)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
 end
 
--- Check if banished by a card that mentions "Dark Fusion"
-function s.placecon(e,tp,eg,ep,ev,re,r,rp)
-    local rc=re:GetHandler()
-    return rc and rc:IsCode(94820406)
+-- Condition: Banished by effect of a card that mentions Dark Fusion
+function s.plcon(e,tp,eg,ep,ev,re,r,rp)
+	return re and re:GetHandler():IsCode(94820406)
 end
-function s.placetg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.fieldfilter,tp,LOCATION_DECK,0,1,nil) end
+function s.pltg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.plfilter,tp,LOCATION_DECK,0,1,nil) end
 end
-function s.placeop(e,tp,eg,ep,ev,re,r,rp)
-    local tc=Duel.GetFirstMatchingCard(s.fieldfilter,tp,LOCATION_DECK,0,nil)
-    if tc then
-        Duel.MoveToField(tc,tp,tp,LOCATION_FZONE,POS_FACEUP,true)
-    end
+function s.plfilter(c)
+	return c:IsCode(72043279) and c:GetType()==TYPE_FIELD and not c:IsForbidden()
 end
-function s.fieldfilter(c)
-    return c:IsCode(72043279) and not c:IsForbidden()
+function s.plop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
+	local g=Duel.SelectMatchingCard(tp,s.plfilter,tp,LOCATION_DECK,0,1,1,nil)
+	local tc=g:GetFirst()
+	if tc then
+		Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+	end
 end
