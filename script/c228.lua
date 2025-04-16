68eb4fe265ae0e8ea8715c4c2fee59719dd196d0
Chips <aminekhoury0411@gmail.com>
2025-04-15 23:42:44 -0400

    Add files via upload

---
+++ script/c228.lua
@@ -0,0 +1,73 @@
+--HERO Force!!
+local s,id=GetID()
+function s.initial_effect(c)
+    -- Activate: Add Neos/Neo Space/Neo-Spacian
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+    e1:SetTarget(s.target)
+    e1:SetOperation(s.activate)
+    c:RegisterEffect(e1)
+
+    -- GY Effect: Banish to search Elemental HERO with same Attribute
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetRange(LOCATION_GRAVE)
+    e2:SetCountLimit(1,{id,1})
+    e2:SetCost(aux.bfgcost)
+    e2:SetTarget(s.gytg)
+    e2:SetOperation(s.gyop)
+    c:RegisterEffect(e2)
+end
+
+-- Deck search filter (Neos, Neo Space, Neo-Spacian)
+function s.thfilter1(c)
+    return c:IsAbleToHand() and (
+        c:IsCode(89943723) -- Elemental HERO Neos
+        or c:IsCode(42015635) -- Neo Space
+        or (c:IsSetCard(0x1f) and c:IsMonster()) -- Neo-Spacian monsters
+    )
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter1,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.thfilter1,tp,LOCATION_DECK,0,1,1,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
+    end
+end
+
+-- GY Effect: Banish to search Elemental HERO with same Attribute
+function s.cfilter(c)
+    return c:IsFaceup() and c:IsSetCard(0x1f) -- Neo-Spacian
+end
+function s.thfilter2(c,att)
+    return c:IsSetCard(0x3008) and c:IsType(TYPE_EFFECT) and c:IsAttribute(att) and c:IsAbleToHand()
+end
+function s.gytg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.cfilter(chkc) end
+    if chk==0 then return Duel.IsExistingTarget(s.cfilter,tp,LOCATION_MZONE,0,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+    Duel.SelectTarget(tp,s.cfilter,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function s.gyop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+        local att=tc:GetAttribute()
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+        local g=Duel.SelectMatchingCard(tp,s.thfilter2,tp,LOCATION_DECK,0,1,1,nil,att)
+        if #g>0 then
+            Duel.SendtoHand(g,nil,REASON_EFFECT)
+            Duel.ConfirmCards(1-tp,g)
+        end
+    end
+end
