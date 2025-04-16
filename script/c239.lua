253a354d61d36be8d17cc022f423080f956bfa1a
Chips <aminekhoury0411@gmail.com>
2025-04-16 16:18:21 -0400

    Update c239.lua

--- script/c239.lua
+++ script/c239.lua
@@ -42,32 +42,44 @@ function s.initial_effect(c)
 	e3:SetOperation(s.protop)
 	c:RegisterEffect(e3)
 
-	-- Negate spell/trap by banishing + revive a Sunrise/Nightfall
+	-- Negate Spell
 	local e4=Effect.CreateEffect(c)
 	e4:SetDescription(aux.Stringid(id,0))
-	e4:SetCategory(CATEGORY_NEGATE+CATEGORY_SPECIAL_SUMMON)
+	e4:SetCategory(CATEGORY_NEGATE)
 	e4:SetType(EFFECT_TYPE_QUICK_O)
 	e4:SetCode(EVENT_CHAINING)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetCountLimit(1,id)
-	e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL+EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
-	e4:SetCondition(s.negcon)
+	e4:SetCondition(s.negcon_spell)
 	e4:SetCost(s.negcost)
 	e4:SetTarget(s.negtg)
 	e4:SetOperation(s.negop)
 	c:RegisterEffect(e4)
 
-	-- Revive from grave
+	-- Negate Attack
 	local e5=Effect.CreateEffect(c)
-	e5:SetDescription(aux.Stringid(id,3))
-	e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e5:SetType(EFFECT_TYPE_IGNITION)
-	e5:SetRange(LOCATION_GRAVE)
+	e5:SetDescription(aux.Stringid(id,4))
+	e5:SetCategory(CATEGORY_DISABLE)
+	e5:SetType(EFFECT_TYPE_QUICK_O)
+	e5:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e5:SetRange(LOCATION_MZONE)
 	e5:SetCountLimit(1,{id,1})
-	e5:SetCost(s.revcost)
-	e5:SetTarget(s.revtg)
-	e5:SetOperation(s.revop)
+	e5:SetCondition(s.atkcon)
+	e5:SetCost(s.negcost)
+	e5:SetOperation(s.atkop)
 	c:RegisterEffect(e5)
+
+	-- Revive from grave
+	local e6=Effect.CreateEffect(c)
+	e6:SetDescription(aux.Stringid(id,3))
+	e6:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e6:SetType(EFFECT_TYPE_IGNITION)
+	e6:SetRange(LOCATION_GRAVE)
+	e6:SetCountLimit(1,{id,2})
+	e6:SetCost(s.revcost)
+	e6:SetTarget(s.revtg)
+	e6:SetOperation(s.revop)
+	c:RegisterEffect(e6)
 end
 
 s.listed_names={238,94820406,22908820}
@@ -108,9 +120,9 @@ function s.protcon(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.prottg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and chkc:IsSetCard(0x8) end
-	if chk==0 then return Duel.IsExistingTarget(Card.IsSetCard(0x8),tp,LOCATION_MZONE,0,1,nil,0x8) end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsSetCard,tp,LOCATION_MZONE,0,1,nil,0x8) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local g=Duel.SelectTarget(tp,Card.IsSetCard(0x8),tp,LOCATION_MZONE,0,1,1,nil,0x8)
+	local g=Duel.SelectTarget(tp,Card.IsSetCard,tp,LOCATION_MZONE,0,1,1,nil,0x8)
 	Duel.SetTargetCard(g)
 end
 function s.protop(e,tp,eg,ep,ev,re,r,rp)
@@ -126,39 +138,41 @@ function s.protop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
-function s.negcon(e,tp,eg,ep,ev,re,r,rp)
-	return re:IsActiveType(TYPE_SPELL+TYPE_TRAP) and Duel.IsChainNegatable(ev)
+function s.negcon_spell(e,tp,eg,ep,ev,re,r,rp)
+	return re:IsActiveType(TYPE_SPELL) and Duel.IsChainNegatable(ev)
+end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetAttacker() and Duel.GetAttacker():IsControler(1-tp)
 end
 function s.negcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk==0 then return c:IsAbleToRemoveAsCost() end
+	if chk==0 then return c:IsAbleToRemoveAsCost() and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.Remove(c,POS_FACEUP,REASON_COST+REASON_TEMPORARY)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		Duel.Remove(g,POS_FACEUP,REASON_COST)
+	end
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_PHASE+PHASE_END)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	e1:SetLabelObject(c)
-	e1:SetOperation(function(_,tp)
-		Duel.ReturnToField(c)
-	end)
+	e1:SetOperation(function(_,tp) Duel.ReturnToField(c) end)
 	Duel.RegisterEffect(e1,tp)
 end
-function s.spfilter(c,e,tp)
-	return c:IsCode(22908820,238) and c:IsCanBeSpecialSummoned(e,0,tp,true,true)
+function s.cfilter(c)
+	return c:IsSetCard(0x8) and c:IsType(TYPE_FUSION) and c:IsAbleToRemoveAsCost()
 end
 function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp) end
+	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_REMOVED)
 end
 function s.negop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.NegateActivation(ev) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_REMOVED,0,1,1,nil,e,tp)
-		if #g>0 then
-			Duel.SpecialSummon(g,0,tp,tp,true,true,POS_FACEUP)
-		end
-	end
+	Duel.NegateActivation(ev)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.NegateAttack()
 end
 
 -- Graveyard revival effect
