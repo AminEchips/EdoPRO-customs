61398e892b1176ebe5ea4ca782e40d6b1e1ff8c1
Chips <aminekhoury0411@gmail.com>
2025-04-16 00:13:14 -0400

    Update c230.lua

--- script/c230.lua
+++ script/c230.lua
@@ -3,7 +3,6 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Fusion Material
 	c:EnableReviveLimit()
-	Fusion.AddProcMixN(c,true,true,s.matfilter,3,3) -- dynamically validated below
 
 	--Custom Contact Fusion
 	local e0=Effect.CreateEffect(c)
@@ -27,7 +26,7 @@ function s.initial_effect(c)
 	e1:SetOperation(s.desop)
 	c:RegisterEffect(e1)
 
-	--Return to Extra Deck replacement
+	--Return to Extra Deck when leaving field
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EVENT_LEAVE_FIELD)
@@ -35,55 +34,51 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 
--- Fusion Material Filter
-function s.matfilter(c,fc,sub,mg,sg)
-	if not c:IsMonster() then return false end
-	if c:IsCode(89943723) then return true end -- Elemental HERO Neos
-	if c:IsType(TYPE_FUSION) and c:ListsCode(89943723) then return true end
-	if c:IsSetCard(0x1f) then return true end -- Neo-Spacian
-	return false
+-- Filter
+function s.sprfilter(c)
+	return c:IsAbleToDeckOrExtraAsCost() and (c:IsCode(89943723) or (c:IsType(TYPE_FUSION) and c:ListsCode(89943723)) or c:IsSetCard(0x1f))
 end
 
--- Special Summon condition (Contact Fusion)
-function s.sprfilter(c)
-	return c:IsAbleToDeckOrExtraAsCost() and (c:IsCode(89943723) or c:IsType(TYPE_FUSION) and c:ListsCode(89943723) or c:IsSetCard(0x1f))
-end
-function s.groupcheck(g)
-	local count_neos=0
-	local count_neospacian=0
+function s.validmatgroup(g)
+	local neos=0
+	local spacians=0
 	for tc in aux.Next(g) do
 		if tc:IsCode(89943723) or (tc:IsType(TYPE_FUSION) and tc:ListsCode(89943723)) then
-			count_neos=count_neos+1
+			neos=neos+1
 		elseif tc:IsSetCard(0x1f) then
-			count_neospacian=count_neospacian+1
+			spacians=spacians+1
 		end
 	end
-	return count_neos==1 and (count_neospacian==2 or count_neospacian==4)
+	return neos==1 and (spacians==2 or spacians==4)
 end
+
 function s.sprcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
 	local g=Duel.GetMatchingGroup(s.sprfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,nil)
-	return g:CheckSubGroup(s.groupcheck,3,5)
+	return g:GetCount()>=3 and g:CheckWithSumEqual(Card.GetLevel,8,3,5) and g:IsExists(aux.FilterBoolFunction(Card.IsAbleToDeckOrExtraAsCost),3,nil)
 end
+
 function s.sprop(e,tp,eg,ep,ev,re,r,rp,c)
 	local g=Duel.GetMatchingGroup(s.sprfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,nil)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local sg=g:SelectSubGroup(tp,s.groupcheck,false,3,5)
-	if not sg then return end
+	local sg=g:Select(tp,3,5,nil)
+	while not s.validmatgroup(sg) do
+		sg=g:Select(tp,3,5,nil)
+	end
 	c:SetMaterial(sg)
 	Duel.SendtoDeck(sg,nil,SEQ_DECKSHUFFLE,REASON_COST)
+	c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,EFFECT_FLAG_CLIENT_HINT,1,0,sg:GetCount()) -- Store material count
 end
 
--- Destroy 1 card
+-- Destroy effect if summoned with 4 Neo-Spacians
 function s.descon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return c:GetMaterialCount()==5 -- special summoned with 4 Neo-Spacian monsters
+	return c:GetFlagEffectLabel(id)==5
 end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
-	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,0,0)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
@@ -93,10 +88,9 @@ function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- Return to Extra Deck when leaving field
 function s.retop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsLocation(LOCATION_GRAVE) and c:IsReason(REASON_EFFECT+REASON_DESTROY) then
+	if c:IsLocation(LOCATION_GRAVE) then
 		Duel.SendtoDeck(c,nil,SEQ_DECKTOP,REASON_EFFECT)
 	end
 end
