68981651a753bff82bc1c8f980f774d8ae2ee05a
Chips <aminekhoury0411@gmail.com>
2025-04-17 11:35:25 -0400

    Update c232.lua

--- script/c232.lua
+++ script/c232.lua
@@ -16,25 +16,28 @@ function s.initial_effect(c)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e2)
 
-	-- Set "Supreme King's Castle" when banished by "Dark Fusion"
+	-- Add 1 "Evil HERO" (except self) if banished by effect of a card mentioning "Dark Fusion"
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetCode(EVENT_REMOVE)
 	e3:SetProperty(EFFECT_FLAG_DELAY)
 	e3:SetCountLimit(1,{id,1})
-	e3:SetCondition(s.setcon)
-	e3:SetTarget(s.settg)
-	e3:SetOperation(s.setop)
+	e3:SetCondition(s.thcon)
+	e3:SetTarget(s.thtg)
+	e3:SetOperation(s.thop)
 	c:RegisterEffect(e3)
 end
-s.listed_names={94820406,72043279} -- Dark Fusion, Supreme King's Castle
+
+s.listed_names={94820406} -- Dark Fusion
 
 -- Token summon
 function s.toktg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-			and Duel.IsPlayerCanSpecialSummonMonster(tp,253,0x6008,TYPES_TOKEN,0,0,6,RACE_FIEND,ATTRIBUTE_DARK) end
+			and Duel.IsPlayerCanSpecialSummonMonster(tp,253,0x6008,TYPES_TOKEN,0,0,6,RACE_FIEND,ATTRIBUTE_DARK)
+	end
 	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,tp,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0)
 end
@@ -57,21 +60,24 @@ function s.splimit(e,c)
 	return not c:IsSetCard(0x8) -- Not a "HERO"
 end
 
--- Condition to trigger when banished by a "Dark Fusion" effect
-function s.setcon(e,tp,eg,ep,ev,re,r,rp)
-	return re and re:GetHandler():IsCode(94820406)
+-- Condition: banished by effect of card that mentions "Dark Fusion"
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	local rc=re and re:GetHandler()
+	return rc and rc:IsType(TYPE_SPELL+TYPE_TRAP) and rc:ListsCode(94820406)
 end
-function s.setfilter(c)
-	return c:IsCode(72043279) and c:IsSSetable()
+
+function s.thfilter(c)
+	return c:IsSetCard(0x6008) and not c:IsCode(id) and c:IsAbleToHand()
 end
-function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_DECK,0,1,nil) end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function s.setop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
-	local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,1,1,nil)
-	local tc=g:GetFirst()
-	if tc then
-		Duel.SSet(tp,tc)
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
 	end
 end
