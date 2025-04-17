a6d05cdfc39ec213bdf81cdbd39ac18243630037
Chips <aminekhoury0411@gmail.com>
2025-04-16 21:37:48 -0400

    Update c249.lua

--- script/c249.lua
+++ script/c249.lua
@@ -1,67 +1,82 @@
 --I - Infernal Blast
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Activate and equip to 1 monster Special Summoned to your opponent's field
+    --Activate and equip to opponent's monster when exactly 1 is Special Summoned
     local e1=Effect.CreateEffect(c)
     e1:SetCategory(CATEGORY_EQUIP)
     e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
     e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-    e1:SetRange(LOCATION_HAND+LOCATION_GRAVE)
+    e1:SetRange(LOCATION_SZONE)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
     e1:SetCondition(s.eqcon)
     e1:SetTarget(s.eqtg)
     e1:SetOperation(s.eqop)
     c:RegisterEffect(e1)
 
-    -- Burn and destroy during Standby Phase
+    --Burn effect during each Standby Phase
     local e2=Effect.CreateEffect(c)
-    e2:SetCategory(CATEGORY_DAMAGE+CATEGORY_DESTROY)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_DAMAGE)
     e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
     e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
     e2:SetRange(LOCATION_SZONE)
     e2:SetCountLimit(1)
-    e2:SetCondition(s.burncon)
-    e2:SetTarget(s.burntg)
-    e2:SetOperation(s.burnop)
+    e2:SetCondition(s.damcon)
+    e2:SetTarget(s.damtg)
+    e2:SetOperation(s.damop)
     c:RegisterEffect(e2)
 
-    -- Indestructible by card effects if "Dark Fusion" is in your GY
+    --Indestructible by effects if Dark Fusion is in your GY
     local e3=Effect.CreateEffect(c)
     e3:SetType(EFFECT_TYPE_SINGLE)
     e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e3:SetRange(LOCATION_SZONE)
     e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+    e3:SetRange(LOCATION_SZONE)
     e3:SetCondition(s.indcon)
     e3:SetValue(1)
     c:RegisterEffect(e3)
 end
 
-s.listed_names={94820406} -- Dark Fusion
+s.listed_names={94820406}
 
+--Condition: exactly 1 monster was Special Summoned to opponent's field
 function s.eqcon(e,tp,eg,ep,ev,re,r,rp)
-    return eg:FilterCount(Card.IsControler,nil,1-tp)==1 and eg:GetCount()==1
+    return #eg==1 and eg:GetFirst():IsControler(1-tp) and eg:GetFirst():IsFaceup()
 end
-function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and eg:GetFirst():IsFaceup() end
-    Duel.SetTargetCard(eg:GetFirst())
+function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    local tc=eg:GetFirst()
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and tc:IsControler(1-tp) and tc:IsFaceup() end
+    Duel.SetTargetCard(tc)
+    Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
 end
 function s.eqop(e,tp,eg,ep,ev,re,r,rp)
-    local tc=eg:GetFirst()
     local c=e:GetHandler()
-    if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) and tc:IsFaceup() then
-        Duel.Equip(tp,c,tc)
-    end
+    local tc=Duel.GetFirstTarget()
+    if not c:IsRelateToEffect(e) or not tc or not tc:IsRelateToEffect(e) or not tc:IsFaceup() then return end
+    Duel.Equip(tp,c,tc)
+    --Equip limit
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_EQUIP_LIMIT)
+    e1:SetProperty(EFFECT_FLAG_COPY_INHERIT+EFFECT_FLAG_OWNER_RELATE)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+    e1:SetValue(function(e,c) return c==tc end)
+    c:RegisterEffect(e1)
 end
 
-function s.burncon(e,tp,eg,ep,ev,re,r,rp)
-    return Duel.GetTurnPlayer()~=tp
+--Burn effect during Standby Phase if equipped
+function s.damcon(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    return Duel.GetTurnPlayer()==tp and c:GetEquipTarget()
 end
-function s.burntg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return true end
     Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,1000)
 end
-function s.burnop(e,tp,eg,ep,ev,re,r,rp)
-    local tc=e:GetHandler():GetEquipTarget()
-    if tc then
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local eqtc=c:GetEquipTarget()
+    if eqtc and eqtc:IsOnField() then
         Duel.Damage(1-tp,1000,REASON_EFFECT)
         local g=Duel.SelectMatchingCard(tp,nil,1-tp,LOCATION_ONFIELD,0,1,1,nil)
         if #g>0 then
@@ -70,6 +85,7 @@ function s.burnop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
+--Condition for indestructibility if Dark Fusion is in your GY
 function s.indcon(e)
     return Duel.IsExistingMatchingCard(Card.IsCode,e:GetHandlerPlayer(),LOCATION_GRAVE,0,1,nil,94820406)
 end
