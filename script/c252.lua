90b34252cd19ab68b5bdc4ac90d0a353592a4696
Chips <aminekhoury0411@gmail.com>
2025-04-16 22:22:20 -0400

    Add files via upload

--- script/c252.lua
+++ script/c252.lua
@@ -1,77 +1,75 @@
 --Evil Backlash
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Banish 1 Fusion, then revive materials
+    -- Effect: Banish Fusion, summon its materials
     local e1=Effect.CreateEffect(c)
     e1:SetCategory(CATEGORY_REMOVE+CATEGORY_SPECIAL_SUMMON)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_FREE_CHAIN)
-    e1:SetTarget(s.target)
-    e1:SetOperation(s.activate)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+    e1:SetTarget(s.rmtg)
+    e1:SetOperation(s.rmop)
     c:RegisterEffect(e1)
-    -- GY ATK boost effect
+
+    -- GY effect: Give ATK boost during damage calculation
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
-    e2:SetCategory(CATEGORY_ATKCHANGE)
     e2:SetType(EFFECT_TYPE_QUICK_O)
-    e2:SetRange(LOCATION_GRAVE)
     e2:SetCode(EVENT_FREE_CHAIN)
-    e2:SetCountLimit(1,id)
+    e2:SetRange(LOCATION_GRAVE)
+    e2:SetCountLimit(1,id+100,EFFECT_COUNT_CODE_OATH)
     e2:SetCost(aux.bfgcost)
     e2:SetTarget(s.atktg)
     e2:SetOperation(s.atkop)
     c:RegisterEffect(e2)
 end
 
-s.listed_names={94820406}
-s.listed_series={0x6008}
-
-function s.matfilter(c,e,tp)
-    return c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-
-function s.fusfilter(c,e,tp)
-    return c:IsType(TYPE_FUSION) and c:IsSummonType(SUMMON_TYPE_FUSION)
+function s.fusionfilter(c)
+    return c:IsFaceup() and c:IsType(TYPE_FUSION) and c:IsSummonType(SUMMON_TYPE_FUSION) 
         and c:GetMaterialCount()>0 and c:IsAbleToRemove()
-        and c:IsSummonLocation(LOCATION_EXTRA)
-        and c:GetReasonEffect() and c:GetReasonEffect():GetHandler():IsCode(94820406)
-        and c:GetMaterial():FilterCount(aux.NecroValleyFilter(s.matfilter),nil,e,tp)==c:GetMaterialCount()
+        and c:GetSummonEffect() and c:GetSummonEffect():GetHandler():IsCode(94820406)
 end
 
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.fusfilter,tp,LOCATION_MZONE,0,1,nil,e,tp) end
+function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsControler(tp) and s.fusionfilter(chkc) end
+    if chk==0 then return Duel.IsExistingTarget(s.fusionfilter,tp,LOCATION_MZONE,0,1,nil) end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-    local g=Duel.SelectMatchingCard(tp,s.fusfilter,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
+    local g=Duel.SelectTarget(tp,s.fusionfilter,tp,LOCATION_MZONE,0,1,1,nil)
     Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,g:GetFirst():GetMaterialCount(),tp,LOCATION_GRAVE)
-    e:SetLabelObject(g:GetFirst())
 end
 
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-    local fc=e:GetLabelObject()
-    if fc and fc:IsRelateToEffect(e) and Duel.Remove(fc,POS_FACEUP,REASON_EFFECT)>0 then
-        local mg=fc:GetMaterial():Filter(aux.NecroValleyFilter(s.matfilter),nil,e,tp)
-        if #mg>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>=#mg then
-            Duel.BreakEffect()
-            Duel.SpecialSummon(mg,0,tp,tp,false,false,POS_FACEUP)
-        end
+function s.rmop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if not tc:IsRelateToEffect(e) or Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)==0 then return end
+    local mg=tc:GetMaterial()
+    if #mg>0 and mg:FilterCount(Card.IsLocation,nil,LOCATION_GRAVE)==#mg 
+        and Duel.GetLocationCount(tp,LOCATION_MZONE)>=#mg then
+        Duel.BreakEffect()
+        Duel.SpecialSummon(mg,0,tp,tp,false,false,POS_FACEUP)
     end
 end
 
+function s.atkfilter(c)
+    return c:IsFaceup() and c:IsSetCard(0x6008)
+end
+
 function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and chkc:IsSetCard(0x6008) end
-    if chk==0 then return Duel.IsExistingTarget(aux.FaceupFilter(Card.IsSetCard,0x6008),tp,LOCATION_MZONE,0,1,nil) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-    Duel.SelectTarget(tp,aux.FaceupFilter(Card.IsSetCard,0x6008),tp,LOCATION_MZONE,0,1,1,nil)
+    if chkc then return chkc:IsControler(tp) and s.atkfilter(chkc) end
+    if chk==0 then return Duel.IsExistingTarget(s.atkfilter,tp,LOCATION_MZONE,0,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+    Duel.SelectTarget(tp,s.atkfilter,tp,LOCATION_MZONE,0,1,1,nil)
 end
 
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
     local tc=Duel.GetFirstTarget()
-    if not tc:IsRelateToEffect(e) or not tc:IsFaceup() then return end
+    if not tc or not tc:IsRelateToEffect(e) then return end
+
     local e1=Effect.CreateEffect(e:GetHandler())
     e1:SetType(EFFECT_TYPE_SINGLE)
     e1:SetCode(EFFECT_UPDATE_ATTACK)
     e1:SetValue(2100)
-    e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE_CAL)
+    e1:SetCondition(function(e) return Duel.GetCurrentPhase()==PHASE_DAMAGE_CAL and tc==Duel.GetAttacker() end)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE)
     tc:RegisterEffect(e1)
 end
