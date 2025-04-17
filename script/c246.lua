d2f6b3ee3d9b2be6d4d77b3a8f0cd858493eff4e
Chips <aminekhoury0411@gmail.com>
2025-04-16 20:18:26 -0400

    Update c246.lua

--- script/c246.lua
+++ script/c246.lua
@@ -1,99 +1,72 @@
 --Sinister Aura
 local s,id=GetID()
 function s.initial_effect(c)
-    --Return 1 Fusion Monster Special Summoned with "Dark Fusion", then Special Summon 1 banished "Evil HERO"
-    local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e1:SetType(EFFECT_TYPE_ACTIVATE)
-    e1:SetCode(EVENT_FREE_CHAIN)
-    e1:SetCountLimit(1,id)
-    e1:SetTarget(s.target)
-    e1:SetOperation(s.activate)
-    c:RegisterEffect(e1)
-
-    --GY effect: Add up to 1 "Elemental HERO Neos" and 1 "Evil HERO Malicious Edge" to hand
-    local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,1))
-    e2:SetCategory(CATEGORY_TOHAND)
-    e2:SetType(EFFECT_TYPE_IGNITION)
-    e2:SetRange(LOCATION_GRAVE)
-    e2:SetCountLimit(1,{id,1})
-    e2:SetCondition(s.thcon)
-    e2:SetCost(s.thcost)
-    e2:SetTarget(s.thtg)
-    e2:SetOperation(s.thop)
-    c:RegisterEffect(e2)
+	--Return Fusion and Special Summon banished Evil HERO
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+	--Add monsters from GY to hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(s.thcon)
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
 end
 
-s.listed_names={94820406,89943723,58554959}
 s.listed_series={0x6008}
+s.listed_names={94820406,21844576,13293158,58554959} -- Dark Fusion, Elemental HERO Neos, Evil HERO Malicious Edge
 
 function s.fusfilter(c)
-    return c:IsType(TYPE_FUSION) and c:IsSummonType(SUMMON_TYPE_FUSION)
-        and c:IsSummonLocation(LOCATION_EXTRA)
-        and c:IsCode(94820406) and c:IsAbleToExtra()
+	return c:IsType(TYPE_FUSION) and c:IsSetCard(0x6008) and c:IsAbleToExtraAsCost()
 end
-
 function s.spfilter(c,e,tp)
-    return c:IsSetCard(0x6008) and c:IsFaceup() and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsSetCard(0x6008) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
 end
-
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then
-        return Duel.IsExistingMatchingCard(s.fusfilter,tp,LOCATION_MZONE,0,1,nil)
-            and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp)
-    end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_REMOVED)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.fusfilter,tp,LOCATION_MZONE,0,1,nil)
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_REMOVED)
 end
-
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-    local tc=Duel.SelectMatchingCard(tp,s.fusfilter,tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
-    if tc and Duel.SendtoDeck(tc,nil,SEQ_DECKTOP,REASON_EFFECT)>0 then
-        Duel.BreakEffect()
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-        local sc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_REMOVED,0,1,1,nil,e,tp):GetFirst()
-        if sc then
-            Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)
-        end
-    end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local rg=Duel.SelectMatchingCard(tp,s.fusfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	if #rg==0 or Duel.SendtoDeck(rg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_REMOVED,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
 end
 
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-    return not e:GetHandler():IsTurnID()
+	return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) and Duel.GetTurnCount()~=e:GetHandler():GetTurnID()
 end
-
-function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
-    Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
-end
-
 function s.thfilter(c)
-    return (c:IsCode(89943723) or c:IsCode(58554959)) and c:IsAbleToHand()
+	return (c:IsCode(21844576) or c:IsCode(13293158) or c:IsCode(58554959)) and c:IsAbleToHand()
 end
-
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_GRAVE,0,nil)
+	if #g==0 then return end
+	local sg=aux.SelectUnselectGroup(g,e,tp,1,2,aux.dncheck,1,tp,HINTMSG_ATOHAND)
+	if #sg>0 then
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg)
+	end
 end
 
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
-    local g=Group.CreateGroup()
-    local g1=Duel.GetMatchingGroup(Card.IsCode,tp,LOCATION_GRAVE,0,nil,89943723)
-    local g2=Duel.GetMatchingGroup(Card.IsCode,tp,LOCATION_GRAVE,0,nil,58554959)
-    if #g1>0 then
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-        local sg=g1:Select(tp,1,1,nil)
-        g:Merge(sg)
-    end
-    if #g2>0 then
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-        local sg=g2:Select(tp,1,1,nil)
-        g:Merge(sg)
-    end
-    if #g>0 then
-        Duel.SendtoHand(g,nil,REASON_EFFECT)
-        Duel.ConfirmCards(1-tp,g)
-    end
-end
