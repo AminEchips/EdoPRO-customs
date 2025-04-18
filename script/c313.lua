eaddb5490c99fc759ba62fb20e1883879d78d5ac
Chips <aminekhoury0411@gmail.com>
2025-04-18 17:48:04 -0400

    Add files via upload

--- script/c313.lua
+++ script/c313.lua
@@ -5,7 +5,7 @@ function s.initial_effect(c)
     Xyz.AddProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0xef),8,2)
     c:EnableReviveLimit()
 
-    -- Detach to send 1 opponent's monster to the GY
+    -- Detach and target to send opponent's monster to GY (targeting is part of cost)
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_TOGRAVE)
@@ -13,11 +13,10 @@ function s.initial_effect(c)
     e1:SetRange(LOCATION_MZONE)
     e1:SetCountLimit(1)
     e1:SetCost(s.tgcost)
-    e1:SetTarget(s.tgtg)
     e1:SetOperation(s.tgop)
     c:RegisterEffect(e1)
 
-    -- If LP paid for Darklord effect: Fusion Summon from hand/field using GY target
+    -- If LP paid for Darklord effect: target, then Fusion Summon using hand/field (targeting in cost)
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
     e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
@@ -26,11 +25,11 @@ function s.initial_effect(c)
     e2:SetRange(LOCATION_MZONE)
     e2:SetCountLimit(1,id)
     e2:SetCondition(s.fuscon)
-    e2:SetTarget(s.fustg)
+    e2:SetCost(s.fuscost)
     e2:SetOperation(s.fusop)
     c:RegisterEffect(e2)
 
-    -- If destroyed and sent to GY: return to Extra Deck
+    -- If destroyed or tributed: return to Extra Deck
     local e3=Effect.CreateEffect(c)
     e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
     e3:SetCode(EVENT_TO_GRAVE)
@@ -40,24 +39,23 @@ function s.initial_effect(c)
 end
 s.listed_series={0xef}
 
--- Effect 1: detach to send opponent's monster to GY
+-- Effect 1: Detach and target to send to GY
 function s.tgcost(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+    if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST)
+        and Duel.IsExistingMatchingCard(Card.IsMonster,tp,0,LOCATION_MZONE,1,nil) end
     e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
-end
-function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsMonster,tp,0,LOCATION_MZONE,1,nil) end
-    Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,1-tp,LOCATION_MZONE)
-end
-function s.tgop(e,tp,eg,ep,ev,re,r,rp)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
     local g=Duel.SelectMatchingCard(tp,Card.IsMonster,tp,0,LOCATION_MZONE,1,1,nil)
-    if #g>0 then
-        Duel.SendtoGrave(g,REASON_EFFECT)
+    e:SetLabelObject(g:GetFirst())
+end
+function s.tgop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=e:GetLabelObject()
+    if tc and tc:IsRelateToEffect(e) then
+        Duel.SendtoGrave(tc,REASON_EFFECT)
     end
 end
 
--- Effect 2: if LP paid for Darklord monster effect
+-- Effect 2: LP paid, retrieve Darklord + Fusion Summon
 function s.fuscon(e,tp,eg,ep,ev,re,r,rp)
     local rc=re:GetHandler()
     return rc and rc:IsSetCard(0xef) and rc:IsMonster()
@@ -66,33 +64,34 @@ end
 function s.fusfilter(c)
     return c:IsSetCard(0xef) and c:IsAbleToHand()
 end
-function s.fustg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.fusfilter,tp,LOCATION_GRAVE,0,1,nil)
-        and Duel.IsExistingMatchingCard(aux.FilterBoolFunction(Card.IsType,TYPE_MONSTER),tp,LOCATION_HAND+LOCATION_MZONE,0,2,nil) end
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
-end
-function s.fusop(e,tp,eg,ep,ev,re,r,rp)
+function s.fuscost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then
+        return Duel.IsExistingMatchingCard(s.fusfilter,tp,LOCATION_GRAVE,0,1,nil)
+    end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
     local g=Duel.SelectMatchingCard(tp,s.fusfilter,tp,LOCATION_GRAVE,0,1,1,nil)
-    if #g>0 and Duel.SendtoHand(g,nil,REASON_EFFECT)>0 then
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_COST)
         Duel.ConfirmCards(1-tp,g)
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-        local fusion=Duel.SelectMatchingCard(tp,Card.IsType,tp,LOCATION_EXTRA,0,1,1,nil,TYPE_FUSION):GetFirst()
-        if fusion then
-            local mat=Duel.SelectMatchingCard(tp,aux.FilterBoolFunction(Card.IsType,TYPE_MONSTER),tp,LOCATION_HAND+LOCATION_MZONE,0,2,2,nil)
-            if #mat==2 then
-                Duel.SendtoGrave(mat,REASON_MATERIAL+REASON_FUSION)
-                Duel.SpecialSummon(fusion,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
-                fusion:CompleteProcedure()
-            end
+        e:SetLabelObject(g:GetFirst())
+    end
+end
+function s.fusop(e,tp,eg,ep,ev,re,r,rp)
+    local fusion=Duel.SelectMatchingCard(tp,Card.IsType,tp,LOCATION_EXTRA,0,1,1,nil,TYPE_FUSION):GetFirst()
+    if fusion then
+        local mat=Duel.SelectMatchingCard(tp,aux.FilterBoolFunction(Card.IsType,TYPE_MONSTER),tp,LOCATION_HAND+LOCATION_MZONE,0,2,2,nil)
+        if #mat==2 then
+            Duel.SendtoGrave(mat,REASON_MATERIAL+REASON_FUSION)
+            Duel.SpecialSummon(fusion,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
+            fusion:CompleteProcedure()
         end
     end
 end
 
--- Effect 3: return to Extra Deck if destroyed
+-- Effect 3: return to Extra Deck if destroyed or tributed
 function s.recon(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
-    return c:IsReason(REASON_BATTLE+REASON_EFFECT) and c:IsPreviousLocation(LOCATION_ONFIELD)
+    return c:IsReason(REASON_BATTLE+REASON_EFFECT+REASON_RELEASE) and c:IsPreviousLocation(LOCATION_ONFIELD)
 end
 function s.retop(e,tp,eg,ep,ev,re,r,rp)
     Duel.SendtoDeck(e:GetHandler(),nil,SEQ_DECKTOP,REASON_EFFECT)
