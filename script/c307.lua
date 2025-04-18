caf34588f3d7e5aa402ecdc6bb1e1c11ae182464
Chips <aminekhoury0411@gmail.com>
2025-04-18 16:01:58 -0400

    Add files via upload

--- script/c307.lua
+++ script/c307.lua
@@ -4,7 +4,7 @@ function s.initial_effect(c)
     c:EnableReviveLimit()
     Fusion.AddProcFunRep(c,s.ffilter,2,true)
 
-    -- Gains 1000 ATK and 2 attacks if you paid LP this turn
+    -- Gains 1000 ATK and 2 attacks if LP was paid this turn
     local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_SINGLE)
     e1:SetCode(EFFECT_UPDATE_ATTACK)
@@ -23,7 +23,7 @@ function s.initial_effect(c)
     e2:SetValue(1)
     c:RegisterEffect(e2)
 
-    -- Replace shuffling of exactly 1 Darklord S/T from GY into Deck with adding it to hand
+    -- Replace shuffle of exactly 1 Darklord S/T from GY into Deck with adding to hand
     local e3=Effect.CreateEffect(c)
     e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
     e3:SetCode(EVENT_TO_DECK)
@@ -32,7 +32,7 @@ function s.initial_effect(c)
     e3:SetOperation(s.repop)
     c:RegisterEffect(e3)
 
-    -- GY effect: Send 2 Darklords from hand/field to revive this, banish on leave
+    -- GY revive: Send 2 "Darklord" cards to SS this, banish on leave
     local e4=Effect.CreateEffect(c)
     e4:SetDescription(aux.Stringid(id,0))
     e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -47,42 +47,36 @@ function s.initial_effect(c)
 end
 s.listed_series={0xef}
 
--- Fusion materials: 2 Level 5+ "Darklord" monsters
+-- Fusion materials: 2 Level 5+ Darklords
 function s.ffilter(c,fc,sub,mg,sg,chkfn)
     return c:IsSetCard(0xef) and c:IsLevelAbove(5)
 end
 
--- Check if player paid LP this turn
+-- ATK/Attack boost condition
 function s.atkcon(e)
     return Duel.GetFlagEffect(e:GetHandlerPlayer(),id)>0
 end
 
--- Track LP payment this turn
-local echeck=Effect.CreateEffect(s)
-echeck:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-echeck:SetCode(EVENT_PAY_LPCOST)
-echeck:SetOperation(function(_,tp,eg,ep,ev,re,r,rp) Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1) end)
-Duel.RegisterEffect(echeck,0)
-
--- Replacement effect: Add S/T instead of shuffle if exactly 1
+-- Shuffle replacement condition
 function s.repcon(e,tp,eg,ep,ev,re,r,rp)
-    return eg:IsExists(s.repfilter,1,nil,tp)
+    local g=eg:Filter(s.repfilter,nil,tp)
+    return #g==1
 end
 function s.repfilter(c,tp)
-    return c:IsSetCard(0xef) and c:IsType(TYPE_SPELL+TYPE_TRAP)
-        and c:IsLocation(LOCATION_DECK) and c:IsPreviousLocation(LOCATION_GRAVE) and c:IsControler(tp)
-        and c:GetReason()==REASON_EFFECT and Duel.GetOperatedGroup():FilterCount(Card.IsControler,nil,tp)==1
+    return c:IsSetCard(0xef) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsPreviousLocation(LOCATION_GRAVE)
+        and c:IsLocation(LOCATION_DECK) and c:IsControler(tp) and c:IsAbleToHand()
 end
 function s.repop(e,tp,eg,ep,ev,re,r,rp)
     local g=eg:Filter(s.repfilter,nil,tp)
-    if #g==1 and g:GetFirst():IsAbleToHand() then
-        Duel.SendtoHand(g,nil,REASON_EFFECT)
-        Duel.ConfirmCards(1-tp,g)
+    if #g==1 then
+        local tc=g:GetFirst()
+        Duel.SendtoHand(tc,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,tc)
         Duel.ShuffleDeck(tp)
     end
 end
 
--- GY effect: revive self by sending 2 "Darklord" cards
+-- GY revive cost
 function s.gyfilter(c)
     return c:IsSetCard(0xef) and c:IsAbleToGraveAsCost()
 end
@@ -92,14 +86,18 @@ function s.gycost(e,tp,eg,ep,ev,re,r,rp,chk)
     local g=Duel.SelectMatchingCard(tp,s.gyfilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,2,2,nil)
     Duel.SendtoGrave(g,REASON_COST)
 end
+
+-- GY revive target
 function s.gytg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
         and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
+
+-- GY revive operation
 function s.gyop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
-    if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
+    if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
         local e1=Effect.CreateEffect(c)
         e1:SetType(EFFECT_TYPE_SINGLE)
         e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
@@ -109,3 +107,10 @@ function s.gyop(e,tp,eg,ep,ev,re,r,rp)
         c:RegisterEffect(e1,true)
     end
 end
+
+-- Global LP cost tracker
+local ge1=Effect.CreateEffect({})
+ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+ge1:SetCode(EVENT_PAY_LPCOST)
+ge1:SetOperation(function(_,tp,_,_,_,_,_,_) Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1) end)
+Duel.RegisterEffect(ge1,0)
