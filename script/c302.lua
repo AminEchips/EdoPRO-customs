08f6a2e75078605c00abf5c2f09e13f30b5f62f0
Chips <aminekhoury0411@gmail.com>
2025-04-18 15:26:53 -0400

    Add files via upload

---
+++ script/c302.lua
@@ -0,0 +1,92 @@
+--Darklord Sammael
+local s,id=GetID()
+function s.initial_effect(c)
+    -- Special Summon restriction
+    c:SetSPSummonOnce(id)
+
+    -- Search "Darklord Morningstar" or card that mentions it
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCountLimit(1,id)
+    e1:SetCost(s.thcost)
+    e1:SetTarget(s.thtg)
+    e1:SetOperation(s.thop)
+    c:RegisterEffect(e1)
+
+    -- Copy "Darklord" Spell/Trap in GY (Quick Effect)
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_TODECK)
+    e2:SetType(EFFECT_TYPE_QUICK_O)
+    e2:SetCode(EVENT_FREE_CHAIN)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1,{id,1})
+    e2:SetCost(s.cost)
+    e2:SetTarget(s.target)
+    e2:SetOperation(s.operation)
+    c:RegisterEffect(e2)
+end
+s.listed_names={25451652} -- Darklord Morningstar
+s.listed_series={0xef} -- Darklord
+
+-- Discard this card as cost
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return e:GetHandler():IsDiscardable() end
+    Duel.SendtoGrave(e:GetHandler(),REASON_COST+REASON_DISCARD)
+end
+
+-- Filter: Searchable cards that are "Darklord Morningstar" or mention it (excluding this card)
+function s.thfilter(c)
+    return (c:IsCode(25451652) or aux.IsCodeListed(c,25451652)) 
+        and not c:IsCode(id) and c:IsAbleToHand()
+end
+
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
+    end
+end
+
+-- Effect 2: Copy "Darklord" Spell/Trap in GY
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.CheckLPCost(tp,1000) end
+    Duel.PayLPCost(tp,1000)
+end
+
+function s.cpyfilter(c)
+    return c:IsSetCard(0xef) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:CheckActivateEffect(false,true,false)~=nil
+end
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.cpyfilter,tp,LOCATION_GRAVE,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_GRAVE)
+end
+
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+    local tc=Duel.SelectMatchingCard(tp,s.cpyfilter,tp,LOCATION_GRAVE,0,1,1,nil):GetFirst()
+    if not tc then return end
+    local te=tc:CheckActivateEffect(false,true,true)
+    if te then
+        local tg=te:GetTarget()
+        local op=te:GetOperation()
+        e:SetCategory(te:GetCategory())
+        Duel.ClearTargetCard()
+        if tg then tg(e,tp,eg,ep,ev,re,r,rp,1) end
+        Duel.BreakEffect()
+        if op then op(e,tp,eg,ep,ev,re,r,rp) end
+        Duel.BreakEffect()
+        Duel.SendtoDeck(tc,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+    end
+end
