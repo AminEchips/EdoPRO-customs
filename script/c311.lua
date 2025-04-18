a25b24790a849fb872ae2b673b38153058f59224
Chips <aminekhoury0411@gmail.com>
2025-04-18 16:52:45 -0400

    Add files via upload

--- script/c311.lua
+++ script/c311.lua
@@ -1,11 +1,11 @@
 --Darklord Lillia
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Xyz summon procedure
+    -- Xyz Summon Procedure
     Xyz.AddProcedure(c,aux.FilterBoolFunction(Card.IsRace,RACE_FAIRY),4,2,s.ovfilter,aux.Stringid(id,0),2)
     c:EnableReviveLimit()
 
-    -- Effect 1: Detach + discard + both players draw + recover LP
+    -- Effect 1: Detach + discard, opponent random discard, draw & recover
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,1))
     e1:SetCategory(CATEGORY_HANDES+CATEGORY_DRAW+CATEGORY_RECOVER)
@@ -17,25 +17,34 @@ function s.initial_effect(c)
     e1:SetOperation(s.operation1)
     c:RegisterEffect(e1)
 
-    -- Effect 2: If sent to GY for Darklord S/T activation, halve LP costs for rest of turn
+    -- Effect 2 (Trigger): If sent to GY as cost for a Darklord Spell/Trap, flag for halving LP cost
     local e2=Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e2:SetCode(EVENT_TO_GRAVE)
-    e2:SetRange(LOCATION_GRAVE)
-    e2:SetOperation(s.halveop)
+    e2:SetProperty(EFFECT_FLAG_DELAY)
+    e2:SetCondition(s.halvecon)
+    e2:SetOperation(s.setreg)
     c:RegisterEffect(e2)
 
-    -- Effect 3: Banish from GY to search 1 Level 8 "Darklord"
+    -- Effect 2b (Continuous): After chain resolves, apply halving LP effect if flagged
     local e3=Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(id,2))
-    e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-    e3:SetType(EFFECT_TYPE_IGNITION)
+    e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+    e3:SetCode(EVENT_CHAIN_SOLVED)
     e3:SetRange(LOCATION_GRAVE)
-    e3:SetCountLimit(1,{id,1})
-    e3:SetCost(aux.bfgcost)
-    e3:SetTarget(s.thtg)
-    e3:SetOperation(s.thop)
+    e3:SetOperation(s.halveop)
     c:RegisterEffect(e3)
+
+    -- Effect 3: Banish from GY to add 1 Level 8 "Darklord" from Deck to hand
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,2))
+    e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e4:SetType(EFFECT_TYPE_IGNITION)
+    e4:SetRange(LOCATION_GRAVE)
+    e4:SetCountLimit(1,{id,1})
+    e4:SetCost(aux.bfgcost)
+    e4:SetTarget(s.thtg)
+    e4:SetOperation(s.thop)
+    c:RegisterEffect(e4)
 end
 s.listed_series={0xef}
 
@@ -43,7 +52,7 @@ function s.ovfilter(c,tp,lc)
     return c:IsFaceup() and c:IsRace(RACE_FAIRY) and c:IsAttribute(ATTRIBUTE_DARK)
 end
 
--- Effect 1
+-- Effect 1: Discard combo
 function s.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST)
         and Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
@@ -65,25 +74,32 @@ function s.operation1(e,tp,eg,ep,ev,re,r,rp)
     if p2>0 then Duel.Recover(1-tp,Duel.GetFieldGroupCount(1-tp,LOCATION_HAND,0)*100,REASON_EFFECT) end
 end
 
--- Effect 2: Halve LP costs
-function s.halveop(e,tp,eg,ep,ev,re,r,rp)
+-- Effect 2a: Check if sent as cost for Darklord S/T
+function s.halvecon(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
-    if not eg:IsContains(c) then return end
-    local re=Duel.GetChainInfo(0,CHAININFO_TRIGGERING_EFFECT)
-    if not re then return end
-    local rc=re:GetHandler()
-    if rc:IsSetCard(0xef) and rc:IsType(TYPE_SPELL+TYPE_TRAP) and rc:IsControler(tp) then
-        local e1=Effect.CreateEffect(c)
-        e1:SetType(EFFECT_TYPE_FIELD)
-        e1:SetCode(EFFECT_CHANGE_LPCOST)
-        e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-        e1:SetTargetRange(1,0)
-        e1:SetValue(s.costchange)
-        e1:SetReset(RESET_PHASE+PHASE_END)
-        Duel.RegisterEffect(e1,tp)
-    end
+    local re=c:GetReasonEffect()
+    local rc=re and re:GetHandler()
+    return c:IsReason(REASON_COST) and re and rc:IsSetCard(0xef) and rc:IsType(TYPE_SPELL+TYPE_TRAP)
+end
+function s.setreg(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    c:RegisterFlagEffect(id,RESET_PHASE+PHASE_END,0,1)
 end
 
+-- Effect 2b: Apply LP halving effect after resolution if flagged
+function s.halveop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if c:GetFlagEffect(id)==0 then return end
+    c:ResetFlagEffect(id)
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_CHANGE_LPCOST)
+    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e1:SetTargetRange(1,0)
+    e1:SetValue(s.costchange)
+    e1:SetReset(RESET_PHASE+PHASE_END)
+    Duel.RegisterEffect(e1,tp)
+end
 function s.costchange(e,re,rp,val)
     if rp==e:GetHandlerPlayer() then
         return math.ceil(val/2)
@@ -92,7 +108,7 @@ function s.costchange(e,re,rp,val)
     end
 end
 
--- Effect 3: Search Level 8 Darklord
+-- Effect 3: Search Level 8 "Darklord"
 function s.thfilter(c)
     return c:IsSetCard(0xef) and c:IsLevel(8) and c:IsAbleToHand()
 end
