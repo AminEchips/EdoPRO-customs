6783a7e4fccabac532fed5908da06ceb05b14798
Chips <aminekhoury0411@gmail.com>
2025-04-18 17:06:23 -0400

    Add files via upload

---
+++ script/c312.lua
@@ -0,0 +1,124 @@
+--Darklord Astharoth
+local s,id=GetID()
+function s.initial_effect(c)
+    -- Xyz Summon Procedure
+    Xyz.AddProcedure(c,nil,10,2)
+    c:EnableReviveLimit()
+
+    -- On Xyz Summon: reduce ATK/DEF of opponent's monsters, lock positions, enable piercing
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e1:SetCondition(s.xyzcon)
+    e1:SetOperation(s.xyzop)
+    c:RegisterEffect(e1)
+
+    -- Piercing damage
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_PIERCE)
+    c:RegisterEffect(e2)
+
+    -- Battle destruction: add or Special Summon if Level 11 mat
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_SPECIAL_SUMMON)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e3:SetCode(EVENT_BATTLE_DESTROYING)
+    e3:SetCondition(aux.bdocon)
+    e3:SetTarget(s.thtg)
+    e3:SetOperation(s.thop)
+    c:RegisterEffect(e3)
+
+    -- End Phase banish if LP paid for Darklord effect
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,2))
+    e4:SetCategory(CATEGORY_REMOVE)
+    e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e4:SetCode(EVENT_PHASE+PHASE_END)
+    e4:SetRange(LOCATION_MZONE)
+    e4:SetCountLimit(1,id)
+    e4:SetCondition(s.rmcon)
+    e4:SetTarget(s.rmtg)
+    e4:SetOperation(s.rmop)
+    c:RegisterEffect(e4)
+end
+s.listed_series={0xef}
+
+-- Effect 1: Xyz Summon condition
+function s.xyzcon(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ)
+end
+function s.xyzop(e,tp,eg,ep,ev,re,r,rp)
+    local g=Duel.GetMatchingGroup(nil,tp,0,LOCATION_MZONE,nil)
+    for tc in g:Iter() do
+        -- Set ATK/DEF to 0
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+        e1:SetValue(0)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+        tc:RegisterEffect(e1)
+        local e2=e1:Clone()
+        e2:SetCode(EFFECT_SET_DEFENSE_FINAL)
+        tc:RegisterEffect(e2)
+
+        -- Lock position & stat changes
+        local e3=Effect.CreateEffect(e:GetHandler())
+        e3:SetType(EFFECT_TYPE_SINGLE)
+        e3:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
+        e3:SetReset(RESET_EVENT+RESETS_STANDARD)
+        tc:RegisterEffect(e3)
+
+        local e4=e3:Clone()
+        e4:SetCode(EFFECT_CANNOT_CHANGE_ATTRIBUTE)
+        tc:RegisterEffect(e4)
+
+        local e5=e3:Clone()
+        e5:SetCode(EFFECT_CANNOT_CHANGE_RACE)
+        tc:RegisterEffect(e5)
+    end
+end
+
+-- Effect 2: Battle destroy trigger
+function s.thfilter(c,e,tp)
+    return c:IsSetCard(0xef) and c:IsLevelAbove(9) and (c:IsAbleToHand()
+        or (e:GetHandler():GetOverlayGroup():IsExists(Card.IsLevel,1,nil,11)
+        and c:IsCanBeSpecialSummoned(e,0,tp,false,false)))
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+    local tc=g:GetFirst()
+    if tc then
+        if tc:IsCanBeSpecialSummoned(e,0,tp,false,false)
+            and e:GetHandler():GetOverlayGroup():IsExists(Card.IsLevel,1,nil,11)
+            and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
+            Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+        else
+            Duel.SendtoHand(tc,nil,REASON_EFFECT)
+            Duel.ConfirmCards(1-tp,tc)
+        end
+    end
+end
+
+-- Effect 3: End Phase banish
+function s.rmcon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetFlagEffect(tp,id)>0
+end
+function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,1-tp,LOCATION_ONFIELD)
+end
+function s.rmop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+    local g=Duel.SelectMatchingCard(tp,aux.TRUE,tp,0,LOCATION_ONFIELD,1,1,nil)
+    if #g>0 then
+        Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+    end
+end
