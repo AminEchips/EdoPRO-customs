8be46d5b912edee519cb0ad606ec6c79a8225e09
Chips <aminekhoury0411@gmail.com>
2025-04-18 14:17:26 -0400

    Add files via upload

---
+++ script/c300.lua
@@ -0,0 +1,85 @@
+--Darklord Azazel, the Neonate
+local s,id=GetID()
+function s.initial_effect(c)
+    --Cannot be Special Summoned more than once per turn
+    c:SetUniqueOnField(1,0,id)
+    
+    --Special Summon limit
+    local e0=Effect.CreateEffect(c)
+    e0:SetType(EFFECT_TYPE_SINGLE)
+    e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+    e0:SetCode(EFFECT_SPSUMMON_CONDITION)
+    e0:SetValue(aux.limitOncePerTurn)
+    c:RegisterEffect(e0)
+
+    --Effect on Normal or Special Summon
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_SUMMON_SUCCESS)
+    e1:SetProperty(EFFECT_FLAG_DELAY)
+    e1:SetCountLimit(1,id)
+    e1:SetTarget(s.target)
+    e1:SetOperation(s.operation)
+    c:RegisterEffect(e1)
+    local e2=e1:Clone()
+    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+    c:RegisterEffect(e2)
+
+    --ATK/DEF boost if you control "Darklord Morningstar"
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE)
+    e3:SetCode(EFFECT_UPDATE_ATTACK)
+    e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCondition(s.adcon)
+    e3:SetValue(s.adval)
+    c:RegisterEffect(e3)
+    local e4=e3:Clone()
+    e4:SetCode(EFFECT_UPDATE_DEFENSE)
+    c:RegisterEffect(e4)
+end
+
+-- Summon success target
+function s.filter(c,e,tp)
+    return c:IsSetCard(0xef) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    local b1=Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,e,tp)
+    local b2=Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_MZONE,0,1,nil,25451652) -- Darklord Morningstar
+    if chk==0 then return b1 or b2 end
+    local opt=0
+    if b1 and b2 then
+        opt=Duel.SelectOption(tp,aux.Stringid(id,0),aux.Stringid(id,1))
+    elseif b1 then
+        opt=0
+    else
+        opt=1
+    end
+    e:SetLabel(opt)
+    if opt==0 then
+        e:SetCategory(CATEGORY_SPECIAL_SUMMON)
+        Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+    else
+        e:SetCategory(0)
+    end
+end
+
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+    local opt=e:GetLabel()
+    if opt==0 then
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+        local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+        if #g>0 then
+            Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+        end
+    end
+end
+
+function s.adcon(e)
+    return Duel.IsExistingMatchingCard(Card.IsCode,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil,25451652) -- Darklord Morningstar
+end
+function s.adval(e,c)
+    return c:GetBaseAttack()
+end
