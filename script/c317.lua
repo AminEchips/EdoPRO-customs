ee7dac9532e448a876ad52caa29ddd3e07cdbcfa
Chips <aminekhoury0411@gmail.com>
2025-04-18 23:39:44 -0400

    Update c317.lua

--- script/c317.lua
+++ script/c317.lua
@@ -1,11 +1,11 @@
 --Darklord Twin Covenant
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Link Summon
-    Link.AddProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0xef),2,2)
     c:EnableReviveLimit()
-
-    -- If Link Summoned using "Darklord Twin Ark": Destroy a card
+    --Link Summon
+    Link.AddProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0xef),2,2)
+    
+    --Draw 1 if summoned using "Darklord Twin Ark"
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_DESTROY)
@@ -13,16 +13,16 @@ function s.initial_effect(c)
     e1:SetCode(EVENT_SPSUMMON_SUCCESS)
     e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
     e1:SetCountLimit(1,id)
-    e1:SetCondition(s.descon)
+    e1:SetCondition(s.lkcon)
     e1:SetTarget(s.destg)
     e1:SetOperation(s.desop)
     c:RegisterEffect(e1)
 
-    -- If you control a Link-2 "Darklord": Special Summon this from GY
+    --Graveyard revival effect
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
-    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-    e2:SetCode(EVENT_PHASE+PHASE_END)
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e2:SetType(EFFECT_TYPE_IGNITION)
     e2:SetRange(LOCATION_GRAVE)
     e2:SetCountLimit(1,{id,1})
     e2:SetCondition(s.spcon)
@@ -30,38 +30,43 @@ function s.initial_effect(c)
     e2:SetOperation(s.spop)
     c:RegisterEffect(e2)
 end
-s.listed_names={316}
 s.listed_series={0xef}
 
-function s.descon(e,tp,eg,ep,ev,re,r,rp)
-    return e:GetHandler():GetMaterial():IsExists(Card.IsCode,1,nil,316)
+--e1: Destroy a target if Link Summoned using "Darklord Twin Ark" (ID: 316)
+function s.lkcon(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    return c:IsSummonType(SUMMON_TYPE_LINK) and c:GetMaterial():IsExists(Card.IsCode,1,nil,316)
 end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return chkc:IsOnField() end
-    if chk==0 then return Duel.IsExistingTarget(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+    if chkc then return chkc:IsOnField() and chkc:IsDestructable() end
+    if chk==0 then return Duel.IsExistingTarget(Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-    local g=Duel.SelectTarget(tp,nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+    local g=Duel.SelectTarget(tp,Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
     Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
     local tc=Duel.GetFirstTarget()
-    if tc and tc:IsRelateToEffect(e) then
+    if tc:IsRelateToEffect(e) then
         Duel.Destroy(tc,REASON_EFFECT)
     end
 end
 
+--e2: GY Revival if you control a Link-2 "Darklord"
+function s.cfilter(c)
+    return c:IsSetCard(0xef) and c:IsType(TYPE_LINK) and c:GetLink()==2
+end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-    return Duel.IsExistingMatchingCard(function(c) return c:IsFaceup() and c:IsSetCard(0xef) and c:IsLink(2) end,tp,LOCATION_MZONE,0,1,nil)
+    return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-    local c=e:GetHandler()
     if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-        and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+        and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
-    if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsRelateToEffect(e) then
+        Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
         local e1=Effect.CreateEffect(c)
         e1:SetType(EFFECT_TYPE_SINGLE)
         e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
@@ -71,3 +76,4 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
         c:RegisterEffect(e1,true)
     end
 end
+
