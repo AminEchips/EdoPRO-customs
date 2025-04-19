dad8d6a266e9d7fb8c282cee25abfe0f2785964e
Chips <aminekhoury0411@gmail.com>
2025-04-19 00:18:47 -0400

    Update c321.lua

--- script/c321.lua
+++ script/c321.lua
@@ -1,80 +1,87 @@
-
 --Darklord Sword of Rebellion
 local s,id=GetID()
 function s.initial_effect(c)
-    --Activate
+    -- Activate one of two effects
     local e1=Effect.CreateEffect(c)
     e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_SPECIAL_SUMMON)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_FREE_CHAIN)
-    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
     e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
     e1:SetTarget(s.target)
     e1:SetOperation(s.activate)
     c:RegisterEffect(e1)
 end
-s.listed_names={25451652}
+s.listed_names={25451652} -- Darklord Morningstar
+s.listed_series={0xef}
 
-function s.morningstarfilter(c)
+-- Option selector
+function s.filter1(c)
     return c:IsFaceup() and c:IsCode(25451652)
 end
-
-function s.fusfilter(c,e,tp,mg,f)
-    return c:IsType(TYPE_FUSION) and c:IsRace(RACE_FIEND) and c:IsAttribute(ATTRIBUTE_DARK) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
-        and (not f or f(c))
-        and Duel.GetFusionMaterial(tp):IsExists(Card.IsCanBeFusionMaterial,2,nil,c)
+function s.fusionfilter(c,e,tp,mg)
+    return c:IsType(TYPE_FUSION) and c:IsRace(RACE_FIEND+RACE_FAIRY) and c:IsAttribute(ATTRIBUTE_DARK)
+        and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
+        and c:CheckFusionMaterial(mg,nil,tp)
 end
-
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chk==0 then return true end
-    local opt=Duel.SelectOption(tp,aux.Stringid(id,0),aux.Stringid(id,1))
-    e:SetLabel(opt)
-    if opt==0 then
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    local b1=Duel.IsExistingTarget(s.filter1,tp,LOCATION_MZONE,0,1,nil)
+    local mg=Duel.GetMatchingGroup(Card.IsCanBeFusionMaterial,tp,LOCATION_MZONE,0,nil)
+    local b2=Duel.IsExistingMatchingCard(s.fusionfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg)
+    if chk==0 then return b1 or b2 end
+    local op=0
+    if b1 and b2 then
+        op=Duel.SelectOption(tp,aux.Stringid(id,0),aux.Stringid(id,1))
+    elseif b1 then
+        Duel.SelectOption(tp,aux.Stringid(id,0))
+        op=0
+    else
+        Duel.SelectOption(tp,aux.Stringid(id,1))
+        op=1
+    end
+    e:SetLabel(op)
+    if op==0 then
         Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-        local g=Duel.SelectTarget(tp,s.morningstarfilter,tp,LOCATION_MZONE,0,1,1,nil)
-        e:SetCategory(CATEGORY_ATKCHANGE)
+        local g=Duel.SelectTarget(tp,s.filter1,tp,LOCATION_MZONE,0,1,1,nil)
         Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,g,1,0,0)
     else
-        e:SetCategory(CATEGORY_SPECIAL_SUMMON)
         Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
     end
 end
 
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-    if e:GetLabel()==0 then
+    local op=e:GetLabel()
+    if op==0 then
         local tc=Duel.GetFirstTarget()
         if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+            -- Gain 500 ATK
             local e1=Effect.CreateEffect(e:GetHandler())
             e1:SetType(EFFECT_TYPE_SINGLE)
             e1:SetCode(EFFECT_UPDATE_ATTACK)
             e1:SetValue(500)
             e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
             tc:RegisterEffect(e1)
+            -- Indestructible by opponent's card effects
             local e2=Effect.CreateEffect(e:GetHandler())
             e2:SetType(EFFECT_TYPE_SINGLE)
             e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-            e2:SetValue(1)
-            e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-            e2:SetRange(LOCATION_MZONE)
+            e2:SetValue(function(e,te) return te:GetOwnerPlayer()~=e:GetHandlerPlayer() end)
             e2:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
             tc:RegisterEffect(e2)
         end
     else
-        local chkf=tp
-        local mg=Duel.GetFusionMaterial(tp)
-        local sg=Duel.GetMatchingGroup(s.fusfilter,tp,LOCATION_EXTRA,0,nil,e,tp,mg,nil)
-        if #sg>0 then
-            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-            local tc=sg:Select(tp,1,1,nil):GetFirst()
-            if tc then
-                local mat=Duel.SelectFusionMaterial(tp,tc,mg,nil,chkf)
-                if not mat or #mat<2 then return end
-                tc:SetMaterial(mat)
-                Duel.SendtoGrave(mat,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
-                Duel.BreakEffect()
-                Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
-                tc:CompleteProcedure()
-            end
-        end
+        local mg=Duel.GetMatchingGroup(Card.IsCanBeFusionMaterial,tp,LOCATION_MZONE,0,nil)
+        local sg=Duel.GetMatchingGroup(s.fusionfilter,tp,LOCATION_EXTRA,0,nil,e,tp,mg)
+        if #sg==0 then return end
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+        local sc=sg:Select(tp,1,1,nil):GetFirst()
+        if not sc then return end
+        local mat=Duel.SelectFusionMaterial(tp,sc,mg,nil,tp)
+        if not mat or #mat==0 then return end
+        sc:SetMaterial(mat)
+        Duel.SendtoGrave(mat,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
+        Duel.BreakEffect()
+        Duel.SpecialSummon(sc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
+        sc:CompleteProcedure()
     end
 end
