bd198c833598f5655a3ece25a016cf3aefe2d145
Chips <aminekhoury0411@gmail.com>
2025-04-18 23:55:15 -0400

    Add files via upload

---
+++ script/c319.lua
@@ -0,0 +1,47 @@
+--Darklord Destiny
+local s,id=GetID()
+function s.initial_effect(c)
+    --Activate
+    local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+    e1:SetTarget(s.target)
+    e1:SetOperation(s.activate)
+    c:RegisterEffect(e1)
+end
+s.listed_names={25451652} -- Darklord Morningstar
+
+function s.spfilter1(c,e,tp)
+    return c:IsSetCard(0xef) and c:IsCanBeSpecialSummoned(e,0,tp,true,true)
+end
+function s.spfilter2(c,e,tp)
+    return c:IsRace(RACE_FAIRY) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.morningstar_check(tp)
+    return Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_MZONE,0,1,nil,25451652)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.IsExistingMatchingCard(s.spfilter1,tp,LOCATION_HAND,0,1,nil,e,tp) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g1=Duel.SelectMatchingCard(tp,s.spfilter1,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+    local tc=g1:GetFirst()
+    if tc and Duel.SpecialSummon(tc,0,tp,tp,true,true,POS_FACEUP)>0 then
+        if s.morningstar_check(tp) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+            and Duel.IsExistingMatchingCard(s.spfilter2,tp,LOCATION_GRAVE,0,1,nil,e,tp)
+            and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+            Duel.BreakEffect()
+            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+            local g2=Duel.SelectMatchingCard(tp,s.spfilter2,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+            if #g2>0 then
+                Duel.SpecialSummon(g2,0,tp,tp,false,false,POS_FACEUP)
+            end
+        end
+    end
+end
