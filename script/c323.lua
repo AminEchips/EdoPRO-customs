8e80a3314c678f7b577e62e1540ee9a428fe16d6
Chips <aminekhoury0411@gmail.com>
2025-04-19 00:24:44 -0400

    Update c323.lua

--- script/c323.lua
+++ script/c323.lua
@@ -4,7 +4,7 @@ local s,id=GetID()
 function s.initial_effect(c)
     --Activate
     local e1=Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SEARCH+CATEGORY_TOHAND+CATEGORY_TOFIELD)
+    e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SET)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_FREE_CHAIN)
     e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
