bf5d24f6fc5bfdbb14e2c2dff95d9dab7b26d51f
Chips <aminekhoury0411@gmail.com>
2025-04-19 11:17:12 -0400

    Add files via upload

--- script/c329.lua
+++ script/c329.lua
@@ -1,35 +1,36 @@
+
 --Condemned Goddess
 local s,id=GetID()
 function s.initial_effect(c)
-    c:SetUniqueOnField(1,0,id)
     c:EnableReviveLimit()
     Fusion.AddProcMix(c,true,true,35306215,aux.FilterBoolFunctionEx(Card.IsRace,RACE_FAIRY))
-
-    -- Set 1 Spell/Trap from hand when Fusion Summoned
+    
+    --Set 1 Spell/Trap from hand on Fusion Summon
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e1:SetProperty(EFFECT_FLAG_DELAY)
+    e1:SetCountLimit(1,id)
     e1:SetCondition(s.setcon)
     e1:SetTarget(s.settg)
     e1:SetOperation(s.setop)
-    e1:SetCountLimit(1,id)
     c:RegisterEffect(e1)
-
-    -- Quick Effect: Draw 1 and activate Quick-Play Spell from hand
+    
+    --Chain to Quick-Play Spell
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
     e2:SetCategory(CATEGORY_DRAW)
-    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_QUICK_O)
     e2:SetCode(EVENT_CHAINING)
     e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1,{id,1})
     e2:SetCondition(s.qpcon)
     e2:SetTarget(s.qptg)
     e2:SetOperation(s.qpop)
-    e2:SetCountLimit(1,{id,1})
     c:RegisterEffect(e2)
 
-    -- Revive itself if opponent controls negated monster
+    --GY effect: revive if opponent controls negated card
     local e3=Effect.CreateEffect(c)
     e3:SetDescription(aux.Stringid(id,2))
     e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -43,6 +44,7 @@ function s.initial_effect(c)
     c:RegisterEffect(e3)
 end
 
+-- Fusion Summon condition
 function s.setcon(e,tp,eg,ep,ev,re,r,rp)
     return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION)
 end
@@ -60,8 +62,10 @@ function s.setop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
+-- Chain to Quick-Play Spell effect
 function s.qpcon(e,tp,eg,ep,ev,re,r,rp)
-    return re:IsActiveType(TYPE_SPELL) and re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:GetHandler():IsType(TYPE_QUICKPLAY)
+    local rc=re:GetHandler()
+    return rc:IsType(TYPE_SPELL) and rc:IsQuickPlaySpell()
 end
 function s.qptg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
@@ -69,19 +73,25 @@ function s.qptg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.qpop(e,tp,eg,ep,ev,re,r,rp)
     if Duel.Draw(tp,1,REASON_EFFECT)>0 then
-        Duel.BreakEffect()
-        local g=Duel.GetMatchingGroup(function(c) return c:IsQuickPlaySpell() and c:IsSSetable() end,tp,LOCATION_HAND,0,nil)
+        local g=Duel.GetMatchingGroup(Card.IsQuickPlaySpell,tp,LOCATION_HAND,0,nil)
         if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
-            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
-            local sc=g:Select(tp,1,1,nil):GetFirst()
-            if sc then
-                Duel.SSet(tp,sc)
-                Duel.ConfirmCards(1-tp,sc)
-            end
+            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_OPERATECARD)
+            local sg=g:Select(tp,1,1,nil)
+            local sc=sg:GetFirst()
+            Duel.Hint(HINT_CARD,tp,sc:GetCode())
+            Duel.MoveToField(sc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+            local te=sc:GetActivateEffect()
+            if not te then return end
+            local tg=te:GetTarget()
+            local op=te:GetOperation()
+            if tg then tg(te,tp,eg,ep,ev,re,r,rp,1) end
+            Duel.BreakEffect()
+            if op then op(te,tp,eg,ep,ev,re,r,rp) end
         end
     end
 end
 
+-- GY revive effect
 function s.negfilter(c)
     return c:IsFaceup() and c:IsDisabled()
 end
@@ -95,7 +105,7 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
-    if c:IsRelateToEffect(e) then
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsRelateToEffect(e) then
         if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
             local e1=Effect.CreateEffect(c)
             e1:SetType(EFFECT_TYPE_SINGLE)
