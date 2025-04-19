8c084f01bdba0f834781b961c82a4673dccbd7e9
Chips <aminekhoury0411@gmail.com>
2025-04-19 11:32:29 -0400

    Update c329.lua

--- script/c329.lua
+++ script/c329.lua
@@ -3,47 +3,49 @@ local s,id=GetID()
 function s.initial_effect(c)
     c:EnableReviveLimit()
     Fusion.AddProcMix(c,true,true,35306215,aux.FilterBoolFunctionEx(Card.IsRace,RACE_FAIRY))
-    
-    -- Set 1 Spell/Trap from hand (on Fusion Summon)
+
+    -- Set 1 Spell/Trap from hand on Fusion Summon
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_LEAVE_GRAVE)
     e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-    e1:SetProperty(EFFECT_FLAG_DELAY)
-    e1:SetCountLimit(1,id)
-    e1:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION) end)
+    e1:SetCondition(s.setcon)
     e1:SetTarget(s.settg)
     e1:SetOperation(s.setop)
     c:RegisterEffect(e1)
 
-    -- Draw and activate a Quick-Play Spell
+    -- Chain effect to a Quick-Play Spell: draw 1, then (optional) activate another from hand
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
-    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e2:SetCategory(CATEGORY_DRAW)
+    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_QUICK_O)
     e2:SetCode(EVENT_CHAINING)
     e2:SetRange(LOCATION_MZONE)
-    e2:SetProperty(EFFECT_FLAG_DELAY)
-    e2:SetCountLimit(1,{id,1})
+    e2:SetCountLimit(1)
     e2:SetCondition(s.qpcon)
     e2:SetTarget(s.qptg)
     e2:SetOperation(s.qpop)
     c:RegisterEffect(e2)
 
-    -- Special Summon itself if opponent controls negated face-up monster
+    -- Special Summon self from GY if opponent controls a negated face-up monster
     local e3=Effect.CreateEffect(c)
     e3:SetDescription(aux.Stringid(id,2))
     e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
     e3:SetCode(EVENT_PHASE+PHASE_END)
     e3:SetRange(LOCATION_GRAVE)
-    e3:SetCountLimit(1,{id,2})
+    e3:SetCountLimit(1,id)
     e3:SetCondition(s.spcon)
     e3:SetTarget(s.sptg)
     e3:SetOperation(s.spop)
     c:RegisterEffect(e3)
 end
 
--- e1: Set from hand
+-- Effect 1: On Fusion Summon
+function s.setcon(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION)
+end
 function s.setfilter(c)
     return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsSSetable()
 end
@@ -54,20 +56,21 @@ function s.setop(e,tp,eg,ep,ev,re,r,rp)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
     local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_HAND,0,1,1,nil)
     if #g>0 then
-        Duel.SSet(tp,g:GetFirst())
+        Duel.SSet(tp,g)
     end
 end
 
--- e2: Chain to Quick-Play Spell
+-- Effect 2: Chain to Quick-Play Spell activation
 function s.qpcon(e,tp,eg,ep,ev,re,r,rp)
-    return re:IsActiveType(TYPE_SPELL) and re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:GetHandler():IsType(TYPE_QUICKPLAY)
+    return re:IsActiveType(TYPE_SPELL) and re:IsHasType(EFFECT_TYPE_ACTIVATE)
+        and re:GetHandler():IsType(TYPE_QUICKPLAY)
 end
 function s.qptg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
-    Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+    Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,1,tp,0)
 end
 function s.qpfilter(c)
-    return c:IsType(TYPE_QUICKPLAY) and c:IsType(TYPE_SPELL) and c:IsSSetable()
+    return c:IsType(TYPE_QUICKPLAY) and c:IsType(TYPE_SPELL) and c:IsSSetable() and c:IsActivateable()
 end
 function s.qpop(e,tp,eg,ep,ev,re,r,rp)
     if Duel.Draw(tp,1,REASON_EFFECT)==0 then return end
@@ -75,33 +78,30 @@ function s.qpop(e,tp,eg,ep,ev,re,r,rp)
     local g=Duel.GetMatchingGroup(s.qpfilter,tp,LOCATION_HAND,0,nil)
     if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
         Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
-        local sg=g:Select(tp,1,1,nil):GetFirst()
-        Duel.ConfirmCards(1-tp,sg)
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ACTIVATE)
-        local te=sg:GetActivateEffect()
-        if te and te:IsActivatable(tp,true,true) then
-            Duel.MoveToField(sg,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
-            Duel.ClearTargetCard()
-            sg:CreateEffectRelation(te)
-            local cost=te:GetCost()
-            if cost then cost(te,tp,eg,ep,ev,re,r,rp,1) end
-            local target=te:GetTarget()
-            if target then target(te,tp,eg,ep,ev,re,r,rp,1) end
-            local op=te:GetOperation()
-            if op then op(te,tp,eg,ep,ev,re,r,rp) end
+        local sg=g:Select(tp,1,1,nil)
+        local sc=sg:GetFirst()
+        Duel.ConfirmCards(1-tp,sc)
+        Duel.BreakEffect()
+        Duel.MoveToField(sc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+        local te=sc:GetActivateEffect()
+        if te then
+            te:UseCountLimit(tp,1,true)
+            te:Reset()
+            Duel.RaiseEvent(sc,EVENT_CHAIN_SOLVED,e,REASON_EFFECT,tp,tp,0)
         end
     end
 end
 
--- e3: Special Summon from GY
-function s.negatedfilter(c)
-    return c:IsFaceup() and (c:IsDisabled() or not c:IsType(TYPE_EFFECT))
+-- Effect 3: GY summon if opponent controls a negated face-up monster
+function s.spfilter(c)
+    return c:IsFaceup() and ((c:IsType(TYPE_EFFECT) and c:IsDisabled()) or not c:IsType(TYPE_EFFECT))
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-    return Duel.IsExistingMatchingCard(s.negatedfilter,tp,0,LOCATION_MZONE,1,nil)
+    return Duel.IsExistingMatchingCard(s.spfilter,tp,0,LOCATION_MZONE,1,nil)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
