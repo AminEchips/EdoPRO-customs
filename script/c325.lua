19ff2a1927e38436c5c071cb56618a1769c41028
Chips <aminekhoury0411@gmail.com>
2025-04-19 10:10:11 -0400

    Update c325.lua

--- script/c325.lua
+++ script/c325.lua
@@ -1,85 +1,85 @@
 --Exiled Darklord
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Link Summon
+    --Link Summon
     c:EnableReviveLimit()
     Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_FAIRY),1,1)
 
-    -- On Link Summon: Add "The Sanctuary in the Sky"
+    --On Link Summon: Add "The Sanctuary in the Sky"
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
     e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e1:SetCode(EVENT_SPSUMMON_SUCCESS)
     e1:SetProperty(EFFECT_FLAG_DELAY)
-    e1:SetCountLimit(1,id)
-    e1:SetCondition(s.addcon)
-    e1:SetTarget(s.addtg)
-    e1:SetOperation(s.addop)
+    e1:SetCondition(s.thcon)
+    e1:SetTarget(s.thtg)
+    e1:SetOperation(s.thop)
     c:RegisterEffect(e1)
 
-    -- GY effect: Discard to summon a Level 8 or lower Fairy from GY
+    --GY effect: Banish self, discard 1 Darklord, target & Special Summon
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
     e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e2:SetType(EFFECT_TYPE_IGNITION)
     e2:SetRange(LOCATION_GRAVE)
-    e2:SetCountLimit(1,{id,1})
+    e2:SetCountLimit(1,id)
     e2:SetCost(s.spcost)
     e2:SetTarget(s.sptg)
     e2:SetOperation(s.spop)
     c:RegisterEffect(e2)
 end
-
 s.listed_names={56433456} -- The Sanctuary in the Sky
+s.listed_series={0xef} -- Darklord
 
--- Check summon was Link Summon
-function s.addcon(e,tp,eg,ep,ev,re,r,rp)
+--e1: Add "The Sanctuary in the Sky"
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
     return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
 end
-
--- Search The Sanctuary in the Sky
-function s.addfilter(c)
+function s.thfilter(c)
     return c:IsCode(56433456) and c:IsAbleToHand()
 end
-function s.addtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.addfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
     Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
 end
-function s.addop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-    local g=Duel.SelectMatchingCard(tp,s.addfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+    local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
     if #g>0 then
         Duel.SendtoHand(g,nil,REASON_EFFECT)
         Duel.ConfirmCards(1-tp,g)
     end
 end
 
--- Cost: Discard 1 "Darklord" card
-function s.cfilter(c)
+--e2: Cost (banish self & discard 1 Darklord)
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    local c=e:GetHandler()
+    if chk==0 then
+        return c:IsAbleToRemoveAsCost()
+            and Duel.IsExistingMatchingCard(s.disfilter,tp,LOCATION_HAND,0,1,nil)
+    end
+    Duel.Remove(c,POS_FACEUP,REASON_COST)
+    Duel.DiscardHand(tp,s.disfilter,1,1,REASON_COST+REASON_DISCARD)
+end
+function s.disfilter(c)
     return c:IsSetCard(0xef) and c:IsDiscardable()
 end
-function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,1,nil) end
-    Duel.DiscardHand(tp,s.cfilter,1,1,REASON_COST+REASON_DISCARD)
-end
 
--- Special Summon a Level 8 or lower Fairy from GY
+--e2: Target Level 8 or lower Fairy in GY
 function s.spfilter(c,e,tp)
-    return c:IsRace(RACE_FAIRY) and c:IsLevelBelow(8) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+    return c:IsRace(RACE_FAIRY) and c:IsLevelBelow(8) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_DEFENSE)
 end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.spfilter(chkc,e,tp) end
     if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-        and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+        and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-    local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
-    if #g>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_DEFENSE)>0 then
-        Duel.BreakEffect()
-        Duel.Banish(c,REASON_EFFECT)
+    local tc=Duel.GetFirstTarget()
+    if tc and tc:IsRelateToEffect(e) then
+        Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_DEFENSE)
     end
 end
