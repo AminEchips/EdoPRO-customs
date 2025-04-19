d5082d36af3b94e7f13ad0b045a8828d8259bd53
Chips <aminekhoury0411@gmail.com>
2025-04-18 23:51:28 -0400

    Add files via upload

---
+++ script/c318.lua
@@ -0,0 +1,41 @@
+
+--Darklord Scorns of Sanctity
+local s,id=GetID()
+function s.initial_effect(c)
+    --Activate
+    local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_RECOVER)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+    e1:SetTarget(s.target)
+    e1:SetOperation(s.activate)
+    c:RegisterEffect(e1)
+end
+s.listed_series={0xef}
+
+function s.filter(c)
+    return c:IsSetCard(0xef) and c:IsMonster() and c:IsAbleToGrave()
+end
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+end
+
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+    local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
+    local tc=g:GetFirst()
+    if tc and Duel.SendtoGrave(tc,REASON_EFFECT)>0 then
+        if tc:IsLocation(LOCATION_GRAVE) then
+            local rec=0
+            if tc:GetOriginalCode()==40008897 then -- Darklord Morningstar
+                rec=2000
+            else
+                rec=tc:GetLevel()*100
+            end
+            Duel.Recover(tp,rec,REASON_EFFECT)
+        end
+    end
+end
