8927045626babe6b703d9c98457dcd2379995013
Chips <aminekhoury0411@gmail.com>
2025-04-19 23:12:11 -0400

    Update c402.lua

--- script/c402.lua
+++ script/c402.lua
@@ -1,7 +1,7 @@
 --Starry Knight's Wyvern
 local s,id=GetID()
 function s.initial_effect(c)
-    --Effect 1: Quick Effect in hand to bounce a Starry Knight card you control, then Special Summon this
+    -- Effect 1: Quick effect in hand to bounce any face-up Starry Knight card to Special Summon this card
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
@@ -15,7 +15,7 @@ function s.initial_effect(c)
     e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
 
-    --Effect 2: Bounce this and one opponent's Special Summoned monster (no targeting)
+    -- Effect 2: If opponent Special Summons, return this card to hand as cost, then bounce 1 of those monsters (non-targeting)
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
     e2:SetCategory(CATEGORY_TOHAND)
@@ -25,18 +25,19 @@ function s.initial_effect(c)
     e2:SetProperty(EFFECT_FLAG_DELAY)
     e2:SetCountLimit(1,id+100)
     e2:SetCondition(s.bouncecon)
-    e2:SetCost(s.bouncecost)
+    e2:SetTarget(s.bouncetg)
     e2:SetOperation(s.bounceop)
     c:RegisterEffect(e2)
-    
 end
 
 s.listed_series={0x15b}
 
--- Effect 1: Special Summon from hand by bouncing a Starry Knight card you control
+-- Effect 1 functions
+
 function s.tgfilter(c)
     return c:IsFaceup() and c:IsSetCard(0x15b) and c:IsAbleToHand()
 end
+
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
     if chkc then return chkc:IsLocation(LOCATION_ONFIELD) and chkc:IsControler(tp) and s.tgfilter(chkc) end
     local c=e:GetHandler()
@@ -49,6 +50,7 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
     Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
 end
+
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     local tc=Duel.GetFirstTarget()
@@ -59,28 +61,28 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
--- Check if opponent Special Summoned at least 1 monster
+-- Effect 2 functions
+
 function s.bouncecon(e,tp,eg,ep,ev,re,r,rp)
-    return eg:IsExists(Card.IsControler,1,nil,1-tp)
+    return rp==1-tp and eg:IsExists(Card.IsControler,1,nil,1-tp)
 end
 
--- Return this card to the hand as cost
-function s.bouncecost(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.bouncetg(e,tp,eg,ep,ev,re,r,rp,chk)
     local c=e:GetHandler()
-    if chk==0 then return c:IsAbleToHandAsCost() end
+    local g=eg:Filter(function(c) return c:IsControler(1-tp) and c:IsAbleToHand() and c:IsOnField() end,nil)
+    if chk==0 then return #g>0 and c:IsAbleToHandAsCost() end
+    e:SetLabelObject(g:Clone()) -- Store group of Special Summoned monsters
     Duel.SendtoHand(c,nil,REASON_COST)
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,1-tp,LOCATION_MZONE)
 end
 
 function s.bounceop(e,tp,eg,ep,ev,re,r,rp)
     local g=e:GetLabelObject()
-    if not g then return end
-    local og=g:Filter(function(c)
-        return c:IsControler(1-tp) and c:IsRelateToEffect(e) and c:IsAbleToHand()
-    end,nil)
-    if #og>0 then
+    if not g or #g==0 then return end
+    local dg=g:Filter(Card.IsOnField,nil)
+    if #dg>0 then
         Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
-        local sg=og:Select(tp,1,1,nil)
+        local sg=dg:Select(tp,1,1,nil)
         Duel.SendtoHand(sg,nil,REASON_EFFECT)
     end
 end
-
