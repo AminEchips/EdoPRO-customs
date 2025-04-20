4bb5cd87939008021647b63f99aab4bb4c45533f
Chips <aminekhoury0411@gmail.com>
2025-04-19 23:20:02 -0400

    Update c402.lua

--- script/c402.lua
+++ script/c402.lua
@@ -1,59 +1,67 @@
 --Starry Knight's Wyvern
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Effect 1: Quick Summon from hand by returning 1 face-up "Starry Knight" card you control
+    -- Effect 1: Quick Summon from hand by bouncing a "Starry Knight" card as cost
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e1:SetType(EFFECT_TYPE_QUICK_O)
     e1:SetCode(EVENT_FREE_CHAIN)
     e1:SetRange(LOCATION_HAND)
     e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
     e1:SetCountLimit(1,id)
+    e1:SetCost(s.spcost)
     e1:SetTarget(s.sptg)
     e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
 
-    -- Effect 2: If opponent Special Summons a monster(s), draw 1 then discard this card
+    -- Effect 2: If opponent Special Summons a monster, return self as cost, draw, then discard
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
     e2:SetCategory(CATEGORY_DRAW+CATEGORY_HANDES)
     e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
     e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e2:SetProperty(EFFECT_FLAG_DELAY)
     e2:SetRange(LOCATION_MZONE)
     e2:SetCountLimit(1,id+100)
     e2:SetCondition(s.drcon)
+    e2:SetCost(s.drcost)
     e2:SetTarget(s.drtg)
     e2:SetOperation(s.drop)
     c:RegisterEffect(e2)
 end
 s.listed_series={0x15b}
 
--- Effect 1
-function s.thfilter(c)
-    return c:IsFaceup() and c:IsSetCard(0x15b) and c:IsAbleToHand()
+-- First Effect
+function s.costfilter(c)
+    return c:IsSetCard(0x15b) and c:IsFaceup() and c:IsAbleToHandAsCost()
+end
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_ONFIELD,0,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_ONFIELD,0,1,1,nil)
+    Duel.SendtoHand(g,nil,REASON_COST)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then 
-        return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_ONFIELD,0,1,nil)
-            and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false)
-    end
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_ONFIELD)
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+    local c=e:GetHandler()
+    if chk==0 then return c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
-    local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_ONFIELD,0,1,1,nil)
-    if #g>0 and Duel.SendtoHand(g,nil,REASON_EFFECT)>0 then
-        Duel.BreakEffect()
+    if c:IsRelateToEffect(e) then
         Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
     end
 end
 
--- Effect 2: Draw 1, then discard this card (not cost)
+-- Second Effect
 function s.drcon(e,tp,eg,ep,ev,re,r,rp)
     return eg:IsExists(Card.IsSummonPlayer,1,nil,1-tp)
+end
+function s.drcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    local c=e:GetHandler()
+    if chk==0 then return c:IsAbleToHandAsCost() end
+    Duel.SendtoHand(c,nil,REASON_COST)
 end
 function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
@@ -61,8 +69,12 @@ function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.drop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
-    if Duel.Draw(tp,1,REASON_EFFECT)>0 and c:IsRelateToEffect(e) and c:IsAbleToGrave() then
-        Duel.BreakEffect()
-        Duel.SendtoGrave(c,REASON_EFFECT+REASON_DISCARD)
+    if Duel.Draw(tp,1,REASON_EFFECT)>0 then
+        local hand=Duel.GetMatchingGroup(Card.IsCode,tp,LOCATION_HAND,0,nil,c:GetCode())
+        if #hand>0 then
+            Duel.BreakEffect()
+            Duel.SendtoGrave(hand,REASON_EFFECT+REASON_DISCARD)
+        end
     end
 end
+
