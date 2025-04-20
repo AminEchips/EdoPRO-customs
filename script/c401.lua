ef04df5a6f82f45d5a059c6055dc18e17a08aeb5
Chips <aminekhoury0411@gmail.com>
2025-04-19 22:48:56 -0400

    Update c401.lua

--- script/c401.lua
+++ script/c401.lua
@@ -11,7 +11,7 @@ function s.initial_effect(c)
     e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
 
-    -- Tribute LIGHT monster to Summon another Starry Knight
+    -- Tribute 1 LIGHT monster to Summon a "Starry Knight" monster
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
     e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -19,31 +19,32 @@ function s.initial_effect(c)
     e2:SetCode(EVENT_FREE_CHAIN)
     e2:SetRange(LOCATION_MZONE)
     e2:SetCountLimit(1,{id,1})
+    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
     e2:SetTarget(s.sptg)
     e2:SetOperation(s.spop2)
     c:RegisterEffect(e2)
 end
 s.listed_series={0x15b}
 
--- Check if another Starry Knight or Level 7 LIGHT Dragon exists in hand
-function s.cfilter(c)
+-- Effect 1: Reveal another Starry Knight or Level 7 LIGHT Dragon to Special Summon this
+function s.revealfilter(c)
     return (c:IsSetCard(0x15b) or (c:IsRace(RACE_DRAGON) and c:IsLevel(7) and c:IsAttribute(ATTRIBUTE_LIGHT)))
         and not c:IsPublic()
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-    return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,1,e:GetHandler())
+    return Duel.IsExistingMatchingCard(s.revealfilter,tp,LOCATION_HAND,0,1,e:GetHandler())
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
-    local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_HAND,0,1,1,c)
+    local g=Duel.SelectMatchingCard(tp,s.revealfilter,tp,LOCATION_HAND,0,1,1,c)
     if #g==0 then return end
     Duel.ConfirmCards(1-tp,g)
     Duel.ShuffleHand(tp)
     if Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-        and c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP) > 0
+        and c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0
         and c:IsFaceup() and c:IsLevelBelow(6) and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
-        -- Make its level 7
         local e1=Effect.CreateEffect(c)
         e1:SetType(EFFECT_TYPE_SINGLE)
         e1:SetCode(EFFECT_CHANGE_LEVEL)
@@ -53,7 +54,7 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
--- Second effect: Tribute 1 LIGHT monster; Special Summon 1 "Starry Knight" from hand or GY
+-- Effect 2: Tribute 1 LIGHT monster to summon 1 "Starry Knight" monster from hand or GY
 function s.spfilter2(c,e,tp)
     return c:IsSetCard(0x15b) and not c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
@@ -62,13 +63,15 @@ function s.trfilter(c)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
     if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.trfilter(chkc) end
-    if chk==0 then return Duel.IsExistingTarget(s.trfilter,tp,LOCATION_MZONE,0,1,nil)
-        and Duel.IsExistingMatchingCard(s.spfilter2,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp) end
+    if chk==0 then
+        return Duel.IsExistingTarget(s.trfilter,tp,LOCATION_MZONE,0,1,nil)
+            and Duel.IsExistingMatchingCard(s.spfilter2,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp)
+    end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
     local g=Duel.SelectTarget(tp,s.trfilter,tp,LOCATION_MZONE,0,1,1,nil)
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_GRAVE)
 end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
+function s.spop2(e,tp,eg,ep,ev,re,r,rp)
     local tc=Duel.GetFirstTarget()
     if not tc or not tc:IsRelateToEffect(e) then return end
     Duel.HintSelection(Group.FromCards(tc))
@@ -79,4 +82,3 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
         Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
     end
 end
-
