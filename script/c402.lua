967b5e1d905f7c9c09a72354203f693455723ffc
Chips <aminekhoury0411@gmail.com>
2025-04-19 22:56:48 -0400

    Update c402.lua

--- script/c402.lua
+++ script/c402.lua
@@ -1,7 +1,7 @@
 --Starry Knight's Wyvern
 local s,id=GetID()
 function s.initial_effect(c)
-    --Effect 1: Quick Effect in hand to bounce a Starry Knight you control, then Special Summon this
+    --Effect 1: Quick Effect in hand to bounce a Starry Knight card you control, then Special Summon this
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
@@ -15,7 +15,7 @@ function s.initial_effect(c)
     e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
 
-    --Effect 2: Bounce self & opponent's monster when they Special Summon
+    --Effect 2: Bounce this and one opponent's Special Summoned monster (no targeting)
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
     e2:SetCategory(CATEGORY_TOHAND)
@@ -23,28 +23,27 @@ function s.initial_effect(c)
     e2:SetCode(EVENT_SPSUMMON_SUCCESS)
     e2:SetRange(LOCATION_MZONE)
     e2:SetCountLimit(1,id+100)
-    e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+    e2:SetProperty(EFFECT_FLAG_DELAY)
     e2:SetCondition(s.thcon)
-    e2:SetTarget(s.thtg)
     e2:SetOperation(s.thop)
     c:RegisterEffect(e2)
 end
 
 s.listed_series={0x15b}
 
---Effect 1
+-- Effect 1: Special Summon from hand by bouncing a Starry Knight card you control
 function s.tgfilter(c)
     return c:IsFaceup() and c:IsSetCard(0x15b) and c:IsAbleToHand()
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.tgfilter(chkc) end
+    if chkc then return chkc:IsLocation(LOCATION_ONFIELD) and chkc:IsControler(tp) and s.tgfilter(chkc) end
     local c=e:GetHandler()
     if chk==0 then
-        return Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_MZONE,0,1,nil)
+        return Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_ONFIELD,0,1,nil)
             and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
     end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
-    local g=Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_MZONE,0,1,1,nil)
+    local g=Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_ONFIELD,0,1,1,nil)
     Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
 end
@@ -58,29 +57,17 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
---Effect 2
+-- Effect 2: Return this + 1 of the opponent’s just Special Summoned monsters
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-    return rp==1-tp
-end
-function s.thfilter(c)
-    return c:IsAbleToHand()
-end
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    local c=e:GetHandler()
-    if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and s.thfilter(chkc) end
-    if chk==0 then return c:IsAbleToHand()
-        and Duel.IsExistingTarget(s.thfilter,tp,0,LOCATION_MZONE,1,nil) end
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,c,1,0,0)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
-    local g=Duel.SelectTarget(tp,s.thfilter,tp,0,LOCATION_MZONE,1,1,nil)
-    Duel.SetTargetCard(g)
+    return rp==1-tp and eg:IsExists(Card.IsControler,1,nil,1-tp)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
-    local tc=Duel.GetFirstTarget()
-    if c:IsRelateToEffect(e) and Duel.SendtoHand(c,nil,REASON_EFFECT)>0 then
-        if tc and tc:IsRelateToEffect(e) then
-            Duel.SendtoHand(tc,nil,REASON_EFFECT)
-        end
+    if not c:IsRelateToEffect(e) or not Duel.SendtoHand(c,nil,REASON_EFFECT)>0 then return end
+    local g=eg:Filter(Card.IsControler,nil,1-tp):Filter(Card.IsRelateToEffect,nil,e)
+    if #g>0 then
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+        local sg=g:Select(tp,1,1,nil)
+        Duel.SendtoHand(sg,nil,REASON_EFFECT)
     end
 end
