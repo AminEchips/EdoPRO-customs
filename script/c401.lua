8cd5d5ceb3ee410b9b6225a9e403982a4bf08efa
Chips <aminekhoury0411@gmail.com>
2025-04-19 22:47:44 -0400

    Update c401.lua

--- script/c401.lua
+++ script/c401.lua
@@ -53,25 +53,22 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
--- Second effect: Tribute 1 LIGHT monster you control; Special Summon 1 "Starry Knight" from hand or GY
+-- Second effect: Tribute 1 LIGHT monster; Special Summon 1 "Starry Knight" from hand or GY
 function s.spfilter2(c,e,tp)
-    return c:IsSetCard(0x15b) and not c:IsCode(id)
-        and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+    return c:IsSetCard(0x15b) and not c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.tgfilter(c)
+function s.trfilter(c)
     return c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsFaceup() and c:IsReleasable()
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.tgfilter(chkc) end
-    if chk==0 then
-        return Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_MZONE,0,1,nil)
-            and Duel.IsExistingMatchingCard(s.spfilter2,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp)
-    end
+    if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.trfilter(chkc) end
+    if chk==0 then return Duel.IsExistingTarget(s.trfilter,tp,LOCATION_MZONE,0,1,nil)
+        and Duel.IsExistingMatchingCard(s.spfilter2,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp) end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-    local g=Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_MZONE,0,1,1,nil)
+    local g=Duel.SelectTarget(tp,s.trfilter,tp,LOCATION_MZONE,0,1,1,nil)
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_GRAVE)
 end
-function s.spop2(e,tp,eg,ep,ev,re,r,rp)
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
     local tc=Duel.GetFirstTarget()
     if not tc or not tc:IsRelateToEffect(e) then return end
     Duel.HintSelection(Group.FromCards(tc))
@@ -82,3 +79,4 @@ function s.spop2(e,tp,eg,ep,ev,re,r,rp)
         Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
     end
 end
+
