798286a589cc50c239b0e27d9139c4d2cc17f6dc
Chips <aminekhoury0411@gmail.com>
2025-04-20 18:44:23 -0400

    Update c407.lua

--- script/c407.lua
+++ script/c407.lua
@@ -1,93 +1,109 @@
---Starry Knight Mirage Dragon
+--Luminary Divine Dragon
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Xyz Summon
-    Xyz.AddProcedure(c,nil,7,2,s.ovfilter,aux.Stringid(id,0))
+    -- Xyz Summon setup
+    Xyz.AddProcedure(c,nil,7,3)
     c:EnableReviveLimit()
 
-    -- This card is always treated as Level 7
+    -- Always treated as Level 7 while face-up
     local e0=Effect.CreateEffect(c)
     e0:SetType(EFFECT_TYPE_SINGLE)
     e0:SetCode(EFFECT_RANK_LEVEL)
     e0:SetValue(7)
     c:RegisterEffect(e0)
 
-    -- On Xyz Summon: Add and optionally Special Summon
+    -- Negate DARK monster effects activated in GY
     local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,1))
-    e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
-    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-    e1:SetCondition(s.thcon)
-    e1:SetTarget(s.thtg)
-    e1:SetOperation(s.thop)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_DISABLE)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetTargetRange(0,LOCATION_GRAVE)
+    e1:SetTarget(s.distg)
     c:RegisterEffect(e1)
 
-    -- Negate DARK monster effects
+    -- Banish all DARK monsters when it attacks
     local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,2))
-    e2:SetCategory(CATEGORY_NEGATE+CATEGORY_TOGRAVE)
-    e2:SetType(EFFECT_TYPE_QUICK_O)
-    e2:SetCode(EVENT_CHAINING)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_REMOVE)
+    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_ATTACK_ANNOUNCE)
     e2:SetRange(LOCATION_MZONE)
-    e2:SetCountLimit(1)
-    e2:SetCondition(s.negcon)
-    e2:SetCost(s.negcost)
-    e2:SetTarget(s.negtg)
-    e2:SetOperation(s.negop)
+    e2:SetCondition(s.rmcon)
+    e2:SetCost(s.rmcost)
+    e2:SetTarget(s.rmtg)
+    e2:SetOperation(s.rmop)
     c:RegisterEffect(e2)
+
+    -- Special Summon banished DARK at End Phase if effect was used
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e3:SetCode(EVENT_PHASE+PHASE_END)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCountLimit(1,id)
+    e3:SetCondition(s.spcon)
+    e3:SetTarget(s.sptg)
+    e3:SetOperation(s.spop)
+    c:RegisterEffect(e3)
+
+    -- Flag effect to track activation
+    local eflag=Effect.CreateEffect(c)
+    eflag:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+    eflag:SetCode(EVENT_ATTACK_ANNOUNCE)
+    eflag:SetOperation(function(e) e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1) end)
+    c:RegisterEffect(eflag)
 end
 
-function s.ovfilter(c,tp,lc)
-    return c:IsFaceup() and c:IsRank(4) and c:IsRace(RACE_FAIRY) and c:IsControler(tp)
+-- Disable DARK effects in GY
+function s.distg(e,c)
+    return c:IsAttribute(ATTRIBUTE_DARK)
 end
 
-function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-    return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ)
+-- Banish all DARK monsters on field if this card attacks
+function s.rmcon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetAttacker()==e:GetHandler()
 end
-function s.thfilter(c,e,tp)
-    return c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsAbleToHand() and (c:IsLocation(LOCATION_GRAVE) or c:IsLocation(LOCATION_OVERLAY))
-        and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then
-        local g=e:GetHandler():GetOverlayGroup()
-        return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) or g:IsExists(s.thfilter,1,nil,e,tp)
-    end
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
-end
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_GRAVE,0,nil,e,tp)
-    g:Merge(c:GetOverlayGroup():Filter(s.thfilter,nil,e,tp))
-    if #g==0 then return end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-    local tc=g:Select(tp,1,1,nil):GetFirst()
-    if tc then
-        Duel.SendtoHand(tc,nil,REASON_EFFECT)
-        Duel.ConfirmCards(1-tp,tc)
-        if tc:IsLocation(LOCATION_HAND) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and tc:IsCanBeSpecialSummoned(e,0,tp,false,false)
-            and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
-            Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
-        end
-    end
-end
-
-function s.negcon(e,tp,eg,ep,ev,re,r,rp)
-    return re:IsActiveType(TYPE_MONSTER) and re:GetHandler():IsAttribute(ATTRIBUTE_DARK) and Duel.IsChainNegatable(ev)
-end
-function s.negcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.rmcost(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
     e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
 end
-function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return true end
-    Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
-    Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,eg,1,0,0)
+function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local g=Duel.GetMatchingGroup(Card.IsAttribute,tp,LOCATION_MZONE,LOCATION_MZONE,nil,ATTRIBUTE_DARK)
+    if chk==0 then return #g>0 end
+    Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,#g,0,0)
 end
-function s.negop(e,tp,eg,ep,ev,re,r,rp)
-    if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
-        Duel.SendtoGrave(re:GetHandler(),REASON_EFFECT)
+function s.rmop(e,tp,eg,ep,ev,re,r,rp)
+    local g=Duel.GetMatchingGroup(Card.IsAttribute,tp,LOCATION_MZONE,LOCATION_MZONE,nil,ATTRIBUTE_DARK)
+    if #g>0 then
+        Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+    end
+end
+
+-- End Phase revival
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler():GetFlagEffect(id)>0
+end
+function s.spfilter(c,e,tp)
+    return c:IsAttribute(ATTRIBUTE_DARK) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_REMOVED)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_REMOVED,0,1,1,nil,e,tp)
+    local tc=g:GetFirst()
+    if tc then
+        Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_CHANGE_ATTRIBUTE)
+        e1:SetValue(ATTRIBUTE_LIGHT)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+        tc:RegisterEffect(e1)
     end
 end
