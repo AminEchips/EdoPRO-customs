6e0439f213390b9560c2efb86ebb404c99378b05
Chips <aminekhoury0411@gmail.com>
2025-04-21 15:12:27 -0400

    Update c414.lua

--- script/c414.lua
+++ script/c414.lua
@@ -1,61 +1,61 @@
 --Starry Knight Distortion
 local s,id=GetID()
 function s.initial_effect(c)
-    --Activate: Optional Special Summon 1 LIGHT monster from hand or 1 "Starry Knight" from GY
+    -- Activate effect: Optional Special Summon from hand or GY
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
     e1:SetOperation(s.activate)
     c:RegisterEffect(e1)
 
-    --Main Phase Choice: Set another "Starry Knight" S/T from GY or Set itself face-down
+    -- Main Phase Effect: Set 1 "Starry Knight" S/T or reset this card
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_TOHAND+CATEGORY_LEAVE_GRAVE)
     e2:SetType(EFFECT_TYPE_QUICK_O)
     e2:SetCode(EVENT_FREE_CHAIN)
     e2:SetRange(LOCATION_SZONE)
     e2:SetCountLimit(1,{id,1})
+    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
     e2:SetCondition(function(e,tp) return Duel.IsMainPhase() end)
     e2:SetTarget(s.settg)
     e2:SetOperation(s.setop)
     c:RegisterEffect(e2)
 end
-s.listed_series={0x15b}
 
-function s.spfilter(c,e,tp)
+function s.filter(c,e,tp)
     return c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-        and (c:IsLocation(LOCATION_HAND) or (c:IsLocation(LOCATION_GRAVE) and c:IsSetCard(0x15b)))
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-    local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,nil,e,tp)
-    if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+    local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_HAND+LOCATION_GRAVE,0,nil,e,tp)
+    if #g>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
         Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
         local sg=g:Select(tp,1,1,nil)
-        if #sg>0 then
-            Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
-        end
+        Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
     end
 end
 
--- Set 1 "Starry Knight" Spell/Trap from GY or Set this card face-down
 function s.setfilter(c)
-    return c:IsSetCard(0x15b) and c:IsType(TYPE_SPELL+TYPE_TRAP) and not c:IsCode(id) and c:IsSSetable()
+    return c:IsSetCard(0x15b) and c:IsSpellTrap() and not c:IsCode(id) and c:IsSSetable()
 end
 function s.settg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.setfilter(chkc) end
+    if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.setfilter(chkc) end
+    if chk==0 then return Duel.IsExistingTarget(s.setfilter,tp,LOCATION_GRAVE,0,1,nil)
+        or e:GetHandler():IsSSetable() end
     local b1=Duel.IsExistingTarget(s.setfilter,tp,LOCATION_GRAVE,0,1,nil)
     local b2=e:GetHandler():IsSSetable()
-    if chk==0 then return b1 or b2 end
     local op=0
     if b1 and b2 then
-        op=Duel.SelectOption(tp,aux.Stringid(id,3),aux.Stringid(id,4))
+        op=Duel.SelectOption(tp,aux.Stringid(id,1),aux.Stringid(id,2))
     elseif b1 then
-        op=Duel.SelectOption(tp,aux.Stringid(id,3))
+        Duel.SelectOption(tp,aux.Stringid(id,1))
+        op=0
     else
-        op=Duel.SelectOption(tp,aux.Stringid(id,4))+1
+        Duel.SelectOption(tp,aux.Stringid(id,2))
+        op=1
     end
     e:SetLabel(op)
     if op==0 then
@@ -71,9 +71,9 @@ function s.setop(e,tp,eg,ep,ev,re,r,rp)
         if tc and tc:IsRelateToEffect(e) then
             Duel.SSet(tp,tc)
         end
-    elseif op==1 then
+    else
         local c=e:GetHandler()
-        if c:IsRelateToEffect(e) and c:IsSSetable() then
+        if c:IsRelateToEffect(e) and c:IsFaceup() and c:IsSSetable() then
             Duel.ChangePosition(c,POS_FACEDOWN)
             Duel.RaiseEvent(c,EVENT_SSET,e,REASON_EFFECT,tp,tp,0)
         end
