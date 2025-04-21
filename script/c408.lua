5ea127cddc12767f584a65b389e61ace4a58d3bd
Chips <aminekhoury0411@gmail.com>
2025-04-20 21:23:25 -0400

    Update c408.lua

--- script/c408.lua
+++ script/c408.lua
@@ -31,27 +31,25 @@ function s.initial_effect(c)
     e2b:SetCode(EFFECT_UPDATE_DEFENSE)
     c:RegisterEffect(e2b)
 
-    -- Change Attribute to DARK
+    -- All opponent monsters become DARK
     local e3=Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(id,1))
-    e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-    e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-    e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e3:SetType(EFFECT_TYPE_FIELD)
+    e3:SetCode(EFFECT_CHANGE_ATTRIBUTE)
     e3:SetRange(LOCATION_MZONE)
-    e3:SetCountLimit(1,id)
-    e3:SetCondition(s.attrcon)
-    e3:SetTarget(s.attrtg)
-    e3:SetOperation(s.attrop)
+    e3:SetTargetRange(0,LOCATION_MZONE)
+    e3:SetValue(ATTRIBUTE_DARK)
     c:RegisterEffect(e3)
 
-    -- Destroy 1 card when another is destroyed by card effect
+    -- Destroy 1 card on the field when a card is destroyed by a card effect
     local e4=Effect.CreateEffect(c)
-    e4:SetDescription(aux.Stringid(id,2))
+    e4:SetDescription(aux.Stringid(id,1))
     e4:SetCategory(CATEGORY_DESTROY)
-    e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-    e4:SetCode(EVENT_DESTROYED)
+    e4:SetType(EFFECT_TYPE_QUICK_O)
+    e4:SetCode(EVENT_FREE_CHAIN)
+    e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
     e4:SetRange(LOCATION_MZONE)
-    e4:SetCountLimit(1,id+100)
+    e4:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
+    e4:SetCountLimit(1,id)
     e4:SetCondition(s.descon)
     e4:SetCost(s.descost)
     e4:SetTarget(s.destg)
@@ -59,56 +57,29 @@ function s.initial_effect(c)
     c:RegisterEffect(e4)
 end
 
--- Xyz condition
 function s.ovfilter(c,tp,lc)
     return c:IsFaceup() and c:IsLevel(4) and c:IsRace(RACE_FAIRY) and c:IsAttribute(ATTRIBUTE_LIGHT)
 end
 
--- ATK/DEF boost
 function s.atktg(e,c)
     return c:IsAttribute(ATTRIBUTE_LIGHT) and c~=e:GetHandler()
 end
-
 function s.atktg2(e,c)
     return c:IsAttribute(ATTRIBUTE_DARK)
 end
 
--- Change Attribute condition (if has overlay)
-function s.attrcon(e,tp,eg,ep,ev,re,r,rp)
-    return e:GetHandler():GetOverlayCount()>0
-end
-
--- Target DARK monster to change its Attribute
-function s.attrfilter(c)
-    return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_DARK)
-end
-function s.attrtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and s.attrfilter(chkc) end
-    if chk==0 then return Duel.IsExistingTarget(s.attrfilter,tp,0,LOCATION_MZONE,1,nil) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-    local g=Duel.SelectTarget(tp,s.attrfilter,tp,0,LOCATION_MZONE,1,1,nil)
-    Duel.SetOperationInfo(0,CATEGORY_CHANGE_ATTRIBUTE,g,1,0,0)
-end
-function s.attrop(e,tp,eg,ep,ev,re,r,rp)
-    local tc=Duel.GetFirstTarget()
-    if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
-        local e1=Effect.CreateEffect(e:GetHandler())
-        e1:SetType(EFFECT_TYPE_SINGLE)
-        e1:SetCode(EFFECT_CHANGE_ATTRIBUTE)
-        e1:SetValue(ATTRIBUTE_DARK)
-        e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-        tc:RegisterEffect(e1)
-    end
-end
-
--- Destroy a card when another is destroyed by effect
+-- Only trigger if a card was destroyed by effect this turn
 function s.descon(e,tp,eg,ep,ev,re,r,rp)
-    return eg:IsExists(Card.IsReason,1,nil,REASON_EFFECT)
+    return Duel.GetFlagEffect(tp,id)>0
 end
+
+-- Pay cost by detaching 1 material
 function s.descost(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
     e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
 end
+
+-- Target any card on the field
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
     if chkc then return chkc:IsOnField() end
     if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
@@ -116,9 +87,25 @@ function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
     local g=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
     Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
+
+-- Destroy it
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
     local tc=Duel.GetFirstTarget()
     if tc and tc:IsRelateToEffect(e) then
         Duel.Destroy(tc,REASON_EFFECT)
     end
+end
+
+-- Global check for any card destroyed by effect this turn
+if not s.global_check then
+    s.global_check=true
+    local ge1=Effect.CreateEffect(s)
+    ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+    ge1:SetCode(EVENT_DESTROYED)
+    ge1:SetOperation(function(e,tp,eg,ep,ev,re,r,rp)
+        if eg:IsExists(Card.IsReason,1,nil,REASON_EFFECT) then
+            Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
+        end
+    end)
+    Duel.RegisterEffect(ge1,0)
 end
