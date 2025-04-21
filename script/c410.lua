a6792008c9efa92f1581ec7e1d653c2f65685a1f
Chips <aminekhoury0411@gmail.com>
2025-04-21 14:12:19 -0400

    Update c410.lua

--- script/c410.lua
+++ script/c410.lua
@@ -1,11 +1,10 @@
 --Starry Night Flash Dragon
 local s,id=GetID()
 function s.initial_effect(c)
-    --Link Summon
-    Link.addProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x15b),1,1,s.lcheck)
     c:EnableReviveLimit()
+    Link.AddProcedure(c,s.matfilter,1,1)
 
-    --Add 1 "Starry Knight Sky" from Deck to hand
+    --Add 1 "Starry Knight Sky" on Link Summon
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
@@ -16,31 +15,31 @@ function s.initial_effect(c)
     e1:SetOperation(s.thop)
     c:RegisterEffect(e1)
 
-    --Tribute 1 LIGHT monster; Special Summon 1 LIGHT monster from hand
+    --Tribute and Special Summon
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
     e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e2:SetType(EFFECT_TYPE_QUICK_O)
     e2:SetCode(EVENT_FREE_CHAIN)
-    e2:SetRange(LOCATION_MZONE)
-    e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
-    e2:SetCountLimit(1,{id,1})
     e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1)
+    e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
     e2:SetTarget(s.sptg)
     e2:SetOperation(s.spop)
     c:RegisterEffect(e2)
 end
 s.listed_series={0x15b}
 
-function s.lcheck(g,lc)
-    return not g:IsExists(Card.IsType,1,nil,TYPE_LINK)
+function s.matfilter(c,lc,sumtype,tp)
+    return c:IsSetCard(0x15b) and not c:IsType(TYPE_LINK)
 end
 
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
     return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
 end
 function s.thfilter(c)
-    return c:IsCode(5840694) and c:IsAbleToHand() -- "Starry Knight Sky"
+    return c:IsCode(65403020) and c:IsAbleToHand() -- "Starry Knight Sky"
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
@@ -55,29 +54,34 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
-function s.cfilter(c)
+function s.trfilter(c)
     return c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsFaceup() and c:IsReleasable()
 end
 function s.spfilter(c,e,tp,zone)
     return c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,tp,zone)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.cfilter(chkc) end
+    if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.trfilter(chkc) end
+    local zone=e:GetHandler():GetLinkedZone(tp)
     if chk==0 then
-        return Duel.IsExistingTarget(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
-            and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp,e:GetHandler():GetLinkedZone(tp))
+        return Duel.IsExistingTarget(s.trfilter,tp,LOCATION_MZONE,0,1,e:GetHandler())
+            and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp,zone)
+            and zone~=0
     end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-    local g=Duel.SelectTarget(tp,s.cfilter,tp,LOCATION_MZONE,0,1,1,nil)
+    local g=Duel.SelectTarget(tp,s.trfilter,tp,LOCATION_MZONE,0,1,1,e:GetHandler())
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-    local tc=Duel.GetFirstTarget()
-    if not tc or not tc:IsRelateToEffect(e) or Duel.Release(tc,REASON_COST)==0 then return end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
     local zone=e:GetHandler():GetLinkedZone(tp)
-    local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp,zone)
-    if #g>0 then
-        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP,zone)
+    if zone==0 then return end
+    local tc=Duel.GetFirstTarget()
+    if tc and Duel.Release(tc,REASON_COST)>0 then
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+        local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp,zone)
+        if #g>0 then
+            Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP,zone)
+        end
     end
 end
+
