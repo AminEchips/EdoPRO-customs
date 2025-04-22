f6047c08437edcf2b73bc45e0988b711a61dbbf1
Chips <aminekhoury0411@gmail.com>
2025-04-22 11:52:17 -0400

    Update c404.lua

--- script/c404.lua
+++ script/c404.lua
@@ -1,94 +1,101 @@
 --Herensuge, Bounder of Skies
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Special Summon if DARK monster is Special Summoned
+    -- Discard to search
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e1:SetType(EFFECT_TYPE_IGNITION)
     e1:SetRange(LOCATION_HAND)
     e1:SetCountLimit(1,id)
-    e1:SetCondition(s.spcon)
-    e1:SetTarget(s.sptg)
-    e1:SetOperation(s.spop)
+    e1:SetCost(s.cost)
+    e1:SetTarget(s.target)
+    e1:SetOperation(s.operation)
     c:RegisterEffect(e1)
 
-    -- During opponent's turn, tribute this card to search + summon
+    -- Quick Effect: Bounce and negate
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
-    e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_SPECIAL_SUMMON)
+    e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON+CATEGORY_DISABLE)
     e2:SetType(EFFECT_TYPE_QUICK_O)
     e2:SetCode(EVENT_FREE_CHAIN)
-    e2:SetRange(LOCATION_MZONE)
-    e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
+    e2:SetRange(LOCATION_HAND)
+    e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
     e2:SetCountLimit(1,id+100)
-    e2:SetCondition(s.thcon)
-    e2:SetCost(s.thcost)
-    e2:SetTarget(s.thtg)
-    e2:SetOperation(s.thop)
+    e2:SetCondition(s.qecon)
+    e2:SetTarget(s.qetg)
+    e2:SetOperation(s.qeop)
     c:RegisterEffect(e2)
 end
 s.listed_series={0x15b}
 
-function s.cfilter(c,tp)
-    return c:IsSummonPlayer(1-tp) and c:IsAttribute(ATTRIBUTE_DARK)
-end
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-    return eg:IsExists(s.cfilter,1,nil,tp) and Duel.GetCurrentPhase()~=PHASE_DAMAGE
-end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+-- e1: Discard to search
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
     local c=e:GetHandler()
-    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-        and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+    if chk==0 then return c:IsDiscardable() end
+    Duel.SendtoGrave(c,REASON_COST+REASON_DISCARD)
 end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
-        local e1=Effect.CreateEffect(c)
-        e1:SetType(EFFECT_TYPE_FIELD)
-        e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-        e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-        e1:SetTargetRange(1,0)
-        e1:SetTarget(s.splimit)
-        e1:SetReset(RESET_PHASE+PHASE_END+RESET_OPPO_TURN)
-        Duel.RegisterEffect(e1,tp)
+function s.thfilter(c)
+    return c:IsSetCard(0x15b) and c:IsAbleToHand()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
     end
 end
-function s.splimit(e,c,sump,sumtype,sumpos,targetp,se)
-    return not c:IsAttribute(ATTRIBUTE_LIGHT)
-end
 
-function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-    return Duel.GetTurnPlayer()~=tp
+-- e2: Bounce + summon + negate
+function s.qecon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetTurnPlayer()~=tp and Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsLevel,7),tp,LOCATION_MZONE,0,1,nil)
 end
-function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return e:GetHandler():IsReleasable() end
-    Duel.Release(e:GetHandler(),REASON_COST)
+function s.qefilter(c)
+    return c:IsLevel(7) and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsAbleToHand()
 end
-function s.thfilter(c,e,tp)
-    return c:IsSetCard(0x15b) and c:IsAbleToHand() and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,c,c:GetCode(),e,tp)
+function s.qeopfilter(c,e,tp)
+    return c:IsSetCard(0x15b) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.spfilter(c,code,e,tp)
-    return c:IsSetCard(0x15b) and not c:IsCode(code) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
+function s.qetg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then
+        return Duel.IsExistingMatchingCard(s.qefilter,tp,LOCATION_HAND,0,1,nil)
+            and Duel.IsExistingMatchingCard(s.qeopfilter,tp,LOCATION_HAND,0,1,e:GetHandler(),e,tp)
+            and Duel.IsExistingMatchingCard(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil)
+            and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+    end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_HAND)
 end
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
-end
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-    local g1=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
-    if #g1>0 then
-        local tc1=g1:GetFirst()
-        Duel.SendtoHand(tc1,nil,REASON_EFFECT)
-        Duel.ConfirmCards(1-tp,tc1)
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-        local g2=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,tc1:GetCode(),e,tp)
-        if #g2>0 then
-            Duel.SpecialSummon(g2,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+function s.qeop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if not c:IsRelateToEffect(e) or Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+    local g1=Duel.SelectMatchingCard(tp,s.qefilter,tp,LOCATION_HAND,0,1,1,nil)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g2=Duel.SelectMatchingCard(tp,s.qeopfilter,tp,LOCATION_HAND,0,1,1,c,e,tp)
+    if #g1>0 and #g2>0 and Duel.SendtoHand(g1,nil,REASON_EFFECT)>0 and Duel.SpecialSummon(g2,0,tp,tp,false,false,POS_FACEUP)>0 then
+        if Duel.IsExistingMatchingCard(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) then
+            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+            local tg=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
+            if #tg>0 then
+                Duel.HintSelection(tg)
+                local tc=tg:GetFirst()
+                local e1=Effect.CreateEffect(c)
+                e1:SetType(EFFECT_TYPE_SINGLE)
+                e1:SetCode(EFFECT_DISABLE)
+                e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+                tc:RegisterEffect(e1)
+                local e2=Effect.CreateEffect(c)
+                e2:SetType(EFFECT_TYPE_SINGLE)
+                e2:SetCode(EFFECT_DISABLE_EFFECT)
+                e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+                tc:RegisterEffect(e2)
+            end
         end
     end
 end
