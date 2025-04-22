79894b27f1cb2a4a51c8c5c6d90b1f84336672e5
Chips <aminekhoury0411@gmail.com>
2025-04-22 12:03:42 -0400

    Update c404.lua

--- script/c404.lua
+++ script/c404.lua
@@ -1,48 +1,47 @@
 --Herensuge, Bounder of Skies
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Discard to search
+    -- Discard to search a "Starry Knight"
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
     e1:SetType(EFFECT_TYPE_IGNITION)
     e1:SetRange(LOCATION_HAND)
     e1:SetCountLimit(1,id)
-    e1:SetCost(s.cost)
-    e1:SetTarget(s.target)
-    e1:SetOperation(s.operation)
+    e1:SetCost(s.thcost)
+    e1:SetTarget(s.thtg)
+    e1:SetOperation(s.thop)
     c:RegisterEffect(e1)
 
-    -- Quick Effect: Bounce and negate
+    -- Quick Summon + negate during opponent's turn
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
-    e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON+CATEGORY_DISABLE)
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND+CATEGORY_DISABLE)
     e2:SetType(EFFECT_TYPE_QUICK_O)
     e2:SetCode(EVENT_FREE_CHAIN)
     e2:SetRange(LOCATION_HAND)
     e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
     e2:SetCountLimit(1,id+100)
-    e2:SetCondition(s.qecon)
-    e2:SetTarget(s.qetg)
-    e2:SetOperation(s.qeop)
+    e2:SetCondition(s.spcon)
+    e2:SetTarget(s.sptg)
+    e2:SetOperation(s.spop)
     c:RegisterEffect(e2)
 end
 s.listed_series={0x15b}
 
 -- e1: Discard to search
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-    local c=e:GetHandler()
-    if chk==0 then return c:IsDiscardable() end
-    Duel.SendtoGrave(c,REASON_COST+REASON_DISCARD)
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return e:GetHandler():IsDiscardable() end
+    Duel.SendtoGrave(e:GetHandler(),REASON_COST+REASON_DISCARD)
 end
 function s.thfilter(c)
     return c:IsSetCard(0x15b) and c:IsAbleToHand()
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
     Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
     local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
     if #g>0 then
@@ -51,51 +50,41 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
--- e2: Bounce + summon + negate
-function s.qecon(e,tp,eg,ep,ev,re,r,rp)
-    return Duel.GetTurnPlayer()~=tp and Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsLevel,7),tp,LOCATION_MZONE,0,1,nil)
+-- e2: Quick effect to bounce, summon, negate
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetTurnPlayer()~=tp and Duel.IsExistingMatchingCard(Auxiliary.FaceupFilter(Card.IsLevel,7),tp,LOCATION_MZONE,0,1,nil)
 end
-function s.qefilter(c)
-    return c:IsLevel(7) and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsAbleToHand()
-end
-function s.qeopfilter(c,e,tp)
+function s.spfilter(c,e,tp)
     return c:IsSetCard(0x15b) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.qetg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then
-        return Duel.IsExistingMatchingCard(s.qefilter,tp,LOCATION_HAND,0,1,nil)
-            and Duel.IsExistingMatchingCard(s.qeopfilter,tp,LOCATION_HAND,0,1,e:GetHandler(),e,tp)
-            and Duel.IsExistingMatchingCard(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil)
-            and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-    end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_HAND)
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+        and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,e:GetHandler(),e,tp)
+        and e:GetHandler():IsAbleToHand() end
 end
-function s.qeop(e,tp,eg,ep,ev,re,r,rp)
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
-    if not c:IsRelateToEffect(e) or Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
-    local g1=Duel.SelectMatchingCard(tp,s.qefilter,tp,LOCATION_HAND,0,1,1,nil)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-    local g2=Duel.SelectMatchingCard(tp,s.qeopfilter,tp,LOCATION_HAND,0,1,1,c,e,tp)
-    if #g1>0 and #g2>0 and Duel.SendtoHand(g1,nil,REASON_EFFECT)>0 and Duel.SpecialSummon(g2,0,tp,tp,false,false,POS_FACEUP)>0 then
-        if Duel.IsExistingMatchingCard(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) then
-            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-            local tg=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
-            if #tg>0 then
-                Duel.HintSelection(tg)
-                local tc=tg:GetFirst()
-                local e1=Effect.CreateEffect(c)
-                e1:SetType(EFFECT_TYPE_SINGLE)
-                e1:SetCode(EFFECT_DISABLE)
-                e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-                tc:RegisterEffect(e1)
-                local e2=Effect.CreateEffect(c)
-                e2:SetType(EFFECT_TYPE_SINGLE)
-                e2:SetCode(EFFECT_DISABLE_EFFECT)
-                e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-                tc:RegisterEffect(e2)
-            end
+    if Duel.SendtoHand(c,nil,REASON_COST)~=0 then
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+        local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+        if #g>0 and Duel.SpecialSummonStep(g:GetFirst(),0,tp,tp,false,false,POS_FACEUP) then
+            Duel.SpecialSummonComplete()
+        end
+
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+        local og=Duel.SelectMatchingCard(tp,aux.FaceupFilter(Card.IsControler,1-tp),tp,0,LOCATION_MZONE,1,1,nil)
+        local tc=og:GetFirst()
+        if tc then
+            local e1=Effect.CreateEffect(c)
+            e1:SetType(EFFECT_TYPE_SINGLE)
+            e1:SetCode(EFFECT_DISABLE)
+            e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+            tc:RegisterEffect(e1)
+            local e2=Effect.CreateEffect(c)
+            e2:SetType(EFFECT_TYPE_SINGLE)
+            e2:SetCode(EFFECT_DISABLE_EFFECT)
+            e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+            tc:RegisterEffect(e2)
         end
     end
 end
