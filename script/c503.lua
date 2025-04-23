c614a204ab1e00ba09a827f74db28aa577bf3cfd
Chips <aminekhoury0411@gmail.com>
2025-04-23 17:28:43 -0400

    Create c503.lua

---
+++ script/c503.lua
@@ -0,0 +1,74 @@
+--Altergeist Splitting
+local s,id=GetID()
+function s.initial_effect(c)
+    -- You can only control 1 Altergeist Splitting
+    c:SetUniqueOnField(1,0,id)
+
+    -- Effect 1: Send 1 Trap from Deck to GY when an Altergeist monster is Tributed
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_TOGRAVE)
+    e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_RELEASE)
+    e1:SetRange(LOCATION_SZONE)
+    e1:SetProperty(EFFECT_FLAG_DELAY)
+    e1:SetCountLimit(1,id)
+    e1:SetCondition(s.tgcon)
+    e1:SetTarget(s.tgtg)
+    e1:SetOperation(s.tgop)
+    c:RegisterEffect(e1)
+
+    -- Effect 2: If sent to GY, Set 1 "Altergeist" Trap (except itself) or "Personal Spoofing"
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_SET)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_TO_GRAVE)
+    e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+    e2:SetCountLimit(1,id)
+    e2:SetTarget(s.settg)
+    e2:SetOperation(s.setop)
+    c:RegisterEffect(e2)
+end
+
+s.listed_series={0x103}
+
+-- Effect 1: Tribute condition
+function s.cfilter(c,tp)
+    return c:IsSetCard(0x103) and c:IsPreviousControler(tp) and c:IsPreviousLocation(LOCATION_MZONE)
+end
+function s.tgcon(e,tp,eg,ep,ev,re,r,rp)
+    return eg:IsExists(s.cfilter,1,nil,tp)
+end
+function s.tgfilter(c)
+    return c:IsType(TYPE_TRAP) and c:IsAbleToGrave()
+end
+function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+end
+function s.tgop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+    local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
+    if #g>0 then
+        Duel.SendtoGrave(g,REASON_EFFECT)
+    end
+end
+
+-- Effect 2: Set target
+function s.setfilter(c)
+    return ((c:IsSetCard(0x103) and not c:IsCode(id)) or c:IsCode(53936268)) and c:IsSSetable()
+end
+function s.settg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.setfilter(chkc) end
+    if chk==0 then return Duel.IsExistingTarget(s.setfilter,tp,LOCATION_GRAVE,0,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+    local g=Duel.SelectTarget(tp,s.setfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,g,1,0,0)
+end
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc and tc:IsRelateToEffect(e) then
+        Duel.SSet(tp,tc)
+    end
+end
