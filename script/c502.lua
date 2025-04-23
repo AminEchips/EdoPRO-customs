277fa135f379b0a6900dd19a7a9a29def60bd46c
Chips <aminekhoury0411@gmail.com>
2025-04-23 17:21:37 -0400

    Update c502.lua

--- script/c502.lua
+++ script/c502.lua
@@ -22,6 +22,7 @@ function s.initial_effect(c)
     e2:SetTarget(s.sptg)
     e2:SetOperation(s.spop)
     c:RegisterEffect(e2)
+
 end
 
 s.listed_series={0x103}
@@ -49,27 +50,21 @@ function s.spcon(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     local seq=c:GetSequence()
     local zone=0
-    local g=Duel.GetMatchingGroup(Card.IsSetCard,tp,LOCATION_MZONE,0,nil,0x103)
-    for tc in aux.Next(g) do
+    for tc in aux.Next(Duel.GetMatchingGroup(Card.IsSetCard,tp,LOCATION_MZONE,0,nil,0x103)) do
         if tc:IsType(TYPE_LINK) then
-            zone = zone | tc:GetLinkedZone()
+            zone=zone | tc:GetLinkedZone()
         end
     end
-    return bit.extract(zone, seq)~=0
-end
-
-function s.samecodefilter(c,code)
-    return c:IsCode(code)
-end
-
-function s.setfilter(c,tp)
-    return c:IsSetCard(0x103) and c:IsType(TYPE_LINK)
-        and c:IsCanBeSpecialSummoned(nil,0,tp,false,false)
-        and not Duel.IsExistingMatchingCard(s.samecodefilter,tp,LOCATION_MZONE,0,1,nil,c:GetCode())
+    return bit.extract(zone,seq)~=0
 end
 
 function s.spfilter(c)
     return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToGrave()
+end
+
+function s.linkfilter(c,code)
+    return c:IsSetCard(0x103) and c:IsType(TYPE_LINK) and c:IsCanBeSpecialSummoned(nil,0,tp,false,false)
+        and not Duel.IsExistingMatchingCard(function(tc) return tc:IsCode(code) end,tp,LOCATION_MZONE,0,1,nil)
 end
 
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
@@ -77,11 +72,13 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
     if chk==0 then
         return Duel.IsExistingTarget(s.spfilter,tp,LOCATION_SZONE,0,1,nil)
             and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-            and Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_GRAVE,0,1,nil,tp)
+            and Duel.IsExistingMatchingCard(function(c)
+                return c:IsSetCard(0x103) and c:IsType(TYPE_LINK) and Duel.IsPlayerCanSpecialSummon(tp)
+            end,tp,LOCATION_GRAVE,0,1,nil)
     end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-    local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_SZONE,0,1,1,nil)
-    Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,1,0,0)
+    Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_SZONE,0,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_SZONE)
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
 
@@ -89,12 +86,16 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
     local tc=Duel.GetFirstTarget()
     if tc and tc:IsRelateToEffect(e) and Duel.SendtoGrave(tc,REASON_EFFECT)~=0 then
         Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-        local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_GRAVE,0,1,1,nil,tp)
+        local g=Duel.SelectMatchingCard(tp,function(c)
+            return c:IsSetCard(0x103) and c:IsType(TYPE_LINK) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+                and not Duel.IsExistingMatchingCard(function(tc) return tc:IsCode(c:GetCode()) end,tp,LOCATION_MZONE,0,1,nil)
+        end,tp,LOCATION_GRAVE,0,1,1,nil)
         if #g>0 then
             Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
         end
     end
 end
+
 
 
 
