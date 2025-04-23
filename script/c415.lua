5fd359a079ca5189f03118d94740a88d7c6fca38
Chips <aminekhoury0411@gmail.com>
2025-04-22 20:25:31 -0400

    Update c415.lua

--- script/c415.lua
+++ script/c415.lua
@@ -1,14 +1,10 @@
 --Starry Knight Mikael
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Fusion materials
-    c:EnableReviveLimit()
-    Fusion.AddProcMix(c,true,true,s.matfilter1,s.matfilter2)
-
-    -- (Quick Effect) Discard to Fusion Summon
+    -- Quick Effect: Discard to Fusion Summon
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e1:SetType(EFFECT_TYPE_QUICK_O)
     e1:SetCode(EVENT_FREE_CHAIN)
     e1:SetRange(LOCATION_HAND)
@@ -18,7 +14,7 @@ function s.initial_effect(c)
     e1:SetOperation(s.fsop)
     c:RegisterEffect(e1)
 
-    -- GY Effect: Summon this when a L7 LIGHT Dragon leaves field
+    -- GY Effect: Special Summon and Banish when leaves field
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
     e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -33,63 +29,40 @@ function s.initial_effect(c)
     c:RegisterEffect(e2)
 end
 
--- Fusion materials
-function s.matfilter1(c)
-    return c:IsRace(RACE_FAIRY) and c:IsAttribute(ATTRIBUTE_LIGHT)
-end
-function s.matfilter2(c)
-    return c:IsRace(RACE_FAIRY) and c:IsAttribute(ATTRIBUTE_LIGHT)
-end
+s.listed_series={0x15b}
 
--- Fusion Summon (discard self)
+-- Fusion Summon Setup
 function s.fscost(e,tp,eg,ep,ev,re,r,rp,chk)
-    local c=e:GetHandler()
-    if chk==0 then return c:IsDiscardable() end
-    Duel.SendtoGrave(c,REASON_COST+REASON_DISCARD)
+    if chk==0 then return e:GetHandler():IsDiscardable() end
+    Duel.SendtoGrave(e:GetHandler(),REASON_COST+REASON_DISCARD)
 end
 function s.fstg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then
-        local chkf=tp
-        local mg=Duel.GetFusionMaterial(tp)
-        return Duel.IsExistingMatchingCard(s.fsfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg,chkf)
-    end
+    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFusionSummonable,tp,LOCATION_EXTRA,0,1,nil,nil) end
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
-function s.fsfilter(c,e,tp,mg,chkf)
-    return c:IsType(TYPE_FUSION) and c:IsRace(RACE_FAIRY) and c:IsAttribute(ATTRIBUTE_LIGHT)
-        and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
-        and Duel.GetLocationCountFromEx(tp,tp,nil,c)>0
-        and aux.FCheckMixN(c,true,s.matfilter1,1,s.matfilter2,1)(c,mg,nil,chkf)
-end
 function s.fsop(e,tp,eg,ep,ev,re,r,rp)
-    local chkf=tp
-    local mg=Duel.GetFusionMaterial(tp)
-    local sg=Duel.GetMatchingGroup(s.fsfilter,tp,LOCATION_EXTRA,0,nil,e,tp,mg,chkf)
-    if #sg>0 then
+    local g=Duel.GetMatchingGroup(Card.IsFusionSummonable,tp,LOCATION_EXTRA,0,nil,nil)
+    if #g>0 then
         Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-        local sc=sg:Select(tp,1,1,nil):GetFirst()
-        if sc then
-            local mat=aux.SelectFusionMaterial(tp,sc,mg,nil,chkf)
-            sc:SetMaterial(mat)
-            Duel.SendtoGrave(mat,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
-            Duel.BreakEffect()
-            Duel.SpecialSummon(sc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
-            sc:CompleteProcedure()
+        local sg=g:Select(tp,1,1,nil)
+        local tc=sg:GetFirst()
+        if tc then
+            Duel.FusionSummon(tp,tc,nil)
         end
     end
 end
 
--- GY Condition: L7 LIGHT Dragon leaves field by effect on opponent's turn
-function s.spfilter1(c,tp)
-    return c:IsPreviousControler(tp) and c:IsReason(REASON_EFFECT)
+-- GY Condition
+function s.cfilter(c,tp)
+    return c:IsPreviousControler(tp) and c:IsPreviousPosition(POS_FACEUP)
         and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsLevel(7) and c:IsRace(RACE_DRAGON)
+        and c:IsReason(REASON_EFFECT)
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-    return Duel.GetTurnPlayer()~=tp and eg:IsExists(s.spfilter1,1,nil,tp)
+    return eg:IsExists(s.cfilter,1,nil,tp)
 end
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
-    Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
+    return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil,REASON_COST)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
@@ -98,16 +71,13 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
-    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-    if c:IsRelateToEffect(e) then
-        Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
-        -- Banish it if it leaves field
+    if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
         local e1=Effect.CreateEffect(c)
         e1:SetType(EFFECT_TYPE_SINGLE)
         e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
         e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
         e1:SetReset(RESET_EVENT+RESETS_REDIRECT)
         e1:SetValue(LOCATION_REMOVED)
-        c:RegisterEffect(e1)
+        c:RegisterEffect(e1,true)
     end
 end
