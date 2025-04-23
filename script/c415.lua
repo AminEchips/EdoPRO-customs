a19a3c8a338b9f3b730f99ec6b0bf2eba7e64e58
Chips <aminekhoury0411@gmail.com>
2025-04-22 20:31:59 -0400

    Update c415.lua

--- script/c415.lua
+++ script/c415.lua
@@ -4,14 +4,14 @@ function s.initial_effect(c)
     -- Quick Effect: Discard to Fusion Summon
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
     e1:SetType(EFFECT_TYPE_QUICK_O)
     e1:SetCode(EVENT_FREE_CHAIN)
     e1:SetRange(LOCATION_HAND)
     e1:SetCountLimit(1,id)
     e1:SetCost(s.fscost)
-    e1:SetTarget(s.fstg)
-    e1:SetOperation(s.fsop)
+    e1:SetTarget(s.fustg)
+    e1:SetOperation(s.fusop)
     c:RegisterEffect(e1)
 
     -- GY Effect: Special Summon and Banish when leaves field
@@ -36,20 +36,33 @@ function s.fscost(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return e:GetHandler():IsDiscardable() end
     Duel.SendtoGrave(e:GetHandler(),REASON_COST+REASON_DISCARD)
 end
-function s.fstg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFusionSummonable,tp,LOCATION_EXTRA,0,1,nil,nil) end
+function s.fusfilter(c,e,tp,mg,chkf)
+    return c:IsType(TYPE_FUSION) and c:IsAttribute(ATTRIBUTE_LIGHT)
+        and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
+        and c:CheckFusionMaterial(mg,nil,chkf)
+end
+function s.fustg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local chkf=tp
+    local mg=Duel.GetFusionMaterial(tp)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.fusfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg,chkf) end
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
-function s.fsop(e,tp,eg,ep,ev,re,r,rp)
-    local g=Duel.GetMatchingGroup(Card.IsFusionSummonable,tp,LOCATION_EXTRA,0,nil,nil)
-    if #g>0 then
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-        local sg=g:Select(tp,1,1,nil)
-        local tc=sg:GetFirst()
-        if tc then
-            Duel.FusionSummon(tp,tc,nil)
-        end
-    end
+function s.fusop(e,tp,eg,ep,ev,re,r,rp)
+    local chkf=tp
+    local mg=Duel.GetFusionMaterial(tp)
+    local sg=Duel.GetMatchingGroup(s.fusfilter,tp,LOCATION_EXTRA,0,nil,e,tp,mg,chkf)
+    if #sg==0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local tc=sg:Select(tp,1,1,nil):GetFirst()
+    if not tc then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FMATERIAL)
+    local mat=Duel.SelectFusionMaterial(tp,tc,mg,nil,chkf)
+    if not mat or #mat==0 then return end
+    tc:SetMaterial(mat)
+    Duel.SendtoGrave(mat,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
+    Duel.BreakEffect()
+    Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
+    tc:CompleteProcedure()
 end
 
 -- GY Condition
