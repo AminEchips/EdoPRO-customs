9063fd8e5827c7a156b03a6fb07bdfed46b84858
Chips <aminekhoury0411@gmail.com>
2025-04-23 16:51:31 -0400

    Update c502.lua

--- script/c502.lua
+++ script/c502.lua
@@ -1,74 +1,85 @@
 --Altergeist Abyzec
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Special Summon this card as a monster when an Altergeist is Special Summoned from the Extra Deck
+    --Activate and Special Summon this card as a monster
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-    e1:SetRange(LOCATION_SZONE)
-    e1:SetCountLimit(1,id)
-    e1:SetCondition(s.spcon)
-    e1:SetTarget(s.sptg)
-    e1:SetOperation(s.spop)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetTarget(s.target)
+    e1:SetOperation(s.activate)
     c:RegisterEffect(e1)
 
-    -- Send S/T to GY and Special Summon Altergeist Link
+    --If Summoned to a zone an "Altergeist" Link Monster points to
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
     e2:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON)
     e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e2:SetProperty(EFFECT_FLAG_DELAY)
     e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-    e2:SetCondition(s.lkcon)
-    e2:SetTarget(s.lktg)
-    e2:SetOperation(s.lkop)
+    e2:SetCondition(s.spcon)
+    e2:SetTarget(s.sptg)
+    e2:SetOperation(s.spop)
     c:RegisterEffect(e2)
 end
+
 s.listed_series={0x103}
 
-function s.spfilter(c,tp)
-    return c:IsSetCard(0x103) and c:IsSummonPlayer(tp) and c:IsLocation(LOCATION_MZONE) and c:IsType(TYPE_MONSTER)
-end
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-    return eg:IsExists(s.spfilter,1,nil,tp)
-end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then
-        return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-            and Duel.IsPlayerCanSpecialSummonMonster(tp,id,0x103,TYPES_EFFECT+TYPE_TUNER,0,1000,2,RACE_SPELLCASTER,ATTRIBUTE_WATER)
-    end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.IsPlayerCanSpecialSummonMonster(tp,id,0x103,TYPES_EFFECT+TYPE_MONSTER+TYPE_TUNER,0,1000,2,RACE_SPELLCASTER,ATTRIBUTE_WATER) end
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     if not c:IsRelateToEffect(e) then return end
     if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-    if Duel.IsPlayerCanSpecialSummonMonster(tp,id,0x103,TYPES_EFFECT+TYPE_TUNER,0,1000,2,RACE_SPELLCASTER,ATTRIBUTE_WATER) then
-        c:AddMonsterAttribute(TYPE_EFFECT+TYPE_TUNER)
+    if Duel.IsPlayerCanSpecialSummonMonster(tp,id,0x103,TYPES_EFFECT+TYPE_MONSTER+TYPE_TUNER,0,1000,2,RACE_SPELLCASTER,ATTRIBUTE_WATER) then
+        c:AddMonsterAttribute(TYPE_EFFECT+TYPE_MONSTER+TYPE_TUNER)
         Duel.SpecialSummon(c,0,tp,tp,true,false,POS_FACEUP)
     end
 end
 
-function s.lkcon(e,tp,eg,ep,ev,re,r,rp)
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
-    return c:IsSummonType(SUMMON_TYPE_SPECIAL) and c:IsLocation(LOCATION_MZONE)
+    local seq=c:GetSequence()
+    local zone=0
+    local g=Duel.GetMatchingGroup(Card.IsSetCard,tp,LOCATION_MZONE,0,nil,0x103)
+    for tc in aux.Next(g) do
+        if tc:IsType(TYPE_LINK) then
+            zone=zone|tc:GetLinkedZone()
+        end
+    end
+    return bit.extract(zone,seq)~=0
 end
-function s.lkfilter(c,tp)
-    return c:IsSetCard(0x103) and c:IsType(TYPE_LINK) and c:IsCanBeSpecialSummoned(nil,0,tp,false,false) and not Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_MZONE,0,1,nil,c:GetCode())
+
+function s.filter(c)
+    return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToGrave()
 end
-function s.lktg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(nil,tp,LOCATION_SZONE,0,1,nil)
-        and Duel.IsExistingMatchingCard(s.lkfilter,tp,LOCATION_GRAVE,0,1,nil,tp)
-        and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
+function s.spfilter(c,tp)
+    return c:IsSetCard(0x103) and c:IsType(TYPE_LINK) and c:IsCanBeSpecialSummoned(nil,0,tp,false,false) and not Duel.IsExistingMatchingCard(s.samecodefilter,tp,LOCATION_MZONE,0,1,nil,c:GetCode())
+end
+function s.samecodefilter(c,code)
+    return c:IsCode(code)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_SZONE,0,1,nil)
+        and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,tp) end
     Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_SZONE)
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
-function s.lkop(e,tp,eg,ep,ev,re,r,rp)
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-    local g=Duel.SelectMatchingCard(tp,nil,tp,LOCATION_SZONE,0,1,1,nil)
-    if #g==0 or Duel.SendtoGrave(g,REASON_EFFECT)==0 then return end
+    local tg=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_SZONE,0,1,1,nil)
+    if #tg==0 then return end
+    if Duel.SendtoGrave(tg,REASON_EFFECT)==0 then return end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-    local sc=Duel.SelectMatchingCard(tp,s.lkfilter,tp,LOCATION_GRAVE,0,1,1,nil,tp):GetFirst()
-    if sc then Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP) end
+    local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,tp)
+    local tc=g:GetFirst()
+    if tc then
+        Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+    end
 end
+
