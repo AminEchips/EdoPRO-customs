48aa7b68049261d71b40bfb61dc6200ae2581fb3
Chips <aminekhoury0411@gmail.com>
2025-04-23 20:21:44 -0400

    Update c503.lua

--- script/c503.lua
+++ script/c503.lua
@@ -7,11 +7,13 @@ function s.initial_effect(c)
     e1:SetCode(EVENT_FREE_CHAIN)
     c:RegisterEffect(e1)
 
-    -- Send 1 Trap from Deck to GY when Altergeist monster is Tributed
+    -- Send 1 Trap from Deck to GY when an Altergeist monster is Tributed
     local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
     e2:SetCategory(CATEGORY_TOGRAVE)
     e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
     e2:SetCode(EVENT_RELEASE)
+    e2:SetProperty(EFFECT_FLAG_DELAY)
     e2:SetRange(LOCATION_SZONE)
     e2:SetCountLimit(1,id)
     e2:SetCondition(s.tgcon)
@@ -21,7 +23,8 @@ function s.initial_effect(c)
 
     -- When sent to GY: Set 1 Altergeist Trap or Personal Spoofing
     local e3=Effect.CreateEffect(c)
-    e3:SetCategory(CATEGORY_LEAVE_GRAVE)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetCategory(CATEGORY_TOGRAVE+CATEGORY_LEAVE_GRAVE)
     e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e3:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
     e3:SetCode(EVENT_TO_GRAVE)
@@ -33,16 +36,16 @@ end
 
 s.listed_series={0x103}
 
--- e2: Trigger when an Altergeist monster is Tributed
-function s.tgcon(e,tp,eg,ep,ev,re,r,rp)
-    return eg:IsExists(function(c)
-        return c:IsPreviousControler(tp)
-            and c:IsSetCard(0x103)
-            and c:IsType(TYPE_MONSTER)
-    end,1,nil)
+-- Tribute detection condition (based on Diabolos/Rikka)
+function s.tgfilter_con(c,tp)
+    return c:IsPreviousSetCard(0x103) -- Altergeist
+        and c:IsPreviousLocation(LOCATION_MZONE)
+        and c:IsPreviousControler(tp)
+        and c:IsType(TYPE_MONSTER)
 end
-
-
+function s.tgcon(e,tp,eg,ep,ev,re,r,rp)
+    return eg:IsExists(s.tgfilter_con,1,nil,tp)
+end
 function s.tgfilter(c)
     return c:IsType(TYPE_TRAP) and c:IsAbleToGrave()
 end
@@ -58,7 +61,7 @@ function s.tgop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
--- e3: Set 1 Trap from GY
+-- GY set effect
 function s.setfilter(c)
     return ((c:IsSetCard(0x103) and not c:IsCode(id)) or c:IsCode(53936268)) and c:IsSSetable()
 end
