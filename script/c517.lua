f71db3fd57739420a6f7d53d584839a51c437692
Chips <aminekhoury0411@gmail.com>
2025-04-24 13:14:25 -0400

    Update c517.lua

--- script/c517.lua
+++ script/c517.lua
@@ -2,10 +2,9 @@
 local s,id=GetID()
 function s.initial_effect(c)
     c:EnableReviveLimit()
-    -- Synchro Summon procedure
-    Synchro.AddProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x103),1,1,aux.NonTuner(nil),1,99)
-
-    -- On Synchro Summon: Destroy up to # of your face-up cards, then draw if you banish 1 Trap from GY
+    aux.AddSynchroProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x103),aux.NonTuner(nil),1)
+    
+    -- On Synchro Summon: destroy up to the number of your face-up cards, then draw
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DRAW)
@@ -18,7 +17,7 @@ function s.initial_effect(c)
     e1:SetOperation(s.desop)
     c:RegisterEffect(e1)
 
-    -- If Tributed and sent to GY: Special Summon this card
+    -- If Tributed and sent to the GY: Special Summon itself
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
     e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -26,27 +25,25 @@ function s.initial_effect(c)
     e2:SetCode(EVENT_RELEASE)
     e2:SetProperty(EFFECT_FLAG_DELAY)
     e2:SetCountLimit(1,{id,1})
-    e2:SetCondition(s.spcon)
     e2:SetTarget(s.sptg)
     e2:SetOperation(s.spop)
     c:RegisterEffect(e2)
 end
-
 s.listed_series={0x103}
 
--- Condition: Only if Synchro Summoned
+-- Only if Synchro Summoned
 function s.descon(e,tp,eg,ep,ev,re,r,rp)
     return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO)
 end
 
--- Target cards to destroy and check draw condition
+-- Target destruction and setup optional draw
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
     local ct=Duel.GetMatchingGroupCount(Card.IsFaceup,tp,LOCATION_ONFIELD,0,nil)
     if chk==0 then return ct>0 and Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil) end
     Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,ct,1-tp,LOCATION_ONFIELD)
 end
 
--- Operation: destroy up to # of face-up cards you control, then optional draw if Trap banished
+-- Destroy + draw by banishing Trap from GY
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
     local ct=Duel.GetMatchingGroupCount(Card.IsFaceup,tp,LOCATION_ONFIELD,0,nil)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
@@ -54,8 +51,6 @@ function s.desop(e,tp,eg,ep,ev,re,r,rp)
     if #g>0 then
         Duel.Destroy(g,REASON_EFFECT)
     end
-    -- Optional: Banish 1 Trap to draw 1
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
     local tg=Duel.SelectMatchingCard(tp,aux.FilterFaceupFunction(Card.IsType,TYPE_TRAP),tp,LOCATION_GRAVE,0,1,1,nil)
     if #tg>0 and Duel.Remove(tg,POS_FACEUP,REASON_EFFECT)>0 then
         Duel.BreakEffect()
@@ -63,21 +58,13 @@ function s.desop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
--- Condition: This card was Tributed and sent to GY
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    return c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsReason(REASON_RELEASE)
-end
-
--- Target: Can it be summoned back?
+-- Revival if Tributed
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
     local c=e:GetHandler()
     if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
         and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,LOCATION_GRAVE)
 end
-
--- Operation: Special Summon itself from GY
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     if c:IsRelateToEffect(e) then
