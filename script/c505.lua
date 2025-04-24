84c96ccaf03a7216681980424ba978671c6f95cc
Chips <aminekhoury0411@gmail.com>
2025-04-24 11:34:44 -0400

    Update c505.lua

--- script/c505.lua
+++ script/c505.lua
@@ -1,101 +1,35 @@
---Altergeist Formatting
-local s,id=GetID()
-function s.initial_effect(c)
-    --Activate
-    local e0=Effect.CreateEffect(c)
-    e0:SetType(EFFECT_TYPE_ACTIVATE)
-    e0:SetCode(EVENT_FREE_CHAIN)
-    c:RegisterEffect(e0)
-
-    --Synchro Summon (Opponent's Turn)
-    local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,0))
-    e1:SetType(EFFECT_TYPE_QUICK_O)
-    e1:SetCode(EVENT_FREE_CHAIN)
-    e1:SetRange(LOCATION_SZONE)
-    e1:SetCountLimit(1,id)
-    e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E+TIMING_MAIN_END)
-    e1:SetCondition(function(e,tp) return Duel.GetTurnPlayer()~=tp end)
-    e1:SetTarget(s.sytg)
-    e1:SetOperation(s.syop)
-    c:RegisterEffect(e1)
-
-    --Ritual Summon (Either Main Phase)
-    local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,1))
-    e2:SetType(EFFECT_TYPE_QUICK_O)
-    e2:SetCode(EVENT_FREE_CHAIN)
-    e2:SetRange(LOCATION_SZONE)
-    e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
-    e2:SetCountLimit(1,{id,1})
-    e2:SetCondition(function(e,tp) return Duel.IsMainPhase() end)
-    e2:SetTarget(s.ritg)
-    e2:SetOperation(s.riop)
-    c:RegisterEffect(e2)
+-- Altergeist Formatting - Synchro Summon
+function s.synfilter(c,e,tp)
+    return c:IsType(TYPE_SYNCHRO) and c:IsRace(RACE_SPELLCASTER) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_SYNCHRO,tp,false,false)
 end
-
--- SYNCHRO SUMMON (basic valid summon)
+function s.mfilter1(c,syncard)
+    return c:IsCanBeSynchroMaterial(syncard)
+end
 function s.sytg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then
         return Duel.IsExistingMatchingCard(s.synfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp)
     end
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
-function s.synfilter(c,e,tp)
-    return c:IsType(TYPE_SYNCHRO) and c:IsRace(RACE_SPELLCASTER)
-        and Duel.GetLocationCountFromEx(tp,tp,nil,c)>0
-        and c:IsSpecialSummonable(SUMMON_TYPE_SYNCHRO)
-end
 function s.syop(e,tp,eg,ep,ev,re,r,rp)
-    local g=Duel.GetMatchingGroup(s.synfilter,tp,LOCATION_EXTRA,0,nil,e,tp)
-    if #g==0 then return end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-    local sc=g:Select(tp,1,1,nil):GetFirst()
-    if sc and Duel.SpecialSummon(sc,SUMMON_TYPE_SYNCHRO,tp,tp,false,false,POS_FACEUP)~=0 then
-        sc:CompleteProcedure()
-        -- Gain 500 ATK
-        local e1=Effect.CreateEffect(e:GetHandler())
-        e1:SetType(EFFECT_TYPE_SINGLE)
-        e1:SetCode(EFFECT_UPDATE_ATTACK)
-        e1:SetValue(500)
-        e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
-        sc:RegisterEffect(e1)
-    end
-end
-
--- RITUAL SUMMON
-function s.ritfilter(c)
-    return c:IsSetCard(0x103) and c:IsRitualMonster()
-end
-function s.matfilter(c)
-    return c:IsAbleToRelease() and (c:IsLevelAbove(1) or c:IsType(TYPE_LINK))
-end
-function s.checkritual(g,rc)
-    local lv=rc:GetLevel()
-    local sum=0
-    for tc in aux.Next(g) do
-        sum = sum + (tc:IsType(TYPE_LINK) and tc:GetLink() or tc:GetLevel())
-    end
-    return sum>=lv
-end
-function s.ritg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then 
-        return Duel.IsExistingMatchingCard(s.ritfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil)
-           and Duel.IsExistingMatchingCard(s.matfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,nil)
-    end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_GRAVE)
-end
-function s.riop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-    local rc=Duel.SelectMatchingCard(tp,s.ritfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil):GetFirst()
-    if not rc then return end
-    local mg=Duel.GetMatchingGroup(s.matfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,nil)
-    local g=aux.SelectUnselectGroup(mg,e,tp,1,#mg,s.checkritual,false,rc)
-    if not g then return end
-    rc:SetMaterial(g)
-    Duel.Release(g,REASON_EFFECT+REASON_MATERIAL+REASON_RITUAL)
-    Duel.SpecialSummon(rc,SUMMON_TYPE_RITUAL,tp,tp,false,true,POS_FACEUP)
-    rc:CompleteProcedure()
+    local g=Duel.SelectMatchingCard(tp,s.synfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+    local sc=g:GetFirst()
+    if not sc then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SMATERIAL)
+    local mg=Duel.GetMatchingGroup(s.mfilter1,tp,LOCATION_MZONE,0,nil,sc)
+    local mat=Duel.SelectSynchroMaterial(tp,sc,nil,mg)
+    if #mat==0 then return end
+    sc:SetMaterial(mat)
+    Duel.SendtoGrave(mat,REASON_MATERIAL+REASON_SYNCHRO)
     Duel.BreakEffect()
-    Duel.Draw(tp,1,REASON_EFFECT)
+    Duel.SpecialSummon(sc,SUMMON_TYPE_SYNCHRO,tp,tp,false,false,POS_FACEUP)
+    sc:CompleteProcedure()
+    -- Apply bonus ATK
+    local e1=Effect.CreateEffect(e:GetHandler())
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_UPDATE_ATTACK)
+    e1:SetValue(500)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+    sc:RegisterEffect(e1)
 end
