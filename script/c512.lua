27d4ff8ccde366bdca54df7799472b80e321e182
Chips <aminekhoury0411@gmail.com>
2025-04-23 21:55:22 -0400

    Update c512.lua

--- script/c512.lua
+++ script/c512.lua
@@ -1,112 +1,128 @@
 --Altergeist Harpydrive
 local s,id=GetID()
 function s.initial_effect(c)
-    c:EnableReviveLimit()
-    Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x103),2,99,s.matcheck)
-    
-    -- Set 1 Trap from hand, can activate this turn
-    local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,0))
-    e1:SetType(EFFECT_TYPE_IGNITION)
-    e1:SetRange(LOCATION_MZONE)
-    e1:SetCountLimit(1,id)
-    e1:SetCondition(s.setcon)
-    e1:SetTarget(s.settg)
-    e1:SetOperation(s.setop)
-    c:RegisterEffect(e1)
+	c:EnableReviveLimit()
+	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x103),2,99,s.lcheck)
+	
+	-- Store summon turn
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetCode(EFFECT_MATERIAL_CHECK)
+	e0:SetValue(function(e,c)
+		if c:IsSummonType(SUMMON_TYPE_LINK) then
+			c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1,Duel.GetTurnCount())
+		end
+	end)
+	c:RegisterEffect(e0)
 
-    -- If sent to GY: Choose 1 you control & 1 opponent controls; banish when leaves field
-    local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,1))
-    e2:SetCategory(CATEGORY_REMOVE)
-    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
-    e2:SetCode(EVENT_TO_GRAVE)
-    e2:SetCountLimit(1,{id,1})
-    e2:SetCondition(s.bancon)
-    e2:SetTarget(s.bantg)
-    e2:SetOperation(s.banop)
-    c:RegisterEffect(e2)
+	-- Set 1 Trap from hand (can be activated this turn)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_LEAVE_GRAVE) -- not a real category but placeholder
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.setcon)
+	e1:SetTarget(s.settg)
+	e1:SetOperation(s.setop)
+	c:RegisterEffect(e1)
 
-    -- GY: Banish to draw 1
-    local e3=Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(id,2))
-    e3:SetCategory(CATEGORY_DRAW)
-    e3:SetType(EFFECT_TYPE_IGNITION)
-    e3:SetRange(LOCATION_GRAVE)
-    e3:SetCountLimit(1,{id,2})
-    e3:SetCost(aux.bfgcost)
-    e3:SetTarget(s.drtg)
-    e3:SetOperation(s.drop)
-    c:RegisterEffect(e3)
+	-- If sent from field to GY: Banish both targets when they leave field
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_REMOVE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(s.bancon)
+	e2:SetTarget(s.bantg)
+	e2:SetOperation(s.banop)
+	c:RegisterEffect(e2)
+
+	-- GY: Draw 1
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_DRAW)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCountLimit(1,{id,2})
+	e3:SetCost(aux.bfgcost)
+	e3:SetTarget(s.drtg)
+	e3:SetOperation(s.drop)
+	c:RegisterEffect(e3)
 end
+
 s.listed_series={0x103}
 
--- Link Summon material check
-function s.matcheck(g,lc,sumtype,tp)
-    return g:IsExists(Card.IsType,1,nil,TYPE_LINK,lc,sumtype,tp)
+-- Link summon material check
+function s.lcheck(g,lc,sumtype,tp)
+	return g:IsExists(Card.IsType,1,nil,TYPE_LINK)
 end
 
--- e1: Set 1 Trap from hand and allow activation this turn
+-- Condition: Only during the turn it was Link Summoned
 function s.setcon(e,tp,eg,ep,ev,re,r,rp)
-    return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK) and e:GetHandler():IsSummonTurn()
+	local c=e:GetHandler()
+	return c:IsSummonType(SUMMON_TYPE_LINK) and c:GetFlagEffect(id)>0 and Duel.GetTurnCount()==c:GetFlagEffectLabel(id)
 end
+
+-- Set 1 Trap from hand
 function s.setfilter(c)
-    return c:IsType(TYPE_TRAP) and c:IsSSetable()
+	return c:IsType(TYPE_TRAP) and c:IsSSetable()
 end
 function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_HAND,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_HAND,0,1,nil) end
 end
 function s.setop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
-    local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_HAND,0,1,1,nil)
-    local tc=g:GetFirst()
-    if tc then
-        Duel.SSet(tp,tc)
-        local e1=Effect.CreateEffect(e:GetHandler())
-        e1:SetType(EFFECT_TYPE_SINGLE)
-        e1:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
-        e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
-        e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-        tc:RegisterEffect(e1)
-    end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_HAND,0,1,1,nil)
+	local tc=g:GetFirst()
+	if tc and Duel.SSet(tp,tc)>0 then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
+		e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+	end
 end
 
--- e2: Banish on leave field
+-- GY Trigger condition
 function s.bancon(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    return c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsSummonType(SUMMON_TYPE_LINK)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_ONFIELD)
 end
+
 function s.banfilter(c)
-    return c:IsFaceup() and c:IsAbleToRemove()
+	return c:IsOnField()
 end
 function s.bantg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.banfilter(chkc) end
-    if chk==0 then return Duel.IsExistingTarget(s.banfilter,tp,LOCATION_MZONE,0,1,nil)
-        and Duel.IsExistingMatchingCard(s.banfilter,tp,0,LOCATION_MZONE,1,nil) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-    Duel.SelectTarget(tp,s.banfilter,tp,LOCATION_MZONE,0,1,1,nil)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-    Duel.SelectTarget(tp,s.banfilter,tp,0,LOCATION_MZONE,1,1,nil)
+	if chkc then return chkc:IsOnField() end
+	if chk==0 then return Duel.IsExistingTarget(s.banfilter,tp,LOCATION_ONFIELD,0,1,nil)
+		and Duel.IsExistingTarget(s.banfilter,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g1=Duel.SelectTarget(tp,s.banfilter,tp,LOCATION_ONFIELD,0,1,1,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g2=Duel.SelectTarget(tp,s.banfilter,tp,0,LOCATION_ONFIELD,1,1,nil)
 end
 function s.banop(e,tp,eg,ep,ev,re,r,rp)
-    local tg=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
-    for tc in aux.Next(tg) do
-        local e1=Effect.CreateEffect(e:GetHandler())
-        e1:SetType(EFFECT_TYPE_SINGLE)
-        e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
-        e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-        e1:SetReset(RESET_EVENT+RESETS_REDIRECT)
-        e1:SetValue(LOCATION_REMOVED)
-        tc:RegisterEffect(e1,true)
-    end
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	for tc in aux.Next(g) do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_REDIRECT)
+		e1:SetValue(LOCATION_REMOVED)
+		tc:RegisterEffect(e1,true)
+	end
 end
 
--- e3: Banish from GY to draw 1
+-- GY: Draw 1
 function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
-    Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 end
 function s.drop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Draw(tp,1,REASON_EFFECT)
+	Duel.Draw(tp,1,REASON_EFFECT)
 end
