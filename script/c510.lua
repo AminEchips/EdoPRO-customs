71a5f1116994eb955c9a7a476cde68e781b0f2e4
Chips <aminekhoury0411@gmail.com>
2025-04-23 21:06:18 -0400

    Create c510.lua

---
+++ script/c510.lua
@@ -0,0 +1,79 @@
+--Altergeist Denobanshee
+local s,id=GetID()
+function s.initial_effect(c)
+    c:EnableReviveLimit()
+    Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x103),2,99)
+
+    -- Target 1 monster; it can't be destroyed by card effects
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+    e1:SetCondition(s.indcon)
+    e1:SetTarget(s.indtg)
+    e1:SetOperation(s.indop)
+    c:RegisterEffect(e1)
+
+    -- Banish from GY to summon a different Altergeist Link-4
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_TO_GRAVE)
+    e2:SetProperty(EFFECT_FLAG_DELAY)
+    e2:SetRange(LOCATION_GRAVE)
+    e2:SetCountLimit(1,id)
+    e2:SetCondition(s.spcon)
+    e2:SetCost(aux.bfgcost)
+    e2:SetTarget(s.sptg)
+    e2:SetOperation(s.spop)
+    c:RegisterEffect(e2)
+end
+
+function s.indcon(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
+end
+function s.indtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsLocation(LOCATION_MZONE) end
+    if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+    local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+end
+function s.indop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc and tc:IsRelateToEffect(e) then
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+        e1:SetValue(1)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+        tc:RegisterEffect(e1)
+    end
+end
+
+function s.cfilter(c,tp)
+    return c:IsSetCard(0x103) and c:IsType(TYPE_LINK) and c:IsControler(tp) and c:IsPreviousLocation(LOCATION_MZONE) and c:GetLink()>=4
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.IsBattlePhase() and eg:IsExists(s.cfilter,1,nil,tp)
+end
+function s.spfilter(c,code)
+    return c:IsSetCard(0x103) and c:IsType(TYPE_LINK) and c:GetLink()==4 and not c:IsCode(code)
+        and (c:IsLocation(LOCATION_EXTRA) or c:IsLocation(LOCATION_GRAVE))
+        and Duel.GetLocationCountFromEx(tp,tp,nil,c)>0 and c:IsCanBeSpecialSummoned(nil,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local code=eg:GetFirst():GetCode()
+    if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA+LOCATION_GRAVE,0,1,nil,code) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA+LOCATION_GRAVE)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    local code=eg:GetFirst():GetCode()
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_EXTRA+LOCATION_GRAVE,0,1,1,nil,code)
+    local tc=g:GetFirst()
+    if tc then
+        Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+    end
+end
