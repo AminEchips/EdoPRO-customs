0b2dca7dd0cf45576ff1f684385f7c8ddb501f1a
Chips <aminekhoury0411@gmail.com>
2025-04-24 19:14:29 -0400

    Update c518.lua

--- script/c518.lua
+++ script/c518.lua
@@ -14,13 +14,13 @@ function s.initial_effect(c)
     e1:SetValue(s.atkval)
     c:RegisterEffect(e1)
 
-    -- Destruction protection
+    -- Destruction replacement
     local e2=Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
     e2:SetCode(EFFECT_DESTROY_REPLACE)
     e2:SetRange(LOCATION_MZONE)
     e2:SetTarget(s.reptg)
-    e2:SetOperation(s.repop)
+    e2:SetValue(function(e,_c) return s.repfilter(_c,e:GetHandlerPlayer()) end)
     c:RegisterEffect(e2)
 
     -- Special Summon from GY
@@ -49,24 +49,20 @@ function s.atkval(e,c)
     return atk
 end
 
--- Protect Altergeist monsters from destruction by card effects
+-- Replace destruction of Altergeist monsters
 function s.repfilter(c,tp)
-    return c:IsControler(tp) and c:IsFaceup() and c:IsSetCard(0x103) and c:IsOnField() and not c:IsReason(REASON_REPLACE)
+    return c:IsFaceup() and c:IsSetCard(0x103) and c:IsControler(tp) and c:IsOnField() and c:IsReason(REASON_BATTLE+REASON_EFFECT) and not c:IsReason(REASON_REPLACE)
 end
 function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return eg:IsExists(s.repfilter,1,nil,tp) and e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_EFFECT) end
-    if Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
-        e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_EFFECT)
+    local c=e:GetHandler()
+    if chk==0 then return eg:IsExists(s.repfilter,1,nil,tp) and c:CheckRemoveOverlayCard(tp,1,REASON_EFFECT) end
+    if Duel.SelectEffectYesNo(tp,c,96) then
+        c:RemoveOverlayCard(tp,1,1,REASON_EFFECT)
         return true
-    else
-        return false
-    end
-end
-function s.repop(e,tp,eg,ep,ev,re,r,rp)
-    -- Nothing else needed; overlay removal handled above
+    else return false end
 end
 
--- Banish 2 Altergeist to revive self
+-- Banish 2 "Altergeist" to revive this card
 function s.cfilter(c)
     return c:IsSetCard(0x103) and c:IsAbleToRemove()
 end
@@ -77,15 +73,14 @@ function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
     local c=e:GetHandler()
-    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-        and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     if c:IsRelateToEffect(e) then
         Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
-        -- Banish when it leaves the field
+        -- Banish when leaves field
         local e1=Effect.CreateEffect(c)
         e1:SetType(EFFECT_TYPE_SINGLE)
         e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
