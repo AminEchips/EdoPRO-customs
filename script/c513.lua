dd37acb872e379cc968e37c5734d5a39da736cb0
Chips <aminekhoury0411@gmail.com>
2025-04-23 22:08:41 -0400

    Update c513.lua

--- script/c513.lua
+++ script/c513.lua
@@ -2,17 +2,18 @@
 local s,id=GetID()
 function s.initial_effect(c)
     c:EnableReviveLimit()
-    -- Link Summon
-    Link.AddProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x103),2,nil,s.lcheck)
-    
-    -- Extra attacks based on unique "Altergeist" Traps in GY
+    Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x103),2,nil,s.lcheck)
+
+    --Multi attack
     local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetCode(EFFECT_EXTRA_ATTACK)
-    e1:SetValue(s.extraval)
+    e1:SetCode(EFFECT_EXTRA_ATTACK_MONSTER)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetValue(s.mtval)
     c:RegisterEffect(e1)
 
-    -- Destroy a card if a Trap is sent to GY
+    --Destroy 1 card if a Trap is sent to GY (except Damage Step)
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,0))
     e2:SetCategory(CATEGORY_DESTROY)
@@ -26,73 +27,78 @@ function s.initial_effect(c)
     e2:SetOperation(s.desop)
     c:RegisterEffect(e2)
 
-    -- Set 1 Altergeist Trap when sent from field to GY
+    --Set Altergeist Trap from hand, field or GY when this card leaves field
     local e3=Effect.CreateEffect(c)
     e3:SetDescription(aux.Stringid(id,1))
+    e3:SetCategory(CATEGORY_TOHAND)
     e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e3:SetProperty(EFFECT_FLAG_DELAY)
-    e3:SetCode(EVENT_TO_GRAVE)
+    e3:SetCode(EVENT_LEAVE_FIELD)
     e3:SetCountLimit(1,{id,1})
-    e3:SetCondition(function(e) return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD) end)
     e3:SetTarget(s.settg)
     e3:SetOperation(s.setop)
     c:RegisterEffect(e3)
 end
 s.listed_series={0x103}
 
+-- Link check
 function s.lcheck(g,lc,sumtype,tp)
     return g:IsExists(Card.IsSetCard,1,nil,0x103)
 end
 
--- Extra attacks
-function s.extraval(e,c)
-    local g=Duel.GetMatchingGroup(Card.IsSetCard,c:GetControler(),LOCATION_GRAVE,0,nil,0x103)
-    local codes={}
+-- Multi-attack value
+function s.mtval(e,c)
+    local g=Duel.GetMatchingGroup(Card.IsType,e:GetHandlerPlayer(),LOCATION_GRAVE,0,nil,TYPE_TRAP)
+    local names={}
     local count=0
-    for tc in aux.Next(g) do
-        if tc:IsType(TYPE_TRAP) and not codes[tc:GetCode()] then
-            codes[tc:GetCode()] = true
-            count = count + 1
+    for tc in g:Iter() do
+        local code=tc:GetCode()
+        if not names[code] and tc:IsSetCard(0x103) then
+            names[code]=true
+            count=count+1
         end
     end
     return math.max(0,count-1)
 end
 
--- Destroy condition
+-- Trap sent to GY condition
 function s.cfilter(c,tp)
-    return c:IsType(TYPE_TRAP) and c:IsPreviousControler(tp) and not c:IsReason(REASON_REPLACE)
+    return c:IsType(TYPE_TRAP) and c:IsControler(tp)
 end
 function s.descon(e,tp,eg,ep,ev,re,r,rp)
-    return eg:IsExists(s.cfilter,1,nil,tp) and not Duel.CheckEvent(EVENT_DAMAGE_STEP)
+    return not Duel.IsDamageStep() and eg:IsExists(s.cfilter,1,nil,tp)
 end
-function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return chkc:IsOnField() and chkc:IsControler(1-tp) end
-    if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-    local g=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
-    Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,0,LOCATION_ONFIELD)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
-    local tc=Duel.GetFirstTarget()
-    if tc and tc:IsRelateToEffect(e) then
-        Duel.Destroy(tc,REASON_EFFECT)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+    local g=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+    if #g>0 then
+        Duel.Destroy(g,REASON_EFFECT)
     end
 end
 
--- Set trap
+-- Set Altergeist Trap when this card leaves field
 function s.setfilter(c)
-    return c:IsSetCard(0x103) and c:IsType(TYPE_TRAP) and c:IsSSetable()
+    return c:IsType(TYPE_TRAP) and c:IsSetCard(0x103) and c:IsSSetable()
 end
 function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then
-        return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_HAND+LOCATION_GRAVE+LOCATION_ONFIELD,0,1,nil)
-    end
-    Duel.SetOperationInfo(0,0,nil,1,tp,LOCATION_HAND+LOCATION_GRAVE+LOCATION_ONFIELD)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_HAND+LOCATION_GRAVE+LOCATION_ONFIELD,0,1,nil) end
 end
 function s.setop(e,tp,eg,ep,ev,re,r,rp)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
     local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_HAND+LOCATION_GRAVE+LOCATION_ONFIELD,0,1,1,nil)
-    if #g>0 then
-        Duel.SSet(tp,g)
+    local tc=g:GetFirst()
+    if tc then
+        Duel.SSet(tp,tc)
+        -- Allow activation this turn
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
+        e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+        tc:RegisterEffect(e1)
     end
 end
