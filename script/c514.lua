ec2627ae6eb1dd328f74969db9ecba8ad970760f
Chips <aminekhoury0411@gmail.com>
2025-04-23 23:50:22 -0400

    Create c514.lua

---
+++ script/c514.lua
@@ -0,0 +1,93 @@
+--Altergeist Codecuseleus
+local s,id=GetID()
+function s.initial_effect(c)
+    c:EnableReviveLimit()
+    Fusion.AddProcMix(c,true,true,s.matfilter1,s.matfilter2)
+
+    --Cannot be destroyed by battle
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetValue(1)
+    c:RegisterEffect(e1)
+
+    --Negate and destroy
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+    e2:SetType(EFFECT_TYPE_QUICK_O)
+    e2:SetCode(EVENT_CHAINING)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1,id)
+    e2:SetCondition(s.negcon)
+    e2:SetCost(s.negcost)
+    e2:SetTarget(s.negtg)
+    e2:SetOperation(s.negop)
+    c:RegisterEffect(e2)
+
+    --Add or Special Summon Level 1 Spellcaster
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e3:SetCode(EVENT_TO_GRAVE)
+    e3:SetProperty(EFFECT_FLAG_DELAY)
+    e3:SetCountLimit(1,{id,1})
+    e3:SetTarget(s.thtg)
+    e3:SetOperation(s.thop)
+    c:RegisterEffect(e3)
+end
+
+-- Fusion Materials
+function s.matfilter1(c,fc,sumtype,tp)
+    return c:IsSetCard(0x103,fc,sumtype,tp)
+end
+function s.matfilter2(c,fc,sumtype,tp)
+    return c:IsRace(RACE_SPELLCASTER,fc,sumtype,tp) or c:IsType(TYPE_LINK,fc,sumtype,tp)
+end
+
+-- Negate
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+    return re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) and Duel.IsChainNegatable(ev)
+        and re:IsActiveType(TYPE_SPELL+TYPE_TRAP+TYPE_MONSTER) and re:IsHasCategory(CATEGORY_DESTROY)
+end
+function s.negcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.CheckReleaseGroup(tp,Card.IsSetCard,1,nil,0x103) end
+    local g=Duel.SelectReleaseGroup(tp,Card.IsSetCard,1,1,nil,0x103)
+    Duel.Release(g,REASON_COST)
+end
+function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+    Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+end
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+        Duel.Destroy(eg,REASON_EFFECT)
+    end
+end
+
+-- Add/Summon Level 1 Spellcaster
+function s.thfilter(c,e,tp)
+    return c:IsLevel(1) and c:IsRace(RACE_SPELLCASTER) and (c:IsAbleToHand() or c:IsCanBeSpecialSummoned(e,0,tp,false,false))
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+    local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+    local tc=g:GetFirst()
+    if tc then
+        if tc:IsCanBeSpecialSummoned(e,0,tp,false,false) and (not tc:IsAbleToHand() or Duel.SelectYesNo(tp,aux.Stringid(id,2))) then
+            Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+        else
+            Duel.SendtoHand(tc,nil,REASON_EFFECT)
+            Duel.ConfirmCards(1-tp,tc)
+        end
+    end
+end
