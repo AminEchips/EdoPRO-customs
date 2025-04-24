2d8ac42eb112d7d312748f36ce9a7787bc4c638b
Chips <aminekhoury0411@gmail.com>
2025-04-24 12:11:16 -0400

    Update c505.lua

--- script/c505.lua
+++ script/c505.lua
@@ -1,32 +1,34 @@
 --Altergeist Formatting
 local s,id=GetID()
 function s.initial_effect(c)
-    --Activate
+    -- Activate this card
     local e0=Effect.CreateEffect(c)
     e0:SetType(EFFECT_TYPE_ACTIVATE)
     e0:SetCode(EVENT_FREE_CHAIN)
     c:RegisterEffect(e0)
 
-    --Synchro Summon during opponent's turn
+    -- Quick Effect: Synchro Summon during opponent's turn
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e1:SetType(EFFECT_TYPE_QUICK_O)
     e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetHintTiming(0,TIMING_MAIN_END)
     e1:SetRange(LOCATION_SZONE)
-    e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E+TIMING_MAIN_END)
     e1:SetCountLimit(1,id)
     e1:SetCondition(function(e,tp) return Duel.GetTurnPlayer()~=tp end)
     e1:SetTarget(s.syntg)
     e1:SetOperation(s.synop)
     c:RegisterEffect(e1)
 
-    --Ritual Summon during either Main Phase
+    -- Quick Effect: Ritual Summon during either Main Phase
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DRAW)
     e2:SetType(EFFECT_TYPE_QUICK_O)
     e2:SetCode(EVENT_FREE_CHAIN)
+    e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
     e2:SetRange(LOCATION_SZONE)
-    e2:SetHintTiming(TIMING_MAIN_END,TIMING_MAIN_END)
     e2:SetCountLimit(1,{id,1})
     e2:SetCondition(function(e,tp) return Duel.IsMainPhase() end)
     e2:SetTarget(s.rittg)
@@ -34,76 +36,79 @@ function s.initial_effect(c)
     c:RegisterEffect(e2)
 end
 
--- Synchro Summon logic
--- Valid Synchro Monster Filter
-function s.synfilter(c)
-    return c:IsType(TYPE_SYNCHRO) and c:IsRace(RACE_SPELLCASTER)
-        and Duel.IsExistingMatchingCard(Card.IsCanBeSynchroMaterial,tp,LOCATION_MZONE,0,1,nil,c)
-        and Duel.IsPlayerCanSpecialSummon(tp)
-end
-
--- Synchro Summon Target
+-----------------------
+-- SYNCHRO SUMMON EFFECT
+-----------------------
 function s.syntg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then
-        return Duel.IsExistingMatchingCard(s.synfilter,tp,LOCATION_EXTRA,0,1,nil)
-    end
+    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsSynchroSummonable,tp,LOCATION_EXTRA,0,1,nil,nil) end
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
-
--- Synchro Summon Operation
 function s.synop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-    local g=Duel.SelectMatchingCard(tp,s.synfilter,tp,LOCATION_EXTRA,0,1,1,nil)
+    local g=Duel.SelectMatchingCard(tp,Card.IsSynchroSummonable,tp,LOCATION_EXTRA,0,1,1,nil,nil)
     local sc=g:GetFirst()
-    if not sc then return end
-    Duel.SynchroSummon(tp,sc,nil)
-    if sc:IsFaceup() then
-        local e1=Effect.CreateEffect(e:GetHandler())
-        e1:SetType(EFFECT_TYPE_SINGLE)
-        e1:SetCode(EFFECT_UPDATE_ATTACK)
-        e1:SetValue(500)
-        e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
-        sc:RegisterEffect(e1)
+    if sc then
+        Duel.SynchroSummon(tp,sc,nil)
+        -- Give 500 ATK
+        if sc:IsFaceup() then
+            local e1=Effect.CreateEffect(e:GetHandler())
+            e1:SetType(EFFECT_TYPE_SINGLE)
+            e1:SetCode(EFFECT_UPDATE_ATTACK)
+            e1:SetValue(500)
+            e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+            sc:RegisterEffect(e1)
+        end
     end
 end
 
--- Ritual Summon logic
+-----------------------
+-- RITUAL SUMMON EFFECT
+-----------------------
 function s.ritfilter(c)
-    return c:IsSetCard(0x103) and c:IsRitualMonster()
+    return c:IsSetCard(0x103) and c:IsRitualMonster() and c:IsCanBeSpecialSummoned(nil,SUMMON_TYPE_RITUAL,tp,false,true)
 end
 function s.matfilter(c)
-    return c:IsAbleToGrave() and (c:IsLevelAbove(1) or c:IsType(TYPE_LINK))
+    return (c:IsLevelAbove(1) or c:IsType(TYPE_LINK)) and c:IsReleasable()
 end
-function s.getLevelOrLink(c)
+function s.getMaterialValue(c)
     return c:IsType(TYPE_LINK) and c:GetLink() or c:GetLevel()
 end
 function s.rittg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then 
+    if chk==0 then
         return Duel.IsExistingMatchingCard(s.ritfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil)
             and Duel.IsExistingMatchingCard(s.matfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,nil)
     end
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_GRAVE)
 end
 function s.ritop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
     local rc=Duel.SelectMatchingCard(tp,s.ritfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil):GetFirst()
     if not rc then return end
+
     local lv=rc:GetLevel()
     local mg=Duel.GetMatchingGroup(s.matfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,nil)
-    aux.GCheckAdditional=function(g)
-        local sum=0
-        for tc in aux.Next(g) do
-            sum=sum+s.getLevelOrLink(tc)
-        end
-        return sum>=lv
-    end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-    local mat=mg:SelectSubGroup(tp,aux.TRUE,false,1,99)
-    aux.GCheckAdditional=nil
-    if not mat then return end
-    rc:SetMaterial(mat)
-    Duel.Release(mat,REASON_EFFECT+REASON_MATERIAL+REASON_RITUAL)
+
+    -- Select materials with sum >= ritual monster's level
+    local selected=aux.SelectUnselectGroup(mg,e,tp,nil,lv,
+        function(g,...) return s.getTotalLevel(g)>=lv end,
+        true,tp,HINTMSG_RELEASE,nil,nil,true)
+
+    if #selected==0 then return end
+
+    rc:SetMaterial(selected)
+    Duel.Release(selected,REASON_EFFECT+REASON_MATERIAL+REASON_RITUAL)
+    Duel.BreakEffect()
     Duel.SpecialSummon(rc,SUMMON_TYPE_RITUAL,tp,tp,false,true,POS_FACEUP)
     rc:CompleteProcedure()
+
+    -- Draw 1 if successful
     Duel.BreakEffect()
     Duel.Draw(tp,1,REASON_EFFECT)
+end
+
+function s.getTotalLevel(g)
+    local total=0
+    for tc in aux.Next(g) do
+        total=total+(tc:IsType(TYPE_LINK) and tc:GetLink() or tc:GetLevel())
+    end
+    return total
 end
