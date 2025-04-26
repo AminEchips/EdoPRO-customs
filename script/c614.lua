eeb0ae404feeff47b66f80a9e7778c0e3cf0bd12
Chips <aminekhoury0411@gmail.com>
2025-04-26 18:53:51 -0400

    Update c614.lua

--- script/c614.lua
+++ script/c614.lua
@@ -1,6 +1,6 @@
 --Blackwing - Puelche the Blazing
 local s,id=GetID()
-s.listed_series={SET_BLACKWING,0x33,0x1033}
+s.listed_series={SET_BLACKWING,0x33,0x1033} -- Blackwing, Black-Winged Dragon, Assault Blackwing
 function s.initial_effect(c)
     -- Synchro Summon procedure
     Synchro.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_DARK),1,1,Synchro.NonTuner(nil),1,99)
@@ -20,6 +20,7 @@ function s.initial_effect(c)
     e2:SetRange(LOCATION_MZONE)
     e2:SetTarget(s.reptg)
     e2:SetValue(s.repval)
+    e2:SetOperation(s.repop)
     c:RegisterEffect(e2)
     -- Special Summon Assault Blackwing + Level reduce + make Tuner
     local e3=Effect.CreateEffect(c)
@@ -28,6 +29,7 @@ function s.initial_effect(c)
     e3:SetType(EFFECT_TYPE_IGNITION)
     e3:SetRange(LOCATION_MZONE)
     e3:SetCountLimit(1,id)
+    e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
     e3:SetTarget(s.sptg)
     e3:SetOperation(s.spop)
     c:RegisterEffect(e3)
@@ -44,19 +46,25 @@ function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
     local g=eg:Filter(s.repfilter,nil,tp)
     if chk==0 then return #g>0 end
     if Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
-        -- Immediately change position here
-        for tc in aux.Next(g) do
-            if tc:IsFaceup() and tc:IsAttackPos() then
-                Duel.ChangePosition(tc,POS_FACEUP_DEFENSE)
-            end
-        end
+        e:SetLabelObject(g)
         return true
     else
         return false
     end
 end
 function s.repval(e,c)
-    return s.repfilter(c,c:GetControler())
+    local g=e:GetLabelObject()
+    return g and g:IsContains(c)
+end
+function s.repop(e,tp,eg,ep,ev,re,r,rp)
+    local g=e:GetLabelObject()
+    if not g then return end
+    for tc in aux.Next(g) do
+        if tc:IsFaceup() and tc:IsAttackPos() then
+            Duel.ChangePosition(tc,POS_FACEUP_DEFENSE)
+        end
+    end
+    g:DeleteGroup()
 end
 
 ----------------------------------------------------------
@@ -66,37 +74,39 @@ function s.spfilter(c,e,tp)
     return c:IsSetCard(0x1033) and c:IsLevelBelow(7) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.spfilter(chkc,e,tp) end
+    if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.spfilter(chkc,e,tp) end
     if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
         and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-    Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+    local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
     if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
     local tc=Duel.GetFirstTarget()
-    local c=e:GetHandler()
-    if tc and tc:IsRelateToEffect(e) and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE)>0 and c:IsFaceup() and c:IsRelateToEffect(e) then
-        -- Reduce this card's Level
-        local lv=tc:GetOriginalLevel()
-        if lv>0 then
-            local e1=Effect.CreateEffect(c)
-            e1:SetType(EFFECT_TYPE_SINGLE)
-            e1:SetCode(EFFECT_UPDATE_LEVEL)
-            e1:SetValue(-lv)
-            e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-            c:RegisterEffect(e1)
-        end
-        -- Optionally make the monster a Tuner
-        if Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
-            local e2=Effect.CreateEffect(tc)
-            e2:SetType(EFFECT_TYPE_SINGLE)
-            e2:SetCode(EFFECT_ADD_TYPE)
-            e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-            e2:SetValue(TYPE_TUNER)
-            e2:SetReset(RESET_EVENT+RESETS_STANDARD)
-            tc:RegisterEffect(e2)
+    if tc and tc:IsRelateToEffect(e) then
+        if Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE)>0 and c:IsFaceup() and c:IsRelateToEffect(e) then
+            -- Reduce Level
+            local lv=tc:GetOriginalLevel()
+            if lv>0 then
+                local e1=Effect.CreateEffect(c)
+                e1:SetType(EFFECT_TYPE_SINGLE)
+                e1:SetCode(EFFECT_UPDATE_LEVEL)
+                e1:SetValue(-lv)
+                e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+                c:RegisterEffect(e1)
+            end
+            -- Make Tuner?
+            if Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+                local e2=Effect.CreateEffect(tc)
+                e2:SetType(EFFECT_TYPE_SINGLE)
+                e2:SetCode(EFFECT_ADD_TYPE)
+                e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+                e2:SetValue(TYPE_TUNER)
+                e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+                tc:RegisterEffect(e2)
+            end
         end
     end
 end
