2e0e06c5d64d6923516865f690bff95cc2ea4ae6
Chips <aminekhoury0411@gmail.com>
2025-04-26 11:27:29 -0400

    Update c603.lua

--- script/c603.lua
+++ script/c603.lua
@@ -2,20 +2,19 @@
 local s,id=GetID()
 s.listed_series={0x33} -- Blackwing archetype
 function s.initial_effect(c)
-    -- Special Summon itself from hand
+    -- Special Summon itself from hand without activating
     local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_SPSUMMON_PROC)
+    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
     e1:SetRange(LOCATION_HAND)
     e1:SetCountLimit(1,id)
     e1:SetCondition(s.spcon)
     e1:SetTarget(s.sptg)
-    e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
-    -- Decrease Level on Normal/Special Summon
+    -- Decrease Level on Normal or Special Summon
     local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetDescription(aux.Stringid(id,0))
     e2:SetCategory(CATEGORY_LVCHANGE)
     e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e2:SetCode(EVENT_SUMMON_SUCCESS)
@@ -29,27 +28,25 @@ function s.initial_effect(c)
     c:RegisterEffect(e3)
 end
 
--- Special Summon Condition from hand
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-    return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_MZONE,0,1,nil)
-end
+----------------------------------------------------------
+-- Special Summon Procedure
+----------------------------------------------------------
 function s.spfilter(c)
     return c:IsSetCard(0x33) and not c:IsCode(id)
 end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-    local c=e:GetHandler()
-    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-        and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+function s.spcon(e,c)
+    if c==nil then return true end
+    local tp=c:GetControler()
+    return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_MZONE,0,1,nil)
 end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    if c:IsRelateToEffect(e) then
-        Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
-    end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
+    -- No cost needed, just fulfill condition
 end
 
--- Level Change on Summon
+----------------------------------------------------------
+-- Decrease Level Effect
+----------------------------------------------------------
 function s.lvfilter(c)
     return c:IsFaceup() and c:IsSetCard(0x33) and not c:IsCode(id)
 end
