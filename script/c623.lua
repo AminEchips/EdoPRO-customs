c6a47396b591f8c1e6e3be5e5c30e59ff01cf2cf
Chips <aminekhoury0411@gmail.com>
2025-04-27 19:31:20 -0400

    Update c623.lua

--- script/c623.lua
+++ script/c623.lua
@@ -1,6 +1,12 @@
 --Blackwing - Roost
 local s,id=GetID()
 function s.initial_effect(c)
+    -- Activate this card normally from hand
+    local e0=Effect.CreateEffect(c)
+    e0:SetType(EFFECT_TYPE_ACTIVATE)
+    e0:SetCode(EVENT_FREE_CHAIN)
+    c:RegisterEffect(e0)
+
     -- Burn when a monster is summoned (optional)
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
@@ -18,37 +24,41 @@ function s.initial_effect(c)
     e1b:SetCode(EVENT_SPSUMMON_SUCCESS)
     c:RegisterEffect(e1b)
 
-    -- Double ATK when Black-Winged Dragon or a monster that lists it becomes 0
+    -- Monitor ATK 0 continuously
     local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,1))
-    e2:SetCategory(CATEGORY_ATKCHANGE)
-    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
     e2:SetCode(EVENT_ADJUST)
-    e2:SetProperty(EFFECT_FLAG_DELAY)
     e2:SetRange(LOCATION_SZONE)
-    e2:SetCondition(s.dblcon)
-    e2:SetTarget(s.dbltg)
-    e2:SetOperation(s.dblop)
-    e2:SetCountLimit(1,id)
+    e2:SetOperation(s.checkop)
     c:RegisterEffect(e2)
 
-    -- Bounce when this leaves the field because of opponent
+    -- Double ATK trigger
     local e3=Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(id,2))
-    e3:SetCategory(CATEGORY_TOHAND)
-    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e3:SetProperty(EFFECT_FLAG_DELAY)
-    e3:SetCode(EVENT_LEAVE_FIELD)
-    e3:SetCondition(s.bouncecon)
-    e3:SetTarget(s.bouncetg)
-    e3:SetOperation(s.bounceop)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetCategory(CATEGORY_ATKCHANGE)
+    e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e3:SetCode(id) -- Custom event
+    e3:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+    e3:SetRange(LOCATION_SZONE)
+    e3:SetTarget(s.dbltg)
+    e3:SetOperation(s.dblop)
     c:RegisterEffect(e3)
+
+    -- Bounce opponent's monsters when this card leaves field
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,2))
+    e4:SetCategory(CATEGORY_TOHAND)
+    e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e4:SetProperty(EFFECT_FLAG_DELAY)
+    e4:SetCode(EVENT_LEAVE_FIELD)
+    e4:SetCondition(s.bouncecon)
+    e4:SetTarget(s.bouncetg)
+    e4:SetOperation(s.bounceop)
+    c:RegisterEffect(e4)
 end
 
-s.listed_names={9012916}
-
 -------------------------------------------------------
--- 1st effect: Burn
+-- Burn damage
 -------------------------------------------------------
 function s.damcon(e,tp,eg,ep,ev,re,r,rp)
     return eg:IsExists(Card.IsControler,1,nil,1-tp)
@@ -67,19 +77,23 @@ function s.damop(e,tp,eg,ep,ev,re,r,rp)
 end
 
 -------------------------------------------------------
--- 2nd effect: Double ATK
+-- Watch for ATK becoming 0
 -------------------------------------------------------
-function s.dblfilter(c)
-    return c:IsFaceup() and c:GetAttack()==0 and (c:IsCode(9012916) or c:ListsCode(9012916))
+function s.atk0filter(c)
+    return c:IsFaceup() and c:GetAttack()==0 and (c:IsCode(9012916) or c:ListsCode(9012916)) and not c:IsStatus(STATUS_BATTLE_DESTROYED)
 end
-function s.dblcon(e,tp,eg,ep,ev,re,r,rp)
-    return Duel.IsExistingMatchingCard(s.dblfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+    local g=Duel.GetMatchingGroup(s.atk0filter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+    if #g>0 then
+        Duel.RaiseEvent(e:GetHandler(),id,e,0,tp,tp,0)
+    end
 end
-function s.dbltg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.dblfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+
+function s.dbltg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsFaceup() and chkc:IsControler(tp) end
+    if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil) end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-    local g=Duel.SelectMatchingCard(tp,s.dblfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
-    Duel.SetTargetCard(g)
+    Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,nil)
 end
 function s.dblop(e,tp,eg,ep,ev,re,r,rp)
     local tc=Duel.GetFirstTarget()
@@ -97,14 +111,14 @@ function s.dblop(e,tp,eg,ep,ev,re,r,rp)
 end
 
 -------------------------------------------------------
--- 3rd effect: Bounce when leaves field
+-- Bounce when leaves field
 -------------------------------------------------------
 function s.bouncecon(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     return rp==1-tp and c:IsPreviousControler(tp) and c:IsPreviousLocation(LOCATION_ONFIELD)
 end
 function s.bouncetg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToHand,tp,0,LOCATION_MZONE,1,nil) end
+    if chk==0 then return Duel.IsExistingMatchingGroup(Card.IsAbleToHand,tp,0,LOCATION_MZONE,1,nil) end
     local g=Duel.GetMatchingGroup(Card.IsAbleToHand,tp,0,LOCATION_MZONE,nil)
     Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,#g,0,0)
 end
