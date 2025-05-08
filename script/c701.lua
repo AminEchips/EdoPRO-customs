735b42f68c7ac3211ef8cb39b08d598e279c7104
Chips <aminekhoury0411@gmail.com>
2025-05-08 07:43:12 -0400

    Update c701.lua

--- script/c701.lua
+++ script/c701.lua
@@ -1,21 +1,20 @@
 --Infernoble Knight Fiery Athen
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Quick Effect: Equip self to Warrior from hand/field
+    -- Quick Effect: Equip this card to a Warrior
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_EQUIP)
     e1:SetType(EFFECT_TYPE_QUICK_O)
     e1:SetCode(EVENT_FREE_CHAIN)
     e1:SetRange(LOCATION_HAND+LOCATION_MZONE)
     e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-    e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
+    e1:SetCategory(CATEGORY_EQUIP)
     e1:SetCountLimit(1,id)
-    e1:SetTarget(s.eqtg)
-    e1:SetOperation(s.eqop)
+    e1:SetTarget(s.equiptg)
+    e1:SetOperation(s.equipop)
     c:RegisterEffect(e1)
 
-    -- Draw if another monster is destroyed while equipped
+    -- Draw 1 if another monster is destroyed while this is equipped
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
     e2:SetCategory(CATEGORY_DRAW)
@@ -29,40 +28,45 @@ function s.initial_effect(c)
     c:RegisterEffect(e2)
 end
 
--- Equip Target: 1 face-up Warrior you control
+-- Equip to Warrior monster you control
 function s.eqfilter(c)
     return c:IsFaceup() and c:IsRace(RACE_WARRIOR)
 end
-function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.eqfilter(chkc) end
-    local c=e:GetHandler()
+function s.equiptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.eqfilter(chkc) end
     if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-        and Duel.IsExistingTarget(s.eqfilter,tp,LOCATION_MZONE,0,1,nil)
-        and c:IsType(TYPE_MONSTER) and not c:IsForbidden() end
+        and Duel.IsExistingTarget(s.eqfilter,tp,LOCATION_MZONE,0,1,nil) end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
     Duel.SelectTarget(tp,s.eqfilter,tp,LOCATION_MZONE,0,1,1,nil)
-    Duel.SetOperationInfo(0,CATEGORY_EQUIP,c,1,0,0)
+    Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
 end
-function s.eqop(e,tp,eg,ep,ev,re,r,rp)
+function s.equipop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
+    if not c:IsRelateToEffect(e) then return end
+    if c:IsLocation(LOCATION_MZONE) and c:IsFacedown() then return end
     local tc=Duel.GetFirstTarget()
-    if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 or not c:IsRelateToEffect(e) or not tc:IsRelateToEffect(e)
-        or tc:IsFacedown() or not tc:IsControler(tp) then return end
-    if not Duel.Equip(tp,c,tc) then return end
-    -- Equip limit
+    if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 or tc:IsFacedown() or not tc:IsRelateToEffect(e) then
+        Duel.SendtoGrave(c,REASON_EFFECT)
+        return
+    end
+    Duel.Equip(tp,c,tc,true)
+
+    -- Equip Limit
     local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_SINGLE)
     e1:SetCode(EFFECT_EQUIP_LIMIT)
-    e1:SetProperty(EFFECT_FLAG_COPY_INHERIT+EFFECT_FLAG_OWNER_RELATE)
-    e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-    e1:SetValue(function(e,tc) return tc==e:GetOwner() end)
+    e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+    e1:SetValue(s.eqlimit)
+    e1:SetLabelObject(tc)
     c:RegisterEffect(e1)
 end
+function s.eqlimit(e,c)
+    return c==e:GetLabelObject()
+end
 
--- Draw condition: a monster is destroyed by battle or effect, but not the equipped monster
+-- Draw effect when a monster is destroyed, excluding equipped monster
 function s.drcon(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    local ec=c:GetEquipTarget()
+    local ec=e:GetHandler():GetEquipTarget()
     return ec and eg:IsExists(function(tc) return tc:IsType(TYPE_MONSTER) and tc~=ec end,1,nil)
 end
 function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
