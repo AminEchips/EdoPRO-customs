3b2eb8e0624b3eff6e33bbb99f49430c047e9626
Chips <aminekhoury0411@gmail.com>
2025-05-08 08:53:52 -0400

    Update c719.lua

--- script/c719.lua
+++ script/c719.lua
@@ -1,10 +1,77 @@
+--Noble Knight Geraint
+local s,id=GetID()
+function s.initial_effect(c)
+    -- Effect 1: Discard to search Merlin, Lady, or Avalon
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCountLimit(1,id)
+    e1:SetCost(s.thcost)
+    e1:SetTarget(s.thtg)
+    e1:SetOperation(s.thop)
+    c:RegisterEffect(e1)
+
+    -- Effect 2: On Special Summon, empower LIGHT/DARK NK Synchro/Xyz
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_ATKCHANGE)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+    e2:SetCountLimit(1,{id,1})
+    e2:SetCondition(s.atkcon)
+    e2:SetTarget(s.atktg)
+    e2:SetOperation(s.atkop)
+    c:RegisterEffect(e2)
+end
+
+s.listed_series={0x107a} -- Noble Knight
+s.listed_names={03580032,10736540,82140600,24027078} -- Merlin, Lady, Avalon cards
+
+-- Effect 1: Discard to search
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return e:GetHandler():IsDiscardable() end
+    Duel.SendtoGrave(e:GetHandler(),REASON_COST+REASON_DISCARD)
+end
+function s.thfilter(c)
+    return c:IsCode(03580032,10736540,82140600,24027078) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
+    end
+end
+
+-- Effect 2: Condition to trigger on Special Summon
+function s.synxyzfilter(c)
+    return c:IsFaceup() and c:IsSetCard(0x107a)
+        and (c:IsType(TYPE_SYNCHRO) or c:IsType(TYPE_XYZ))
+        and (c:IsAttribute(ATTRIBUTE_LIGHT) or c:IsAttribute(ATTRIBUTE_DARK))
+end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.IsExistingMatchingCard(s.synxyzfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.synxyzfilter(chkc) end
+    if chk==0 then return Duel.IsExistingTarget(s.synxyzfilter,tp,LOCATION_MZONE,0,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+    Duel.SelectTarget(tp,s.synxyzfilter,tp,LOCATION_MZONE,0,1,1,nil)
+end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
     local tc=Duel.GetFirstTarget()
     if not (tc and tc:IsFaceup() and tc:IsRelateToEffect(e)) then return end
 
-    -- Schedule the second attack effect during next Battle Phase
+    -- Schedule the extra attack for next Battle Phase
     local e0=Effect.CreateEffect(e:GetHandler())
-    e0:SetDescription(aux.Stringid(id,2))
     e0:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
     e0:SetCode(EVENT_PHASE+PHASE_BATTLE_START)
     e0:SetCountLimit(1)
