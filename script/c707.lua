31770093ba08966130519f4588a142444d486c57
Chips <aminekhoury0411@gmail.com>
2025-05-08 09:19:13 -0400

    Update c707.lua

--- script/c707.lua
+++ script/c707.lua
@@ -1,24 +1,134 @@
--- Effect 3: Flip any monster on attack (this or equipped monster)
-local e3=Effect.CreateEffect(c)
-e3:SetDescription(aux.Stringid(id,2))
-e3:SetCategory(CATEGORY_POSITION)
-e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-e3:SetCode(EVENT_ATTACK_ANNOUNCE)
-e3:SetRange(LOCATION_MZONE+LOCATION_SZONE)
-e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
-e3:SetCountLimit(1,{id,2})
-e3:SetCondition(s.poscon)
-e3:SetTarget(s.postg)
-e3:SetOperation(s.posop)
-c:RegisterEffect(e3)
+--Infernoble Knight Commandant Marfisa
+local s,id=GetID()
+function s.initial_effect(c)
+    -- Synchro Summon procedure
+    Synchro.AddProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x107a),1,1,aux.FilterBoolFunction(Card.IsAttribute,ATTRIBUTE_FIRE),1,99)
+    c:EnableReviveLimit()
 
+    -- Effect 1: Equip FIRE from GY and change Level to 5
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_EQUIP+CATEGORY_LVCHANGE)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCountLimit(1,id)
+    e1:SetCondition(function(e) return e:GetHandler():GetLevel()==6 end)
+    e1:SetTarget(s.eqtg)
+    e1:SetOperation(s.eqop)
+    c:RegisterEffect(e1)
+
+    -- Effect 2: When this card becomes equipped, Synchro Summon + re-equip
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_BE_EQUIPEE)
+    e2:SetProperty(EFFECT_FLAG_DELAY)
+    e2:SetCountLimit(1,{id,1})
+    e2:SetTarget(s.syntg)
+    e2:SetOperation(s.synop)
+    c:RegisterEffect(e2)
+
+    -- Effect 3: Flip target when this or equipped monster attacks
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,2))
+    e3:SetCategory(CATEGORY_POSITION)
+    e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e3:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e3:SetRange(LOCATION_MZONE+LOCATION_SZONE)
+    e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e3:SetCountLimit(1,{id,2})
+    e3:SetCondition(s.poscon)
+    e3:SetTarget(s.postg)
+    e3:SetOperation(s.posop)
+    c:RegisterEffect(e3)
+end
+
+s.listed_series={0x107a}
+
+-- Equip FIRE monster from GY and reduce level
+function s.eqfilter(c)
+    return c:IsAttribute(ATTRIBUTE_FIRE) and c:IsType(TYPE_MONSTER) and not c:IsForbidden()
+end
+function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+        and Duel.IsExistingMatchingCard(s.eqfilter,tp,LOCATION_GRAVE,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_EQUIP,nil,1,tp,LOCATION_GRAVE)
+end
+function s.eqop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 or not c:IsRelateToEffect(e) then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+    local g=Duel.SelectMatchingCard(tp,s.eqfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+    local ec=g:GetFirst()
+    if ec and Duel.Equip(tp,ec,c) then
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_EQUIP_LIMIT)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+        e1:SetValue(function(e,c) return c==e:GetOwner():GetEquipTarget() end)
+        ec:RegisterEffect(e1)
+        -- Change level to 5
+        local e2=Effect.CreateEffect(c)
+        e2:SetType(EFFECT_TYPE_SINGLE)
+        e2:SetCode(EFFECT_CHANGE_LEVEL)
+        e2:SetValue(5)
+        e2:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+        c:RegisterEffect(e2)
+    end
+end
+
+-- Quick Synchro Summon (use this + other monsters, then optionally equip this from GY)
+function s.matfilter(c,sc,mc)
+    return c~=mc and c:IsCanBeSynchroMaterial(sc,mc)
+end
+function s.synfilter(c,e,tp,mc)
+    return c:IsType(TYPE_SYNCHRO) and c:IsRace(RACE_WARRIOR)
+        and Duel.IsExistingMatchingCard(s.matfilter,tp,LOCATION_MZONE,0,1,nil,c,mc)
+        and mc:IsCanBeSynchroMaterial(c)
+        and Duel.GetLocationCountFromEx(tp,tp,Group.FromCards(mc),c)>0
+end
+function s.syntg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then
+        local c=e:GetHandler()
+        return Duel.IsExistingMatchingCard(s.synfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,c)
+    end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function s.synop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if not c:IsRelateToEffect(e) or not c:IsControler(tp) then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,s.synfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,c)
+    local sc=g:GetFirst()
+    if not sc then return end
+    local mg=Duel.GetMatchingGroup(s.matfilter,tp,LOCATION_MZONE,0,nil,sc,c)
+    if #mg==0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SMATERIAL)
+    local m2=mg:Select(tp,1,1,nil)
+    m2:AddCard(c)
+    Duel.SetSynchroMaterial(m2)
+    if Duel.SynchroSummon(tp,sc,nil)==0 then return end
+
+    if c:IsLocation(LOCATION_GRAVE) and Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+        and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
+        Duel.Equip(tp,c,sc)
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_EQUIP_LIMIT)
+        e1:SetProperty(EFFECT_FLAG_COPY_INHERIT+EFFECT_FLAG_OWNER_RELATE)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+        e1:SetValue(function(e,c) return c==e:GetOwner() end)
+        c:RegisterEffect(e1)
+    end
+end
+
+-- Flip a monster face-down when this or equipped monster attacks
 function s.poscon(e,tp,eg,ep,ev,re,r,rp)
     local at=Duel.GetAttacker()
     local ec=e:GetHandler()
     return (ec:IsLocation(LOCATION_MZONE) and at==ec)
         or (ec:IsLocation(LOCATION_SZONE) and at:GetEquipGroup():IsContains(ec))
 end
-
 function s.postg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
     if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsCanTurnSet() end
     if chk==0 then return Duel.IsExistingTarget(Card.IsCanTurnSet,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
@@ -26,7 +136,6 @@ function s.postg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
     Duel.SelectTarget(tp,Card.IsCanTurnSet,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
     Duel.SetOperationInfo(0,CATEGORY_POSITION,nil,1,0,0)
 end
-
 function s.posop(e,tp,eg,ep,ev,re,r,rp)
     local tc=Duel.GetFirstTarget()
     if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsCanTurnSet() then
