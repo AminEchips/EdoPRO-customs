a8bda3edc0ee9d8411671f93bd9a25ad30a24c7b
Chips <aminekhoury0411@gmail.com>
2025-05-08 07:23:08 -0400

    Update c700.lua

--- script/c700.lua
+++ script/c700.lua
@@ -1,19 +1,19 @@
 --Infernoble Knight Prince Roy
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Normal Summon: Send Gwen to GY + Attribute change
+    -- Normal Summon: Equip Gwen and change Attribute
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_TOGRAVE)
+    e1:SetCategory(CATEGORY_EQUIP+CATEGORY_TOHAND+CATEGORY_SEARCH)
     e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e1:SetCode(EVENT_SUMMON_SUCCESS)
     e1:SetProperty(EFFECT_FLAG_DELAY)
     e1:SetCountLimit(1,id)
-    e1:SetTarget(s.gwytg)
-    e1:SetOperation(s.gwyop)
+    e1:SetTarget(s.eqtg)
+    e1:SetOperation(s.eqop)
     c:RegisterEffect(e1)
 
-    -- When targeted (attack or effect): Special Summon + optional equip and redirect
+    -- Quick Effect: Redirect if targeted
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
     e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_EQUIP)
@@ -27,56 +27,79 @@ function s.initial_effect(c)
     c:RegisterEffect(e2)
 end
 
--- IDs
+-- Gwenhwyfar ID
 s.listed_names={19748583}
-s.listed_series={0x507a,0x607a,0x149}
 
--- EFFECT 1: Send Gwen to GY + Attribute change
+-- EQUIP: Gwen from Deck
 function s.eqfilter(c)
-    return c:IsCode(19748583) and c:IsAbleToGrave()
+    return c:IsCode(19748583) and not c:IsForbidden()
 end
-function s.gwytg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then
-        return Duel.IsExistingMatchingCard(s.eqfilter,tp,LOCATION_DECK,0,1,nil)
+        return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+            and Duel.IsExistingMatchingCard(s.eqfilter,tp,LOCATION_DECK,0,1,nil)
     end
-    Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+    Duel.SetOperationInfo(0,CATEGORY_EQUIP,nil,1,tp,LOCATION_DECK)
 end
-function s.gwyop(e,tp,eg,ep,ev,re,r,rp)
+function s.eqop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
     local c=e:GetHandler()
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-    local tc=Duel.SelectMatchingCard(tp,s.eqfilter,tp,LOCATION_DECK,0,1,1,nil):GetFirst()
-    if tc and Duel.SendtoGrave(tc,REASON_EFFECT)~=0 and c:IsFaceup() and c:IsRelateToEffect(e) then
-        -- Change Attribute
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATTRIBUTE)
-        local att=Duel.AnnounceAttribute(tp,1,ATTRIBUTE_LIGHT+ATTRIBUTE_DARK)
-        local e1=Effect.CreateEffect(c)
-        e1:SetType(EFFECT_TYPE_SINGLE)
-        e1:SetCode(EFFECT_CHANGE_ATTRIBUTE)
-        e1:SetValue(att)
-        e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
-        c:RegisterEffect(e1)
+    if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+    local ec=Duel.SelectMatchingCard(tp,s.eqfilter,tp,LOCATION_DECK,0,1,1,nil):GetFirst()
+    if not ec then return end
+
+    -- Try to simulate Gwen's own Equip effect (as if it was used)
+    local effs={ec:GetCardEffect(75402014)} -- Standard Noble Arms equip effect
+    for _,te in ipairs(effs) do
+        local val=te:GetValue()
+        if not val or val(c,ec,tp) then
+            te:GetOperation()(ec,te:GetLabelObject(),tp,c)
+            goto equipped
+        end
     end
+
+    -- Fallback: force equip with manual Equip Limit
+    if Duel.Equip(tp,ec,c) then
+        local e1=Effect.CreateEffect(ec)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_EQUIP_LIMIT)
+        e1:SetProperty(EFFECT_FLAG_COPY_INHERIT+EFFECT_FLAG_OWNER_RELATE)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+        e1:SetValue(function(e,mc) return mc==e:GetOwner() end)
+        ec:RegisterEffect(e1)
+    end
+
+    ::equipped::
+    -- Change Attribute
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATTRIBUTE)
+    local att=Duel.AnnounceAttribute(tp,1,ATTRIBUTE_LIGHT+ATTRIBUTE_DARK)
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_CHANGE_ATTRIBUTE)
+    e2:SetValue(att)
+    e2:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+    c:RegisterEffect(e2)
 end
 
--- EFFECT 2: When targeted by attack or effect
+-- REDIRECT TRIGGER: when targeted by attack or card effect
 function s.tgcon(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    if not eg:IsContains(c) then return false end
-    -- For attack or effect
-    return (re~=nil or Duel.GetAttacker()==e:GetHandler())
+    return eg:IsContains(e:GetHandler())
 end
 function s.spfilter(c,e,tp)
     return c:IsRace(RACE_WARRIOR) and c:IsAttribute(ATTRIBUTE_FIRE) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.eqgyfilter(c)
+function s.eqfilter2(c)
     return (c:IsSetCard(0x507a) or (c:IsSetCard(0x607a) and c:IsType(TYPE_EQUIP))) and not c:IsForbidden()
 end
 function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then
         return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
             and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp)
+            and Duel.IsExistingMatchingCard(s.eqfilter2,tp,LOCATION_GRAVE,0,1,nil)
     end
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_GRAVE)
+    Duel.SetOperationInfo(0,CATEGORY_EQUIP,nil,1,tp,LOCATION_GRAVE)
 end
 function s.tgop(e,tp,eg,ep,ev,re,r,rp)
     if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
@@ -84,38 +107,14 @@ function s.tgop(e,tp,eg,ep,ev,re,r,rp)
     local sc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,e,tp):GetFirst()
     if not sc or Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)==0 then return end
 
-    -- If it's a "Roland" monster, equip from GY
-    if sc:IsSetCard(0x149) and Duel.IsExistingMatchingCard(s.eqgyfilter,tp,LOCATION_GRAVE,0,1,nil) and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 then
-        if Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
-            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-            local eqc=Duel.SelectMatchingCard(tp,s.eqgyfilter,tp,LOCATION_GRAVE,0,1,1,nil):GetFirst()
-            if eqc and Duel.Equip(tp,eqc,sc) then
-                -- Equip Limit
-                local e1=Effect.CreateEffect(eqc)
-                e1:SetType(EFFECT_TYPE_SINGLE)
-                e1:SetCode(EFFECT_EQUIP_LIMIT)
-                e1:SetProperty(EFFECT_FLAG_COPY_INHERIT+EFFECT_FLAG_OWNER_RELATE)
-                e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-                e1:SetValue(function(e,mc) return mc==e:GetOwner() end)
-                eqc:RegisterEffect(e1)
-            end
-        end
-    end
-
-    -- Redirect target if possible
-    if re and re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then
-        local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
-        if g and g:IsContains(e:GetHandler()) and sc:IsCanBeEffectTarget(re) then
-            if Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
-                Duel.ChangeTargetCard(ev,Group.FromCards(sc))
-            end
-        end
-    end
-
-    -- If it was an attack, redirect it
-    if Duel.GetAttacker() and Duel.GetAttackTarget()==e:GetHandler() and Duel.GetAttacker():IsControler(1-tp) and sc:IsAttackable() then
-        if Duel.SelectYesNo(tp,aux.Stringid(id,4)) then
-            Duel.CalculateDamage(Duel.GetAttacker(),sc)
+    Duel.BreakEffect()
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+    local ec=Duel.SelectMatchingCard(tp,s.eqfilter2,tp,LOCATION_GRAVE,0,1,1,nil):GetFirst()
+    if ec then
+        Duel.Equip(tp,ec,sc)
+        -- Optional redirect
+        if re and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+            Duel.ChangeTargetCard(ev,Group.FromCards(sc))
         end
     end
 end
