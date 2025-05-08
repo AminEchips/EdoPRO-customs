e0a060ec2f8472112f814923b3bd4a7c46a38925
Chips <aminekhoury0411@gmail.com>
2025-05-08 08:14:19 -0400

    Create c702.lua

---
+++ script/c702.lua
@@ -0,0 +1,88 @@
+--Infernoble Knight Commandant Charles
+local s,id=GetID()
+function s.initial_effect(c)
+    -- On Special Summon: add 2 "Noble Knight" cards from GY to hand
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_TOHAND)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1:SetProperty(EFFECT_FLAG_DELAY)
+    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e1:SetCountLimit(1,id)
+    e1:SetTarget(s.thtg)
+    e1:SetOperation(s.thop)
+    c:RegisterEffect(e1)
+
+    -- Tribute + Summon "Emperor Charles"
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1,{id,1})
+    e2:SetCost(s.spcost)
+    e2:SetTarget(s.sptg)
+    e2:SetOperation(s.spop)
+    c:RegisterEffect(e2)
+end
+
+s.listed_names={77656797}
+s.listed_series={0x107a} -- Noble Knight
+
+-- Effect 1: Add 2 "Noble Knight" cards from GY to hand
+function s.thfilter(c)
+    return c:IsSetCard(0x107a) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,2,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_GRAVE)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+    local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,2,2,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
+    end
+end
+
+-- Effect 2: Tribute + Summon "Emperor Charles"
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
+    Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
+end
+function s.trbfilter(c)
+    return c:IsFaceup() and c:IsType(TYPE_MONSTER) and c:GetEquipGroup():GetCount()>0 and c:IsReleasable()
+end
+function s.spfilter(c,e,tp)
+    return c:IsCode(77656797) and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCountFromEx(tp,tp,nil,TYPE_MONSTER)>0
+        and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA+LOCATION_GRAVE,0,1,nil,e,tp) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA+LOCATION_GRAVE)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local g=Duel.GetMatchingGroup(s.trbfilter,tp,LOCATION_MZONE,0,nil)
+    local ct=0
+    if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+        local rg=g:Select(tp,1,#g,nil)
+        ct=Duel.Release(rg,REASON_EFFECT)
+    end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local sc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_EXTRA+LOCATION_GRAVE,0,1,1,nil,e,tp):GetFirst()
+    if sc and Duel.SpecialSummon(sc,0,tp,tp,true,false,POS_FACEUP)>0 and ct==0 then
+        -- Negate its effects
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_DISABLE)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+        sc:RegisterEffect(e1)
+        local e2=Effect.CreateEffect(c)
+        e2:SetType(EFFECT_TYPE_SINGLE)
+        e2:SetCode(EFFECT_DISABLE_EFFECT)
+        e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+        sc:RegisterEffect(e2)
+    end
+end
