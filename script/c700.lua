2977e76f223835db5de92a781da8e5eaab8d2db0
Chips <aminekhoury0411@gmail.com>
2025-05-07 20:13:22 -0400

    Create c700.lua

---
+++ script/c700.lua
@@ -0,0 +1,90 @@
+--Infernoble Knight Prince Roy
+local s,id=GetID()
+function s.initial_effect(c)
+    --Equip Gwenhwyfar from Deck and change Attribute
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_EQUIP+CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_SUMMON_SUCCESS)
+    e1:SetProperty(EFFECT_FLAG_DELAY)
+    e1:SetCountLimit(1,id)
+    e1:SetTarget(s.eqtg)
+    e1:SetOperation(s.eqop)
+    c:RegisterEffect(e1)
+
+    --Redirect target (Quick Effect)
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_EQUIP)
+    e2:SetType(EFFECT_TYPE_QUICK_O)
+    e2:SetCode(EVENT_FREE_CHAIN)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
+    e2:SetCountLimit(1,{id,1})
+    e2:SetCondition(s.tgcon)
+    e2:SetTarget(s.tgtg)
+    e2:SetOperation(s.tgop)
+    c:RegisterEffect(e2)
+end
+
+-- Effect 1: Equip Gwenhwyfar from Deck and change Attribute
+function s.eqfilter(c)
+    return c:IsCode(19748583) and not c:IsForbidden()
+end
+function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+        and Duel.IsExistingMatchingCard(s.eqfilter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_EQUIP,nil,1,tp,LOCATION_DECK)
+end
+function s.eqop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+    local tc=Duel.SelectMatchingCard(tp,s.eqfilter,tp,LOCATION_DECK,0,1,1,nil):GetFirst()
+    local c=e:GetHandler()
+    if tc and c:IsRelateToEffect(e) then
+        Duel.Equip(tp,tc,c)
+        -- Change Attribute to LIGHT or DARK (player's choice)
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATTRIBUTE)
+        local att=Duel.AnnounceAttribute(tp,1,ATTRIBUTE_LIGHT+ATTRIBUTE_DARK)
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_CHANGE_ATTRIBUTE)
+        e1:SetValue(att)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+        c:RegisterEffect(e1)
+    end
+end
+
+-- Effect 2: Redirect target, equip from GY
+function s.tgcon(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    return Duel.IsChainDisablable(ev) and re:IsHasProperty(EFFECT_FLAG_CARD_TARGET)
+        and Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS):IsContains(c)
+end
+function s.tgfilter(c,e,tp)
+    return c:IsRace(RACE_WARRIOR) and c:IsAttribute(ATTRIBUTE_FIRE) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.eqfilter2(c)
+    return (c:IsSetCard(0x507a) or (c:IsSetCard(0x607a) and c:IsType(TYPE_EQUIP))) and not c:IsForbidden()
+end
+function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then
+        return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp)
+            and Duel.IsExistingMatchingCard(s.eqfilter2,tp,LOCATION_GRAVE,0,1,nil)
+    end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_GRAVE)
+    Duel.SetOperationInfo(0,CATEGORY_EQUIP,nil,1,tp,LOCATION_GRAVE)
+end
+function s.tgop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local sc=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,e,tp):GetFirst()
+    if sc and Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)>0 then
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+        local ec=Duel.SelectMatchingCard(tp,s.eqfilter2,tp,LOCATION_GRAVE,0,1,1,nil):GetFirst()
+        if ec and Duel.Equip(tp,ec,sc,true) then
+            Duel.BreakEffect()
+            Duel.ChangeTargetCard(ev,Group.FromCards(sc))
+        end
+    end
+end
