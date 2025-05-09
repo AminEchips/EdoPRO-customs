4c046be90c140bae36d080329129c4adf9d67fb1
Chips <aminekhoury0411@gmail.com>
2025-05-09 09:22:50 -0400

    Update c715.lua

--- script/c715.lua
+++ script/c715.lua
@@ -3,7 +3,6 @@ local s,id=GetID()
 local COUNTER_HOLY = 0x1b
 
 function s.initial_effect(c)
-    -- Enable counters
     c:EnableCounterPermit(COUNTER_HOLY)
     c:SetCounterLimit(COUNTER_HOLY,5)
 
@@ -13,9 +12,8 @@ function s.initial_effect(c)
     e1:SetCode(EVENT_FREE_CHAIN)
     c:RegisterEffect(e1)
 
-    -- Add counter when Noble Knight is Special Summoned from Extra Deck
+    -- Gain counters when Noble Knight summoned from Extra
     local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,0))
     e2:SetCategory(CATEGORY_COUNTER)
     e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
     e2:SetCode(EVENT_SPSUMMON_SUCCESS)
@@ -25,7 +23,7 @@ function s.initial_effect(c)
     e2:SetOperation(s.ctop)
     c:RegisterEffect(e2)
 
-    -- Special Summon 4 monsters from different locations
+    -- Special Summon 4 monsters from 4 different locations, each different archetype
     local e3=Effect.CreateEffect(c)
     e3:SetDescription(aux.Stringid(id,1))
     e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -39,10 +37,12 @@ function s.initial_effect(c)
     c:RegisterEffect(e3)
 end
 
--- Counter gain condition
+-- Counter condition
 function s.ctfilter(c,tp)
-    return c:IsFaceup() and c:IsSetCard(0x107a) and c:IsSummonType(SUMMON_TYPE_SPECIAL)
-        and c:GetSummonLocation()==LOCATION_EXTRA and c:IsControler(tp)
+    return c:IsFaceup() and c:IsSetCard(0x107a)
+        and c:IsSummonType(SUMMON_TYPE_SPECIAL)
+        and c:GetSummonLocation()==LOCATION_EXTRA
+        and c:IsControler(tp)
 end
 function s.ctcon(e,tp,eg,ep,ev,re,r,rp)
     return eg:IsExists(s.ctfilter,1,nil,tp)
@@ -51,14 +51,13 @@ function s.cttg(e,tp,eg,ep,ev,re,r,rp,chk)
     return true
 end
 function s.ctop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
     local ct=eg:FilterCount(s.ctfilter,nil,tp)
-    if ct>0 and c:IsRelateToEffect(e) then
-        c:AddCounter(COUNTER_HOLY,ct)
+    if ct>0 then
+        e:GetHandler():AddCounter(COUNTER_HOLY,ct)
     end
 end
 
--- Special Summon condition
+-- Summon condition
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
     return e:GetHandler():GetCounter(COUNTER_HOLY)==5
 end
@@ -67,40 +66,53 @@ function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
     Duel.SendtoGrave(e:GetHandler(),REASON_COST)
 end
 
--- Filters for 4 archetypes and 4 locations
-function s.handfilter(c,e,tp)
-    return c:IsSetCard(0xa7) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-function s.deckfilter(c,e,tp)
-    return c:IsSetCard(0xa8) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-function s.gravefilter(c,e,tp)
-    return c:IsSetCard(0x149) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-function s.extraFilter(c,e,tp)
-    return c:IsCode(77656797) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_SPECIAL,tp,false,false)
+-- Unified summon filter
+function s.validTarget(c,e,tp)
+    if not c:IsCanBeSpecialSummoned(e,0,tp,false,false) then return false end
+    if not (c:IsSetCard(0xa7) or c:IsSetCard(0xa8) or c:IsSetCard(0x149) or c:IsCode(77656797)) then return false end
+    return true
 end
 
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then
-        return Duel.GetLocationCount(tp,LOCATION_MZONE)>=4
-            and Duel.IsExistingMatchingCard(s.handfilter,tp,LOCATION_HAND,0,1,nil,e,tp)
-            and Duel.IsExistingMatchingCard(s.deckfilter,tp,LOCATION_DECK,0,1,nil,e,tp)
-            and Duel.IsExistingMatchingCard(s.gravefilter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
-            and Duel.IsExistingMatchingCard(s.extraFilter,tp,LOCATION_EXTRA,0,1,nil,e,tp)
+        local g=Duel.GetMatchingGroup(s.validTarget,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE+LOCATION_EXTRA,0,nil,e,tp)
+        return Duel.GetLocationCount(tp,LOCATION_MZONE)>=4 and s.checkValidGroups(g)
     end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,4,tp,
-        LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE+LOCATION_EXTRA)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,4,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE+LOCATION_EXTRA)
+end
+
+-- Check that we can choose 4 cards of different archetypes AND locations
+function s.checkValidGroups(g)
+    return g:CheckSubGroup(s.groupValid,4,4)
+end
+
+function s.groupValid(g)
+    local archetypes={}
+    local zones={}
+    for tc in g:Iter() do
+        local zone = tc:IsLocation(LOCATION_EXTRA) and LOCATION_EXTRA or tc:GetLocation()
+        local arch = s.getArchetype(tc)
+        if not arch or zones[zone] or archetypes[arch] then return false end
+        zones[zone] = true
+        archetypes[arch] = true
+    end
+    return true
+end
+
+function s.getArchetype(c)
+    if c:IsSetCard(0xa7) then return 0xa7
+    elseif c:IsSetCard(0xa8) then return 0xa8
+    elseif c:IsSetCard(0x149) then return 0x149
+    elseif c:IsCode(77656797) then return 77656797
+    else return nil end
 end
 
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    local g=Duel.GetMatchingGroup(s.validTarget,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE+LOCATION_EXTRA,0,nil,e,tp)
     if Duel.GetLocationCount(tp,LOCATION_MZONE)<4 then return end
-    local g1=Duel.SelectMatchingCard(tp,s.handfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
-    local g2=Duel.SelectMatchingCard(tp,s.deckfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
-    local g3=Duel.SelectMatchingCard(tp,s.gravefilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
-    local g4=Duel.SelectMatchingCard(tp,s.extraFilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
-    local g=g1+g2+g3+g4
-    if #g==4 then
-        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local sg=g:SelectSubGroup(tp,s.groupValid,false,4,4)
+    if sg then
+        Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
     end
 end
