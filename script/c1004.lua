51e49fca8af8fccbf4d24d6594badb44d37e353b
Chips <aminekhoury0411@gmail.com>
2025-05-12 22:20:47 -0400

    Update c1004.lua

--- script/c1004.lua
+++ script/c1004.lua
@@ -4,14 +4,15 @@ function s.initial_effect(c)
 	s.listed_series={0x16d} -- Swordsoul
 	s.listed_names={68468459} -- Fallen of Albaz
 
-	-- Return a monster to hand; Special Summon this and a Token
+	-- Bounce a monster; Special Summon this + Token
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
-	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_HAND+LOCATION_GRAVE)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_DUEL)
+	e1:SetCondition(aux.exccon) -- allows activation from GY
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
@@ -30,7 +31,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 
--- Target a valid monster to bounce
+-- Valid bounce targets
 function s.tgfilter(c)
 	return c:IsFaceup() and (c:IsSetCard(0x16d) or c:IsRace(RACE_WYRM) or c:IsCode(68468459) or c:ListsCode(68468459)) and c:IsAbleToHand()
 end
@@ -47,7 +48,7 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
 	local g=Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_MZONE,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,LOCATION_HAND+LOCATION_GRAVE)
 end
 
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
@@ -59,35 +60,36 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	-- Bounce first
 	if Duel.SendtoHand(tc,nil,REASON_EFFECT)==0 then return end
 
-	-- Then check again and Special Summon this card
-	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
-		-- Then create and summon token
-		if Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,0,0,4,RACE_WYRM,ATTRIBUTE_WATER,tp) then
-			local token=Duel.CreateToken(tp,id+1)
-			Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP)
+	-- Then summon this card
+	if not c:IsRelateToEffect(e) then return end
+	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)==0 then return end
 
-			-- Restrict to Synchros only
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_FIELD)
-			e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-			e1:SetRange(LOCATION_MZONE)
-			e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-			e1:SetAbsoluteRange(tp,1,0)
-			e1:SetTarget(function(_,c) return c:IsLocation(LOCATION_EXTRA) and not c:IsType(TYPE_SYNCHRO) end)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-			token:RegisterEffect(e1,true)
+	-- Then summon token
+	if Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,0,0,4,RACE_WYRM,ATTRIBUTE_WATER,tp) then
+		local token=Duel.CreateToken(tp,id+1)
+		Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP)
 
-			-- Lizard check
-			local e2=aux.createContinuousLizardCheck(c,LOCATION_MZONE,function(_,c) return not c:IsOriginalType(TYPE_SYNCHRO) end)
-			e2:SetReset(RESET_EVENT+RESETS_STANDARD)
-			token:RegisterEffect(e2,true)
+		-- Extra Deck Synchro-only restriction
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+		e1:SetAbsoluteRange(tp,1,0)
+		e1:SetTarget(function(_,c) return c:IsLocation(LOCATION_EXTRA) and not c:IsType(TYPE_SYNCHRO) end)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		token:RegisterEffect(e1,true)
 
-			Duel.SpecialSummonComplete()
-		end
+		-- Lizard check
+		local e2=aux.createContinuousLizardCheck(c,LOCATION_MZONE,function(_,c) return not c:IsOriginalType(TYPE_SYNCHRO) end)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+		token:RegisterEffect(e2,true)
+
+		Duel.SpecialSummonComplete()
 	end
 end
 
--- LP gain on Synchro use
+-- LP gain if used as Synchro Material
 function s.reccon(e,tp,eg,ep,ev,re,r,rp)
 	return r==REASON_SYNCHRO and e:GetHandler():IsLocation(LOCATION_GRAVE)
 end
