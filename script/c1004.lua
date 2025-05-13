8011b7cf7e3b45828083623535ef53721906089a
Chips <aminekhoury0411@gmail.com>
2025-05-12 22:12:00 -0400

    Update c1004.lua

--- script/c1004.lua
+++ script/c1004.lua
@@ -4,7 +4,7 @@ function s.initial_effect(c)
 	s.listed_series={0x16d} -- Swordsoul
 	s.listed_names={68468459} -- Fallen of Albaz
 
-	-- Return a monster to hand; then Special Summon this + Token
+	-- Return a monster to hand; Special Summon this and a Token
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
@@ -30,11 +30,10 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 
--- Valid target to bounce
+-- Target a valid monster to bounce
 function s.tgfilter(c)
-	return c:IsFaceup() and (c:IsSetCard(0x16d) or c:IsRace(RACE_WYRM)
-		or c:IsCode(68468459) or (c.ListsCode and c:ListsCode(68468459)))
-		and c:IsAbleToHand()
+	return c:IsFaceup() and (c:IsSetCard(0x16d) or c:IsRace(RACE_WYRM) or
+		c:IsCode(68468459) or (c.ListsCode and c:ListsCode(68468459))) and c:IsAbleToHand()
 end
 
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
@@ -42,6 +41,7 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.tgfilter(chkc) end
 	if chk==0 then
 		return Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_MZONE,0,1,nil)
+			and Duel.GetLocationCount(tp,LOCATION_MZONE)>1
 			and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 			and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,0,0,4,RACE_WYRM,ATTRIBUTE_WATER,tp)
 	end
@@ -49,7 +49,6 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local g=Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_MZONE,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,tp,0)
 end
 
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
@@ -58,28 +57,33 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if not tc or not tc:IsRelateToEffect(e) or Duel.GetLocationCount(tp,LOCATION_MZONE)<2 then return end
 
 	if Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 and c:IsRelateToEffect(e) then
-		-- Summon Zhan Lu
-		if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
-			-- Summon token
-			if Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,0,0,4,RACE_WYRM,ATTRIBUTE_WATER,tp) then
-				local token=Duel.CreateToken(tp,id+1)
-				Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)
+		if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)==0 then return end
+		if not Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,0,0,4,RACE_WYRM,ATTRIBUTE_WATER,tp) then return end
 
-				-- Restrict to Synchros from Extra Deck while token is up
-				local e1=Effect.CreateEffect(c)
-				e1:SetType(EFFECT_TYPE_FIELD)
-				e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-				e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-				e1:SetTargetRange(1,0)
-				e1:SetTarget(function(_,c) return c:IsLocation(LOCATION_EXTRA) and not c:IsType(TYPE_SYNCHRO) end)
-				e1:SetReset(RESET_PHASE+PHASE_END)
-				Duel.RegisterEffect(e1,tp)
-			end
-		end
+		local token=Duel.CreateToken(tp,id+1)
+		Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP)
+
+		-- Cannot Special Summon non-Synchro from Extra Deck
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+		e1:SetAbsoluteRange(tp,1,0)
+		e1:SetTarget(function(_,c) return c:IsLocation(LOCATION_EXTRA) and not c:IsType(TYPE_SYNCHRO) end)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		token:RegisterEffect(e1,true)
+
+		-- Lizard check (prevents cheating out non-Synchros)
+		local e2=aux.createContinuousLizardCheck(c,LOCATION_MZONE,function(_,c) return not c:IsOriginalType(TYPE_SYNCHRO) end)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+		token:RegisterEffect(e2,true)
+
+		Duel.SpecialSummonComplete()
 	end
 end
 
--- LP gain if used as Synchro Material
+-- Gain 1200 LP if used as Synchro Material
 function s.reccon(e,tp,eg,ep,ev,re,r,rp)
 	return r==REASON_SYNCHRO and e:GetHandler():IsLocation(LOCATION_GRAVE)
 end
