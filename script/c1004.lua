425ae2acd462d4e168c5b55abfbf425d8d26e5b9
Chips <aminekhoury0411@gmail.com>
2025-05-12 22:07:35 -0400

    Update c1004.lua

--- script/c1004.lua
+++ script/c1004.lua
@@ -1,10 +1,10 @@
 --Swordsoul Zhan Lu
 local s,id=GetID()
 function s.initial_effect(c)
-	s.listed_series={0x16d} -- Correct Swordsoul archetype code
+	s.listed_series={0x16d} -- Swordsoul
 	s.listed_names={68468459} -- Fallen of Albaz
 
-	-- Return a monster to hand, then Special Summon this card or a Token
+	-- Return a monster to hand; then Special Summon this + Token
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
@@ -30,61 +30,56 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 
--- Target a monster you control (Swordsoul, Wyrm, Albaz, or card that lists Albaz)
+-- Valid target to bounce
 function s.tgfilter(c)
 	return c:IsFaceup() and (c:IsSetCard(0x16d) or c:IsRace(RACE_WYRM)
-		or c:IsCode(68468459) or (c.ListsCode and c:ListsCode(68468459))) and c:IsAbleToHand()
+		or c:IsCode(68468459) or (c.ListsCode and c:ListsCode(68468459)))
+		and c:IsAbleToHand()
 end
+
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local c=e:GetHandler()
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.tgfilter(chkc) end
 	if chk==0 then
 		return Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_MZONE,0,1,nil)
-			and (c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-				or Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,0,0,4,RACE_WYRM,ATTRIBUTE_WATER,tp))
+			and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+			and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,0,0,4,RACE_WYRM,ATTRIBUTE_WATER,tp)
 	end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
 	local g=Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_MZONE,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_GRAVE)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,tp,0)
 end
+
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if not tc or not tc:IsRelateToEffect(e) then return end
-	if Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
-		local opt=0
-		local can_token=Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,0,0,4,RACE_WYRM,ATTRIBUTE_WATER,tp)
-		local can_self=c:IsRelateToEffect(e) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	if not tc or not tc:IsRelateToEffect(e) or Duel.GetLocationCount(tp,LOCATION_MZONE)<2 then return end
 
-		if can_self and can_token then
-			opt=Duel.SelectOption(tp,aux.Stringid(id,2),aux.Stringid(id,3)) -- 0 = this card, 1 = token
-		elseif can_self then
-			opt=0
-		elseif can_token then
-			opt=1
-		else return end
+	if Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 and c:IsRelateToEffect(e) then
+		-- Summon Zhan Lu
+		if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
+			-- Summon token
+			if Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,0,0,4,RACE_WYRM,ATTRIBUTE_WATER,tp) then
+				local token=Duel.CreateToken(tp,id+1)
+				Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)
 
-		if opt==0 then
-			Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
-		else
-			local token=Duel.CreateToken(tp,id+1)
-			Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)
-
-			-- Restrict to Synchro Summons from Extra Deck
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_FIELD)
-			e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-			e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-			e1:SetTargetRange(1,0)
-			e1:SetTarget(function(_,c) return c:IsLocation(LOCATION_EXTRA) and not c:IsType(TYPE_SYNCHRO) end)
-			e1:SetReset(RESET_PHASE+PHASE_END)
-			Duel.RegisterEffect(e1,tp)
+				-- Restrict to Synchros from Extra Deck while token is up
+				local e1=Effect.CreateEffect(c)
+				e1:SetType(EFFECT_TYPE_FIELD)
+				e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+				e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+				e1:SetTargetRange(1,0)
+				e1:SetTarget(function(_,c) return c:IsLocation(LOCATION_EXTRA) and not c:IsType(TYPE_SYNCHRO) end)
+				e1:SetReset(RESET_PHASE+PHASE_END)
+				Duel.RegisterEffect(e1,tp)
+			end
 		end
 	end
 end
 
--- LP gain effect
+-- LP gain if used as Synchro Material
 function s.reccon(e,tp,eg,ep,ev,re,r,rp)
 	return r==REASON_SYNCHRO and e:GetHandler():IsLocation(LOCATION_GRAVE)
 end
