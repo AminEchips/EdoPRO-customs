9ab22df5b3d45da76143f3272e5b1c7c48315cbf
Chips <aminekhoury0411@gmail.com>
2025-05-13 20:38:28 -0400

    Create c1013.lua

---
+++ script/c1013.lua
@@ -0,0 +1,120 @@
+--Endalion the Unshackled Dragon
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,68468459,s.matfilter)
+
+	-- Effect 1: Send 1 card from hand or field to GY, target 1 card, it is banished when it leaves
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOGRAVE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetCost(s.rmcost)
+	e1:SetTarget(s.rmtg)
+	e1:SetOperation(s.rmop)
+	c:RegisterEffect(e1)
+
+	-- Effect 2: Gain ATK during battle
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(s.atkcon)
+	e2:SetOperation(s.atkop)
+	c:RegisterEffect(e2)
+
+	-- Effect 3: Banish from GY to summon Albaz or monster that mentions it
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_PHASE+PHASE_END)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCountLimit(1,id+100,EFFECT_COUNT_CODE_DUEL)
+	e3:SetCost(aux.bfgcost)
+	e3:SetTarget(s.sptg)
+	e3:SetOperation(s.spop)
+	c:RegisterEffect(e3)
+end
+s.listed_names={68468459}
+
+-- Fusion Material filter
+function s.matfilter(c,scard,sumtype,tp)
+	return c:IsRace(RACE_DRAGON) and c:IsLevel(6)
+end
+
+-- Effect 1: Cost
+function s.rmcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,nil)
+	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGraveAsCost,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,1,nil)
+	Duel.SendtoGrave(g,REASON_COST)
+end
+function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() end
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+end
+function s.rmop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		-- Apply leave field redirection
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_REDIRECT)
+		e1:SetValue(LOCATION_REMOVED)
+		tc:RegisterEffect(e1)
+	end
+end
+
+-- Effect 2: Gain ATK based on materials
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local bc=c:GetBattleTarget()
+	return bc and bc:IsControler(1-tp)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=c:GetMaterial()
+	local atk=0
+	for tc in g:Iter() do
+		local a=tc:GetBaseAttack()
+		if a<0 then a=0 end
+		atk=atk+a
+	end
+	if atk>0 then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(math.floor(atk/2))
+		e1:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
+		c:RegisterEffect(e1)
+	end
+end
+
+-- Effect 3: Special Summon Albaz or card that lists it
+function s.spfilter(c,e,tp)
+	return c:IsMonster() and (c:IsCode(68468459) or c:ListsCode(68468459))
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
