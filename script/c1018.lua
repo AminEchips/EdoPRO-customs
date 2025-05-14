31805722d501b32856c22c78545dca5ec65cdb29
Chips <aminekhoury0411@gmail.com>
2025-05-14 18:00:38 -0400

    Create c1018.lua

---
+++ script/c1018.lua
@@ -0,0 +1,98 @@
+--The Abyss Mirrorjade
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,44146295,aux.FilterBoolFunction(Card.IsSetCard,0x166)) -- Mirrorjade + 1 Despia monster
+
+	-- Cannot be destroyed or banished if you control another Fusion Monster that mentions Fallen of Albaz
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e1:SetCondition(s.indcon)
+	e1:SetValue(s.indval)
+	c:RegisterEffect(e1)
+
+	local e1b=e1:Clone()
+	e1b:SetCode(EFFECT_CANNOT_REMOVE)
+	c:RegisterEffect(e1b)
+
+	-- If a card is banished: Special Summon 1 Extra Deck monster from GYs, then optionally redirect attack to this
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_REMOVE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+
+	-- During the End Phase, if sent to GY this turn: Banish 1 card
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_REMOVE)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_PHASE+PHASE_END)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetCondition(s.rmcon)
+	e3:SetTarget(s.rmtg)
+	e3:SetOperation(s.rmop)
+	c:RegisterEffect(e3)
+end
+
+function s.indcon(e)
+	return Duel.IsExistingMatchingCard(function(c)
+		return c:IsType(TYPE_FUSION) and c:ListsCode(68468459) and c~=e:GetHandler()
+	end,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+end
+function s.indval(e,re,tp)
+	return re:IsActivated() and re:GetHandler():IsControler(1-tp)
+end
+
+function s.spfilter(c,e,tp)
+	return c:IsType(TYPE_FUSION+TYPE_SYNCHRO+TYPE_XYZ+TYPE_LINK)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,PLAYER_ALL,LOCATION_GRAVE)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,1,nil,e,tp)
+	local tc=g:GetFirst()
+	if tc and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)>0 then
+		local a=Duel.GetAttacker()
+		local d=Duel.GetAttackTarget()
+		if a and d and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+			if a:IsControler(1-tp) then
+				Duel.ChangeAttackTarget(e:GetHandler())
+			else
+				Duel.CalculateDamage(a,e:GetHandler())
+			end
+		end
+	end
+end
+
+function s.rmcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsReason(REASON_EFFECT+REASON_BATTLE) and c:IsPreviousLocation(LOCATION_ONFIELD)
+end
+function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_ONFIELD+LOCATION_GRAVE,LOCATION_ONFIELD+LOCATION_GRAVE,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,PLAYER_ALL,LOCATION_ONFIELD+LOCATION_GRAVE)
+end
+function s.rmop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_ONFIELD+LOCATION_GRAVE,LOCATION_ONFIELD+LOCATION_GRAVE,1,1,nil)
+	if #g>0 then
+		Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+	end
+end
