10368f46936208f8a63bf6ddb1001837ac50e561
Chips <aminekhoury0411@gmail.com>
2025-05-13 20:42:08 -0400

    Update c1013.lua

--- script/c1013.lua
+++ script/c1013.lua
@@ -4,11 +4,12 @@ function s.initial_effect(c)
 	c:EnableReviveLimit()
 	Fusion.AddProcMix(c,true,true,68468459,s.matfilter)
 
-	-- Effect 1: Send 1 card from hand or field to GY, target 1 card, it is banished when it leaves
+	-- Effect 1: Send 1 card to GY; target 1 card; banish it when it leaves the field
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOGRAVE)
 	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1,id)
 	e1:SetCost(s.rmcost)
@@ -16,7 +17,7 @@ function s.initial_effect(c)
 	e1:SetOperation(s.rmop)
 	c:RegisterEffect(e1)
 
-	-- Effect 2: Gain ATK during battle
+	-- Effect 2: Gain ATK during battle based on fusion materials
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
@@ -26,14 +27,14 @@ function s.initial_effect(c)
 	e2:SetOperation(s.atkop)
 	c:RegisterEffect(e2)
 
-	-- Effect 3: Banish from GY to summon Albaz or monster that mentions it
+	-- Effect 3: Banish self from GY; target 1 "Fallen of Albaz" or monster that mentions it in GY; Special Summon it
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,2))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e3:SetCode(EVENT_PHASE+PHASE_END)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e3:SetRange(LOCATION_GRAVE)
-	e3:SetCountLimit(1,id+100,EFFECT_COUNT_CODE_DUEL)
+	e3:SetCountLimit(1,{id,2})
 	e3:SetCost(aux.bfgcost)
 	e3:SetTarget(s.sptg)
 	e3:SetOperation(s.spop)
@@ -41,12 +42,12 @@ function s.initial_effect(c)
 end
 s.listed_names={68468459}
 
--- Fusion Material filter
+-- Fusion material filter
 function s.matfilter(c,scard,sumtype,tp)
 	return c:IsRace(RACE_DRAGON) and c:IsLevel(6)
 end
 
--- Effect 1: Cost
+-- Effect 1: Send 1 card as cost, target 1 card, banish it when it leaves
 function s.rmcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,nil)
@@ -64,7 +65,6 @@ end
 function s.rmop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) then
-		-- Apply leave field redirection
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
@@ -75,7 +75,7 @@ function s.rmop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- Effect 2: Gain ATK based on materials
+-- Effect 2: Gain ATK if attacking opponent’s monster
 function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local bc=c:GetBattleTarget()
@@ -100,21 +100,20 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- Effect 3: Special Summon Albaz or card that lists it
+-- Effect 3: Banish this card to Special Summon Albaz or monster that lists it
 function s.spfilter(c,e,tp)
 	return c:IsMonster() and (c:IsCode(68468459) or c:ListsCode(68468459))
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.spfilter(chkc,e,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+		and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
-	if #g>0 then
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-	end
+	local tc=Duel.GetFirstTarget()
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 or not tc:IsRelateToEffect(e) then return end
+	Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 end
