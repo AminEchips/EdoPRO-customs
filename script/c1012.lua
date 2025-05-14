ba810918310944fbfa8a278a9a57673ea54f97de
Chips <aminekhoury0411@gmail.com>
2025-05-13 20:33:06 -0400

    Update c1012.lua

--- script/c1012.lua
+++ script/c1012.lua
@@ -2,10 +2,10 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	--Fusion Materials: Fallen of Albaz + 1 Fusion Monster that lists it
+	--Fusion Materials
 	Fusion.AddProcMix(c,true,true,68468459,s.matfilter)
 
-	--Copy End Phase effect from a Fusion Monster that lists "Fallen of Albaz"
+	--Immediate copy and resolve: Add/Summon Albaz or archetype monster
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
@@ -17,7 +17,7 @@ function s.initial_effect(c)
 	e1:SetOperation(s.copyop)
 	c:RegisterEffect(e1)
 
-	--If a Fusion Summoned monster leaves the field due to an opponent’s card: destroy all their Spells/Traps in End Phase
+	--If Fusion leaves field due to opponent → blow up Spells/Traps
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EVENT_LEAVE_FIELD)
@@ -29,12 +29,12 @@ end
 s.listed_names={68468459}
 s.listed_series={0x16f} -- Despia
 
--- Fusion Material Filter
+-- Fusion Material must list Albaz
 function s.matfilter(c,scard,sumtype,tp)
 	return c:IsType(TYPE_FUSION) and c:ListsCode(68468459)
 end
 
--- e1: Copy Effect
+-- Immediate Copy Target
 function s.copyfilter(c)
 	return c:IsType(TYPE_FUSION) and c:ListsCode(68468459) and not c:IsCode(id) and c:IsAbleToExtra()
 end
@@ -47,50 +47,43 @@ end
 function s.copyop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if not (tc and tc:IsRelateToEffect(e) and c:IsRelateToEffect(e)) then return end
+	if not (tc and tc:IsRelateToEffect(e)) then return end
 
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,1))
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_PHASE+PHASE_END)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1,id+100)
-	e1:SetTarget(s.endtg)
-	e1:SetOperation(s.endop)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-	c:RegisterEffect(e1)
+	local albaz_code=68468459
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 
-	-- Return target to Extra Deck
+	-- Gather all setcodes on the targeted Fusion
+	local codes = {}
+	for i=1,0xffff do
+		if tc:IsSetCard(i) then table.insert(codes,i) end
+	end
+
+	-- Build filter
+	local g=Duel.GetMatchingGroup(function(card)
+		if not card:IsMonster() then return false end
+		if card:IsCode(albaz_code) then return true end
+		for _,sc in ipairs(codes) do
+			if card:IsSetCard(sc) then return true end
+		end
+		return false
+	end,tp,LOCATION_DECK,0,nil)
+
+	if #g==0 then return end
+
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local sg=g:Select(tp,1,1,nil)
+	local sc=sg:GetFirst()
+	if not sc then return end
+	aux.ToHandOrElse(sc,tp,
+		function(c) return ft>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end,
+		function(c) Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP) end,
+		aux.Stringid(id,1))
+
+	-- Return targeted Fusion to Extra Deck
 	Duel.SendtoDeck(tc,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
 end
 
-function s.thfilter(c,e,tp,ft)
-	return c:IsMonster() and (c:IsSetCard(0x16f) or c:IsCode(68468459))
-		and (c:IsAbleToHand() or (ft>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)))
-end
-function s.endtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if chk==0 then
-		return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,e,tp,ft)
-	end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
-end
-function s.endop(e,tp,eg,ep,ev,re,r,rp)
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp,ft)
-	local tc=g:GetFirst()
-	if tc then
-		aux.ToHandOrElse(tc,tp,
-			function(c) return ft>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end,
-			function(c) Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP) end,
-			aux.Stringid(id,2))
-	end
-end
-
--- e2: Fusion monster leaves field due to opponent → destroy all their S/T
+-- Explosion effect trigger
 function s.descon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(function(c)
 		return c:IsType(TYPE_FUSION) and c:IsSummonType(SUMMON_TYPE_FUSION)
@@ -100,12 +93,12 @@ end
 function s.desreg(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,3))
+	e1:SetDescription(aux.Stringid(id,2))
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_PHASE+PHASE_END)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1,id+200)
+	e1:SetCountLimit(1,id+100)
 	e1:SetTarget(s.destg)
 	e1:SetOperation(s.desop)
 	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
