d07156b736ead8475733dbdf5a1327fc9dcb45bd
Chips <aminekhoury0411@gmail.com>
2025-05-14 22:26:39 -0400

    Update c1019.lua

--- script/c1019.lua
+++ script/c1019.lua
@@ -5,19 +5,19 @@ function s.initial_effect(c)
 	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTuner(nil),1,99)
 	c:EnableReviveLimit()
 
-	--On Synchro Summon: Add 1 Dogmatika S/T or "Fallen of Albaz"-mentioning card from GY
+	--If Synchro Summoned: Target 1 Dogmatika S/T or one that mentions "Fallen of Albaz" in GY, add to hand
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
 	e1:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO) end)
 	e1:SetTarget(s.thtg)
 	e1:SetOperation(s.thop)
 	c:RegisterEffect(e1)
 
-	--Send 1 monster from Extra Deck to GY during battle
+	--Send 1 monster from Extra Deck to GY when battling
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TOGRAVE)
@@ -29,7 +29,7 @@ function s.initial_effect(c)
 	e2:SetOperation(s.gyop)
 	c:RegisterEffect(e2)
 
-	--During End Phase if sent to GY this turn: Add 1 LIGHT Ritual Monster and 1 Ritual Spell
+	--During End Phase, if sent to GY: Add 1 LIGHT Ritual Monster and 1 Ritual Spell from Deck to hand
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,2))
 	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
@@ -45,25 +45,27 @@ end
 s.listed_series={0x146} -- Dogmatika
 s.listed_names={68468459} -- Fallen of Albaz
 
--- e1: Search Dogmatika S/T or "Fallen of Albaz"-mentioner
+-- e1: Spell/Trap only, Dogmatika or mentions Fallen of Albaz
 function s.thfilter(c)
-	return (c:IsSetCard(0x146) and c:IsType(TYPE_SPELL+TYPE_TRAP)) or c:ListsCode(68468459)
-	   and c:IsAbleToHand()
+	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToHand()
+		and (c:IsSetCard(0x146) or c:ListsCode(68468459))
 end
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.thfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,tp,LOCATION_GRAVE)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
-	if #g>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,tc)
 	end
 end
 
--- e2: Send Extra Deck monster during battle
+-- e2: Send monster from Extra Deck to GY during battle
 function s.gycon(e,tp,eg,ep,ev,re,r,rp)
 	local a=Duel.GetAttacker()
 	local d=Duel.GetAttackTarget()
@@ -81,9 +83,9 @@ function s.gyop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- e3: Add LIGHT Ritual Monster + Ritual Spell
+-- e3: Add 1 LIGHT Ritual Monster and 1 Ritual Spell
 function s.ritfilter1(c)
-	return c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsType(TYPE_RITUAL+TYPE_MONSTER) and c:IsAbleToHand()
+	return c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsType(TYPE_RITUAL) and c:IsAbleToHand()
 end
 function s.ritfilter2(c)
 	return c:IsType(TYPE_RITUAL+TYPE_SPELL) and c:IsAbleToHand()
@@ -96,6 +98,7 @@ function s.rittg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_DECK)
 end
 function s.ritop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g1=Duel.SelectMatchingCard(tp,s.ritfilter1,tp,LOCATION_DECK,0,1,1,nil)
 	if #g1==0 then return end
