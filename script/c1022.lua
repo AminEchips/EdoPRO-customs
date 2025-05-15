f12bc72623854d75f7f3eea085cab6953a61cf2c
Chips <aminekhoury0411@gmail.com>
2025-05-15 00:08:03 -0400

    Update c1022.lua

--- script/c1022.lua
+++ script/c1022.lua
@@ -21,22 +21,21 @@ function s.initial_effect(c)
 	--Quick Effect: Banish another Xyz → becomes 3000 ATK, destroy 1 face-up
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DESTROY)
+	e2:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_REMOVE+CATEGORY_DESTROY)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetHintTiming(TIMING_BATTLE_PHASE+TIMING_MAIN_END,TIMING_BATTLE_PHASE+TIMING_MAIN_END)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1,{id,1})
 	e2:SetCondition(function(_,tp) return Duel.IsMainPhase() or Duel.IsBattlePhase() end)
-	e2:SetCost(s.atkcost)
 	e2:SetTarget(s.atktg)
 	e2:SetOperation(s.atkop)
 	c:RegisterEffect(e2)
 end
 
---Search Golgonda or card that mentions it
+--Search "Great Sand Sea - Gold Golgonda", or any card that mentions it or Fallen of Albaz
 function s.thfilter(c)
-	return (c:IsCode(60884672) or c:ListsCode(60884672)) and c:IsAbleToHand()
+	return c:IsAbleToHand() and (c:IsCode(60884672) or c:ListsCode(60884672) or c:ListsCode(68468459))
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
@@ -50,37 +49,42 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
-s.listed_names={68468459}
-
---Banish another Xyz monster you control
-function s.cfilter(c)
-	return c:IsFaceup() and c:IsType(TYPE_XYZ) and c:IsAbleToRemoveAsCost()
-end
-function s.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,e:GetHandler()) end
-	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_MZONE,0,1,1,e:GetHandler())
-	Duel.Remove(g,POS_FACEUP,REASON_COST)
-end
+--Effect (not cost): banish another face-up Xyz you control
 function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+	if chk==0 then
+		return Duel.IsExistingMatchingCard(s.banfilter,tp,LOCATION_MZONE,0,1,e:GetHandler())
+			and Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_MZONE)
+	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,e:GetHandler(),1,tp,0)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,0,LOCATION_ONFIELD)
+end
+function s.banfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_XYZ) and c:IsAbleToRemove()
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) and c:IsFaceup() and c:GetAttack()<3000 then
-		-- Gain 3000 ATK
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
-		e1:SetValue(3000)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		c:RegisterEffect(e1)
+	if not c:IsRelateToEffect(e) or not c:IsFaceup() then return end
 
-		-- Destroy 1 face-up card
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-		local g=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
-		if #g>0 then
-			Duel.HintSelection(g,true)
-			Duel.Destroy(g,REASON_EFFECT)
-		end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local rg=Duel.SelectMatchingCard(tp,s.banfilter,tp,LOCATION_MZONE,0,1,1,c)
+	if #rg==0 then return end
+
+	if Duel.Remove(rg,POS_FACEUP,REASON_EFFECT)==0 then return end
+
+	-- Set ATK to 3000
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+	e1:SetValue(3000)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	c:RegisterEffect(e1)
+
+	-- Destroy 1 face-up card
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local dg=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	if #dg>0 then
+		Duel.HintSelection(dg,true)
+		Duel.Destroy(dg,REASON_EFFECT)
 	end
 end
