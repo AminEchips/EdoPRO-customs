6e7e429c0561031a8031cdb2ac7df3916178ecea
Chips <aminekhoury0411@gmail.com>
2025-05-14 23:26:41 -0400

    Update c1021.lua

--- script/c1021.lua
+++ script/c1021.lua
@@ -19,7 +19,7 @@ function s.initial_effect(c)
 	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
 	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetValue(1)
+	e2:SetValue(function(e,re) return re:IsActivated() and re:GetOwnerPlayer()~=e:GetHandlerPlayer() end)
 	c:RegisterEffect(e2)
 
 	-- Tribute hand for control (Quick Effect)
@@ -31,39 +31,42 @@ function s.initial_effect(c)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E+TIMING_MAIN_END)
 	e3:SetCountLimit(1,id)
+	e3:SetCost(s.ctcost)
 	e3:SetTarget(s.cttg)
 	e3:SetOperation(s.ctop)
 	c:RegisterEffect(e3)
 
-	-- End Phase effect when leaves field
+	-- End Phase effect when leaves field (Mirrorjade style)
 	local e4=Effect.CreateEffect(c)
 	e4:SetDescription(aux.Stringid(id,1))
 	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e4:SetCode(EVENT_PHASE+PHASE_END)
-	e4:SetRange(LOCATION_GRAVE)
-	e4:SetCountLimit(1,{id,1})
-	e4:SetCondition(s.spcon)
-	e4:SetTarget(s.sptg)
-	e4:SetOperation(s.spop)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e4:SetCode(EVENT_LEAVE_FIELD)
+	e4:SetCondition(s.regcon)
+	e4:SetOperation(s.regop)
 	c:RegisterEffect(e4)
 end
 
 s.listed_names={68468459} -- Fallen of Albaz
 
 function s.atkval(e,c)
-	local g=Duel.GetMatchingGroup(Card.IsControler,c:GetControler(),LOCATION_MZONE,0,nil,1-c:GetControler())
+	local g=Duel.GetMatchingGroup(function(tc)
+		return tc:IsFaceup() and tc:IsControler(1-c:GetControler()) and tc:GetOwner()~=c:GetControler()
+	end,c:GetControler(),LOCATION_MZONE,LOCATION_MZONE,nil)
 	return g:GetCount()*500
 end
 
+function s.ctcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(Card.IsDiscardable,tp,LOCATION_HAND,0,nil)
+	if chk==0 then return #g>0 end
+	Duel.SendtoGrave(g,REASON_COST+REASON_DISCARD)
+end
 function s.cttg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsControlerCanBeChanged,tp,0,LOCATION_MZONE,1,nil) and Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)>0 end
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsControlerCanBeChanged,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_CONTROL,nil,1,1-tp,LOCATION_MZONE)
 end
 function s.ctop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(Card.IsDiscardable,tp,LOCATION_HAND,0,nil)
-	if #g==0 then return end
-	Duel.SendtoGrave(g,REASON_COST+REASON_DISCARD)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
 	local tg=Duel.SelectMatchingCard(tp,Card.IsControlerCanBeChanged,tp,0,LOCATION_MZONE,1,1,nil)
 	if #tg>0 then
@@ -71,16 +74,29 @@ function s.ctop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+function s.regcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsReason(REASON_DESTROY+REASON_EFFECT+REASON_BATTLE)
+	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsSummonType(SUMMON_TYPE_SYNCHRO)
 end
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetCountLimit(1,{id,1})
+	e1:SetCondition(function(e,tp) return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+
 function s.spfilter(c,e,tp)
-	return c:IsCode(68468459) or (c:IsType(TYPE_FUSION) and c:ListsCode(68468459)) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return (c:IsCode(68468459) or (c:IsType(TYPE_FUSION) and c:ListsCode(68468459))) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and
-		Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
