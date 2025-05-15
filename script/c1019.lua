c2cad99ae3e86a4676ae50d348f6ad94bf523ac1
Chips <aminekhoury0411@gmail.com>
2025-05-14 22:21:57 -0400

    Update c1019.lua

--- script/c1019.lua
+++ script/c1019.lua
@@ -1,108 +1,108 @@
 --Relic Knight of Dogmatika
 local s,id=GetID()
 function s.initial_effect(c)
-    c:EnableReviveLimit()
-    --Synchro Summon procedure
-    Synchro.AddProcedure(c,aux.FilterBoolFunction(Card.IsType,TYPE_TUNER),1,1,aux.FilterBoolFunction(Card.IsNotType,TYPE_TUNER),1,99)
-    
-    --Effect on Synchro Summon: Add Spell/Trap with "Dogmatika" or mentions "Fallen of Albaz" from GY to hand
-    local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_TOHAND)
-    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-    e1:SetCondition(s.thcon)
-    e1:SetTarget(s.thtg)
-    e1:SetOperation(s.thop)
-    c:RegisterEffect(e1)
+	--Synchro Summon
+	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTuner(nil),1,99)
+	c:EnableReviveLimit()
 
-    --Send 1 monster from Extra Deck if your monster battles
-    local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,1))
-    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-    e2:SetCode(EVENT_BATTLE_CONFIRM)
-    e2:SetRange(LOCATION_MZONE)
-    e2:SetCondition(s.gycon)
-    e2:SetOperation(s.gyop)
-    c:RegisterEffect(e2)
+	--On Synchro Summon: Add 1 Dogmatika S/T or "Fallen of Albaz"-mentioning card from GY
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO) end)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
 
-    --During End Phase: Add 1 LIGHT Ritual Monster and 1 Ritual Spell from Deck to hand
-    local e3=Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(id,2))
-    e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-    e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-    e3:SetCode(EVENT_PHASE+PHASE_END)
-    e3:SetRange(LOCATION_GRAVE)
-    e3:SetCountLimit(1,id)
-    e3:SetCondition(s.epcon)
-    e3:SetTarget(s.eptg)
-    e3:SetOperation(s.epop)
-    c:RegisterEffect(e3)
+	--Send 1 monster from Extra Deck to GY during battle
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOGRAVE)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(s.gycon)
+	e2:SetTarget(s.gytg)
+	e2:SetOperation(s.gyop)
+	c:RegisterEffect(e2)
+
+	--During End Phase if sent to GY this turn: Add 1 LIGHT Ritual Monster and 1 Ritual Spell
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_PHASE+PHASE_END)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCountLimit(1,id)
+	e3:SetCondition(function(e,tp) return e:GetHandler():IsReason(REASON_EFFECT+REASON_BATTLE) and e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD) end)
+	e3:SetTarget(s.rittg)
+	e3:SetOperation(s.ritop)
+	c:RegisterEffect(e3)
 end
-s.listed_series={0x146}
+s.listed_series={0x146} -- Dogmatika
 s.listed_names={68468459} -- Fallen of Albaz
 
---e1 condition
-function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-    return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO)
-end
---e1 filter
+-- e1: Search Dogmatika S/T or "Fallen of Albaz"-mentioner
 function s.thfilter(c)
-    return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToHand() and (c:IsSetCard(0x146) or c:ListsCode(68468459))
+	return (c:IsSetCard(0x146) and c:IsType(TYPE_SPELL+TYPE_TRAP)) or c:ListsCode(68468459)
+	   and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-    local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
-    if #g>0 then
-        Duel.SendtoHand(g,nil,REASON_EFFECT)
-        Duel.ConfirmCards(1-tp,g)
-    end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
 end
 
---e2
+-- e2: Send Extra Deck monster during battle
 function s.gycon(e,tp,eg,ep,ev,re,r,rp)
-    local tc=Duel.GetAttacker()
-    local bc=Duel.GetAttackTarget()
-    if not bc then return false end
-    if tc:IsControler(1-tp) then tc=bc end
-    return tc and tc:IsControler(tp) and tc:IsRelateToBattle()
+	local a=Duel.GetAttacker()
+	local d=Duel.GetAttackTarget()
+	return (a and a:IsControler(tp) and a:IsRelateToBattle()) or (d and d:IsControler(tp) and d:IsRelateToBattle())
+end
+function s.gytg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGrave,tp,LOCATION_EXTRA,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_EXTRA)
 end
 function s.gyop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-    local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGrave,tp,LOCATION_EXTRA,0,1,1,nil)
-    if #g>0 then
-        Duel.SendtoGrave(g,REASON_EFFECT)
-    end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGrave,tp,LOCATION_EXTRA,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoGrave(g,REASON_EFFECT)
+	end
 end
 
---e3
-function s.epcon(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    return c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsReason(REASON_EFFECT+REASON_BATTLE)
+-- e3: Add LIGHT Ritual Monster + Ritual Spell
+function s.ritfilter1(c)
+	return c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsType(TYPE_RITUAL+TYPE_MONSTER) and c:IsAbleToHand()
 end
-function s.ritualfilter(c)
-    return c:IsType(TYPE_RITUAL) and c:IsType(TYPE_MONSTER) and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsAbleToHand()
+function s.ritfilter2(c)
+	return c:IsType(TYPE_RITUAL+TYPE_SPELL) and c:IsAbleToHand()
 end
-function s.spellfilter(c)
-    return c:IsType(TYPE_RITUAL+TYPE_SPELL) and c:IsAbleToHand()
+function s.rittg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		return Duel.IsExistingMatchingCard(s.ritfilter1,tp,LOCATION_DECK,0,1,nil)
+			and Duel.IsExistingMatchingCard(s.ritfilter2,tp,LOCATION_DECK,0,1,nil)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_DECK)
 end
-function s.eptg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then
-        return Duel.IsExistingMatchingCard(s.ritualfilter,tp,LOCATION_DECK,0,1,nil)
-            and Duel.IsExistingMatchingCard(s.spellfilter,tp,LOCATION_DECK,0,1,nil)
-    end
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_DECK)
-end
-function s.epop(e,tp,eg,ep,ev,re,r,rp)
-    local g1=Duel.SelectMatchingCard(tp,s.ritualfilter,tp,LOCATION_DECK,0,1,1,nil)
-    local g2=Duel.SelectMatchingCard(tp,s.spellfilter,tp,LOCATION_DECK,0,1,1,nil)
-    g1:Merge(g2)
-    if #g1>0 then
-        Duel.SendtoHand(g1,nil,REASON_EFFECT)
-        Duel.ConfirmCards(1-tp,g1)
-    end
+function s.ritop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g1=Duel.SelectMatchingCard(tp,s.ritfilter1,tp,LOCATION_DECK,0,1,1,nil)
+	if #g1==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g2=Duel.SelectMatchingCard(tp,s.ritfilter2,tp,LOCATION_DECK,0,1,1,nil)
+	if #g2==0 then return end
+	g1:Merge(g2)
+	Duel.SendtoHand(g1,nil,REASON_EFFECT)
+	Duel.ConfirmCards(1-tp,g1)
 end
