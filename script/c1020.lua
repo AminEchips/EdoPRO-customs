5d7e5a581aa57e88850c680872f8cee567cc9e77
Chips <aminekhoury0411@gmail.com>
2025-05-14 22:58:16 -0400

    Update c1020.lua

--- script/c1020.lua
+++ script/c1020.lua
@@ -5,11 +5,12 @@ function s.initial_effect(c)
 	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTuner(nil),1,99)
 	c:EnableReviveLimit()
 
-	--1. If you control or have Fallen of Albaz in GY: Target 1 monster on the field; banish it during the End Phase
+	-- Banish 1 monster during End Phase if "Fallen of Albaz" is present
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_REMOVE)
 	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1,id)
 	e1:SetCondition(s.rmcon)
@@ -17,31 +18,31 @@ function s.initial_effect(c)
 	e1:SetOperation(s.rmop)
 	c:RegisterEffect(e1)
 
-	--2. If a monster is Special Summoned from hand or GY by either player and you control a Fusion or Synchro: Fusion Summon 1 Level 8 or lower
+	-- Fusion Summon if monster is Special Summoned from hand/GY by either player while you control Fusion/Synchro
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e2:SetRange(LOCATION_MZONE)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCountLimit(1,{id,1})
-	e2:SetCondition(s.fucon)
-	e2:SetTarget(s.futg)
-	e2:SetOperation(s.fuop)
+	e2:SetCondition(s.fuscon)
+	e2:SetTarget(s.fustg)
+	e2:SetOperation(s.fusop)
 	c:RegisterEffect(e2)
 end
 
 s.listed_names={68468459} -- Fallen of Albaz
 
---1. Condition for ignition banish
 function s.rmcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,nil,68468459)
+	return Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_GRAVE+LOCATION_MZONE,0,1,nil,68468459)
 end
 function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsAbleToRemove() end
-	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToRemove,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	if chkc then return chkc:IsLocation(LOCATION_ONFIELD) and chkc:IsAbleToRemove() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToRemove,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectTarget(tp,Card.IsAbleToRemove,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToRemove,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
 end
 function s.rmop(e,tp,eg,ep,ev,re,r,rp)
@@ -51,45 +52,35 @@ function s.rmop(e,tp,eg,ep,ev,re,r,rp)
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		e1:SetCode(EVENT_PHASE+PHASE_END)
-		e1:SetReset(RESET_PHASE+PHASE_END)
-		e1:SetLabelObject(tc)
 		e1:SetCountLimit(1)
-		e1:SetOperation(s.rmop2)
+		e1:SetLabelObject(tc)
+		e1:SetCondition(function(e) return e:GetLabelObject():GetFlagEffect(id)>0 end)
+		e1:SetOperation(function(e,tp) Duel.Remove(e:GetLabelObject(),POS_FACEUP,REASON_EFFECT) end)
+		e1:SetReset(RESET_PHASE+PHASE_END)
 		Duel.RegisterEffect(e1,tp)
 	end
 end
-function s.rmop2(e,tp,eg,ep,ev,re,r,rp)
-	local tc=e:GetLabelObject()
-	if tc and tc:GetFlagEffect(id)>0 then
-		Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
-	end
-end
 
---2. Condition to Fusion Summon
-function s.cfilter(c,tp)
-	return c:IsSummonPlayer(tp) and (c:IsSummonLocation(LOCATION_HAND) or c:IsSummonLocation(LOCATION_GRAVE))
+function s.fusfilter(c)
+	return c:IsSummonLocation(LOCATION_HAND+LOCATION_GRAVE)
 end
-function s.fucon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.cfilter,1,nil,PLAYER_ALL)
-		and Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_MZONE,0,1,nil,TYPE_FUSION+TYPE_SYNCHRO)
+function s.fuscon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.fusfilter,1,nil) and Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_MZONE,0,1,nil,TYPE_FUSION+TYPE_SYNCHRO)
 end
-function s.futg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_EXTRA,0,1,nil,TYPE_FUSION) end
+function s.fustg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.fusfilter2,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
-function s.fuop(e,tp,eg,ep,ev,re,r,rp)
-	local chkf=tp
-	local mg=Duel.GetFusionMaterial(tp)
-	local sg=Duel.GetMatchingGroup(function(c)
-		return c:IsType(TYPE_FUSION) and c:IsLevelBelow(8) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-	end,tp,LOCATION_EXTRA,0,nil)
-	if #sg==0 then return end
+function s.fusfilter2(c,e,tp)
+	return c:IsType(TYPE_FUSION) and c:IsLevelBelow(8) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
+end
+function s.fusop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local tg=sg:Select(tp,1,1,nil):GetFirst()
-	if tg then
-		local mat=Duel.SelectFusionMaterial(tp,tg,mg,nil,chkf)
-		tg:SetMaterial(mat)
-		Duel.SendtoGrave(mat,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
-		Duel.SpecialSummon(tg,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
-		tg:CompleteProcedure()
+	local g=Duel.SelectMatchingCard(tp,s.fusfilter2,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+	local tc=g:GetFirst()
+	if tc then
+		Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
+		tc:CompleteProcedure()
 	end
 end
