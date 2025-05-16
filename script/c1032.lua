89bc32eaa8a7318e2f94165ca462ebd0cb0f8a6c
Chips <aminekhoury0411@gmail.com>
2025-05-16 08:37:06 -0400

    Update c1032.lua

--- script/c1032.lua
+++ script/c1032.lua
@@ -2,9 +2,9 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	Fusion.AddProcMix(c,true,true,18666161,s.matfilter) -- Despian Proskenion + Dogmatika Ritual
+	Fusion.AddProcMix(c,true,true,18666161,s.matfilter)
 
-	-- Negate opponent's effect and boost one of your Extra Deck monsters
+	-- Negate opponent's effect, negate your monster, gain ATK
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
@@ -14,7 +14,7 @@ function s.initial_effect(c)
 	e1:SetOperation(s.negop)
 	c:RegisterEffect(e1)
 
-	-- Quick Effect: Banish from GY to summon + send + burn
+	-- GY effect: summon + send + burn
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOGRAVE+CATEGORY_DAMAGE)
@@ -29,14 +29,14 @@ function s.initial_effect(c)
 end
 
 s.listed_names={18666161}
-s.listed_series={0x146} -- Dogmatika
+s.listed_series={0x146}
 
--- Material: 1 Dogmatika Ritual
+-- Fusion requirement: 1 Dogmatika Ritual
 function s.matfilter(c,fc,sub,mg,sg,chkf)
 	return c:IsSetCard(0x146) and c:IsType(TYPE_RITUAL)
 end
 
--- e1: Continuous Negate + Boost Logic
+-- Effect 1 - Negate + Stat Change
 function s.negcon(e,tp,eg,ep,ev,re,r,rp)
 	return rp==1-tp and re:IsMonsterEffect() and Duel.IsChainDisablable(ev)
 end
@@ -46,11 +46,8 @@ function s.negop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.NegateEffect(ev)
 	local rc=re:GetHandler()
 	if not rc then return end
-
-	-- Get higher of base ATK/DEF
 	local val=math.max(rc:GetBaseAttack(), rc:GetBaseDefense())
 
-	-- Choose one of your Extra Deck monsters
 	local g=Duel.GetMatchingGroup(s.monfilter,tp,LOCATION_MZONE,0,nil)
 	if #g==0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
@@ -58,7 +55,7 @@ function s.negop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=sg:GetFirst()
 	if not tc then return end
 
-	-- Negate its effects
+	-- Negate your own monster
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_DISABLE)
@@ -71,19 +68,20 @@ function s.negop(e,tp,eg,ep,ev,re,r,rp)
 	e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 	tc:RegisterEffect(e2)
 
-	-- Gain ATK
+	-- Set ATK to base stat
 	local e3=Effect.CreateEffect(e:GetHandler())
 	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetCode(EFFECT_UPDATE_ATTACK)
+	e3:SetCode(EFFECT_SET_ATTACK_FINAL)
 	e3:SetValue(val)
 	e3:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 	tc:RegisterEffect(e3)
 end
 function s.monfilter(c)
-	return c:IsFaceup() and (c:IsType(TYPE_FUSION) or c:IsType(TYPE_SYNCHRO) or c:IsType(TYPE_XYZ) or c:IsType(TYPE_LINK))
+	return c:IsFaceup() and (c:IsType(TYPE_FUSION) or c:IsType(TYPE_SYNCHRO)
+		or c:IsType(TYPE_XYZ) or c:IsType(TYPE_LINK))
 end
 
--- e2: GY Quick Effect
+-- Effect 2 - GY effect
 function s.spfilter(c,e,tp)
 	return c:IsAttribute(ATTRIBUTE_LIGHT+ATTRIBUTE_DARK) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
@@ -93,21 +91,19 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if ft<=0 then return end
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local sc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp):GetFirst()
 	if not sc or Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)==0 then return end
 
-	-- Check level
-	if sc:IsLevelAbove(8) then
-		local g=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,nil)
-		if #g==0 then return end
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-		local tg=g:Select(tp,1,1,nil)
-		local tc=tg:GetFirst()
-		if tc and Duel.SendtoGrave(tc,REASON_EFFECT)>0 and tc:IsPreviousLocation(LOCATION_EXTRA) then
-			Duel.Damage(1-tp,1200,REASON_EFFECT)
+	if sc:IsLevelAbove(8) and Duel.IsExistingMatchingCard(Card.IsMonster,tp,0,LOCATION_MZONE,1,nil) then
+		if Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+			local tg=Duel.SelectMatchingCard(tp,Card.IsMonster,tp,0,LOCATION_MZONE,1,1,nil)
+			local tc=tg:GetFirst()
+			if tc and Duel.SendtoGrave(tc,REASON_EFFECT)>0 and tc:IsPreviousSummonLocation(LOCATION_EXTRA) then
+				Duel.Damage(1-tp,1200,REASON_EFFECT)
+			end
 		end
 	end
 end
