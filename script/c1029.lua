2c88ee222a21472610e073aecb8fa7f763b84482
Chips <aminekhoury0411@gmail.com>
2025-05-15 23:12:39 -0400

    Update c1029.lua

--- script/c1029.lua
+++ script/c1029.lua
@@ -30,7 +30,7 @@ function s.initial_effect(c)
 	e2:SetOperation(s.adop)
 	c:RegisterEffect(e2)
 
-	-- Effect 3: If sent to GY, add 1 "Branded" Spell/Trap from GY or banished to hand
+	-- Effect 3: If sent to GY from field by battle or effect, add 1 "Branded" Spell/Trap from GY or banished
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,2))
 	e3:SetCategory(CATEGORY_TOHAND)
@@ -38,25 +38,28 @@ function s.initial_effect(c)
 	e3:SetCode(EVENT_TO_GRAVE)
 	e3:SetProperty(EFFECT_FLAG_DELAY)
 	e3:SetCountLimit(1,{id,2})
+	e3:SetCondition(s.thcon)
 	e3:SetTarget(s.thtg)
 	e3:SetOperation(s.thop)
 	c:RegisterEffect(e3)
 end
 
+-- List Aluber the Jester of Despia
 s.listed_names={62962630}
 s.listed_series={0x166, 0x1ba} -- Despia, Branded
 
--- Fusion Material filter: 1 monster Special Summoned from Extra Deck
+-- Fusion material filter: Must be Special Summoned from the Extra Deck
 function s.matfilter(c,scard,sumtype,tp)
 	return c:IsSummonLocation(LOCATION_EXTRA)
 end
 
--- Effect 1: Condition
+-- Effect 1: Trigger condition (Main Phase banish)
 function s.retcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetCurrentPhase()>=PHASE_MAIN1 and Duel.GetCurrentPhase()<=PHASE_MAIN2
 		and eg:IsExists(Card.IsFaceup,1,nil)
 end
--- Target 1 face-up banished card
+
+-- Effect 1: Targeting and operation
 function s.rettg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsFaceup() and chkc:IsLocation(LOCATION_REMOVED) and chkc:IsAbleToGrave() end
 	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToGrave,tp,LOCATION_REMOVED,LOCATION_REMOVED,1,nil) end
@@ -64,7 +67,6 @@ function s.rettg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local g=Duel.SelectTarget(tp,Card.IsAbleToGrave,tp,LOCATION_REMOVED,LOCATION_REMOVED,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,1,0,0)
 end
--- Return to GY
 function s.retop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) then
@@ -72,24 +74,22 @@ function s.retop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- Effect 2: Condition (Battle Phase only)
+-- Effect 2: Battle Phase stat equalizer
 function s.adcon(e,tp,eg,ep,ev,re,r,rp)
 	local ph=Duel.GetCurrentPhase()
 	return ph>=PHASE_BATTLE_START and ph<=PHASE_BATTLE
 end
--- Effect 2: Operation
 function s.adop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local g=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsMonster),tp,LOCATION_MZONE,LOCATION_MZONE,nil)
-	if #g==0 then return end
+	if #g==0 or not c:IsFaceup() or not c:IsRelateToEffect(e) then return end
 	local atk=g:GetMaxGroup(Card.GetAttack):GetFirst():GetAttack()
 	local def=g:GetMaxGroup(Card.GetDefense):GetFirst():GetDefense()
-	if not c:IsRelateToEffect(e) or not c:IsFaceup() then return end
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_SET_ATTACK_FINAL)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 	e1:SetValue(atk)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetCode(EFFECT_SET_DEFENSE_FINAL)
@@ -97,7 +97,13 @@ function s.adop(e,tp,eg,ep,ev,re,r,rp)
 	c:RegisterEffect(e2)
 end
 
--- Effect 3: Add Branded S/T from GY or banished
+-- Effect 3: Floating condition
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return (c:IsReason(REASON_EFFECT) or c:IsReason(REASON_BATTLE)) and c:IsPreviousLocation(LOCATION_ONFIELD)
+end
+
+-- Effect 3: Add "Branded" Spell/Trap from GY or banished
 function s.thfilter(c)
 	return c:IsSetCard(0x1ba) and c:IsSpellTrap() and c:IsAbleToHand()
 end
