4cda430e3e035792f365b3724cdc4d4d27dc0763
Chips <aminekhoury0411@gmail.com>
2025-05-16 09:58:59 -0400

    Update c1034.lua

--- script/c1034.lua
+++ script/c1034.lua
@@ -1,56 +1,58 @@
 --Despian Director Logeion
 local s,id=GetID()
 function s.initial_effect(c)
-	c:EnableReviveLimit()
+	--Xyz Summon
 	Xyz.AddProcedure(c,nil,11,2)
+	c:EnableReviveLimit()
 
-	--ATK/DEF Reduction on opponent activation
+	--Effect 1: Reduce ATK/DEF of opponent's monsters after each chain
 	local e1a=Effect.CreateEffect(c)
 	e1a:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1a:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e1a:SetCode(EVENT_CHAINING)
 	e1a:SetRange(LOCATION_MZONE)
 	e1a:SetOperation(aux.chainreg)
 	c:RegisterEffect(e1a)
+
 	local e1b=Effect.CreateEffect(c)
 	e1b:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1b:SetProperty(EFFECT_FLAG_DELAY)
 	e1b:SetCode(EVENT_CHAIN_SOLVED)
 	e1b:SetRange(LOCATION_MZONE)
-	e1b:SetCondition(function(e,tp,eg,ep,ev,re,r,rp)
-		return rp==1-tp and e:GetHandler():HasFlagEffect(1)
-	end)
-	e1b:SetOperation(s.atkdrop)
+	e1b:SetCondition(function(e) return e:GetHandler():HasFlagEffect(1) end)
+	e1b:SetOperation(s.atkdefop)
 	c:RegisterEffect(e1b)
 
-	--If monster becomes 0 ATK/DEF: detach and banish
+	--Effect 2: Detach and banish monsters with 0 ATK/DEF
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_REMOVE)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_ADJUST)
 	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id)
 	e2:SetCondition(s.rmcon)
+	e2:SetCost(s.rmcost)
+	e2:SetTarget(s.rmtg)
 	e2:SetOperation(s.rmop)
 	c:RegisterEffect(e2)
 
-	--If sent to GY: Special Summon Level 8 Despia with battle protection
+	--Effect 3: If sent to GY, revive a Level 8 Despia and give it battle protection
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e3:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
 	e3:SetCode(EVENT_TO_GRAVE)
-	e3:SetCountLimit(1,id)
+	e3:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e3:SetCountLimit(1,{id,1})
 	e3:SetTarget(s.sptg)
 	e3:SetOperation(s.spop)
 	c:RegisterEffect(e3)
 end
-s.listed_series={0x166}
-s.xyz_number=0
 
--- e1b: ATK/DEF drop on chain resolve
-function s.atkdrop(e,tp,eg,ep,ev,re,r,rp)
+function s.atkdefop(e,tp,eg,ep,ev,re,r,rp)
+	if rp~=1-tp then return end
 	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
-	if #g==0 then return end
 	for tc in g:Iter() do
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
@@ -64,30 +66,34 @@ function s.atkdrop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- e2: Detach 1 to banish any monster whose ATK or DEF became 0
 function s.rmcon(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(s.rmfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
-	return #g>0 and e:GetHandler():GetOverlayCount()>0
+	return Duel.IsExistingMatchingCard(s.rmfilter,tp,0,LOCATION_MZONE,1,nil)
+end
+function s.rmcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
 end
 function s.rmfilter(c)
-	return c:IsFaceup() and (c:GetAttack()==0 or c:GetDefense()==0) and c:IsAbleToRemove()
+	return c:IsFaceup() and (c:IsAttack(0) or c:IsDefense(0)) and c:IsAbleToRemove()
+end
+function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.rmfilter,tp,0,LOCATION_MZONE,1,nil) end
+	local g=Duel.GetMatchingGroup(s.rmfilter,tp,0,LOCATION_MZONE,nil)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,#g,0,0)
 end
 function s.rmop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local g=Duel.GetMatchingGroup(s.rmfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
-	if #g>0 and Duel.CheckRemoveOverlayCard(tp,1,0,1,REASON_EFFECT) then
-		Duel.RemoveOverlayCard(tp,1,0,1,1,REASON_EFFECT)
+	local g=Duel.GetMatchingGroup(s.rmfilter,tp,0,LOCATION_MZONE,nil)
+	if #g>0 then
 		Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
 	end
 end
 
--- e3: If sent to GY: revive 1 Level 8 Despia, give it battle protection
 function s.spfilter(c,e,tp)
 	return c:IsSetCard(0x166) and c:IsLevel(8) and not c:IsCode(id)
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED) and s.spfilter(chkc,e,tp) end
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED) and chkc:IsControler(tp) and s.spfilter(chkc,e,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
@@ -96,12 +102,14 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)>0 then
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-		e1:SetValue(1)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		tc:RegisterEffect(e1)
+	if tc and tc:IsRelateToEffect(e) then
+		if Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)>0 then
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+			e1:SetValue(1)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e1)
+		end
 	end
 end
