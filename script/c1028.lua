2178d07bc785ba66b7d190a6c9b8e79a535675de
Chips <aminekhoury0411@gmail.com>
2025-05-15 22:58:14 -0400

    Update c1028.lua

--- script/c1028.lua
+++ script/c1028.lua
@@ -2,12 +2,22 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	aux.AddRitualProcGreaterCode(c,aux.FilterBoolFunction(Card.IsCode,60921537),nil,true,true) -- Dogmatikamacabre
-	-- Burn when opponent's Extra Deck loses cards
+
+	-- Ritual Summon restriction: Only via "Dogmatikamacabre" or a Dogmatika card
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e0:SetValue(function(e,se,sp,st)
+		return se and (se:GetHandler():IsCode(60921537) or se:GetHandler():IsSetCard(0x146))
+	end)
+	c:RegisterEffect(e0)
+
+	-- Burn effect: Each time a card leaves opponent's Extra Deck, inflict 500 damage
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_TO_GRAVE)
 	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EVENT_TO_GRAVE)
 	e1:SetCondition(s.damcon)
 	e1:SetOperation(s.damop)
 	c:RegisterEffect(e1)
@@ -21,11 +31,12 @@ function s.initial_effect(c)
 	e1d:SetCode(EVENT_TO_DECK)
 	c:RegisterEffect(e1d)
 
-	-- End Phase removal effect
+	-- End Phase: If this card was sent from field to GY this turn, target 1 card on the field and send it to the GY
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_TOGRAVE)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
 	e2:SetCode(EVENT_PHASE+PHASE_END)
 	e2:SetRange(LOCATION_GRAVE)
 	e2:SetCountLimit(1,id)
@@ -35,10 +46,11 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 
-s.listed_names={60921537}
-s.listed_series={SET_DOGMATIKA, SET_DESPIA}
+-- IDs
+s.listed_names={60921537} -- Dogmatikamacabre
+s.listed_series={0x146, 0x153} -- Dogmatika, Despia
 
--- Burn condition: card leaves opponent's Extra Deck
+-- Burn effect helpers
 function s.damfilter(c,tp)
 	return c:GetPreviousLocation()==LOCATION_EXTRA and c:GetPreviousControler()==1-tp
 end
@@ -52,13 +64,13 @@ function s.damop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- End Phase GY effect condition
+-- GY end phase trigger condition
 function s.tgcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsReason(REASON_EFFECT+REASON_BATTLE) and c:GetTurnID()==Duel.GetTurnCount()
+	return c:IsPreviousLocation(LOCATION_ONFIELD) and c:GetTurnID()==Duel.GetTurnCount()
 end
 
--- Targeting and sending
+-- Targeting logic
 function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsOnField() and chkc:IsAbleToGrave() end
 	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToGrave,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
