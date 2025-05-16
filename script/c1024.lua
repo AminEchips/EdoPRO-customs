c6959b398be17f7cc63a8432246a4b818051524a
Chips <aminekhoury0411@gmail.com>
2025-05-15 21:35:33 -0400

    Update c1024.lua

--- script/c1024.lua
+++ script/c1024.lua
@@ -3,7 +3,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Link Summon procedure
 	c:EnableReviveLimit()
-	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_BEAST+RACE_BEASTWARRIOR+RACE_WINDBEAST),2,2)
+	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_BEAST+RACE_BEASTWARRIOR+RACE_WINGEDBEAST),2,2)
 
 	--Quick Effect: Banish and Special Summon
 	local e1=Effect.CreateEffect(c)
@@ -20,38 +20,38 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 
--- Cost: Banish 1 Beast, Beast-Warrior, or Winged Beast or "Fallen of Albaz" from hand or field
+-- Cost: Banish 1 Beast, Beast-Warrior, Winged Beast, or "Fallen of Albaz" from hand or field
 function s.cfilter(c)
-	return c:IsRace(RACE_BEAST+RACE_BEASTWARRIOR+RACE_WINDBEAST) or c:IsCode(68468459) -- Fallen of Albaz
+	return (c:IsRace(RACE_BEAST+RACE_BEASTWARRIOR+RACE_WINGEDBEAST) or c:IsCode(68468459)) and c:IsAbleToRemoveAsCost()
 end
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
 	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,1,nil)
-	e:SetLabelObject(g:GetFirst())
+	e:SetLabel(g:GetFirst():GetOriginalType())
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
 
 -- Target for Special Summon from banished zone
-function s.spfilter(c,e,tp)
-	return c:IsFaceup() and c:IsRace(RACE_BEAST+RACE_BEASTWARRIOR+RACE_WINDBEAST) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+function s.spfilter(c,e,tp,typ)
+	return c:IsFaceup() and c:IsRace(RACE_BEAST+RACE_BEASTWARRIOR+RACE_WINGEDBEAST)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and not c:IsType(typ)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
+		local typ=e:GetLabel()
 		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-			and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp)
+			and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp,typ)
 	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_REMOVED)
 end
 
--- Operation: Special Summon 1 banished monster with different Type than cost
+-- Operation: Special Summon 1 banished monster with different Type than the banished cost
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local typ=e:GetLabel()
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	local costcard=e:GetLabelObject()
-	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_REMOVED,0,nil,e,tp)
-	g=g:Filter(function(c)
-		return not costcard:IsType(c:GetType())
-	end,nil)
+	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_REMOVED,0,nil,e,tp,typ)
 	if #g==0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local sg=g:Select(tp,1,1,nil)
