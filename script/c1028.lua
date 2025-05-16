1b0304295b6c647885ca2e1d8fd730dfb2b219f3
Chips <aminekhoury0411@gmail.com>
2025-05-15 23:00:19 -0400

    Update c1028.lua

--- script/c1028.lua
+++ script/c1028.lua
@@ -3,17 +3,18 @@ local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
 
-	-- Ritual Summon restriction: Only via "Dogmatikamacabre" or a Dogmatika card
+	-- Self-Ritual Summon from hand using Fusion/Synchro/Dogmatika/Despia monsters
 	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_SINGLE)
-	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e0:SetValue(function(e,se,sp,st)
-		return se and (se:GetHandler():IsCode(60921537) or se:GetHandler():IsSetCard(0x146))
-	end)
+	e0:SetDescription(aux.Stringid(id,0))
+	e0:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e0:SetType(EFFECT_TYPE_IGNITION)
+	e0:SetRange(LOCATION_HAND)
+	e0:SetCountLimit(1,id)
+	e0:SetTarget(s.ritualtg)
+	e0:SetOperation(s.ritualop)
 	c:RegisterEffect(e0)
 
-	-- Burn effect: Each time a card leaves opponent's Extra Deck, inflict 500 damage
+	-- Burn effect: Each time a card leaves opponent's Extra Deck
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetRange(LOCATION_MZONE)
@@ -21,25 +22,19 @@ function s.initial_effect(c)
 	e1:SetCondition(s.damcon)
 	e1:SetOperation(s.damop)
 	c:RegisterEffect(e1)
-	local e1b=e1:Clone()
-	e1b:SetCode(EVENT_REMOVE)
-	c:RegisterEffect(e1b)
-	local e1c=e1:Clone()
-	e1c:SetCode(EVENT_TO_HAND)
-	c:RegisterEffect(e1c)
-	local e1d=e1:Clone()
-	e1d:SetCode(EVENT_TO_DECK)
-	c:RegisterEffect(e1d)
+	local e1b=e1:Clone() e1b:SetCode(EVENT_REMOVE) c:RegisterEffect(e1b)
+	local e1c=e1:Clone() e1c:SetCode(EVENT_TO_HAND) c:RegisterEffect(e1c)
+	local e1d=e1:Clone() e1d:SetCode(EVENT_TO_DECK) c:RegisterEffect(e1d)
 
-	-- End Phase: If this card was sent from field to GY this turn, target 1 card on the field and send it to the GY
+	-- End Phase: If sent from field to GY this turn, target 1 card on the field; send it to GY
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TOGRAVE)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
 	e2:SetCode(EVENT_PHASE+PHASE_END)
 	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCountLimit(1,id)
+	e2:SetCountLimit(1,id+100)
 	e2:SetCondition(s.tgcon)
 	e2:SetTarget(s.tgtg)
 	e2:SetOperation(s.tgop)
@@ -47,10 +42,35 @@ function s.initial_effect(c)
 end
 
 -- IDs
-s.listed_names={60921537} -- Dogmatikamacabre
-s.listed_series={0x146, 0x153} -- Dogmatika, Despia
+s.listed_names={60921537}
+s.listed_series={0x146, 0x153} -- Dogmatika and Despia
 
--- Burn effect helpers
+-- Custom Ritual Summon Target
+function s.matfilter(c)
+	return c:IsFaceup() and (c:IsType(TYPE_FUSION) or c:IsType(TYPE_SYNCHRO) or c:IsSetCard(0x146) or c:IsSetCard(0x153)) and c:IsReleasable()
+end
+function s.ritualtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then
+		if not Duel.IsPlayerCanRelease(tp) then return false end
+		local g=Duel.GetMatchingGroup(s.matfilter,tp,LOCATION_MZONE+LOCATION_HAND,0,nil)
+		return c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,false,true) and g:CheckWithSumGreater(Card.GetLevel,11,c)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,LOCATION_HAND)
+end
+function s.ritualop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetMatchingGroup(s.matfilter,tp,LOCATION_MZONE+LOCATION_HAND,0,nil)
+	if not c:IsRelateToEffect(e) or not c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,false,true) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	local rg=g:SelectWithSumGreater(tp,Card.GetLevel,11,c)
+	if not rg or #rg==0 then return end
+	Duel.Release(rg,REASON_EFFECT+REASON_RITUAL)
+	Duel.SpecialSummon(c,SUMMON_TYPE_RITUAL,tp,tp,false,true,POS_FACEUP)
+	c:CompleteProcedure()
+end
+
+-- Burn effect logic
 function s.damfilter(c,tp)
 	return c:GetPreviousLocation()==LOCATION_EXTRA and c:GetPreviousControler()==1-tp
 end
@@ -64,13 +84,11 @@ function s.damop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- GY end phase trigger condition
+-- End Phase effect condition and operation
 function s.tgcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return c:IsPreviousLocation(LOCATION_ONFIELD) and c:GetTurnID()==Duel.GetTurnCount()
 end
-
--- Targeting logic
 function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsOnField() and chkc:IsAbleToGrave() end
 	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToGrave,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
