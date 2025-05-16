847837c346b383b2c9d1a187e97b7d7e80ed13fe
Chips <aminekhoury0411@gmail.com>
2025-05-15 23:08:04 -0400

    Create c1029.lua

---
+++ script/c1029.lua
@@ -0,0 +1,115 @@
+--Aluber the Reaper of Despia
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,62962630,s.matfilter)
+
+	-- Effect 1: If a card is banished face-up during the Main Phase, target 1 of them and return it to the GY
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOGRAVE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_REMOVE)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.retcon)
+	e1:SetTarget(s.rettg)
+	e1:SetOperation(s.retop)
+	c:RegisterEffect(e1)
+
+	-- Effect 2: During the Battle Phase (Quick Effect), ATK/DEF become equal to highest on field
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
+	e2:SetCondition(s.adcon)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetOperation(s.adop)
+	c:RegisterEffect(e2)
+
+	-- Effect 3: If sent to GY, add 1 "Branded" Spell/Trap from GY or banished to hand
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_TOHAND)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_TO_GRAVE)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCountLimit(1,{id,2})
+	e3:SetTarget(s.thtg)
+	e3:SetOperation(s.thop)
+	c:RegisterEffect(e3)
+end
+
+s.listed_names={62962630}
+s.listed_series={0x166, 0x1ba} -- Despia, Branded
+
+-- Fusion Material filter: 1 monster Special Summoned from Extra Deck
+function s.matfilter(c,scard,sumtype,tp)
+	return c:IsSummonLocation(LOCATION_EXTRA)
+end
+
+-- Effect 1: Condition
+function s.retcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentPhase()>=PHASE_MAIN1 and Duel.GetCurrentPhase()<=PHASE_MAIN2
+		and eg:IsExists(Card.IsFaceup,1,nil)
+end
+-- Target 1 face-up banished card
+function s.rettg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsFaceup() and chkc:IsLocation(LOCATION_REMOVED) and chkc:IsAbleToGrave() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToGrave,tp,LOCATION_REMOVED,LOCATION_REMOVED,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToGrave,tp,LOCATION_REMOVED,LOCATION_REMOVED,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,1,0,0)
+end
+-- Return to GY
+function s.retop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.SendtoGrave(tc,REASON_EFFECT+REASON_RETURN)
+	end
+end
+
+-- Effect 2: Condition (Battle Phase only)
+function s.adcon(e,tp,eg,ep,ev,re,r,rp)
+	local ph=Duel.GetCurrentPhase()
+	return ph>=PHASE_BATTLE_START and ph<=PHASE_BATTLE
+end
+-- Effect 2: Operation
+function s.adop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsMonster),tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	if #g==0 then return end
+	local atk=g:GetMaxGroup(Card.GetAttack):GetFirst():GetAttack()
+	local def=g:GetMaxGroup(Card.GetDefense):GetFirst():GetDefense()
+	if not c:IsRelateToEffect(e) or not c:IsFaceup() then return end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	e1:SetValue(atk)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_SET_DEFENSE_FINAL)
+	e2:SetValue(def)
+	c:RegisterEffect(e2)
+end
+
+-- Effect 3: Add Branded S/T from GY or banished
+function s.thfilter(c)
+	return c:IsSetCard(0x1ba) and c:IsSpellTrap() and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE+LOCATION_REMOVED)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
