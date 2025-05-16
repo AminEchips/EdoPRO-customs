bde896583c20f765844703aec8bf6f22a599253c
Chips <aminekhoury0411@gmail.com>
2025-05-15 20:45:25 -0400

    Update c1024.lua

--- script/c1024.lua
+++ script/c1024.lua
@@ -14,7 +14,6 @@ function s.initial_effect(c)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1,id)
 	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
-	e1:SetTarget(s.bantg)
 	e1:SetOperation(s.banop)
 	c:RegisterEffect(e1)
 
@@ -23,7 +22,7 @@ function s.initial_effect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TODECK)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
 	e2:SetCode(EVENT_TO_GRAVE)
 	e2:SetCountLimit(1,{id,1})
 	e2:SetTarget(s.tdtg)
@@ -35,18 +34,16 @@ s.listed_names={68468459} -- Fallen of Albaz
 s.listed_series={0x114} -- Tri-Brigade
 
 function s.banfilter(c)
-	return c:IsMonster() and (c:IsRace(RACE_BEAST+RACE_BEASTWARRIOR+RACE_WINDBEAST) or c:IsCode(68468459)) and c:IsAbleToRemove()
-end
-function s.bantg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.banfilter,tp,LOCATION_DECK,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_DECK)
+	return c:IsMonster() and (c:IsRace(RACE_BEAST) or c:IsRace(RACE_BEASTWARRIOR) or c:IsRace(RACE_WINDBEAST) or c:IsCode(68468459)) and c:IsAbleToRemove()
 end
 function s.banop(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.IsExistingMatchingCard(s.banfilter,tp,LOCATION_DECK,0,1,nil) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
 	local g=Duel.SelectMatchingCard(tp,s.banfilter,tp,LOCATION_DECK,0,1,1,nil)
-	if #g>0 and Duel.Remove(g,POS_FACEUP,REASON_EFFECT)>0 then
+	local tc=g:GetFirst()
+	if tc and Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)>0 then
 		local c=e:GetHandler()
-		local lv=g:GetFirst():GetLevel()
+		local lv=tc:GetLevel()
 		if lv>0 and c:IsRelateToEffect(e) and c:IsFaceup() then
 			local atk=lv*100
 			local e1=Effect.CreateEffect(c)
@@ -71,13 +68,16 @@ end
 function s.tdfilter(c,types)
 	return c:IsMonster() and c:IsFaceup() and not types[c:GetRace()] and c:IsAbleToDeck()
 end
-function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_REMOVED,0,nil)
-	if chk==0 then
-		local types={}
-		return g:GetClassCount(Card.GetRace)>=3 and g:CheckSubGroup(s.tdcheck,3,3)
-	end
-	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,3,tp,LOCATION_REMOVED)
+	if chkc then return g:IsContains(chkc) end
+	if chk==0 then return g:CheckSubGroup(s.tdcheck,3,3) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local sg=g:SelectSubGroup(tp,s.tdcheck,false,3,3)
+	if not sg then return false end
+	Duel.SetTargetCard(sg)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,sg,#sg,0,0)
+	return true
 end
 function s.tdcheck(g)
 	local types={}
@@ -89,9 +89,8 @@ function s.tdcheck(g)
 	return true
 end
 function s.tdop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(s.tdfilter,tp,LOCATION_REMOVED,0,nil,{}):Filter(Card.IsAbleToDeck,nil)
-	if #g<3 then return end
-	local sg=g:SelectSubGroup(tp,s.tdcheck,false,3,3)
-	if not sg then return end
-	Duel.SendtoDeck(sg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,e:GetHandler())
+	if #g>0 then
+		Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+	end
 end
