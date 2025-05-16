fa76a33edd7876b90008df0dab5ec720268828f8
Chips <aminekhoury0411@gmail.com>
2025-05-15 21:34:13 -0400

    Update c1024.lua

--- script/c1024.lua
+++ script/c1024.lua
@@ -1,101 +1,61 @@
 --Tri-Brigade Bardawulf the Guiding Glasser
 local s,id=GetID()
 function s.initial_effect(c)
-	--Link Summon
-	Link.AddProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x114),2,2)
+	--Link Summon procedure
 	c:EnableReviveLimit()
+	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_BEAST+RACE_BEASTWARRIOR+RACE_WINDBEAST),2,2)
 
-	--Banish for ATK gain and protection (Quick Effect)
+	--Quick Effect: Banish and Special Summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_REMOVE+CATEGORY_ATKCHANGE)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1,id)
 	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
-	e1:SetOperation(s.banop)
+	e1:SetCountLimit(1,id)
+	e1:SetCost(s.spcost)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-
-	--Recycle 3 monsters with different Types if sent to GY
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_TODECK)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetProperty(EFFECT_FLAG_DELAY)
-	e2:SetCode(EVENT_TO_GRAVE)
-	e2:SetCountLimit(1,{id,1})
-	e2:SetTarget(s.tdtg)
-	e2:SetOperation(s.tdop)
-	c:RegisterEffect(e2)
 end
 
-s.listed_names={68468459} -- Fallen of Albaz
-s.listed_series={0x114} -- Tri-Brigade
-
--- Constants for multi-race check
-RACES_BEAST_BWARRIOR_WINGB = RACE_BEAST+RACE_BEASTWARRIOR+RACE_WINDBEAST
-
-function s.banfilter(c)
-	return c:IsMonster() and c:IsAbleToRemove() and 
-		(bit.band(c:GetRace(), RACES_BEAST_BWARRIOR_WINGB)~=0 or c:IsCode(68468459))
+-- Cost: Banish 1 Beast, Beast-Warrior, or Winged Beast or "Fallen of Albaz" from hand or field
+function s.cfilter(c)
+	return c:IsRace(RACE_BEAST+RACE_BEASTWARRIOR+RACE_WINDBEAST) or c:IsCode(68468459) -- Fallen of Albaz
 end
-function s.banop(e,tp,eg,ep,ev,re,r,rp)
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,s.banfilter,tp,LOCATION_DECK,0,1,1,nil)
-	local tc=g:GetFirst()
-	if tc and Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)>0 then
-		local c=e:GetHandler()
-		local lv=tc:GetLevel()
-		if lv>0 and c:IsRelateToEffect(e) and c:IsFaceup() then
-			local atk=lv*100
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_UPDATE_ATTACK)
-			e1:SetValue(atk)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
-			c:RegisterEffect(e1)
-			--Tri-Brigade protection
-			local e2=Effect.CreateEffect(c)
-			e2:SetType(EFFECT_TYPE_FIELD)
-			e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-			e2:SetTargetRange(LOCATION_MZONE,0)
-			e2:SetTarget(function(e,c) return c:IsSetCard(0x114) end)
-			e2:SetValue(1)
-			e2:SetReset(RESET_PHASE+PHASE_END)
-			Duel.RegisterEffect(e2,tp)
-		end
-	end
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,1,nil)
+	e:SetLabelObject(g:GetFirst())
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
 
-function s.tdfilter(c)
-	return c:IsMonster() and c:IsFaceup() and c:IsAbleToDeck()
+-- Target for Special Summon from banished zone
+function s.spfilter(c,e,tp)
+	return c:IsFaceup() and c:IsRace(RACE_BEAST+RACE_BEASTWARRIOR+RACE_WINDBEAST) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.tdcheck(g)
-	local seen = {}
-	for tc in aux.Next(g) do
-		local race = tc:GetRace()
-		for _,r in ipairs({RACE_BEAST,RACE_BEASTWARRIOR,RACE_WINDBEAST}) do
-			if bit.band(race, r)~=0 then
-				if seen[r] then return false end
-				seen[r] = true
-				break
-			end
-		end
-	end
-	return true
-end
-function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
-		local g=Duel.GetMatchingGroup(s.tdfilter,tp,LOCATION_REMOVED,0,nil)
-		return g:CheckSubGroup(s.tdcheck,3,3)
+		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+			and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp)
 	end
-	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,3,tp,LOCATION_REMOVED)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_REMOVED)
 end
-function s.tdop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(s.tdfilter,tp,LOCATION_REMOVED,0,nil)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local sg=g:SelectSubGroup(tp,s.tdcheck,false,3,3)
-	if not sg then return end
-	Duel.SendtoDeck(sg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+
+-- Operation: Special Summon 1 banished monster with different Type than cost
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	local costcard=e:GetLabelObject()
+	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_REMOVED,0,nil,e,tp)
+	g=g:Filter(function(c)
+		return not costcard:IsType(c:GetType())
+	end,nil)
+	if #g==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sg=g:Select(tp,1,1,nil)
+	if #sg>0 then
+		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+	end
 end
