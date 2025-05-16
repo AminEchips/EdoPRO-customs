808806a06369effcd0513e5a02f04786bfef1327
Chips <aminekhoury0411@gmail.com>
2025-05-15 23:44:58 -0400

    Update c1030.lua

--- script/c1030.lua
+++ script/c1030.lua
@@ -3,15 +3,8 @@ local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
 	Fusion.AddProcMix(c,true,true,72272462,s.matfilter)
-	-- Must be Fusion Summoned
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_SINGLE)
-	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e0:SetValue(aux.fuslimit)
-	c:RegisterEffect(e0)
 
-	-- Your monsters cannot be destroyed by battle, except by Level 8+ Fusion Monsters
+	-- Monsters you control cannot be destroyed by battle, except by Level 8 or higher Fusion Monsters
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
@@ -20,7 +13,7 @@ function s.initial_effect(c)
 	e1:SetValue(s.indval)
 	c:RegisterEffect(e1)
 
-	-- Main Phase Quick Effect: Target 1 other Level 8+ Fusion; banish opponent’s monsters with less ATK
+	-- Quick Effect: During Main Phase, target 1 Level 8+ Fusion on field, banish all opponent monsters with less ATK
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_REMOVE)
@@ -28,12 +21,13 @@ function s.initial_effect(c)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
 	e2:SetRange(LOCATION_MZONE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetCountLimit(1,id)
 	e2:SetTarget(s.rmtg)
 	e2:SetOperation(s.rmop)
 	c:RegisterEffect(e2)
 
-	-- Start of Battle Phase: 1 monster loses ATK equal to total ATK of all monsters on the field
+	-- At start of either player's Battle Phase: Make 1 monster lose ATK equal to total ATK of monsters on the field
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_ATKCHANGE)
@@ -45,27 +39,28 @@ function s.initial_effect(c)
 	e3:SetOperation(s.atkop)
 	c:RegisterEffect(e3)
 end
+
 s.listed_names={72272462} -- Despian Quaeritis
 
--- Fusion material: LIGHT or DARK Fusion Monster
+-- Fusion Material: 1 LIGHT or DARK Fusion Monster
 function s.matfilter(c,scard,sumtype,tp)
 	return c:IsType(TYPE_FUSION) and (c:IsAttribute(ATTRIBUTE_LIGHT) or c:IsAttribute(ATTRIBUTE_DARK))
 end
 
--- e1: Indestructible by battle except by Level 8+ Fusion Monsters
+-- e1: Battle protection - not destroyed except by Level 8+ Fusion
 function s.indval(e,c)
 	return not (c:IsType(TYPE_FUSION) and c:IsLevelAbove(8))
 end
 
--- e2: Quick banish during Main Phase
-function s.rmfilter(c,e,tp)
-	return c:IsType(TYPE_FUSION) and c:IsLevelAbove(8) and c:IsFaceup()
+-- e2: Quick banish setup
+function s.rmfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_FUSION) and c:IsLevelAbove(8)
 end
 function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return false end
-	if chk==0 then return Duel.IsExistingMatchingCard(s.rmfilter,tp,LOCATION_MZONE,0,1,e:GetHandler()) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local g=Duel.SelectTarget(tp,s.rmfilter,tp,LOCATION_MZONE,0,1,1,e:GetHandler())
+	if chkc then return chkc:IsOnField() and s.rmfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.rmfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectTarget(tp,s.rmfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 	e:SetLabel(g:GetFirst():GetAttack())
 end
 function s.rmop(e,tp,eg,ep,ev,re,r,rp)
@@ -78,12 +73,12 @@ function s.rmop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- e3: Start of Battle Phase, reduce ATK
+-- e3: Reduce ATK at start of either Battle Phase
 function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE+LOCATION_MZONE,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE+LOCATION_MZONE,LOCATION_MZONE+LOCATION_MZONE,1,nil) end
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE+LOCATION_MZONE,LOCATION_MZONE+LOCATION_MZONE,nil)
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
 	local totatk=g:GetSum(Card.GetAttack)
 	if totatk==0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
