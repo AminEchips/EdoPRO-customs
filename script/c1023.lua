41d92a86bd7f6550f945fc64f764df035d5f006f
Chips <aminekhoury0411@gmail.com>
2025-05-15 20:34:58 -0400

    Update c1023.lua

--- script/c1023.lua
+++ script/c1023.lua
@@ -1,35 +1,34 @@
 --Spright Flash
 local s,id=GetID()
 function s.initial_effect(c)
-	--Xyz Summon
+	--Xyz Summon procedure
 	Xyz.AddProcedure(c,nil,2,2)
 	c:EnableReviveLimit()
 
-	--On Xyz Summon: Choose 1 of 2 effects
+	--On Xyz Summon: choose one effect
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
-	e1:SetCountLimit(1,id)
 	e1:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ) end)
 	e1:SetTarget(s.sumtg)
 	e1:SetOperation(s.sumop)
 	c:RegisterEffect(e1)
 
-	--If sent to GY or banished
+	--GY or banished trigger: revive another banished Xyz + itself
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_TO_GRAVE)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
-	e2:SetCode(EVENT_REMOVE)
-	e2:SetCountLimit(1,{id,1})
 	e2:SetTarget(s.revivetg)
 	e2:SetOperation(s.reviveop)
 	c:RegisterEffect(e2)
 	local e3=e2:Clone()
-	e3:SetCode(EVENT_TO_GRAVE)
+	e3:SetCode(EVENT_REMOVE)
 	c:RegisterEffect(e3)
 end
 
@@ -37,48 +36,53 @@ s.listed_names={68468459} -- Fallen of Albaz
 
 function s.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,0))
-	e:SetLabel(Duel.SelectOption(tp,aux.Stringid(id,2),aux.Stringid(id,3)))
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 function s.sumop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if e:GetLabel()==0 then
-		if c:CheckRemoveOverlayCard(tp,1,REASON_EFFECT) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-			and Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_DECK,0,1,nil,68468459) then
+	if not c:IsRelateToEffect(e) then return end
+	local opt=Duel.SelectOption(tp,aux.Stringid(id,2),aux.Stringid(id,3))
+	if opt==0 then
+		-- Detach and summon Albaz
+		if c:CheckRemoveOverlayCard(tp,1,REASON_EFFECT) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
 			c:RemoveOverlayCard(tp,1,1,REASON_EFFECT)
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-			local g=Duel.SelectMatchingCard(tp,Card.IsCode,tp,LOCATION_DECK,0,1,1,nil,68468459)
-			if #g>0 then Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP) end
+			local albaz=Duel.SelectMatchingCard(tp,function(c) return c:IsCode(68468459) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end,tp,LOCATION_DECK,0,1,1,nil):GetFirst()
+			if albaz then
+				Duel.SpecialSummon(albaz,0,tp,tp,false,false,POS_FACEUP)
+			end
 		end
 	else
-		local g=Duel.SelectMatchingCard(tp,function(c) return c:IsType(TYPE_FUSION) and c:IsAbleToRemove() end,tp,LOCATION_GRAVE,0,1,1,nil)
+		-- Banish Fusion and draw
+		local g=Duel.SelectMatchingCard(tp,function(c) return c:IsType(TYPE_FUSION) and c:IsAbleToRemove() and c:GetLevel()>=4 end,tp,LOCATION_GRAVE,0,1,1,nil)
 		if #g>0 then
 			local lv=g:GetFirst():GetLevel()
-			if Duel.Remove(g,POS_FACEUP,REASON_EFFECT)>0 and lv>=4 then
+			if Duel.Remove(g,POS_FACEUP,REASON_EFFECT)>0 then
 				Duel.Draw(tp,math.floor(lv/4),REASON_EFFECT)
 			end
 		end
 	end
 end
 
-function s.revivefilter(c)
-	return c:IsFaceup() and c:IsType(TYPE_XYZ) and c:IsAbleToExtra() and c:IsCanBeSpecialSummoned(nil,0,tp,false,false)
+function s.revfilter(c,e,tp)
+	return c:IsType(TYPE_XYZ) and c:IsFaceup() and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.revivetg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.revivefilter,tp,LOCATION_REMOVED,0,1,e:GetHandler()) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_REMOVED)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>1 
+		and Duel.IsExistingMatchingCard(s.revfilter,tp,LOCATION_REMOVED,0,1,e:GetHandler(),e,tp)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,LOCATION_REMOVED)
 end
 function s.reviveop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.revivefilter,tp,LOCATION_REMOVED,0,1,1,c)
-	local tg=g:GetFirst()
-	if tg and Duel.SpecialSummonStep(tg,0,tp,tp,false,false,POS_FACEUP) then
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
+	local tg=Duel.SelectMatchingCard(tp,s.revfilter,tp,LOCATION_REMOVED,0,1,1,c,e,tp):GetFirst()
+	if not tg then return end
+	if Duel.SpecialSummon(tg,0,tp,tp,false,false,POS_FACEUP)>0 then
 		Duel.BreakEffect()
 		Duel.SendtoDeck(tg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
-		if c:IsRelateToEffect(e) and c:IsReason(REASON_MATERIAL+REASON_FUSION) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-			and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP_DEFENSE)>0 then
-			-- Becomes Level 2
+		if c:IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
+			and c:IsPreviousLocation(LOCATION_ONFIELD+LOCATION_REMOVED) and c:GetReason()&REASON_MATERIAL>0 and c:GetReason()&REASON_FUSION>0 then
+			Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
 			local e1=Effect.CreateEffect(c)
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_CHANGE_LEVEL)
@@ -87,5 +91,4 @@ function s.reviveop(e,tp,eg,ep,ev,re,r,rp)
 			c:RegisterEffect(e1)
 		end
 	end
-	Duel.SpecialSummonComplete()
 end
