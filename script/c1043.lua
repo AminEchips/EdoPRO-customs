c99690bb119aa9b168077c9b5fce1bbd78a32b4a
Chips <aminekhoury0411@gmail.com>
2025-05-17 17:53:24 -0400

    Update c1043.lua

--- script/c1043.lua
+++ script/c1043.lua
@@ -1,8 +1,9 @@
 --Branded in Black
 --Scripted by Meuh
 local s,id=GetID()
+
 function s.initial_effect(c)
-	--Shuffle 2 Level 8+ Despia/Fusion monsters from GY into Deck, then draw
+	--Effect 1: Shuffle and draw
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
@@ -14,71 +15,85 @@ function s.initial_effect(c)
 	e1:SetOperation(s.tdop)
 	c:RegisterEffect(e1)
 
-	--GY Fusion Summon using banish, allow Deck material if opp activated a card/effect
-	local params = {
-		handler = c,
-		fusfilter = aux.FilterBoolFunction(Card.IsFusionAttribute,ATTRIBUTE_LIGHT+ATTRIBUTE_DARK),
-		matfilter = Card.IsAbleToRemove,
-		extrafil = s.extrafil,
-		extraop = Fusion.BanishMaterial,
-		extratg = s.extratg
-	}
-	local e2=Fusion.CreateSummonEff(params)
+	--Effect 2: GY Fusion Summon
+	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_GRAVE)
 	e2:SetCountLimit(1,{id,1})
-	e2:SetCost(function(e,tp,eg,ep,ev,re,r,rp,chk)
-		if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
-		Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
-	end)
+	e2:SetCondition(aux.exccon)
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(s.fustg)
+	e2:SetOperation(s.fusop)
 	c:RegisterEffect(e2)
 end
-s.listed_series={0x166} -- Despia
 
--- First effect: Shuffle & draw
+s.listed_series={0x166}
+
+--Effect 1: Shuffle 2 GY monsters and draw 1
 function s.tdfilter(c)
-	return c:IsType(TYPE_MONSTER) and (c:IsSetCard(0x166) or c:IsType(TYPE_FUSION)) and c:IsLevelAbove(8) and c:IsAbleToDeck()
+	return c:IsMonster() and (c:IsSetCard(0x166) or c:IsType(TYPE_FUSION)) and c:IsLevelAbove(8) and c:IsAbleToDeck()
 end
 function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.tdfilter(chkc) end
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and s.tdfilter(chkc) end
 	if chk==0 then return Duel.IsPlayerCanDraw(tp,1)
 		and Duel.IsExistingTarget(s.tdfilter,tp,LOCATION_GRAVE,0,2,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
 	local g=Duel.SelectTarget(tp,s.tdfilter,tp,LOCATION_GRAVE,0,2,2,nil)
-	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,#g,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,2,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 end
 function s.tdop(e,tp,eg,ep,ev,re,r,rp)
 	local tg=Duel.GetTargetCards(e)
 	if #tg<2 then return end
-	Duel.SendtoDeck(tg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
-	local g=Duel.GetOperatedGroup():Filter(Card.IsLocation,nil,LOCATION_DECK+LOCATION_EXTRA)
-	if #g>0 then
+	if Duel.SendtoDeck(tg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)==2 then
 		Duel.BreakEffect()
 		Duel.Draw(tp,1,REASON_EFFECT)
 	end
 end
 
--- GY Fusion: Deck use allowed if opp activated something
-function s.check(g1,g2)
-	return function(tp,sg,fc)
-		local c1=#(sg&g1)
-		local c2=#(sg&g2)
-		return c1<=1 and c2<=1,c1>1 or c2>1
-	end
+--Effect 2: GY Fusion Summon
+function s.fusfilter(c,e,tp,mg,f,chkf)
+	return c:IsType(TYPE_FUSION) and (c:IsAttribute(ATTRIBUTE_LIGHT) or c:IsAttribute(ATTRIBUTE_DARK))
+		and c:IsMonster() and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
+		and c:CheckFusionMaterial(mg,nil,chkf)
 end
-function s.extrafil(e,tp,mg)
-	if Duel.GetCurrentChain()<=0 then return nil end
-	local loc=LOCATION_DECK
-	if not Duel.IsPlayerAffectedByEffect(tp,CARD_SPIRIT_ELIMINATION) then
-		loc=loc|LOCATION_GRAVE
-	end
-	local g=Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToRemove),tp,loc,0,mg)
-	return g,s.check(g:Split(Card.IsLocation,nil,LOCATION_GRAVE))
+function s.fusmatfilter(c,e,tp)
+	return c:IsAbleToRemove() and c:IsMonster() and (c:IsLocation(LOCATION_HAND+LOCATION_GRAVE) or c:IsLocation(LOCATION_DECK))
 end
-function s.extratg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,0,tp,LOCATION_HAND|LOCATION_MZONE)
-	Duel.SetPossibleOperationInfo(0,CATEGORY_REMOVE,nil,0,tp,LOCATION_GRAVE|LOCATION_DECK)
+function s.fustg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local chkf=tp
+		local mg=Duel.GetMatchingGroup(s.fusmatfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,nil,e,tp)
+		if Duel.IsPlayerAffectedByEffect(tp,EFFECT_CHAIN_MATERIAL) then
+			local ce=Duel.GetChainMaterial(tp)
+			local mg2=ce:GetTarget()(ce,e,tp)
+			mg:Merge(mg2)
+		end
+		return Duel.IsExistingMatchingCard(s.fusfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg,nil,chkf)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function s.fusop(e,tp,eg,ep,ev,re,r,rp)
+	local chkf=tp
+	local mg=Duel.GetMatchingGroup(s.fusmatfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,nil,e,tp)
+	if Duel.GetCurrentChain()>0 then
+		local oppActs = Duel.GetChainInfo(0,CHAININFO_TRIGGERING_PLAYER)
+		if oppActs==1-tp then
+			local dg=Duel.GetMatchingGroup(s.fusmatfilter,tp,LOCATION_DECK,0,nil,e,tp)
+			mg:Merge(dg)
+		end
+	end
+	local sg=Duel.GetMatchingGroup(s.fusfilter,tp,LOCATION_EXTRA,0,nil,e,tp,mg,nil,chkf)
+	if #sg==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sc=sg:Select(tp,1,1,nil):GetFirst()
+	if not sc then return end
+	local mat=Duel.SelectFusionMaterial(tp,sc,mg,nil,chkf)
+	if not mat or #mat==0 then return end
+	sc:SetMaterial(mat)
+	Duel.Remove(mat,POS_FACEUP,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
+	Duel.SpecialSummon(sc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
+	sc:CompleteProcedure()
 end
