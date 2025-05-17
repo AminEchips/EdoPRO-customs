6bf26c7b906178717c2854ee731ffe45a7eb94e9
Chips <aminekhoury0411@gmail.com>
2025-05-17 16:26:41 -0400

    Update c1041.lua

--- script/c1041.lua
+++ script/c1041.lua
@@ -24,24 +24,27 @@ end
 
 s.listed_names={68468459} -- Fallen of Albaz
 
--- Fusion Summon Filters
-function s.fusfilter(c,e,tp)
-	return c:IsLevelBelow(8) and c:IsType(TYPE_FUSION) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
-end
+-- Fusion Materials filter
 function s.matfilter(c,e)
 	return c:IsCanBeFusionMaterial() and c:IsAbleToGrave() and not c:IsImmuneToEffect(e)
 end
 
--- Destruction cost: tribute a Fusion monster
+function s.fusfilter(c,e,tp,mat)
+	return c:IsType(TYPE_FUSION) and c:IsLevelBelow(8)
+		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
+		and c:CheckFusionMaterial(mat,nil,tp)
+end
+
 function s.costfilter(c)
 	return c:IsType(TYPE_FUSION) and c:IsReleasable()
 end
 
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return false end
 	local chk1=Duel.IsExistingMatchingCard(s.matfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,nil,e)
+	local mat=Duel.GetMatchingGroup(s.matfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,nil,e)
 	local chk2=Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_MZONE,0,1,nil)
 		and Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,2,nil)
+
 	if chk==0 then return chk1 or chk2 end
 	local opt=0
 	if chk1 and chk2 then
@@ -55,7 +58,8 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	end
 	e:SetLabel(opt)
 	if opt==0 then
-		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+		local fus=Duel.GetMatchingGroup(s.fusfilter,tp,LOCATION_EXTRA,0,nil,e,tp,mat)
+		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,fus,1,tp,LOCATION_EXTRA)
 	else
 		Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,2,0,LOCATION_ONFIELD)
 	end
@@ -65,16 +69,17 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local opt=e:GetLabel()
 	if opt==0 then
 		local mat=Duel.GetMatchingGroup(s.matfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,nil,e)
-		local fus=Duel.GetMatchingGroup(s.fusfilter,tp,LOCATION_EXTRA,0,nil,e,tp)
+		local fus=Duel.GetMatchingGroup(s.fusfilter,tp,LOCATION_EXTRA,0,nil,e,tp,mat)
 		if #mat==0 or #fus==0 then return end
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local tc=fus:Select(tp,1,1,nil):GetFirst()
 		if not tc then return end
+		local chkf=tp
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_MATERIAL)
-		local mat2=mat:Select(tp,1,99,nil)
+		local mat2=Duel.SelectFusionMaterial(tp,tc,mat,nil,chkf)
 		if #mat2==0 then return end
+		tc:SetMaterial(mat2)
 		Duel.SendtoGrave(mat2,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
-		Duel.BreakEffect()
 		if Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)>0 then
 			tc:CompleteProcedure()
 		end
@@ -95,24 +100,27 @@ function s.regop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsReason(REASON_COST) and re and re:IsActivated() and re:IsMonsterEffect()
 		and re:GetHandler():IsCode(68468459) then
-			local e1=Effect.CreateEffect(c)
-			e1:SetDescription(aux.Stringid(id,2))
-			e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-			e1:SetCode(EVENT_PHASE+PHASE_END)
-			e1:SetRange(LOCATION_GRAVE)
-			e1:SetCountLimit(1,{id,1})
-			e1:SetTarget(s.settg)
-			e1:SetOperation(s.setop)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-			c:RegisterEffect(e1)
+		local e1=Effect.CreateEffect(c)
+		e1:SetDescription(aux.Stringid(id,2))
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+		e1:SetCode(EVENT_PHASE+PHASE_END)
+		e1:SetRange(LOCATION_GRAVE)
+		e1:SetCountLimit(1,{id,1})
+		e1:SetTarget(s.settg)
+		e1:SetOperation(s.setop)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
 	end
 end
+
 function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
-	return chk==0 or e:GetHandler():IsSSetable()
+	if chk==0 then return e:GetHandler():IsSSetable() end
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,e:GetHandler(),1,0,0)
 end
+
 function s.setop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) then
+	if c:IsRelateToEffect(e) and c:IsSSetable() then
 		Duel.SSet(tp,c)
 	end
 end
