7de3b2a67d6dd2dad59f88b4342ea0e2f101f68e
Chips <aminekhoury0411@gmail.com>
2025-05-17 16:51:05 -0400

    Create c1042.lua

---
+++ script/c1042.lua
@@ -0,0 +1,99 @@
+--Branded Evolution
+--Scripted by Meuh
+local s,id=GetID()
+
+function s.initial_effect(c)
+	--Activate: Tribute 2 LIGHT/DARK monsters including a "Despia" to Summon "Masquerade the Blazing Dragon"
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+
+	--GY effect: Banish to send 1 "Despia" monster from Deck/Extra to GY if Fusion destroyed
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetOperation(s.regop)
+	c:RegisterEffect(e2)
+end
+
+s.listed_names={68584890,06855503} -- Fallen of Albaz, Masquerade
+s.listed_series={0x166} -- Despia
+
+function s.costfilter(c,tp)
+	return c:IsAttribute(ATTRIBUTE_LIGHT+ATTRIBUTE_DARK) and c:IsReleasable()
+end
+function s.despiafilter(c)
+	return c:IsSetCard(0x166) and c:IsReleasable()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_MZONE,0,2,nil,tp)
+			and Duel.IsExistingMatchingCard(s.despiafilter,tp,LOCATION_MZONE,0,1,nil)
+			and Duel.GetLocationCountFromEx(tp)>0
+	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_MZONE,0,2,2,nil,tp)
+	if #g<2 or not g:IsExists(Card.IsSetCard,1,nil,0x166) then return false end
+	Duel.Release(g,REASON_COST)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCountFromEx(tp)<=0 then return end
+	if not Duel.IsPlayerCanSpecialSummon(tp) then return end
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstMatchingCard(function(c,e,tp)
+		return c:IsCode(06855503) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
+	end,tp,LOCATION_EXTRA,0,nil,e,tp)
+	if tc then
+		Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
+		tc:CompleteProcedure()
+	end
+end
+
+-- GY effect registration
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_TO_GRAVE)
+	e1:SetRange(LOCATION_GRAVE)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,{id,1})
+	e1:SetCondition(s.gycon)
+	e1:SetCost(s.gycost)
+	e1:SetTarget(s.gytg)
+	e1:SetOperation(s.gyop)
+	c:RegisterEffect(e1)
+end
+
+function s.cfilter(c,tp)
+	return c:IsPreviousControler(tp) and c:IsPreviousLocation(LOCATION_MZONE) and c:IsReason(REASON_BATTLE+REASON_EFFECT) and c:IsType(TYPE_FUSION)
+end
+function s.gycon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter,1,nil,tp)
+end
+function s.gycost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
+	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
+end
+function s.gyfilter(c)
+	return c:IsSetCard(0x166) and c:IsType(TYPE_MONSTER) and c:IsAbleToGrave()
+end
+function s.gytg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.gyfilter,tp,LOCATION_DECK+LOCATION_EXTRA,0,1,nil) end
+end
+function s.gyop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.gyfilter,tp,LOCATION_DECK+LOCATION_EXTRA,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoGrave(g,REASON_EFFECT)
+	end
+end
