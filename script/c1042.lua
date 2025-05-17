6f3065ef576d4e686d79b95748f8d41ca313f9ed
Chips <aminekhoury0411@gmail.com>
2025-05-17 17:21:02 -0400

    Update c1042.lua

--- script/c1042.lua
+++ script/c1042.lua
@@ -13,26 +13,38 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 
-	--GY effect: Banish to send 1 "Despia" monster from Deck/Extra to GY if Fusion leaves field
+	--GY effect: If a Fusion Monster you controlled left the field, banish this to send 1 "Despia" from Deck or Extra to GY
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_TO_GRAVE)
-	e2:SetOperation(s.regop)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOGRAVE)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_LEAVE_FIELD)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(s.gycon)
+	e2:SetCost(s.gycost)
+	e2:SetTarget(s.gytg)
+	e2:SetOperation(s.gyop)
 	c:RegisterEffect(e2)
 end
 
-s.listed_names={06855503} -- Fallen of Albaz, Masquerade
+s.listed_names={06855503} -- Masquerade
 s.listed_series={0x166} -- Despia
 
--- Activation effect
+-- Effect 1: Tribute for Fusion Summon
+function s.filter(c)
+	return c:IsAttribute(ATTRIBUTE_LIGHT+ATTRIBUTE_DARK) and c:IsFaceup() and c:IsReleasable()
+end
+function s.despiafilter(c)
+	return c:IsSetCard(0x166)
+end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(function(c)
-		return c:IsAttribute(ATTRIBUTE_LIGHT+ATTRIBUTE_DARK) and c:IsReleasable() and c:IsFaceup()
-	end,tp,LOCATION_MZONE,0,nil)
-	if #g<2 or not g:IsExists(Card.IsSetCard,1,nil,0x166) then return end
+	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,0,nil)
+	if #g<2 or not g:IsExists(s.despiafilter,1,nil) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
 	local rg=g:Select(tp,2,2,nil)
-	if #rg<2 or not rg:IsExists(Card.IsSetCard,1,nil,0x166) then return end
+	if #rg<2 or not rg:IsExists(s.despiafilter,1,nil) then return end
 	if Duel.Release(rg,REASON_EFFECT)==2 and Duel.GetLocationCountFromEx(tp)>0 then
 		local tc=Duel.GetFirstMatchingCard(function(c,e,tp)
 			return c:IsCode(06855503) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
@@ -44,25 +56,9 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- GY effect registration
-function s.regop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,1))
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_LEAVE_FIELD)
-	e1:SetRange(LOCATION_GRAVE)
-	e1:SetProperty(EFFECT_FLAG_DELAY)
-	e1:SetCountLimit(1,{id,1})
-	e1:SetCondition(s.gycon)
-	e1:SetCost(s.gycost)
-	e1:SetTarget(s.gytg)
-	e1:SetOperation(s.gyop)
-	c:RegisterEffect(e1)
-end
-
+-- Effect 2: GY trigger if Fusion monster leaves the field
 function s.cfilter(c,tp)
-	return c:IsPreviousControler(tp) and c:IsPreviousLocation(LOCATION_MZONE) and c:IsType(TYPE_FUSION)
+	return c:IsPreviousControler(tp) and c:IsPreviousLocation(LOCATION_MZONE) and c:IsType(TYPE_FUSION) and c:IsPreviousPosition(POS_FACEUP)
 end
 function s.gycon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(s.cfilter,1,nil,tp)
@@ -76,6 +72,7 @@ function s.gyfilter(c)
 end
 function s.gytg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.gyfilter,tp,LOCATION_DECK+LOCATION_EXTRA,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK+LOCATION_EXTRA)
 end
 function s.gyop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
