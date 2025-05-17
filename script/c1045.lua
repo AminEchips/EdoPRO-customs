d7543a1e8615bc0d247ff8d6d59b8ce006c51220
Chips <aminekhoury0411@gmail.com>
2025-05-17 18:06:56 -0400

    Update c1045.lua

--- script/c1045.lua
+++ script/c1045.lua
@@ -2,7 +2,7 @@
 --Scripted by Meuh
 local s,id=GetID()
 function s.initial_effect(c)
-	-- Activate: Reveal 1 "Tri-Brigade" monster in hand, send same Type non-Link from ED, SS and draw 1
+	-- Activate: Reveal Tri-Brigade, send same-Type non-Link, SS and draw
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOGRAVE+CATEGORY_DRAW)
@@ -13,7 +13,7 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 
-	-- GY Effect: trigger if Link monster is banished
+	-- GY effect: Trigger if Link monster is banished
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TOEXTRA+CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
@@ -30,12 +30,12 @@ end
 
 s.listed_series={0x14f} -- Tri-Brigade
 
--- Activation effect
+-- FIRST EFFECT
 function s.filter_hand(c,tp)
 	return c:IsSetCard(0x14f) and c:IsMonster() and Duel.IsExistingMatchingCard(s.filter_extra,tp,LOCATION_EXTRA,0,1,nil,c,tp)
 end
 function s.filter_extra(c,hc,tp)
-	return c:IsRace(hc:GetRace()) and not c:IsType(TYPE_LINK) and c:IsAbleToGrave() and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	return c:IsRace(hc:GetRace()) and not c:IsType(TYPE_LINK) and c:IsAbleToGrave()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.filter_hand,tp,LOCATION_HAND,0,1,nil,tp) end
@@ -62,38 +62,39 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- GY Trigger: When your Link monster is banished
+-- SECOND EFFECT
 function s.gycon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.cfilter,1,nil,tp)
-end
-function s.cfilter(c,tp)
-	return c:IsPreviousControler(tp) and c:IsPreviousLocation(LOCATION_MZONE)
-		and c:IsType(TYPE_LINK)
+	return eg:IsExists(function(c) return c:IsPreviousControler(tp) and c:IsPreviousLocation(LOCATION_MZONE)
+		and c:IsType(TYPE_LINK) and c:IsFaceup() end, 1, nil)
 end
 function s.gytg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_REMOVED,0,1,nil) and e:GetHandler():IsAbleToRemoveAsCost() end
-	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_REMOVED,0,nil)
+	if chkc then return chkc:IsLocation(LOCATION_REMOVED) and chkc:IsControler(tp) and chkc:IsType(TYPE_LINK) end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsType,tp,LOCATION_REMOVED,0,1,nil,TYPE_LINK)
+		and e:GetHandler():IsAbleToRemoveAsCost() end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	local tg=g:Select(tp,1,1,nil)
-	Duel.SetTargetCard(tg)
+	local g=Duel.SelectTarget(tp,Card.IsType,tp,LOCATION_REMOVED,0,1,1,nil,TYPE_LINK)
+	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
 end
 function s.gyop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if not c:IsRelateToEffect(e) or not tc or not tc:IsRelateToEffect(e) then return end
-	if Duel.Remove(c,POS_FACEUP,REASON_EFFECT)==0 then return end
-
-	if not tc:IsType(TYPE_LINK) or not tc:IsFaceup() then return end
-	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,2)) -- Choose: Return+Add or Special Summon
-	local opt=0
-	if tc:IsAbleToExtra() and c:IsAbleToHand() then opt=0
-	elseif tc:IsCanBeSpecialSummoned(e,0,tp,false,false) then opt=1
+	local c=e:GetHandler()
+	if not tc or not tc:IsRelateToEffect(e) then return end
+	local b1 = tc:IsAbleToExtra()
+	local b2 = tc:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	if b1 and b2 then
+		op=Duel.SelectOption(tp,aux.Stringid(id,2),aux.Stringid(id,3))
+	elseif b1 then
+		op=0
+	elseif b2 then
+		op=1
 	else return end
 
-	if opt==0 and Duel.SendtoDeck(tc,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)>0 then
-		Duel.BreakEffect()
-		Duel.SendtoHand(c,tp,REASON_EFFECT)
-	elseif opt==1 then
+	if op==0 then
+		if Duel.SendtoDeck(tc,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)>0 then
+			Duel.BreakEffect()
+			Duel.SendtoHand(c,tp,REASON_EFFECT)
+		end
+	else
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
