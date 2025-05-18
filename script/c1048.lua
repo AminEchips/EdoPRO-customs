fb886047429247cac64a38a6d73349eb17b7f0ec
Chips <aminekhoury0411@gmail.com>
2025-05-18 12:06:15 -0400

    Create c1048.lua

---
+++ script/c1048.lua
@@ -0,0 +1,97 @@
+--Spright Wacky
+--Scripted by Meuh
+local s,id=GetID()
+
+function s.initial_effect(c)
+	--Link Summon
+	Link.AddProcedure(c,nil,2,2,s.matcheck)
+	c:EnableReviveLimit()
+
+	--Cannot be used as Link Material this turn
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetCode(EFFECT_CANNOT_BE_LINK_MATERIAL)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK) end)
+	e0:SetValue(1)
+	e0:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	c:RegisterEffect(e0)
+
+	--On Link Summon: add or Special Summon a Spright
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK) end)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+
+	--Tribute and bounce S/T
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.bouncecon)
+	e2:SetCost(s.bouncecost)
+	e2:SetOperation(s.bounceop)
+	c:RegisterEffect(e2)
+
+	--Quick effect if opponent controls a Level/Rank/Link 3+
+	local e3=e2:Clone()
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetCondition(s.quickcon)
+	c:RegisterEffect(e3)
+end
+
+s.listed_series={0x181} -- Spright
+
+function s.matcheck(g,lc,sumtype,tp)
+	return g:IsExists(s.lvl2check,1,nil,lc,sumtype,tp)
+end
+function s.lvl2check(c,lc,sumtype,tp)
+	return c:IsLevel(2) or c:IsRank(2) or c:IsLink(2)
+end
+
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(0x181) and c:IsAbleToHand() and (not c:IsMonster() or c:IsCanBeSpecialSummoned(e,0,tp,false,false))
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	local tc=g:GetFirst()
+	if not tc then return end
+	if tc:IsMonster() and tc:IsCanBeSpecialSummoned(e,0,tp,false,false) and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	else
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,tc)
+	end
+end
+
+function s.bouncecon(e,tp,eg,ep,ev,re,r,rp)
+	return not s.quickcon(e,tp,eg,ep,ev,re,r,rp)
+end
+function s.quickcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(function(c)
+		return (c:IsLevelAbove(3) or c:IsRankAbove(3) or c:IsLinkAbove(3))
+	end,tp,0,LOCATION_MZONE,1,nil)
+end
+function s.bouncecost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroup(tp,Card.IsReleasable,1,nil) end
+	local g=Duel.SelectReleaseGroup(tp,Card.IsReleasable,1,1,nil)
+	Duel.Release(g,REASON_COST)
+end
+function s.bounceop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsSpellTrap,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+	if #g==0 then return end
+	Duel.SendtoHand(g,nil,REASON_EFFECT)
+end
