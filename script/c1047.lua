5ddb28014ab27e481e65a9398f28ca44202a9547
Chips <aminekhoury0411@gmail.com>
2025-05-18 01:15:21 -0400

    Update c1047.lua

--- script/c1047.lua
+++ script/c1047.lua
@@ -3,8 +3,8 @@
 local s,id=GetID()
 
 function s.initial_effect(c)
-	--Xyz Summon procedure (3 Level 2, Albaz or Link-2 count as 1)
-	Xyz.AddProcedure(c,s.mfilter,2,3)
+	--Xyz Summon procedure: 3 monsters, allow Albaz/Link-2 substitution
+	aux.AddXyzProcedureCustom(c,s.matfilter,nil,3,3,s.xyzcheck)
 	c:EnableReviveLimit()
 
 	--ATK/DEF boost + immunity if "Fallen of Albaz" is material
@@ -54,11 +54,19 @@ end
 s.listed_series={0x160,0x181,0x17b} -- Branded, Spright, Therion
 s.listed_names={68468459} -- Fallen of Albaz
 
--- XYZ Summon material filter
-function s.mfilter(c,sc,tp)
+-- Xyz Material Conditions
+function s.matfilter(c)
 	return c:IsLevel(2) or c:IsCode(68468459) or (c:IsType(TYPE_LINK) and c:GetLink()==2)
 end
+function s.xyzcheck(g,tp,xyz)
+	if #g~=3 then return false end
+	local albaz=g:FilterCount(Card.IsCode,nil,68468459)
+	local link2=g:FilterCount(function(c) return c:IsType(TYPE_LINK) and c:GetLink()==2 end,nil)
+	local level2=g:FilterCount(function(c) return c:IsLevel(2) and not c:IsType(TYPE_LINK) and not c:IsCode(68468459) end,nil)
+	return albaz<=1 and link2<=1 and (albaz + link2 + level2)==3
+end
 
+-- ATK/DEF boost + immunity
 function s.statcon(e)
 	return e:GetHandler():GetOverlayGroup():IsExists(Card.IsCode,1,nil,68468459)
 end
@@ -69,6 +77,7 @@ function s.efilter(e,te)
 	return te:IsActiveType(TYPE_MONSTER)
 end
 
+-- Destroy effect
 function s.descon(e,tp,eg,ep,ev,re,r,rp)
 	return rp==1-tp and re:IsActiveType(TYPE_MONSTER) and Duel.IsMainPhase()
 end
@@ -89,27 +98,24 @@ function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
+-- Float effect
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return c:IsSummonType(SUMMON_TYPE_XYZ) and c:IsPreviousLocation(LOCATION_ONFIELD) and c:GetReasonPlayer()~=tp
+	return c:IsSummonType(SUMMON_TYPE_XYZ) and c:IsPreviousLocation(LOCATION_ONFIELD)
 end
 function s.thfilter(c)
-	return (c:IsSetCard(0x160) or c:IsSetCard(0x181) or c:IsSetCard(0x17b)) and c:IsSpellTrap() and c:IsAbleToHand()
+	return (c:IsSetCard(0x160) or c:IsSetCard(0x181) or c:IsSetCard(0x17b)) and c:IsAbleToHand() and c:IsSpellTrap()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,2,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_DECK)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil)
-	if #g>=2 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local sg=g:Select(tp,2,2,nil)
-		if #sg==2 then
-			Duel.SendtoHand(sg,nil,REASON_EFFECT)
-			Duel.ConfirmCards(1-tp,sg)
-			Duel.BreakEffect()
-			Duel.DiscardHand(tp,aux.TRUE,1,1,REASON_EFFECT+REASON_DISCARD)
-		end
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,2,2,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+		Duel.BreakEffect()
+		Duel.DiscardHand(tp,aux.TRUE,1,1,REASON_EFFECT+REASON_DISCARD)
 	end
 end
