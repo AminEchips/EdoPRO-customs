03754761e793fb57d4f573ca693e3553d412d70d
Chips <aminekhoury0411@gmail.com>
2025-05-18 01:17:57 -0400

    Update c1047.lua

--- script/c1047.lua
+++ script/c1047.lua
@@ -3,70 +3,91 @@
 local s,id=GetID()
 
 function s.initial_effect(c)
-	--Xyz Summon procedure: 3 monsters, allow Albaz/Link-2 substitution
-	aux.AddXyzProcedureCustom(c,s.matfilter,nil,3,3,s.xyzcheck)
+	--Custom Xyz Summon procedure: 3 monsters, allow Albaz/Link-2 substitution
 	c:EnableReviveLimit()
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_FIELD)
+	e0:SetCode(EFFECT_SPSUMMON_PROC)
+	e0:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e0:SetRange(LOCATION_EXTRA)
+	e0:SetCondition(s.xyzcon)
+	e0:SetOperation(s.xyzop)
+	e0:SetValue(SUMMON_TYPE_XYZ)
+	c:RegisterEffect(e0)
 
 	--ATK/DEF boost + immunity if "Fallen of Albaz" is material
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_SINGLE)
-	e0:SetCode(EFFECT_UPDATE_ATTACK)
-	e0:SetCondition(s.statcon)
-	e0:SetValue(s.statval)
-	c:RegisterEffect(e0)
-	local e0b=e0:Clone()
-	e0b:SetCode(EFFECT_UPDATE_DEFENSE)
-	c:RegisterEffect(e0b)
-	local e0c=Effect.CreateEffect(c)
-	e0c:SetType(EFFECT_TYPE_SINGLE)
-	e0c:SetCode(EFFECT_IMMUNE_EFFECT)
-	e0c:SetCondition(s.statcon)
-	e0c:SetValue(s.efilter)
-	c:RegisterEffect(e0c)
-
-	--Destroy up to 2 cards (Quick Effect)
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,1))
-	e1:SetCategory(CATEGORY_DESTROY)
-	e1:SetType(EFFECT_TYPE_QUICK_O)
-	e1:SetCode(EVENT_CHAINING)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1,id)
-	e1:SetCondition(s.descon)
-	e1:SetCost(s.descost)
-	e1:SetTarget(s.destg)
-	e1:SetOperation(s.desop)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetCondition(s.statcon)
+	e1:SetValue(s.statval)
 	c:RegisterEffect(e1)
+	local e1b=e1:Clone()
+	e1b:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffect(e1b)
+	local e1c=Effect.CreateEffect(c)
+	e1c:SetType(EFFECT_TYPE_SINGLE)
+	e1c:SetCode(EFFECT_IMMUNE_EFFECT)
+	e1c:SetCondition(s.statcon)
+	e1c:SetValue(s.efilter)
+	c:RegisterEffect(e1c)
 
-	--Floating search effect when leaves field
+	--Destroy up to 2 cards
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,2))
-	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_HANDES)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetProperty(EFFECT_FLAG_DELAY)
-	e2:SetCode(EVENT_LEAVE_FIELD)
-	e2:SetCondition(s.thcon)
-	e2:SetTarget(s.thtg)
-	e2:SetOperation(s.thop)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.descon)
+	e2:SetCost(s.descost)
+	e2:SetTarget(s.destg)
+	e2:SetOperation(s.desop)
 	c:RegisterEffect(e2)
+
+	--Floating search
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_HANDES)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_LEAVE_FIELD)
+	e3:SetCondition(s.thcon)
+	e3:SetTarget(s.thtg)
+	e3:SetOperation(s.thop)
+	c:RegisterEffect(e3)
 end
 
 s.listed_series={0x160,0x181,0x17b} -- Branded, Spright, Therion
 s.listed_names={68468459} -- Fallen of Albaz
 
--- Xyz Material Conditions
+-- Custom Summon Logic
 function s.matfilter(c)
-	return c:IsLevel(2) or c:IsCode(68468459) or (c:IsType(TYPE_LINK) and c:GetLink()==2)
+	return c:IsFaceup() and (c:IsLevel(2) or c:IsCode(68468459) or (c:IsType(TYPE_LINK) and c:GetLink()==2))
 end
-function s.xyzcheck(g,tp,xyz)
-	if #g~=3 then return false end
+function s.xyzcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	local g=Duel.GetMatchingGroup(s.matfilter,tp,LOCATION_MZONE,0,nil)
+	return g:CheckSubGroup(s.xyzcheck,3,3)
+end
+function s.xyzcheck(g)
 	local albaz=g:FilterCount(Card.IsCode,nil,68468459)
 	local link2=g:FilterCount(function(c) return c:IsType(TYPE_LINK) and c:GetLink()==2 end,nil)
 	local level2=g:FilterCount(function(c) return c:IsLevel(2) and not c:IsType(TYPE_LINK) and not c:IsCode(68468459) end,nil)
 	return albaz<=1 and link2<=1 and (albaz + link2 + level2)==3
 end
+function s.xyzop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.SelectMatchingGroup(tp,s.matfilter,tp,LOCATION_MZONE,0,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+	local sg=g:SelectSubGroup(tp,s.xyzcheck,false,3,3)
+	if not sg then return end
+	c:SetMaterial(sg)
+	Duel.Overlay(c,sg)
+end
 
--- ATK/DEF boost + immunity
+-- Albaz-based effects
 function s.statcon(e)
 	return e:GetHandler():GetOverlayGroup():IsExists(Card.IsCode,1,nil,68468459)
 end
@@ -77,7 +98,7 @@ function s.efilter(e,te)
 	return te:IsActiveType(TYPE_MONSTER)
 end
 
--- Destroy effect
+-- Destroy 2 on monster activation
 function s.descon(e,tp,eg,ep,ev,re,r,rp)
 	return rp==1-tp and re:IsActiveType(TYPE_MONSTER) and Duel.IsMainPhase()
 end
@@ -91,14 +112,13 @@ function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,0,LOCATION_ONFIELD)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,2,nil)
 	if #g>0 then
 		Duel.Destroy(g,REASON_EFFECT)
 	end
 end
 
--- Float effect
+-- Float
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return c:IsSummonType(SUMMON_TYPE_XYZ) and c:IsPreviousLocation(LOCATION_ONFIELD)
