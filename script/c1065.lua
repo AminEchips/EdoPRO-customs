9e94190bbbf919fdc16a19473c30b9bf07ddb609
Chips <aminekhoury0411@gmail.com>
2025-05-19 13:35:51 -0400

    Update c1065.lua

--- script/c1065.lua
+++ script/c1065.lua
@@ -3,10 +3,10 @@ local s,id=GetID()
 function s.initial_effect(c)
 	s.listed_series={0x16e}
 
-	-- Activate: shuffle 1 monster in GY into Deck, then set 1 Icejade S/T
+	-- Effect 1: Activate - shuffle 1 monster in GY into Deck, set 1 Icejade S/T
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_TODECK+CATEGORY_SEARCH+CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_TODECK+CATEGORY_SEARCH+CATEGORY_TOHAND)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
@@ -15,7 +15,7 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 
-	-- GY effect: Banish to Special Summon 1 monster from S/T zone, send 1 Icejade from Deck to GY
+	-- Effect 2: GY - Target 1 monster in S/T Zone, send 1 Icejade from Deck, then Special Summon target
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON)
@@ -23,14 +23,13 @@ function s.initial_effect(c)
 	e2:SetRange(LOCATION_GRAVE)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetCountLimit(1,{id,1})
-	e2:SetCondition(function() return Duel.GetCurrentPhase() == PHASE_MAIN1 or Duel.GetCurrentPhase() == PHASE_MAIN2 end)
 	e2:SetCost(aux.bfgcost)
 	e2:SetTarget(s.sptg)
 	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
 end
 
--- Effect 1: Target a monster in GY to shuffle + set 1 Icejade S/T
+-- Effect 1 Helpers
 function s.tdfilter(c)
 	return c:IsMonster() and c:IsAbleToDeck()
 end
@@ -49,37 +48,35 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) and Duel.SendtoDeck(tc,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)>0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
-		local sg=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,1,1,nil)
-		if #sg>0 then
-			Duel.SSet(tp,sg:GetFirst())
+		local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,1,1,nil)
+		if #g>0 then
+			Duel.SSet(tp,g:GetFirst())
 		end
 	end
 end
 
--- Effect 2: Target monster in S/T zone, send Icejade from Deck to GY, then Special Summon target
-function s.monsterstzonefilter(c,e,tp)
-	return c:IsFaceup() and c:IsType(TYPE_MONSTER) and c:IsLocation(LOCATION_SZONE)
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+-- Effect 2 Helpers
+function s.stzfilter(c,e,tp)
+	return c:IsFaceup() and c:IsType(TYPE_MONSTER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.decksendfilter(c)
+function s.gyfilter(c)
 	return c:IsSetCard(0x16e) and c:IsMonster() and c:IsAbleToGrave()
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_SZONE) and chkc:IsControler(tp) and s.monsterstzonefilter(chkc,e,tp) end
-	if chk==0 then
-		return Duel.IsExistingTarget(s.monsterstzonefilter,tp,LOCATION_SZONE,0,1,nil,e,tp)
-			and Duel.IsExistingMatchingCard(s.decksendfilter,tp,LOCATION_DECK,0,1,nil)
-	end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	local g=Duel.SelectTarget(tp,s.monsterstzonefilter,tp,LOCATION_SZONE,0,1,1,nil,e,tp)
+	if chkc then return chkc:IsLocation(LOCATION_SZONE) and chkc:IsControler(tp) and s.stzfilter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(s.stzfilter,tp,LOCATION_SZONE,0,1,nil,e,tp)
+		and Duel.IsExistingMatchingCard(s.gyfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.stzfilter,tp,LOCATION_SZONE,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if not (tc and tc:IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0) then return end
+	if not (tc and tc:IsRelateToEffect(e)) or Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,s.decksendfilter,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.gyfilter,tp,LOCATION_DECK,0,1,1,nil)
 	if #g>0 and Duel.SendtoGrave(g,REASON_EFFECT)>0 then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
