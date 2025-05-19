089c2bbffef95178541f4ff91955e23d3605e02a
Chips <aminekhoury0411@gmail.com>
2025-05-19 00:04:47 -0400

    Update c1057.lua

--- script/c1057.lua
+++ script/c1057.lua
@@ -3,12 +3,11 @@
 local s,id=GetID()
 
 function s.initial_effect(c)
-	
 	-- Synchro Summon procedure
 	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTuner(Card.IsAttribute,ATTRIBUTE_WATER),1,99)
 	c:EnableReviveLimit()
 
-	-- Draw, Equip from hand/GY
+	-- Draw, then optionally equip from hand/GY
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DRAW+CATEGORY_EQUIP)
@@ -60,36 +59,34 @@ function s.eqfilter(c)
 end
 
 function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 end
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 end
 
 function s.eqop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.Draw(tp,1,REASON_EFFECT)==1 then
-		Duel.BreakEffect()
-		if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
-		if Duel.IsExistingMatchingCard(s.eqfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-			local g=Duel.SelectMatchingCard(tp,s.eqfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil)
-			local ec=g:GetFirst()
-			local c=e:GetHandler()
-			if ec and Duel.Equip(tp,ec,c,true) then
-				-- Equip limit
-				local e1=Effect.CreateEffect(c)
-				e1:SetType(EFFECT_TYPE_SINGLE)
-				e1:SetProperty(EFFECT_FLAG_COPY_INHERIT+EFFECT_FLAG_OWNER_RELATED)
-				e1:SetCode(EFFECT_EQUIP_LIMIT)
-				e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-				e1:SetValue(function(e,c) return e:GetOwner()==c end)
-				ec:RegisterEffect(e1)
-				-- DEF boost
-				local e2=Effect.CreateEffect(c)
-				e2:SetType(EFFECT_TYPE_EQUIP)
-				e2:SetCode(EFFECT_UPDATE_DEFENSE)
-				e2:SetValue(1000)
-				e2:SetReset(RESET_EVENT+RESETS_STANDARD)
-				ec:RegisterEffect(e2)
-			end
+	if Duel.Draw(tp,1,REASON_EFFECT)==0 then return end
+	local c=e:GetHandler()
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
+	if Duel.IsExistingMatchingCard(s.eqfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+		local g=Duel.SelectMatchingCard(tp,s.eqfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil)
+		local ec=g:GetFirst()
+		if ec and Duel.Equip(tp,ec,c,true) then
+			-- Equip limit
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_EQUIP_LIMIT)
+			e1:SetProperty(EFFECT_FLAG_COPY_INHERIT)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			e1:SetValue(function(e,c) return e:GetOwner()==c end)
+			ec:RegisterEffect(e1)
+			-- DEF boost
+			local e2=Effect.CreateEffect(c)
+			e2:SetType(EFFECT_TYPE_EQUIP)
+			e2:SetCode(EFFECT_UPDATE_DEFENSE)
+			e2:SetValue(1000)
+			e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+			ec:RegisterEffect(e2)
 		end
 	end
 end
