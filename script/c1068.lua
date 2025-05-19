ebbb3c5c4bc5161f1b5e679c9f3e17b8bda66a98
Chips <aminekhoury0411@gmail.com>
2025-05-19 14:53:11 -0400

    Create c1068.lua

---
+++ script/c1068.lua
@@ -0,0 +1,91 @@
+--Roll-Out! Springans
+local s,id=GetID()
+function s.initial_effect(c)
+	s.listed_series={0x270} -- Springans
+
+	-- Effect 1: Trigger when Springans Xyz leaves field to GY/Banished
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_LEAVE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+
+	-- Effect 2: GY effect - detach 1 to Set
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetCategory(CATEGORY_TODECK)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(aux.exccon)
+	e2:SetTarget(s.settg)
+	e2:SetOperation(s.setop)
+	c:RegisterEffect(e2)
+end
+
+-- Store leaving Springans Xyz that went to GY or banished
+function s.cfilter(c,tp)
+	return c:IsPreviousControler(tp) and c:IsSetCard(0x270) and c:IsType(TYPE_XYZ)
+		and c:IsReason(REASON_DESTROY+REASON_BATTLE+REASON_EFFECT)
+		and (c:IsLocation(LOCATION_GRAVE) or c:IsLocation(LOCATION_REMOVED)) and c:IsFaceup()
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter,1,nil,tp)
+end
+
+function s.spfilter(c,e,tp,code)
+	return c:IsRace(RACE_MACHINE) and c:IsAttribute(ATTRIBUTE_FIRE)
+		and c:IsType(TYPE_XYZ) and not c:IsCode(code)
+		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local g=eg:Filter(s.cfilter,nil,tp)
+	if chkc then return false end
+	if chk==0 then
+		return #g>0 and Duel.GetLocationCountFromEx(tp)>0
+			and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,g:GetFirst():GetCode())
+	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local tg=g:Select(tp,1,1,nil)
+	e:SetLabelObject(tg:GetFirst())
+	Duel.SetTargetCard(tg)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	if not tc or not tc:IsRelateToEffect(e) then return end
+	if Duel.GetLocationCountFromEx(tp)<=0 then return end
+	local code=tc:GetCode()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,code):GetFirst()
+	if sc and Duel.SpecialSummon(sc,SUMMON_TYPE_XYZ,tp,tp,false,false,POS_FACEUP)>0 then
+		sc:CompleteProcedure()
+		Duel.BreakEffect()
+		Duel.Overlay(sc,Group.FromCards(tc))
+	end
+end
+
+-- GY Effect: Detach 1 from an Xyz, then Set this card
+function s.xyzfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_XYZ) and c:GetOverlayCount()>0
+end
+function s.settg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.xyzfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.xyzfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DETACHFROM)
+	local g=Duel.SelectTarget(tp,s.xyzfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+end
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and tc:RemoveOverlayCard(tp,1,1,REASON_EFFECT)~=0 then
+		Duel.SSet(tp,c)
+	end
+end
