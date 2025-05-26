2d1fad7b438321383c5a2f96e7db5077efc0ae8c
Chips <aminekhoury0411@gmail.com>
2025-05-25 23:54:17 -0400

    Create c111.lua

---
+++ script/c111.lua
@@ -0,0 +1,155 @@
+--Supreme King Gate Supreme Magician
+local s,id=GetID()
+s.listed_names={13331639} -- Supreme King Z-ARC
+s.listed_series={0xf8} -- Supreme King
+
+function s.initial_effect(c)
+	Pendulum.AddProcedure(c)
+
+	--Pendulum Effect 1: Z-ARC cannot change control
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_CHANGE_CONTROL)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e1:SetTarget(function(e,c) return c:IsCode(13331639) and c:IsControler(e:GetHandlerPlayer()) end)
+	c:RegisterEffect(e1)
+
+	--Pendulum Effect 2: On Dragon Pendulum ED monster destroy, SS this, place Pendulum from ED
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TODECK)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_BATTLE_DESTROYING)
+	e2:SetRange(LOCATION_PZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.pcon)
+	e2:SetTarget(s.ptg)
+	e2:SetOperation(s.pop)
+	c:RegisterEffect(e2)
+
+	--Monster Effect 1: SS self if Supreme King Dragon is in face-up ED, then add/SS 1 SK Dragon from Deck/GY
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_HAND)
+	e3:SetCountLimit(1,id+100)
+	e3:SetCondition(s.sscon)
+	e3:SetTarget(s.sstg)
+	e3:SetOperation(s.ssop)
+	c:RegisterEffect(e3)
+
+	--Monster Effect 2: Fusion Summon during Main or Battle Phase
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,2))
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
+	e4:SetCountLimit(1,id+200)
+	e4:SetCondition(function() return Duel.IsMainPhase() or Duel.IsBattlePhase() end)
+	e4:SetTarget(s.fstg)
+	e4:SetOperation(s.fsop)
+	c:RegisterEffect(e4)
+end
+
+--Pendulum Condition: Dragon Pendulum Extra Deck monster destroyed a monster by battle
+function s.pfilter(c,tp)
+	return c:IsType(TYPE_PENDULUM) and c:IsType(TYPE_DRAGON)
+		and c:IsSummonLocation(LOCATION_EXTRA) and c:IsControler(tp)
+end
+function s.pcon(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+	return s.pfilter(tc,tp)
+end
+function s.ptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_EXTRA,0,1,nil,TYPE_PENDULUM) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.pop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+		local g=Duel.SelectMatchingCard(tp,Card.IsType,tp,LOCATION_EXTRA,0,1,1,nil,TYPE_PENDULUM)
+		if #g>0 then
+			Duel.MoveToField(g:GetFirst(),tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+		end
+	end
+end
+
+--Monster Effect 1: SS self if SK Dragon is in face-up ED, then get 1 SK Dragon from Deck/GY
+function s.skfacefilter(c)
+	return c:IsSetCard(0xf8) and c:IsType(TYPE_MONSTER) and c:IsFaceup()
+end
+function s.skfilter(c)
+	return c:IsSetCard(0xf8) and c:IsType(TYPE_MONSTER) and (c:IsAbleToHand() or c:IsCanBeSpecialSummoned(nil,0,tp,false,false))
+end
+function s.sscon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.skfacefilter,tp,LOCATION_EXTRA,0,1,nil)
+end
+function s.sstg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.skfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+function s.ssop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_OPTION)
+		local g=Duel.SelectMatchingCard(tp,s.skfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+		local tc=g:GetFirst()
+		if tc then
+			if tc:IsCanBeSpecialSummoned(e,0,tp,false,false)
+				and (not tc:IsAbleToHand() or Duel.SelectOption(tp,aux.Stringid(id,3),aux.Stringid(id,4))==1) then
+				Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+			else
+				Duel.SendtoHand(tc,nil,REASON_EFFECT)
+				Duel.ConfirmCards(1-tp,tc)
+			end
+		end
+	end
+end
+
+--Monster Effect 2: Fusion Summon SK/Dragon/Fusion using hand or field
+function s.fsfilter(c,e,tp,mg,fcheck)
+	return c:IsType(TYPE_FUSION)
+		and (c:IsSetCard(0xf8) or c:ListsCode(13331639))
+		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
+		and (not fcheck or fcheck(c))
+end
+function s.fstg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local mg=Duel.GetFusionMaterial(tp)
+	if chk==0 then
+		return Duel.IsExistingMatchingCard(s.fsfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function s.fsop(e,tp,eg,ep,ev,re,r,rp)
+	local chkf=tp
+	local mg=Duel.GetFusionMaterial(tp)
+	local sg=Duel.GetMatchingGroup(s.fsfilter,tp,LOCATION_EXTRA,0,nil,e,tp,mg)
+	if #sg==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local tc=sg:Select(tp,1,1,nil):GetFirst()
+	if tc then
+		local mat=Duel.SelectFusionMaterial(tp,tc,mg,nil,chkf)
+		tc:SetMaterial(mat)
+		Duel.SendtoGrave(mat,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
+		if Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)>0 and tc:IsCode(13331639) then
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetValue(2000)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			tc:RegisterEffect(e1)
+		end
+		tc:CompleteProcedure()
+	end
+end
