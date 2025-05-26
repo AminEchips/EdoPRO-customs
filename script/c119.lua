034d95c50dce97bb04d27d48a45f972ca4daf71a
Chips <aminekhoury0411@gmail.com>
2025-05-26 15:56:10 -0400

    Create c119.lua

---
+++ script/c119.lua
@@ -0,0 +1,114 @@
+--Performapal Chimera
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	-- Fusion Materials: 2 Performapal monsters
+	Fusion.AddProcMixN(c,true,true,s.matfilter,2)
+
+	-- Track Pendulum material count on Fusion Summon
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetCode(EFFECT_MATERIAL_CHECK)
+	e0:SetValue(s.valcheck)
+	c:RegisterEffect(e0)
+	
+	-- Effect: If Fusion Summoned using 0 Pendulum monsters
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.thcon)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+
+	-- Effect: 1+ Pendulums - protection from targeting/destruction
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(s.protcon)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e3:SetValue(aux.tgoval)
+	c:RegisterEffect(e3)
+
+	-- Effect: 2+ Pendulums - negate monster effect & change position
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetCategory(CATEGORY_DISABLE+CATEGORY_POSITION)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_CHAINING)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1,{id,1})
+	e4:SetCondition(s.negcon)
+	e4:SetTarget(s.negtg)
+	e4:SetOperation(s.negop)
+	c:RegisterEffect(e4)
+end
+
+s.listed_series={0x9f} -- Performapal
+
+-- Fusion Material filter: must be Performapal
+function s.matfilter(c,fc,sumtype,tp)
+	return c:IsSetCard(0x9f,fc,sumtype,tp)
+end
+
+-- Track number of Pendulum monsters used as material
+function s.valcheck(e,c)
+	local g=c:GetMaterial()
+	local ct=g:FilterCount(Card.IsType,nil,TYPE_PENDULUM)
+	c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,EFFECT_FLAG_CLIENT_HINT,1,ct)
+end
+
+-- 0 Pendulum materials: Add 1 Pendulum from Extra Deck
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsSummonType(SUMMON_TYPE_FUSION) and c:GetFlagEffect(id)>0 and c:GetFlagEffectLabel(id)==0
+end
+function s.thfilter(c)
+	return c:IsType(TYPE_PENDULUM) and c:IsFaceup() and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_EXTRA,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_EXTRA)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_EXTRA,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+
+-- 1+ Pendulum: Target/destruction protection
+function s.protcon(e)
+	local c=e:GetHandler()
+	return c:GetFlagEffect(id)>0 and c:GetFlagEffectLabel(id)>=1
+end
+
+-- 2+ Pendulums: Negate monster effect and change position
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:GetFlagEffect(id)>0 and c:GetFlagEffectLabel(id)>=2
+		and re:IsActivated() and re:IsActiveType(TYPE_MONSTER)
+		and Duel.IsChainDisablable(ev)
+end
+function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tc=re:GetHandler()
+	if chk==0 then return tc:IsCanChangePosition() end
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,tc,1,0,0)
+end
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=re:GetHandler()
+	if Duel.ChangePosition(tc,POS_FACEUP_DEFENSE,POS_FACEUP_ATTACK,POS_FACEUP_ATTACK,POS_FACEUP_ATTACK)~=0 then
+		Duel.NegateEffect(ev)
+	end
+end
