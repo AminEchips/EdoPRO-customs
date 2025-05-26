999c6e0485447c5fd15c4d933d3edf1cb7a99b27
Chips <aminekhoury0411@gmail.com>
2025-05-26 13:18:57 -0400

    Update c113.lua

--- script/c113.lua
+++ script/c113.lua
@@ -29,7 +29,9 @@ function s.initial_effect(c)
 	e4:SetType(EFFECT_TYPE_SINGLE)
 	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e4:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e4:SetValue(function(e,se,sp,st) return st==SUMMON_TYPE_PENDULUM or se and se:GetHandler()==e:GetHandler() end)
+	e4:SetValue(function(e,se,sp,st)
+		return st==SUMMON_TYPE_PENDULUM or (se and se:GetHandler()==e:GetHandler())
+	end)
 	c:RegisterEffect(e4)
 
 	--Negate Spells/Traps on SS (non-chainable)
@@ -39,14 +41,15 @@ function s.initial_effect(c)
 	e5:SetProperty(EFFECT_FLAG_DELAY)
 	e5:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e5:SetCountLimit(1,id+100)
-	e5:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_SPECIAL) end)
+	e5:SetCondition(function(e)
+		return e:GetHandler():IsSummonType(SUMMON_TYPE_SPECIAL)
+	end)
 	e5:SetOperation(s.negop)
 	c:RegisterEffect(e5)
 
-	--Draw 2 if added to ED face-up
+	--Draw 2 during next Draw Phase if sent to Extra Deck face-up
 	local e6=Effect.CreateEffect(c)
 	e6:SetDescription(aux.Stringid(id,1))
-	e6:SetCategory(CATEGORY_DRAW)
 	e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e6:SetProperty(EFFECT_FLAG_DELAY)
 	e6:SetCode(EVENT_MOVE)
@@ -56,47 +59,54 @@ function s.initial_effect(c)
 		return c:IsFaceup() and c:IsLocation(LOCATION_EXTRA)
 			and c:IsPreviousLocation(LOCATION_ONFIELD+LOCATION_HAND+LOCATION_GRAVE+LOCATION_DECK)
 	end)
-	e6:SetTarget(function(e,tp,eg,ep,ev,re,r,rp,chk)
-		if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
-		Duel.SetTargetPlayer(tp)
-		Duel.SetTargetParam(2)
-		Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
-	end)
 	e6:SetOperation(function(e,tp,eg,ep,ev,re,r,rp)
-		local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-		Duel.Draw(p,d,REASON_EFFECT)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_DRAW_COUNT)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		e1:SetTargetRange(1,0)
+		e1:SetValue(2)
+		e1:SetReset(RESET_PHASE|PHASE_DRAW|RESET_SELF_TURN,1)
+		Duel.RegisterEffect(e1,tp)
 	end)
 	c:RegisterEffect(e6)
 
-	--Place in Pendulum Zone if destroyed
+	--Place in Pendulum Zone if destroyed from Monster Zone
 	local e7=Effect.CreateEffect(c)
+	e7:SetDescription(aux.Stringid(id,2))
 	e7:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e7:SetCode(EVENT_DESTROYED)
 	e7:SetProperty(EFFECT_FLAG_DELAY)
-	e7:SetCondition(function(e) return e:GetHandler():IsPreviousLocation(LOCATION_MZONE) end)
+	e7:SetCondition(function(e)
+		local c=e:GetHandler()
+		return c:IsPreviousLocation(LOCATION_MZONE) and c:IsFaceup()
+	end)
+	e7:SetTarget(s.pztg)
 	e7:SetOperation(s.placeop)
 	c:RegisterEffect(e7)
 end
 
---Scale condition: no other Level 10 Pendulum
+-- Scale becomes 4 unless another Level 10 Pendulum in other zone
 function s.scalecon(e)
 	return not Duel.IsExistingMatchingCard(function(c)
 		return c:IsType(TYPE_PENDULUM) and c:IsLevel(10)
 	end,e:GetHandlerPlayer(),LOCATION_PZONE,0,1,e:GetHandler())
 end
 
---Pendulum Summon trigger
+-- Pendulum Summon of Level 10+ triggers Special Summon from Pendulum Zone
 function s.psfilter(c,tp)
-	return c:IsType(TYPE_PENDULUM) and c:IsLevelAbove(10) and c:IsSummonType(SUMMON_TYPE_PENDULUM) and c:IsControler(tp)
+	return c:IsType(TYPE_PENDULUM) and c:IsLevelAbove(10)
+		and c:IsSummonType(SUMMON_TYPE_PENDULUM) and c:IsControler(tp)
 end
 function s.pscon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(s.psfilter,1,nil,tp)
 end
 function s.psop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsRelateToEffect(e) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) then
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsRelateToEffect(e) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) then
 		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
-		--Trap immunity for all your monsters
+		-- Trap immunity for rest of turn
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_FIELD)
 		e1:SetCode(EFFECT_IMMUNE_EFFECT)
@@ -108,7 +118,7 @@ function s.psop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
---Negation on SS (non-chainable)
+-- Negate Spells/Traps on Special Summon
 function s.negop(e,tp,eg,ep,ev,re,r,rp)
 	local ct=Duel.GetMatchingGroupCount(Card.IsType,tp,LOCATION_MZONE,0,nil,TYPE_PENDULUM)
 	if ct==0 then return end
@@ -129,10 +139,20 @@ function s.negop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
---Place in Pendulum Zone
+-- Target check for placing in Pendulum Zone
+function s.pztg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		return Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1)
+	end
+end
+
+-- Place this card in Pendulum Zone
 function s.placeop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1) then
+	if not c:IsRelateToEffect(e) then return end
+	if Duel.CheckLocation(tp,LOCATION_PZONE,0) then
+		Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+	elseif Duel.CheckLocation(tp,LOCATION_PZONE,1) then
 		Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
 	end
 end
