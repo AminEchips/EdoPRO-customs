a9a6d243164ad100997e923e2db1e8adfa505dfd
Chips <aminekhoury0411@gmail.com>
2025-05-26 15:05:34 -0400

    Update c117.lua

--- script/c117.lua
+++ script/c117.lua
@@ -3,11 +3,16 @@ local s,id=GetID()
 s.listed_series={0xf9,0x99,0xf2} -- Performapal, Odd-Eyes, Pendulum
 
 function s.initial_effect(c)
-	-- Fusion summon procedure
+	-- Fusion Summon Procedure
 	c:EnableReviveLimit()
-	Fusion.AddProcMix(c,true,true,s.matfilter1,s.matfilter2)
+	Fusion.AddProcMix(c,true,true,
+		aux.FilterBoolFunction(Card.IsSetCard,0xf9), -- Performapal
+		function(c) return c:IsSetCard(0x99)
+			and (c:IsType(TYPE_RITUAL) or c:IsType(TYPE_FUSION)
+			or c:IsType(TYPE_SYNCHRO) or c:IsType(TYPE_XYZ)) end
+	)
 
-	-- Protection: cannot be targeted by Spell/Trap effects (except this one)
+	-- Protection: Your Performapal/Odd-Eyes monsters can't be targeted by Spell/Trap (except this one)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
@@ -18,7 +23,7 @@ function s.initial_effect(c)
 	e1:SetValue(function(e,re,rp) return re:IsActiveType(TYPE_SPELL+TYPE_TRAP) end)
 	c:RegisterEffect(e1)
 
-	-- ATK boost
+	-- ATK Boost: +100 per monster on the field, lasts until end of next turn
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_ATKCHANGE)
@@ -28,66 +33,44 @@ function s.initial_effect(c)
 	e2:SetOperation(s.atkop)
 	c:RegisterEffect(e2)
 
-	-- Floating: Add 1 Performapal/Odd-Eyes/Pendulum Spell/Trap from GY to hand
+	-- Floating Effect: Add 1 Performapal/Odd-Eyes/Pendulum Spell/Trap from GY to hand
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_TOHAND)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
 	e3:SetCode(EVENT_DESTROYED)
-	e3:SetCondition(s.thcon)
+	e3:SetCondition(function(e) return e:GetHandler():IsReason(REASON_EFFECT+REASON_BATTLE) end)
 	e3:SetTarget(s.thtg)
 	e3:SetOperation(s.thop)
 	e3:SetCountLimit(1,id+100)
 	c:RegisterEffect(e3)
 end
 
--- Fusion material: Performapal monster
-function s.matfilter1(c,fc,sumtype,tp)
-	return c:IsSetCard(0xf9,fc,sumtype,tp)
-end
-
--- Fusion material: Odd-Eyes Ritual/Fusion/Synchro/Xyz
-function s.matfilter2(c,fc,sumtype,tp)
-	return c:IsSetCard(0x99,fc,sumtype,tp) and (
-		c:IsType(TYPE_RITUAL,fc,sumtype,tp) or
-		c:IsType(TYPE_FUSION,fc,sumtype,tp) or
-		c:IsType(TYPE_SYNCHRO,fc,sumtype,tp) or
-		c:IsType(TYPE_XYZ,fc,sumtype,tp)
-	)
-end
-
--- Protection effect: applies to all other Performapal and Odd-Eyes monsters
+-- Protection applies to all Performapal/Odd-Eyes monsters except this one
 function s.prottg(e,c)
 	return c~=e:GetHandler() and (c:IsSetCard(0xf9) or c:IsSetCard(0x99))
 end
 
--- ATK boost based on number of monsters on field
+-- ATK Boost: applies to all monsters you control
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_MZONE,0,nil)
-	local ct=Duel.GetMatchingGroupCount(aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
-	if ct==0 then return end
-	local atk=ct*100
-	for tc in aux.Next(g) do
+	local count=Duel.GetMatchingGroupCount(aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	if count==0 then return end
+	local boost = count * 100
+	for tc in g:Iter() do
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetValue(atk)
+		e1:SetValue(boost)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,2)
 		tc:RegisterEffect(e1)
 	end
 end
 
--- Floating condition: destroyed by battle or card effect
-function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsReason(REASON_EFFECT+REASON_BATTLE)
-end
-
--- Floating target: Performapal/Odd-Eyes/Pendulum Spell/Trap
+-- Floating: target 1 matching Spell/Trap in GY
 function s.thfilter(c)
-	return c:IsType(TYPE_SPELL+TYPE_TRAP)
-		and (c:IsSetCard(0xf9) or c:IsSetCard(0x99) or c:IsSetCard(0xf2))
-		and c:IsAbleToHand()
+	return c:IsType(TYPE_SPELL+TYPE_TRAP) and (c:IsSetCard(0xf9) or c:IsSetCard(0x99) or c:IsSetCard(0xf2)) and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.thfilter(chkc) end
