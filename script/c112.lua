2b1908753a8b98c773f0d76241fef206ebb31660
Chips <aminekhoury0411@gmail.com>
2025-05-26 10:29:29 -0400

    Update c112.lua

--- script/c112.lua
+++ script/c112.lua
@@ -5,7 +5,7 @@ s.listed_series={0x99}
 function s.initial_effect(c)
 	Pendulum.AddProcedure(c)
 
-	-- Pendulum Effect: Double ATK of Odd-Eyes monster
+	-- Pendulum Effect: Double ATK of an Odd-Eyes monster
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_ATKCHANGE)
@@ -37,7 +37,7 @@ function s.initial_effect(c)
 	end)
 	c:RegisterEffect(e3)
 
-	-- Negate effect that targets a Pendulum monster
+	-- Negate opponent’s monster effect that targets Pendulum
 	local e4=Effect.CreateEffect(c)
 	e4:SetDescription(aux.Stringid(id,1))
 	e4:SetCategory(CATEGORY_DISABLE+CATEGORY_DESTROY)
@@ -50,25 +50,28 @@ function s.initial_effect(c)
 	e4:SetOperation(s.negop)
 	c:RegisterEffect(e4)
 
-	-- Battle effect: reduce ATK of opponent’s monster
+	-- If destroyed: place in Pendulum Zone
 	local e5=Effect.CreateEffect(c)
-	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e5:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
-	e5:SetOperation(s.globaldropop)
+	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e5:SetCode(EVENT_DESTROYED)
+	e5:SetProperty(EFFECT_FLAG_DELAY)
+	e5:SetCountLimit(1,id+200)
+	e5:SetCondition(function(e) return e:GetHandler():IsPreviousLocation(LOCATION_MZONE) end)
+	e5:SetOperation(s.placeop)
 	c:RegisterEffect(e5)
 
-	-- If destroyed, place in Pendulum Zone
-	local e6=Effect.CreateEffect(c)
-	e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e6:SetCode(EVENT_DESTROYED)
-	e6:SetProperty(EFFECT_FLAG_DELAY)
-	e6:SetCountLimit(1,id+200)
-	e6:SetCondition(function(e) return e:GetHandler():IsPreviousLocation(LOCATION_MZONE) end)
-	e6:SetOperation(s.placeop)
-	c:RegisterEffect(e6)
+	-- Global battle effect (once per duel field-wide registration)
+	if not s.global_check then
+		s.global_check=true
+		local ge=Effect.CreateEffect(c)
+		ge:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+		ge:SetOperation(s.globaldropop)
+		Duel.RegisterEffect(ge,0)
+	end
 end
 
--- Pendulum ATK boost
+-- Pendulum effect: ATK boost
 function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0) < Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)
 end
@@ -95,7 +98,7 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- Negate monster effect that targets Pendulum
+-- Targeting negation
 function s.negcon(e,tp,eg,ep,ev,re,r,rp)
 	if not re:IsActiveType(TYPE_MONSTER) or not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
 	local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
@@ -114,27 +117,40 @@ function s.negop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- Battle effect: reduce ATK if this was Pendulum Summoned
+-- Pendulum Zone placement
+function s.placeop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1) then
+		Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+	end
+end
+
+-- Global battle effect for all Pendulum Summoned monsters you control
 function s.globaldropop(e,tp,eg,ep,ev,re,r,rp)
 	local a=Duel.GetAttacker()
 	local d=Duel.GetAttackTarget()
 	if not a or not d then return end
 
-	local you, opp = nil, nil
-	if a:IsControler(tp) and a:IsSummonType(SUMMON_TYPE_PENDULUM) then
+	local p1 = a:IsControler(tp) and a:IsSummonType(SUMMON_TYPE_PENDULUM)
+	local p2 = d:IsControler(tp) and d:IsSummonType(SUMMON_TYPE_PENDULUM)
+
+	local you = nil
+	local opp = nil
+
+	if p1 then
 		you = a
 		opp = d
-	elseif d:IsControler(tp) and d:IsSummonType(SUMMON_TYPE_PENDULUM) then
+	elseif p2 then
 		you = d
 		opp = a
 	end
 
-	if not opp or not you then return end
+	if not you or not opp then return end
 	if not opp:IsSummonType(SUMMON_TYPE_SPECIAL) then return end
 
 	local count=Duel.GetMatchingGroupCount(function(c)
 		return c:IsFaceup() and c:GetOriginalType()&TYPE_PENDULUM~=0
-	end,tp,LOCATION_MZONE,0,nil)
+	end,tp,LOCATION_ONFIELD,0,nil)
 
 	if count==0 then return end
 
@@ -144,14 +160,4 @@ function s.globaldropop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetValue(-count*500)
 	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE_CAL)
 	opp:RegisterEffect(e1)
-end
-
-
-
--- Place in Pendulum Zone
-function s.placeop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1) then
-		Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
-	end
 end
