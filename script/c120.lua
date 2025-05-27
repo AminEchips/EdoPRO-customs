d6def9077fbc246591bbdc30af85a0dccaef0447
Chips <aminekhoury0411@gmail.com>
2025-05-26 22:39:41 -0400

    Update c120.lua

--- script/c120.lua
+++ script/c120.lua
@@ -4,41 +4,61 @@ function s.initial_effect(c)
 	Pendulum.AddProcedure(c)
 	c:EnableReviveLimit()
 
-	-- Fusion Summon requirement: 1 "Odd-Eyes" Dragon + 1 "Magician" Pendulum
+	-- Fusion Materials: 1 "Odd-Eyes" Dragon + 1 "Magician" Pendulum
 	Fusion.AddProcMix(c,true,true,s.mat1filter,s.mat2filter)
 
-	-- Custom Special Summon condition (from Extra Deck via tributing Pendulum Summoned materials)
+	-- Custom Special Summon condition: must be Fusion Summoned or tribute correct Pendulum Summoned monsters
 	local e0=Effect.CreateEffect(c)
 	e0:SetType(EFFECT_TYPE_SINGLE)
-	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
 	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
 	e0:SetValue(s.splimit)
 	c:RegisterEffect(e0)
 
-	-- Quick Effect: Target 1 monster, negate & destroy
+	-- Store number of Pendulum Dragon materials
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_DISABLE+CATEGORY_DESTROY)
-	e1:SetType(EFFECT_TYPE_QUICK_O)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_CHAIN)
-	e1:SetTarget(s.negtg)
-	e1:SetOperation(s.negop)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_MATERIAL_CHECK)
+	e1:SetValue(s.valcheck)
 	c:RegisterEffect(e1)
 
-	-- On destruction: place into Pendulum Zone
+	-- Quick Effect: negate and destroy, up to count of Pendulum Dragon materials
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_DESTROYED)
-	e2:SetProperty(EFFECT_FLAG_DELAY)
-	e2:SetCondition(function(e) return e:GetHandler():IsPreviousLocation(LOCATION_MZONE) end)
-	e2:SetOperation(s.placeop)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_DISABLE+CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id,EFFECT_COUNT_CODE_CHAIN)
+	e2:SetCondition(s.negcon)
+	e2:SetTarget(s.negtg)
+	e2:SetOperation(s.negop)
 	c:RegisterEffect(e2)
+	e2:SetLabelObject(e1)
+
+	-- Floating effect: move to Pendulum Zone
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_DESTROYED)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCondition(function(e) return e:GetHandler():IsPreviousLocation(LOCATION_MZONE) end)
+	e3:SetOperation(s.placeop)
+	c:RegisterEffect(e3)
+
+	-- Pendulum Effect: Destroy this card to summon 1 Pendulum Dragon
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_PZONE)
+	e4:SetCountLimit(1)
+	e4:SetTarget(s.pentg)
+	e4:SetOperation(s.penop)
+	c:RegisterEffect(e4)
 end
 
-s.listed_series={0x99,0x98,0xf2} -- Odd-Eyes, Magician, Pendulum
+s.listed_series={0x99,0x98,0x10f2}
 s.material_setcode={0x99,0x98}
 
 -- Fusion materials
@@ -49,31 +69,43 @@ function s.mat2filter(c,fc,sumtype,tp)
 	return c:IsSetCard(0x98,fc,sumtype,tp) and c:IsType(TYPE_PENDULUM)
 end
 
--- Special summon restriction: Must be Fusion Summoned or from ED by tributing proper Pendulum Summoned monsters
+-- Special Summon condition (Fusion only, or custom future case)
 function s.splimit(e,se,sp,st)
-	local c=e:GetHandler()
-	if st==SUMMON_TYPE_FUSION then return true end
-	if se then
-		local tg=se:GetTarget()
-		if se:IsHasType(EFFECT_TYPE_ACTIONS) or not tg then return false end
-	end
-	return false -- default to Fusion Summon only for safety
+	return st==SUMMON_TYPE_FUSION
 end
 
--- Quick Effect: Negate and destroy a face-up monster
+-- Count Pendulum Dragon (0x10f2) materials
+function s.valcheck(e,c)
+	local g=c:GetMaterial()
+	local ct=g:FilterCount(function(c) return c:IsSetCard(0x10f2) end,nil)
+	e:SetLabel(ct)
+end
+
+-- Negate condition: up to number of Pendulum Dragon materials
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsSummonType(SUMMON_TYPE_FUSION) then return false end
+	local max = e:GetLabelObject():GetLabel()
+	if not max or max <= 0 then return false end
+	if not c.registered_negations then c.registered_negations = 0 end
+	return c.registered_negations < max
+end
+
 function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
-	if chk==0 then return Duel.IsExistingTarget(aux.FaceupFilter(Card.IsMonster),tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_NEGATE)
-	local g=Duel.SelectTarget(tp,aux.FaceupFilter(Card.IsMonster),tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 end
+
 function s.negop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
+	local c=e:GetHandler()
+	if not c.registered_negations then c.registered_negations = 0 end
+	c.registered_negations = c.registered_negations + 1
 	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() and not tc:IsDisabled() then
 		Duel.NegateRelatedChain(tc,RESET_TURN_SET)
-		local e1=Effect.CreateEffect(e:GetHandler())
+		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_DISABLE)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
@@ -86,11 +118,32 @@ function s.negop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- If destroyed: move to Pendulum Zone
+-- Floating to Pendulum Zone
 function s.placeop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e) then return end
 	if Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1) then
 		Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+	end
+end
+
+-- Pendulum effect: destroy itself to summon Pendulum Dragon
+function s.penfilter(c,e,tp)
+	return c:IsSetCard(0x10f2) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.pentg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsDestructable()
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.penfilter,tp,LOCATION_HAND+LOCATION_EXTRA,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,e:GetHandler(),1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_EXTRA)
+end
+function s.penop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) or Duel.Destroy(c,REASON_EFFECT)==0 then return end
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.penfilter,tp,LOCATION_HAND+LOCATION_EXTRA,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
