8c89675d5c884c288dbe66f24e658086161fee9a
Chips <aminekhoury0411@gmail.com>
2025-05-26 23:25:10 -0400

    Update c121.lua

--- script/c121.lua
+++ script/c121.lua
@@ -1,185 +1,163 @@
 --Odd-Eyes Continuum Gazing Magician of Time and Space
 local s,id=GetID()
 function s.initial_effect(c)
-	Pendulum.AddProcedure(c)
-	c:EnableReviveLimit()
+    --Pendulum Attribute
+    Pendulum.AddProcedure(c)
 
-	--Fusion Materials: 1 Odd-Eyes + 2 Magician Pendulums with different Scales
-	Fusion.AddProcMixN(c,true,true,s.matfilter1,1,s.matfilter2,2)
+    --Pendulum Effect: Damage based on attacks
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_DAMAGE)
+    e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_DAMAGE_STEP_END)
+    e1:SetRange(LOCATION_PZONE)
+    e1:SetCountLimit(1,id)
+    e1:SetCondition(s.pdcon)
+    e1:SetOperation(s.pdop)
+    c:RegisterEffect(e1)
 
-	--Negate and optionally destroy immediately
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_DISABLE+CATEGORY_DESTROY)
-	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_CANNOT_INACTIVATE)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1)
-	e1:SetTarget(s.negtg)
-	e1:SetOperation(s.negop)
-	c:RegisterEffect(e1)
+    --Pendulum Effect: Quick LP gain
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_RECOVER)
+    e2:SetType(EFFECT_TYPE_QUICK_O)
+    e2:SetCode(EVENT_FREE_CHAIN)
+    e2:SetRange(LOCATION_PZONE)
+    e2:SetCountLimit(1,id+100)
+    e2:SetOperation(s.lpop)
+    c:RegisterEffect(e2)
 
-	--Battle destruction: SS + burn
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DAMAGE)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_BATTLE_DESTROYING)
-	e2:SetCondition(aux.bdocon)
-	e2:SetTarget(s.sptg)
-	e2:SetOperation(s.spop)
-	c:RegisterEffect(e2)
+    --Fusion Summon procedure
+    c:EnableReviveLimit()
+    Fusion.AddProcMix(c,true,true,s.oddEyesFilter,s.magicianFilter1,s.magicianFilter2)
 
-	--Floating
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,2))
-	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e3:SetCode(EVENT_DESTROYED)
-	e3:SetProperty(EFFECT_FLAG_DELAY)
-	e3:SetCondition(function(e) return e:GetHandler():IsPreviousLocation(LOCATION_MZONE) end)
-	e3:SetTarget(s.float_tg)
-	e3:SetOperation(s.float_op)
-	c:RegisterEffect(e3)
+    --Negate and optionally destroy
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,2))
+    e3:SetCategory(CATEGORY_DISABLE)
+    e3:SetType(EFFECT_TYPE_IGNITION)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCountLimit(1,id+200)
+    e3:SetTarget(s.negtg)
+    e3:SetOperation(s.negop)
+    c:RegisterEffect(e3)
 
-	--Pendulum OPT limiter
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e4:SetCode(EVENT_PHASE+PHASE_DRAW)
-	e4:SetOperation(function(_,tp) Duel.ResetFlagEffect(tp,id) Duel.ResetFlagEffect(tp,id+1) end)
-	Duel.RegisterEffect(e4,0)
+    --Battle destroy: Revive and inflict damage
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,3))
+    e4:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DAMAGE)
+    e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e4:SetCode(EVENT_BATTLE_DESTROYING)
+    e4:SetCondition(aux.bdocon)
+    e4:SetTarget(s.btg)
+    e4:SetOperation(s.bop)
+    c:RegisterEffect(e4)
 
-	--Pendulum Damage at Damage Step End
-	local e5=Effect.CreateEffect(c)
-	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e5:SetCode(EVENT_DAMAGE_STEP_END)
-	e5:SetRange(LOCATION_PZONE)
-	e5:SetCondition(s.damcon)
-	e5:SetOperation(s.damop)
-	c:RegisterEffect(e5)
-
-	--Quick LP Gain
-	local e6=Effect.CreateEffect(c)
-	e6:SetDescription(aux.Stringid(id,3))
-	e6:SetCategory(CATEGORY_RECOVER)
-	e6:SetType(EFFECT_TYPE_QUICK_O)
-	e6:SetCode(EVENT_FREE_CHAIN)
-	e6:SetRange(LOCATION_PZONE)
-	e6:SetCountLimit(1,id)
-	e6:SetCondition(function(_,tp) return Duel.GetFlagEffect(tp,id)==0 end)
-	e6:SetOperation(s.lpop)
-	c:RegisterEffect(e6)
-
-	-- Global attack counter like Meteor Rush - Monochroid
-	if not s.global_check then
-		s.global_check=true
-		local ge1=Effect.CreateEffect(c)
-		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge1:SetCode(EVENT_ATTACK_ANNOUNCE)
-		ge1:SetOperation(function(_,tp,_,ep)
-			Duel.RegisterFlagEffect(ep,id+1,RESET_PHASE+PHASE_END,0,1)
-		end)
-		Duel.RegisterEffect(ge1,0)
-	end
+    --When destroyed: place to Pendulum Zone and recover
+    local e5=Effect.CreateEffect(c)
+    e5:SetDescription(aux.Stringid(id,4))
+    e5:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND)
+    e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e5:SetCode(EVENT_DESTROYED)
+    e5:SetProperty(EFFECT_FLAG_DELAY)
+    e5:SetCountLimit(1,id+300)
+    e5:SetTarget(s.pztg)
+    e5:SetOperation(s.pzop)
+    c:RegisterEffect(e5)
 end
 
---Fusion material filters
-function s.matfilter1(c,fc,sumtype,tp)
-	return c:IsSetCard(0x99,fc,sumtype,tp) -- Odd-Eyes
+--Filters
+function s.oddEyesFilter(c)
+    return c:IsSetCard(0x99)
 end
-function s.matfilter2(c,fc,sumtype,tp)
-	return c:IsSetCard(0x98,fc,sumtype,tp) and c:IsType(TYPE_PENDULUM)
+function s.magicianFilter1(c)
+    return c:IsSetCard(0x98) and c:IsType(TYPE_PENDULUM)
+end
+function s.magicianFilter2(c)
+    return c:IsSetCard(0x98) and c:IsType(TYPE_PENDULUM)
 end
 
---Negate and destroy instantly if chosen
+--Pendulum damage condition
+function s.pdcon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.IsExistingMatchingCard(function(c) return c:IsType(TYPE_PENDULUM) and c:IsStatus(STATUS_BATTLE_DESTROYED) end,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.pdop(e,tp,eg,ep,ev,re,r,rp)
+    local ct=Duel.GetAttackCount()
+    if ct>0 then
+        Duel.Damage(1-tp,ct*600,REASON_EFFECT)
+    end
+end
+
+--LP gain Quick Effect
+function s.lpop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if Duel.Destroy(c,REASON_EFFECT)>0 then
+        Duel.Recover(tp,1200,REASON_EFFECT)
+    end
+end
+
+--Negate Target
+function s.negfilter(c)
+    return c:IsFaceup() and not c:IsDisabled()
+end
 function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
-	local g=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONfFIELD,1,3,nil)
-	e:SetLabel(Duel.SelectYesNo(tp,aux.Stringid(id,4)) and 1 or 0)
-	e:SetLabelObject(g:GetFirst())
+    if chk==0 then return Duel.IsExistingTarget(s.negfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+    local g=Duel.SelectTarget(tp,s.negfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,3,nil)
+    e:SetLabelObject(g)
 end
 function s.negop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
-	if not g then return end
-	for tc in aux.Next(g) do
-		if tc:IsRelateToEffect(e) then
-			local e1=Effect.CreateEffect(e:GetHandler())
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_DISABLE)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_STANDBY,2)
-			tc:RegisterEffect(e1)
-			local e2=e1:Clone()
-			e2:SetCode(EFFECT_DISABLE_EFFECT)
-			tc:RegisterEffect(e2)
-			if e:GetLabel()==1 then
-				Duel.BreakEffect()
-				Duel.Destroy(tc,REASON_EFFECT)
-			end
-		end
-	end
+    local g=e:GetLabelObject()
+    if not g then return end
+    for tc in g:Iter() do
+        if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+            tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_STANDBY,0,1)
+            local e1=Effect.CreateEffect(e:GetHandler())
+            e1:SetType(EFFECT_TYPE_SINGLE)
+            e1:SetCode(EFFECT_DISABLE)
+            e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_STANDBY)
+            tc:RegisterEffect(e1)
+        end
+    end
 end
 
---Battle destruction effect (non-targeting)
-function s.spfilter(c,e,tp)
-	return c:IsType(TYPE_PENDULUM) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-		and (c:IsLocation(LOCATION_GRAVE) or c:IsFaceup())
+--Battle destroy: revive and burn
+function s.btg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
 end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED+LOCATION_PZONE,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE+LOCATION_REMOVED+LOCATION_PZONE)
-	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,0)
-end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED+LOCATION_PZONE,0,1,1,nil,e,tp)
-	if #g>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)>0 then
-		local atk=g:GetFirst():GetAttack()
-		if atk>0 then
-			Duel.Damage(1-tp,atk,REASON_EFFECT)
-		end
-	end
+function s.bop(e,tp,eg,ep,ev,re,r,rp)
+    local g=Duel.GetMatchingGroup(aux.FilterFaceupFunction(Card.IsType,TYPE_PENDULUM),tp,LOCATION_GRAVE+LOCATION_REMOVED+LOCATION_EXTRA,0,nil)
+    if #g>0 then
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+        local sc=g:Select(tp,1,1,nil):GetFirst()
+        if sc and Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)>0 then
+            Duel.Damage(1-tp,sc:GetAttack(),REASON_EFFECT)
+        end
+    end
 end
 
---Floating effect
-function s.float_tg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(nil,tp,LOCATION_PZONE,0,1,nil)
-		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.GetLocationCount(tp,LOCATION_PZONE)>0 end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_PZONE)
+--Destroyed effect
+function s.pzfilter(c)
+    return c:IsType(TYPE_PENDULUM) and c:IsFaceup()
 end
-function s.float_op(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,nil,tp,LOCATION_PZONE,0,1,1,nil)
-	if #g>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)>0
-		and Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true) then
-		if Duel.IsExistingMatchingCard(Card.IsAbleToHand,tp,LOCATION_GRAVE,0,1,nil)
-			and Duel.SelectYesNo(tp,aux.Stringid(id,5)) then
-			local tg=Duel.SelectMatchingCard(tp,Card.IsAbleToHand,tp,LOCATION_GRAVE,0,1,1,nil)
-			if #tg>0 then
-				Duel.SendtoHand(tg,nil,REASON_EFFECT)
-				Duel.ConfirmCards(1-tp,tg)
-			end
-		end
-	end
+function s.pztg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.pzfilter,tp,LOCATION_EXTRA,0,1,nil) end
 end
-
--- Pendulum damage effect
-function s.damcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetFlagEffect(tp,id+1)>0 and Duel.GetCurrentPhase()==PHASE_DAMAGE
-end
-function s.damop(e,tp,eg,ep,ev,re,r,rp)
-	local ct=Duel.GetFlagEffect(tp,id+1)
-	if ct>0 then
-		Duel.Damage(1-tp,ct*600,REASON_EFFECT)
-	end
-end
-
--- Quick LP gain (destroy this)
-function s.lpop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) and Duel.Destroy(c,REASON_EFFECT)>0 then
-		Duel.Recover(tp,1200,REASON_EFFECT)
-		Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
-	end
+function s.pzop(e,tp,eg,ep,ev,re,r,rp)
+    local g=Duel.SelectMatchingCard(tp,s.pzfilter,tp,LOCATION_EXTRA,0,1,1,nil)
+    local tc=g:GetFirst()
+    if tc and Duel.MoveToField(tc,tp,tp,LOCATION_PZONE,POS_FACEUP,true)~=0 then
+        local c=e:GetHandler()
+        if c:IsRelateToEffect(e) and Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true)~=0 then
+            if Duel.IsExistingMatchingCard(Card.IsAbleToHand,tp,LOCATION_GRAVE,0,1,nil)
+                and Duel.SelectYesNo(tp,aux.Stringid(id,5)) then
+                Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+                local g2=Duel.SelectMatchingCard(tp,Card.IsAbleToHand,tp,LOCATION_GRAVE,0,1,1,nil)
+                if #g2>0 then
+                    Duel.SendtoHand(g2,nil,REASON_EFFECT)
+                    Duel.ConfirmCards(1-tp,g2)
+                end
+            end
+        end
+    end
 end
