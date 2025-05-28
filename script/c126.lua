9d65f860e18751572cd619fea35db003173be0bd
Chips <aminekhoury0411@gmail.com>
2025-05-28 00:06:25 -0400

    Update c126.lua

--- script/c126.lua
+++ script/c126.lua
@@ -1,93 +1,89 @@
---[[
-Custom Card Script
-Fusion Monster with Fusion/Polymerization targeting effect, board wipe, and revival effect
-Fusion archetype code: 0x46
-Odd-Eyes Pendulum Dragon: 16178681
-]]
-
+--Supreme King Dragon Greedy Venom
 local s,id=GetID()
 function s.initial_effect(c)
-	--Fusion Summon condition
+	-- Fusion Summon condition
 	c:EnableReviveLimit()
-	Fusion.AddProcMix(c,true,true,aux.FilterBoolFunction(Card.IsSetCard,0x46),aux.FilterBoolFunction(Card.IsType,TYPE_MONSTER))
+	Fusion.AddProcMix(c,true,true,s.fusionfilter,s.pendfilter,s.pendfilter)
 
-	--Target 1 Spell/Trap; negate its effects
+	-- On Fusion Summon: Add 1 "Fusion"/"Polymerization" Spell
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_DISABLE)
-	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetRange(LOCATION_MZONE)
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
 	e1:SetCountLimit(1,id)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetTarget(s.distg)
-	e1:SetOperation(s.disop)
+	e1:SetCondition(s.addcon)
+	e1:SetTarget(s.addtg)
+	e1:SetOperation(s.addop)
 	c:RegisterEffect(e1)
 
-	--If destroyed: destroy all cards your opponent controls
+	-- Board wipe if material was in Pendulum Zone
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_DESTROY)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e2:SetCode(EVENT_DESTROYED)
-	e2:SetCondition(s.descon)
-	e2:SetTarget(s.destg)
-	e2:SetOperation(s.desop)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id+100)
+	e2:SetCondition(s.wipecon)
+	e2:SetOperation(s.wipeop)
 	c:RegisterEffect(e2)
 
-	--If destroyed by effect: revive Odd-Eyes Pendulum Dragon at End Phase
+	-- Register End Phase effect if destroyed
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e3:SetCode(EVENT_TO_GRAVE)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e3:SetOperation(s.regop)
 	c:RegisterEffect(e3)
 end
 
--- Effect 1: Target 1 Spell/Trap
-function s.filter(c)
-	return c:IsFaceup() and c:IsType(TYPE_SPELL+TYPE_TRAP) and aux.disfilter1(c)
+-- Material filters
+function s.fusionfilter(c,fc,sumtype,tp)
+	return c:IsType(TYPE_FUSION,fc,sumtype,tp)
 end
-function s.distg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsOnField() and s.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,0,LOCATION_ONFIELD,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local g=Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_ONFIELD,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
+function s.pendfilter(c,fc,sumtype,tp)
+	return c:IsAttribute(ATTRIBUTE_DARK,fc,sumtype,tp) and c:IsType(TYPE_PENDULUM,fc,sumtype,tp)
 end
-function s.disop(e,tp,eg,ep,ev,re,r,rp)
+
+-- Effect 1: Add "Fusion" or "Polymerization" Spell on Fusion Summon
+function s.addcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION)
+end
+function s.addfilter(c)
+	return c:IsSetCard(0x46) and c:IsType(TYPE_SPELL) and c:IsAbleToHand()
+end
+function s.addtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.addfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.addfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,s.addfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function s.addop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsFaceup() and tc:IsRelateToEffect(e) and aux.disfilter1(tc) then
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_DISABLE)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		tc:RegisterEffect(e1)
-		local e2=Effect.CreateEffect(e:GetHandler())
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_DISABLE_EFFECT)
-		e2:SetValue(RESET_TURN_SET)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		tc:RegisterEffect(e2)
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,tc)
 	end
 end
 
--- Effect 2: Destroy all opponent's cards
-function s.descon(e,tp,eg,ep,ev,re,r,rp)
-	return r&REASON_EFFECT~=0
+-- Effect 2: Board wipe if Pendulum Zone materials used
+function s.wipecon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local mat=c:GetMaterial()
+	return mat:IsExists(function(tc) return tc:IsPreviousLocation(LOCATION_PZONE) end,1,nil)
 end
-function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(nil,tp,0,LOCATION_ONFIELD,1,nil) end
-	local g=Duel.GetMatchingGroup(nil,tp,0,LOCATION_ONFIELD,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
-end
-function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(nil,tp,0,LOCATION_ONFIELD,nil)
+function s.wipeop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(function(c)
+		return c:IsMonster() and not c:IsLocation(LOCATION_PZONE)
+	end,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
 	if #g>0 then
 		Duel.Destroy(g,REASON_EFFECT)
 	end
 end
 
--- Effect 3: Register End Phase Special Summon
+-- Effect 3: Register revival effect if destroyed by card effect
 function s.regop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsReason(REASON_EFFECT) and c:IsReason(REASON_DESTROY) then
@@ -97,7 +93,7 @@ function s.regop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 		e1:SetCode(EVENT_PHASE+PHASE_END)
 		e1:SetRange(LOCATION_GRAVE)
-		e1:SetCountLimit(1,id+100)
+		e1:SetCountLimit(1,id+200)
 		e1:SetTarget(s.sptg)
 		e1:SetOperation(s.spop)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
@@ -105,9 +101,10 @@ function s.regop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
+-- Revival: Special Summon 1 Dragon from GY or face-up Extra Deck
 function s.spfilter(c,e,tp)
-	return c:IsCode(16178681) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-		and (c:IsLocation(LOCATION_GRAVE) or (c:IsFaceup() and c:IsLocation(LOCATION_EXTRA)))
+	return c:IsRace(RACE_DRAGON) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and (c:IsLocation(LOCATION_GRAVE) or (c:IsLocation(LOCATION_EXTRA) and c:IsFaceup()))
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
