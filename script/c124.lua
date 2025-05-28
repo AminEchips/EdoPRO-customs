5bdec915eccef5def18be77942d0e2df512bdc6b
Chips <aminekhoury0411@gmail.com>
2025-05-27 22:47:50 -0400

    Update c124.lua

--- script/c124.lua
+++ script/c124.lua
@@ -1,11 +1,17 @@
 --Odd-Eyes Lightstream Dragon
 local s,id=GetID()
-function s.initial_effect(c)
-	--Link Summon
-	c:EnableReviveLimit()
-	Link.AddProcedure(c,s.matfilter,2,2)
 
-	--On Link Summon: Special Summon from Pendulum Zone + setup Pendulum Zones
+-- Requires 2 Effect Monsters, including an "Odd-Eyes" Pendulum Monster
+function s.lcheck(g,lc,sumtype,tp)
+	return g:IsExists(function(c) return c:IsSetCard(0x99) and c:IsType(TYPE_PENDULUM,lc,sumtype,tp) end,1,nil)
+end
+
+function s.initial_effect(c)
+	-- Link Summon procedure
+	c:EnableReviveLimit()
+	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsType,TYPE_EFFECT),2,2,s.lcheck)
+
+	-- On Link Summon: Special Summon from PZone + place Pendulum Monsters from Extra Deck
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -17,7 +23,7 @@ function s.initial_effect(c)
 	e1:SetCountLimit(1,id)
 	c:RegisterEffect(e1)
 
-	--When Extra Deck monster is summoned to a zone this points to: Add 1 Pendulum Spell/Trap or Pendulum Monster
+	-- If a monster is Special Summoned from Extra Deck to a zone it points to: search
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
@@ -32,23 +38,22 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 
---Requires 2 Effect Monsters, including an "Odd-Eyes" Pendulum Monster
-function s.matfilter(c,lc,sumtype,tp)
-	return c:IsType(TYPE_EFFECT,lc,sumtype,tp) and c:IsSetCard(0x99)
-end
-
---Check if Link Summoned
+-- Check if Link Summoned
 function s.lkcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
 end
 
---Filter for Pendulum Zone monsters
+-- PZone Special Summon targets: Performapal, Magician, or Odd-Eyes
 function s.pzfilter(c,e,tp)
-	return (c:IsSetCard(0x9f) or c:IsSetCard(0x98) or c:IsSetCard(0x99)) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return (c:IsSetCard(0x9f) or c:IsSetCard(0x98) or c:IsSetCard(0x99))
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
+
+-- Face-up Pendulum Monsters from Extra Deck
 function s.extraPendFilter(c)
 	return c:IsType(TYPE_PENDULUM) and c:IsFaceup() and not c:IsForbidden()
 end
+
 function s.lktg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		local zoneCount=Duel.GetLocationCount(tp,LOCATION_MZONE)
@@ -56,42 +61,45 @@ function s.lktg(e,tp,eg,ep,ev,re,r,rp,chk)
 	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_PZONE)
 end
+
 function s.lkop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
 	local zoneCount=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	if zoneCount<=0 then return end
+
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.pzfilter,tp,LOCATION_PZONE,0,1,math.min(2,zoneCount),nil,e,tp)
-	if #g>0 then
-		if Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)>0 then
-			-- Place same number of Pendulum monsters from Extra Deck into Pendulum Zones
-			local count=#g
-			if Duel.GetMatchingGroupCount(s.extraPendFilter,tp,LOCATION_EXTRA,0,nil)>=count
-				and Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1) then
-				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
-				local pg=Duel.SelectMatchingCard(tp,s.extraPendFilter,tp,LOCATION_EXTRA,0,count,count,nil)
-				for tc in aux.Next(pg) do
-					Duel.MoveToField(tc,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
-				end
+
+	if #g>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)>0 then
+		local count=#g
+		if Duel.GetMatchingGroupCount(s.extraPendFilter,tp,LOCATION_EXTRA,0,nil)>=count
+			and (Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1)) then
+
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+			local pg=Duel.SelectMatchingCard(tp,s.extraPendFilter,tp,LOCATION_EXTRA,0,count,count,nil)
+			for tc in aux.Next(pg) do
+				Duel.MoveToField(tc,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
 			end
 		end
 	end
 end
 
---Check for ED monster to zone this card points to
+-- If monster is Summoned from ED to a zone this points to
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return eg:IsExists(function(sc)
 		return sc:IsSummonLocation(LOCATION_EXTRA) and c:GetLinkedGroup():IsContains(sc)
 	end,1,nil)
 end
+
 function s.thfilter(c)
 	return (c:IsType(TYPE_PENDULUM) and c:IsAbleToHand()) or (c:IsSetCard(0xf2) and c:IsAbleToHand())
 end
+
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
+
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
