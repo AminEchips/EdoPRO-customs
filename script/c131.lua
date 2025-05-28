148f5a24b0ff367d0f8f8a76d377be3b2f2daeb9
Chips <aminekhoury0411@gmail.com>
2025-05-28 10:48:30 -0400

    Update c131.lua

--- script/c131.lua
+++ script/c131.lua
@@ -1,45 +1,34 @@
 --Magical Star Illusion
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Always treated as "Pendulumgraph"
-    local e0=Effect.CreateEffect(c)
-    e0:SetType(EFFECT_TYPE_SINGLE)
-    e0:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e0:SetCode(EFFECT_ADD_SETCODE)
-    e0:SetRange(LOCATION_ALL)
-    e0:SetValue(0x254)
-    c:RegisterEffect(e0)
-
-    -- ATK boost (Quick-Play)
+    --Activate: Permanent ATK boost to your face-up monsters
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_ATKCHANGE)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_FREE_CHAIN)
-    e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
-    e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E+TIMING_DAMAGE_STEP)
-    e1:SetCondition(aux.StatChangeDamageStepCondition)
+    e1:SetHintTiming(0,TIMING_DAMAGE_STEP)
+    e1:SetCountLimit(1,id)
     e1:SetTarget(s.target)
     e1:SetOperation(s.activate)
     c:RegisterEffect(e1)
 
-    -- GY effect: Add up to 2 "Magician" Pendulum monsters, then send 1 card
+    --Graveyard effect: Add up to 2 "Magician" Pendulum Monsters, then send 1 card from hand or field to GY
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
     e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_TOGRAVE)
     e2:SetType(EFFECT_TYPE_IGNITION)
     e2:SetRange(LOCATION_GRAVE)
-    e2:SetCountLimit(1,id)
+    e2:SetCountLimit(1,id+100)
     e2:SetCondition(s.gycon)
-    e2:SetCost(aux.bfgcost)
     e2:SetTarget(s.gytg)
     e2:SetOperation(s.gyop)
     c:RegisterEffect(e2)
 end
 
--- FIELD ATK Boost
+-- Field Effect: Boost ATK permanently by 500 x cards in Pendulum Zones
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil) end
+    if chk==0 then return true end
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
     local ct=Duel.GetMatchingGroupCount(nil,tp,LOCATION_PZONE,0,nil)
@@ -49,39 +38,46 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
         local e1=Effect.CreateEffect(e:GetHandler())
         e1:SetType(EFFECT_TYPE_SINGLE)
         e1:SetCode(EFFECT_UPDATE_ATTACK)
-        e1:SetReset(RESETS_STANDARD_PHASE_END)
         e1:SetValue(ct*500)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD)
         tc:RegisterEffect(e1)
     end
 end
 
--- GY Condition: Not the turn it was sent
+-- GY condition: Not the turn this card was sent to the GY
 function s.gycon(e,tp,eg,ep,ev,re,r,rp)
-    return Duel.GetTurnCount()~=e:GetHandler():GetTurnID()
+    local c=e:GetHandler()
+    return Duel.GetTurnCount()~=c:GetTurnID()
+end
+
+-- GY filter: "Magician" Pendulum Monsters
+function s.thfilter(c)
+    return c:IsSetCard(0x98) and c:IsType(TYPE_PENDULUM) and c:IsAbleToHand()
 end
 
 -- GY Target
-function s.gyfilter(c)
-    return c:IsSetCard(0x98) and c:IsType(TYPE_PENDULUM) and c:IsAbleToHand()
-end
 function s.gytg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.gyfilter,tp,LOCATION_DECK,0,1,nil) end
+    if chk==0 then 
+        return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil)
+            and Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil)
+            or Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_ONFIELD,0,1,nil)
+    end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 
 -- GY Operation
 function s.gyop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then return end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-    local g=Duel.SelectMatchingCard(tp,s.gyfilter,tp,LOCATION_DECK,0,1,2,nil)
+    local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,2,nil)
     if #g>0 then
         Duel.SendtoHand(g,nil,REASON_EFFECT)
         Duel.ConfirmCards(1-tp,g)
         Duel.ShuffleHand(tp)
-        Duel.BreakEffect()
-        -- Send 1 card from hand or field to GY
         Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-        local sg=Duel.SelectMatchingCard(tp,Card.IsAbleToGrave,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,1,nil)
-        if #sg>0 then
-            Duel.SendtoGrave(sg,REASON_EFFECT)
+        local dg=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,1,nil)
+        if #dg>0 then
+            Duel.SendtoGrave(dg,REASON_EFFECT)
         end
     end
 end
