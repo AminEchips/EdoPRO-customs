3a0168c6a235c0fd393ff3287ab5e804fff0b90f
Chips <aminekhoury0411@gmail.com>
2025-05-28 10:45:53 -0400

    Update c131.lua

--- script/c131.lua
+++ script/c131.lua
@@ -2,15 +2,15 @@
 local s,id=GetID()
 function s.initial_effect(c)
     -- Always treated as "Pendulumgraph"
-    local e0a=Effect.CreateEffect(c)
-    e0a:SetType(EFFECT_TYPE_SINGLE)
-    e0a:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e0a:SetCode(EFFECT_ADD_SETCODE)
-    e0a:SetRange(LOCATION_ALL)
-    e0a:SetValue(0x254)
-    c:RegisterEffect(e0a)
+    local e0=Effect.CreateEffect(c)
+    e0:SetType(EFFECT_TYPE_SINGLE)
+    e0:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e0:SetCode(EFFECT_ADD_SETCODE)
+    e0:SetRange(LOCATION_ALL)
+    e0:SetValue(0x254)
+    c:RegisterEffect(e0)
 
-    -- Quick-play activation: Boost ATK of all monsters you control by 500 for each card in your Pendulum Zones
+    -- ATK boost (Quick-Play)
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_ATKCHANGE)
@@ -23,12 +23,11 @@ function s.initial_effect(c)
     e1:SetOperation(s.activate)
     c:RegisterEffect(e1)
 
-    -- GY effect: During End Phase, if a Spellcaster left your field by opponent's effect this turn
+    -- GY effect: Add up to 2 "Magician" Pendulum monsters, then send 1 card
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
     e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_TOGRAVE)
-    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-    e2:SetCode(EVENT_PHASE+PHASE_END)
+    e2:SetType(EFFECT_TYPE_IGNITION)
     e2:SetRange(LOCATION_GRAVE)
     e2:SetCountLimit(1,id)
     e2:SetCondition(s.gycon)
@@ -38,58 +37,51 @@ function s.initial_effect(c)
     c:RegisterEffect(e2)
 end
 
--- Apply ATK boost based on Pendulum Zone occupancy
+-- FIELD ATK Boost
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil) end
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-    local pzct=Duel.GetMatchingGroupCount(aux.TRUE,tp,LOCATION_PZONE,0,nil)
-    if pzct==0 then return end
+    local ct=Duel.GetMatchingGroupCount(nil,tp,LOCATION_PZONE,0,nil)
+    if ct==0 then return end
     local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
     for tc in g:Iter() do
         local e1=Effect.CreateEffect(e:GetHandler())
         e1:SetType(EFFECT_TYPE_SINGLE)
         e1:SetCode(EFFECT_UPDATE_ATTACK)
         e1:SetReset(RESETS_STANDARD_PHASE_END)
-        e1:SetValue(pzct*500)
+        e1:SetValue(ct*500)
         tc:RegisterEffect(e1)
     end
 end
 
--- Graveyard condition: a Spellcaster you controlled left field due to opponent's effect this turn
+-- GY Condition: Not the turn it was sent
 function s.gycon(e,tp,eg,ep,ev,re,r,rp)
-    local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_REMOVED,0,nil,e)
-    return g:GetCount()>0
-end
-function s.cfilter(c,e)
-    return c:IsReason(REASON_EFFECT) and c:IsPreviousControler(e:GetHandlerPlayer())
-        and c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsRace(RACE_SPELLCASTER)
+    return Duel.GetTurnCount()~=e:GetHandler():GetTurnID()
 end
 
--- Graveyard target and operation
+-- GY Target
+function s.gyfilter(c)
+    return c:IsSetCard(0x98) and c:IsType(TYPE_PENDULUM) and c:IsAbleToHand()
+end
 function s.gytg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+    if chk==0 then return Duel.IsExistingMatchingCard(s.gyfilter,tp,LOCATION_DECK,0,1,nil) end
 end
-function s.thfilter(c)
-    return c:IsRace(RACE_SPELLCASTER) and (c:IsAbleToHand() or c:IsAbleToGrave())
-end
+
+-- GY Operation
 function s.gyop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SELECT)
-    local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
-    if #g==0 then return end
-    local tc=g:GetFirst()
-    local op=0
-    if tc:IsAbleToHand() and tc:IsAbleToGrave() then
-        op=Duel.SelectOption(tp,aux.Stringid(id,2),aux.Stringid(id,3))
-    elseif tc:IsAbleToHand() then
-        op=0
-    else
-        op=1
-    end
-    if op==0 then
-        Duel.SendtoHand(tc,nil,REASON_EFFECT)
-        Duel.ConfirmCards(1-tp,tc)
-    else
-        Duel.SendtoGrave(tc,REASON_EFFECT)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.gyfilter,tp,LOCATION_DECK,0,1,2,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
+        Duel.ShuffleHand(tp)
+        Duel.BreakEffect()
+        -- Send 1 card from hand or field to GY
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+        local sg=Duel.SelectMatchingCard(tp,Card.IsAbleToGrave,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,1,nil)
+        if #sg>0 then
+            Duel.SendtoGrave(sg,REASON_EFFECT)
+        end
     end
 end
