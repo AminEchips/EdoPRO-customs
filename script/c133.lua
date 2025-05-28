25c2c141ad339e2c4ffc195e16450bd756ef6d05
Chips <aminekhoury0411@gmail.com>
2025-05-28 11:37:12 -0400

    Create c133.lua

---
+++ script/c133.lua
@@ -0,0 +1,91 @@
+--Rank-Up-Magic Supreme Force
+local s,id=GetID()
+function s.initial_effect(c)
+	--Rank-Up
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.rutg)
+	e1:SetOperation(s.ruop)
+	c:RegisterEffect(e1)
+	--GY effect: add 1 Spell/Trap from GY if you control Z-ARC
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(s.gycon)
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(s.gytg)
+	e2:SetOperation(s.gyop)
+	c:RegisterEffect(e2)
+end
+
+-- archetypes
+local SUPREME_KING_DRAGON = 0x20f8
+local REBELLION = 0x13b
+local ODD_EYES = 0x99
+
+-- Rank-Up
+function s.rufilter(c,e,tp)
+	local rk=c:GetRank()
+	return c:IsFaceup() and (c:IsSetCard(SUPREME_KING_DRAGON) or c:IsSetCard(REBELLION))
+		and Duel.IsExistingMatchingCard(s.rufilter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,c,rk)
+end
+function s.rufilter2(c,e,tp,mc,rk)
+	local newrk=c:GetRank()
+	return (c:IsSetCard(SUPREME_KING_DRAGON) or c:IsSetCard(REBELLION) or c:IsSetCard(ODD_EYES))
+		and (newrk==rk+3 or newrk==rk-3)
+		and c:IsType(TYPE_XYZ) and mc:IsCanBeXyzMaterial(c,tp)
+		and Duel.GetLocationCountFromEx(tp,tp,mc,c)>0
+		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false)
+end
+function s.rutg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.rufilter(chkc,e,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.rufilter,tp,LOCATION_MZONE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectTarget(tp,s.rufilter,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function s.ruop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if not tc or not tc:IsRelateToEffect(e) or tc:IsFacedown() then return end
+	local rk=tc:GetRank()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.rufilter2,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,tc,rk)
+	local sc=g:GetFirst()
+	if sc then
+		sc:SetMaterial(Group.FromCards(tc))
+		Duel.Overlay(sc,Group.FromCards(tc))
+		Duel.SpecialSummon(sc,SUMMON_TYPE_XYZ,tp,tp,false,false,POS_FACEUP)
+		sc:CompleteProcedure()
+	end
+end
+
+-- GY effect if you control Z-ARC
+function s.zarcfilter(c)
+	return c:IsFaceup() and c:IsCode(13331639)
+end
+function s.gycon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.zarcfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.gyfilter(c)
+	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToHand()
+end
+function s.gytg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingTarget(s.gyfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,s.gyfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function s.gyop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+	end
+end
