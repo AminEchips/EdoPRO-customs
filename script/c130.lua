53fc1c6d10afda0bd67f1d5b539ea1d4a5e324c3
Chips <aminekhoury0411@gmail.com>
2025-05-28 10:18:29 -0400

    Update c130.lua

--- script/c130.lua
+++ script/c130.lua
@@ -11,9 +11,9 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-s.listed_series={0x99} -- Odd-Eyes
+s.listed_series={0x99}
 
--- Condition: check if player controls 3+ Pendulum Monsters in Monster Zone
+-- Check condition for multiple effect activation
 function s.pendfilter(c)
 	return c:IsType(TYPE_PENDULUM) and c:IsFaceup() and c:IsLocation(LOCATION_MZONE)
 end
@@ -21,7 +21,7 @@ function s.has3Pendulums(tp)
 	return Duel.GetMatchingGroupCount(s.pendfilter,tp,LOCATION_MZONE,0,nil)>=3
 end
 
--- Effect filters
+-- Filters
 function s.spfilter(c,e,tp)
 	return c:IsSetCard(0x99) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
@@ -37,55 +37,64 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local canDoAll = s.has3Pendulums(tp)
+	local b1=Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp)
+	local b2=Duel.IsExistingMatchingCard(s.pzfilter,tp,LOCATION_EXTRA,0,1,nil)
+	local b3=Duel.IsPlayerCanDraw(tp,1)
+	if not (b1 or b2 or b3) then return end
 
-	local choices = {}
-	local labels = {}
+	local multi = s.has3Pendulums(tp)
+	local done1, done2, done3 = false, false, false
 
-	if Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) then
-		table.insert(choices,1)
-		table.insert(labels,aux.Stringid(id,0)) -- "Special Summon from hand"
-	end
-	if Duel.IsExistingMatchingCard(s.pzfilter,tp,LOCATION_EXTRA,0,1,nil) and
-		(Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1)) then
-		table.insert(choices,2)
-		table.insert(labels,aux.Stringid(id,1)) -- "Place in Pendulum Zone"
-	end
-	if Duel.IsPlayerCanDraw(tp,1) then
-		table.insert(choices,3)
-		table.insert(labels,aux.Stringid(id,2)) -- "Draw 1 card"
-	end
+	-- Always allow one effect
+	local opt = {}
+	if b1 then table.insert(opt, aux.Stringid(id,0)) end
+	if b2 then table.insert(opt, aux.Stringid(id,1)) end
+	if b3 then table.insert(opt, aux.Stringid(id,2)) end
 
-	if #choices==0 then return end
-
-	local selected={}
-	local maxSelect = canDoAll and #choices or 1
-	local opt = Duel.SelectMultipleOption(tp,labels,maxSelect,maxSelect,aux.Stringid(id,3))
-	for _,i in ipairs(opt) do
-		selected[choices[i]] = true
-	end
-
-	if selected[1] then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
-		if #g>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
-			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	local sel = Duel.SelectOption(tp,table.unpack(opt))
+	local trueIdx = 0
+	for i=0,2 do
+		if (i==0 and b1) or (i==1 and b2) or (i==2 and b3) then
+			if trueIdx==sel then sel=i break end
+			trueIdx=trueIdx+1
 		end
 	end
 
-	if selected[2] then
+	if sel==0 then
+		done1=true
+	elseif sel==1 then
+		done2=true
+	else
+		done3=true
+	end
+
+	-- Additional choices if allowed
+	if multi then
+		if not done1 and b1 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then done1=true end
+		if not done2 and b2 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then done2=true end
+		if not done3 and b3 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then done3=true end
+	end
+
+	-- Resolve effects
+	if done1 then
+		if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+			if #g>0 then Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP) end
+		end
+	end
+
+	if done2 then
 		if Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1) then
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
 			local g=Duel.SelectMatchingCard(tp,s.pzfilter,tp,LOCATION_EXTRA,0,1,1,nil)
-			local tc=g:GetFirst()
-			if tc then
-				Duel.MoveToField(tc,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+			if #g>0 then
+				Duel.MoveToField(g:GetFirst(),tp,tp,LOCATION_PZONE,POS_FACEUP,true)
 			end
 		end
 	end
 
-	if selected[3] then
+	if done3 then
 		Duel.Draw(tp,1,REASON_EFFECT)
 	end
 end
