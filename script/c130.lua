bb59d4d11c895775e366c428f7ac81a089099401
Chips <aminekhoury0411@gmail.com>
2025-05-28 10:15:45 -0400

    Update c130.lua

--- script/c130.lua
+++ script/c130.lua
@@ -7,27 +7,28 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
-	e1:SetCondition(s.condition)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-s.listed_series={0x99} -- Odd-Eyes archetype
+s.listed_series={0x99} -- Odd-Eyes
 
--- Must control 3 or more Pendulum Monsters in the Monster Zone
+-- Condition: check if player controls 3+ Pendulum Monsters in Monster Zone
 function s.pendfilter(c)
 	return c:IsType(TYPE_PENDULUM) and c:IsFaceup() and c:IsLocation(LOCATION_MZONE)
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
+function s.has3Pendulums(tp)
 	return Duel.GetMatchingGroupCount(s.pendfilter,tp,LOCATION_MZONE,0,nil)>=3
 end
 
+-- Effect filters
 function s.spfilter(c,e,tp)
 	return c:IsSetCard(0x99) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.pzfilter(c)
 	return c:IsSetCard(0x99) and c:IsFaceup() and c:IsType(TYPE_PENDULUM)
 end
+
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local b1=Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp)
 	local b2=Duel.IsExistingMatchingCard(s.pzfilter,tp,LOCATION_EXTRA,0,1,nil)
@@ -36,36 +37,45 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local b1=Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp)
-	local b2=Duel.IsExistingMatchingCard(s.pzfilter,tp,LOCATION_EXTRA,0,1,nil)
-	local b3=Duel.IsPlayerCanDraw(tp,1)
+	local c=e:GetHandler()
+	local canDoAll = s.has3Pendulums(tp)
+
+	local choices = {}
+	local labels = {}
+
+	if Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) then
+		table.insert(choices,1)
+		table.insert(labels,aux.Stringid(id,0)) -- "Special Summon from hand"
+	end
+	if Duel.IsExistingMatchingCard(s.pzfilter,tp,LOCATION_EXTRA,0,1,nil) and
+		(Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1)) then
+		table.insert(choices,2)
+		table.insert(labels,aux.Stringid(id,1)) -- "Place in Pendulum Zone"
+	end
+	if Duel.IsPlayerCanDraw(tp,1) then
+		table.insert(choices,3)
+		table.insert(labels,aux.Stringid(id,2)) -- "Draw 1 card"
+	end
+
+	if #choices==0 then return end
 
 	local selected={}
-	local opcount=0
-	if b1 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-		opcount=opcount+1
-		selected[opcount]=1
-	end
-	if b2 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
-		opcount=opcount+1
-		selected[opcount]=2
-	end
-	if b3 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
-		opcount=opcount+1
-		selected[opcount]=3
+	local maxSelect = canDoAll and #choices or 1
+	local opt = Duel.SelectMultipleOption(tp,labels,maxSelect,maxSelect,aux.Stringid(id,3))
+	for _,i in ipairs(opt) do
+		selected[choices[i]] = true
 	end
 
-	if opcount==0 then return end
-
-	for i=1,opcount do
-		if selected[i]==1 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-			local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
-			if #g>0 then
-				Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-			end
+	if selected[1] then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+		if #g>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 		end
-		if selected[i]==2 and (Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1)) then
+	end
+
+	if selected[2] then
+		if Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1) then
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
 			local g=Duel.SelectMatchingCard(tp,s.pzfilter,tp,LOCATION_EXTRA,0,1,1,nil)
 			local tc=g:GetFirst()
@@ -73,8 +83,9 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 				Duel.MoveToField(tc,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
 			end
 		end
-		if selected[i]==3 then
-			Duel.Draw(tp,1,REASON_EFFECT)
-		end
+	end
+
+	if selected[3] then
+		Duel.Draw(tp,1,REASON_EFFECT)
 	end
 end
