8e6a11a160c5949e0f891325311415c13264e916
Chips <aminekhoury0411@gmail.com>
2025-05-28 21:18:57 -0400

    Update c124.lua

--- script/c124.lua
+++ script/c124.lua
@@ -92,7 +92,7 @@ function s.thcon(e,tp,eg,ep,ev,re,r,rp)
 end
 
 function s.thfilter(c)
-	return (c:IsType(TYPE_PENDULUM) and c:IsAbleToHand()) or (c:IsSetCard(0xf2) and c:IsAbleToHand())
+	return c:IsSetCard(0xf2) and c:IsAbleToHand()
 end
 
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
