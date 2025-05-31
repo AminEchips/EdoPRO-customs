a3f3555319bc9c7b92cd64d97d9066afd0f811ff
Chips <aminekhoury0411@gmail.com>
2025-05-31 19:16:11 -0400

    Update c805.lua

--- script/c805.lua
+++ script/c805.lua
@@ -1,71 +1,41 @@
---Raidraptor - Golden Strix
+""--Raidraptor - Golden Strix
 local s,id=GetID()
 function s.initial_effect(c)
-	--Pendulum Summon procedure
+	--Pendulum Attribute
 	Pendulum.AddProcedure(c)
 
-	--Pendulum Effect: Redirect attack to this card
+	--Special Summon itself from hand if you control no monsters or only DARK Winged Beast monsters
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
-	e1:SetRange(LOCATION_PZONE)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
 	e1:SetCountLimit(1,id)
-	e1:SetCondition(s.atkcon)
-	e1:SetTarget(s.atktg)
-	e1:SetOperation(s.atkop)
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
 
-	--Monster Effect: Special Summon itself from hand
+	--Place in Pendulum Zone if destroyed or sent to GY as cost for a DARK monster
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetRange(LOCATION_HAND)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCountLimit(1,{id,1})
-	e2:SetCondition(s.spcon)
-	e2:SetTarget(s.sptg)
-	e2:SetOperation(s.spop)
+	e2:SetCondition(s.pencon)
+	e2:SetTarget(s.pentg)
+	e2:SetOperation(s.penop)
 	c:RegisterEffect(e2)
-
-	--Move to Pendulum Zone if destroyed or sent as cost for DARK monster's effect
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,2))
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e3:SetCode(EVENT_TO_GRAVE)
-	e3:SetProperty(EFFECT_FLAG_DELAY)
-	e3:SetCountLimit(1,{id,2})
-	e3:SetCondition(s.pzcon)
-	e3:SetTarget(s.pztg)
-	e3:SetOperation(s.pzop)
-	c:RegisterEffect(e3)
-end
-s.listed_series={0xba} -- Raidraptor
-
---Redirect attack to this card
-function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
-	local d=Duel.GetAttackTarget()
-	return d and d:IsControler(tp) and d:IsRace(RACE_WINGEDBEAST) and d:IsType(TYPE_XYZ)
-end
-function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
-end
-function s.atkop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
-		Duel.ChangeAttackTarget(c)
-	end
 end
 
---Special Summon from hand
-function s.onlydarkwinged(c)
-	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsRace(RACE_WINGEDBEAST)
+-- Special Summon condition
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsRace(RACE_WINGEDBEAST) and c:IsAttribute(ATTRIBUTE_DARK)
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
-	return #g==0 or g:FilterCount(s.onlydarkwinged,nil)==#g
+	return #g==0 or (#g>0 and g:FilterCount(s.cfilter,nil)==#g)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
@@ -79,25 +49,28 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
---Place to Pendulum Zone
-function s.pzcon(e,tp,eg,ep,ev,re,r,rp)
+-- Pendulum Zone placement condition
+function s.pencon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if not c:IsPreviousLocation(LOCATION_ONFIELD) then return false end
-	if c:IsReason(REASON_DESTROY) then return true end
-	if not (c:IsReason(REASON_COST) and re and re:IsActivated() and re:IsMonsterEffect()) then return false end
-	local rc=re:GetHandler()
-	if rc:IsRelateToEffect(re) and rc:IsFaceup() then
-		return rc:IsAttribute(ATTRIBUTE_DARK)
-	else
-		return Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_ATTRIBUTE)&ATTRIBUTE_DARK~=0
+	-- If destroyed from Monster Zone by battle or card effect
+	if c:IsPreviousLocation(LOCATION_MZONE) and c:IsReason(REASON_DESTROY) then return true end
+	-- If sent to GY as cost to activate a DARK monster effect
+	if bit.band(r,REASON_COST)~=0 and re and re:IsActivated() and re:IsMonsterEffect() then
+		local rc=re:GetHandler()
+		if rc:IsRace(RACE_DARK) or bit.band(Duel.GetChainInfo(0,CHAININFO_TRIGGERING_RACE),RACE_DARK)~=0 then
+			return true
+		end
 	end
+	return false
 end
-function s.pztg(e,tp,eg,ep,ev,re,r,rp,chk)
+
+function s.pentg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.CheckPendulumZones(tp) end
 end
-function s.pzop(e,tp,eg,ep,ev,re,r,rp)
+function s.penop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) and Duel.CheckPendulumZones(tp) then
+	if not c:IsRelateToEffect(e) then return end
+	if Duel.CheckPendulumZones(tp) then
 		Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
 	end
 end
