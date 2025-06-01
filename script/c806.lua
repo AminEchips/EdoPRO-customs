e8d1b4e4f1c2f35541e58f994a03f02e1c0568c6
Chips <aminekhoury0411@gmail.com>
2025-05-31 23:30:29 -0400

    Update c806.lua

--- script/c806.lua
+++ script/c806.lua
@@ -59,8 +59,7 @@ function s.pzcon(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
 	if not g or #g~=1 then return false end
 	local tc=g:GetFirst()
-	local c=e:GetHandler()
-	return tc:IsFaceup() and tc:IsControler(tp) and tc:IsSetCard(0xba) and Duel.IsChainDisablable(ev)
+	return tc:IsFaceup() and tc:IsControler(tp) and tc:IsSetCard(0xba)
 end
 function s.pztg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
@@ -70,12 +69,16 @@ function s.pzop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) or Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
-		if Duel.SelectYesNo(tp,aux.Stringid(id,4)) then
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-			local validTg=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,c)
-			local tg=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,c)
-			if #tg>0 then
-				Duel.ChangeTargetCard(ev,tg)
+		local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
+		if g and #g==1 then
+			local original=g:GetFirst()
+			if Duel.SelectYesNo(tp,aux.Stringid(id,4)) then
+				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+				local validTg=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,original)
+				local tg=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,original)
+				if #tg>0 then
+					Duel.ChangeTargetCard(ev,tg)
+				end
 			end
 		end
 	end
