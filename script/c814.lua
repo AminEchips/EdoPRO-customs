82882e10bbcb22df58b87dcd6b07c86793165e2d
Chips <aminekhoury0411@gmail.com>
2025-06-01 23:20:20 -0400

    Update c814.lua

--- script/c814.lua
+++ script/c814.lua
@@ -1,11 +1,11 @@
 --Raidraptor - Apex Falcon
 local s,id=GetID()
 function s.initial_effect(c)
-    --Xyz Summon
+    -- Xyz Summon
     Xyz.AddProcedure(c,nil,11,3,s.ovfilter,aux.Stringid(id,0),3,nil)
     c:EnableReviveLimit()
 
-    -- Unaffected by other card effects
+    -- Unaffected by other cards' effects
     local e0=Effect.CreateEffect(c)
     e0:SetType(EFFECT_TYPE_SINGLE)
     e0:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
@@ -24,7 +24,7 @@ function s.initial_effect(c)
     e1:SetValue(-1000)
     c:RegisterEffect(e1)
 
-    -- Destroy opponent's Special Summoned monsters if ATK = 0
+    -- Destroy opponent's monsters with 0 ATK from this effect
     local e2=Effect.CreateEffect(c)
     e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
     e2:SetCode(EVENT_ADJUST)
@@ -46,7 +46,7 @@ function s.initial_effect(c)
     e3:SetOperation(s.ctrlop)
     c:RegisterEffect(e3)
 
-    -- Second attack if it attacked a monster with lower ATK
+    -- Extra attack if it battled a lower ATK monster
     local e4=Effect.CreateEffect(c)
     e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
     e4:SetCode(EVENT_BATTLED)
@@ -55,22 +55,22 @@ function s.initial_effect(c)
     c:RegisterEffect(e4)
 end
 
--- Xyz material must be "Raidraptor" monsters
+-- Must use "Raidraptor" monsters for Xyz Material
 function s.ovfilter(c)
-    return c:IsSetCard(0xba) and c:IsType(TYPE_MONSTER) and c:IsLevel(11)
+    return c:IsSetCard(0xba) and c:IsType(TYPE_MONSTER)
 end
 
--- Unaffected by other cards' effects
+-- Unaffected by other card effects
 function s.efilter(e,te)
     return te:GetOwner()~=e:GetOwner()
 end
 
--- Target opponent’s Special Summoned monsters
+-- Affects opponent's Special Summoned monsters
 function s.atktg(e,c)
     return c:IsSummonType(SUMMON_TYPE_SPECIAL)
 end
 
--- Destroy Special Summoned monsters with 0 ATK
+-- Destroy those with 0 ATK
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
     local g=Duel.GetMatchingGroup(function(c)
         return c:IsFaceup() and c:IsSummonType(SUMMON_TYPE_SPECIAL) and c:GetAttack()==0
@@ -80,32 +80,31 @@ function s.desop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
--- Condition: has a "Raidraptor" Xyz material
+-- Condition: has "Raidraptor" Xyz as material
 function s.ctrlcon(e)
-    local c=e:GetHandler()
-    return c:GetOverlayGroup():IsExists(Card.IsSetCard,1,nil,0xba)
+    return e:GetHandler():GetOverlayGroup():IsExists(Card.IsSetCard,1,nil,0xba)
 end
 
--- Cost: detach 2 materials
+-- Detach 2 materials as cost
 function s.ctrlcost(e,tp,eg,ep,ev,re,r,rp,chk)
     return e:GetHandler():CheckRemoveOverlayCard(tp,2,REASON_COST)
 end
 
--- Proper target filter
+-- Filter for opponent's Xyz monsters
 function s.ctrlfilter(c)
-    return c:IsFaceup() and c:IsType(TYPE_XYZ) and c:IsControler(1-tp) and c:IsAbleToChangeControler()
+    return c:IsFaceup() and c:IsType(TYPE_XYZ) and c:IsControlerCanBeChanged()
 end
 
 -- Target for control effect
 function s.ctrltg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.ctrlfilter(chkc) end
+    if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and s.ctrlfilter(chkc) end
     if chk==0 then return Duel.IsExistingTarget(s.ctrlfilter,tp,0,LOCATION_MZONE,1,nil) end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
     local g=Duel.SelectTarget(tp,s.ctrlfilter,tp,0,LOCATION_MZONE,1,1,nil)
     Duel.SetOperationInfo(0,CATEGORY_CONTROL,g,1,0,0)
 end
 
--- Operation for control
+-- Control operation
 function s.ctrlop(e,tp,eg,ep,ev,re,r,rp)
     local tc=Duel.GetFirstTarget()
     local c=e:GetHandler()
@@ -113,20 +112,19 @@ function s.ctrlop(e,tp,eg,ep,ev,re,r,rp)
         local e1=Effect.CreateEffect(c)
         e1:SetType(EFFECT_TYPE_SINGLE)
         e1:SetCode(EFFECT_ADD_SETCODE)
-        e1:SetValue(0xba) -- Becomes "Raidraptor"
+        e1:SetValue(0xba) -- Become "Raidraptor"
         e1:SetReset(RESET_EVENT+RESETS_STANDARD)
         tc:RegisterEffect(e1)
     end
 end
 
--- Second attack if it battled a monster with lower ATK
+-- If it battled a monster with lower ATK, gain one more attack this turn
 function s.extracon(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     local bc=c:GetBattleTarget()
     return bc and bc:IsRelateToBattle() and bc:IsFaceup() and bc:GetAttack()<c:GetAttack()
 end
 
--- Grant 1 additional attack
 function s.extraop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     if c:IsFaceup() then
