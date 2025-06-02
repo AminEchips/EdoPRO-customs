512e91ae116a9dbce39014bab2458e35bf7f6a98
Chips <aminekhoury0411@gmail.com>
2025-06-02 08:31:12 -0400

    Update c822.lua

--- script/c822.lua
+++ script/c822.lua
@@ -1,7 +1,7 @@
 --The Phantom Knights of Blunt Spear
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Special Summon from hand if you control a "The Phantom Knights" monster (non-chain)
+    -- Special Summon from hand (non-chain)
     local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_FIELD)
     e1:SetCode(EFFECT_SPSUMMON_PROC)
@@ -9,20 +9,28 @@ function s.initial_effect(c)
     e1:SetRange(LOCATION_HAND)
     e1:SetCountLimit(1,id)
     e1:SetCondition(s.spcon)
-    e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
 
-    -- GY effect: Set 1 "Phantom Knights" Spell from GY
+    -- On successful Special Summon by its own effect: optional level +1
     local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,1))
-    e2:SetCategory(CATEGORY_LEAVE_GRAVE)
-    e2:SetType(EFFECT_TYPE_IGNITION)
-    e2:SetRange(LOCATION_GRAVE)
-    e2:SetCountLimit(1,{id,1})
-    e2:SetCost(s.setcost)
-    e2:SetTarget(s.settg)
-    e2:SetOperation(s.setop)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e2:SetCondition(s.lvcon)
+    e2:SetOperation(s.lvop)
     c:RegisterEffect(e2)
+
+    -- GY effect: Set 1 "Phantom Knights" Spell
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetCategory(CATEGORY_LEAVE_GRAVE)
+    e3:SetType(EFFECT_TYPE_IGNITION)
+    e3:SetRange(LOCATION_GRAVE)
+    e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e3:SetCountLimit(1,{id,1})
+    e3:SetCost(s.setcost)
+    e3:SetTarget(s.settg)
+    e3:SetOperation(s.setop)
+    c:RegisterEffect(e3)
 end
 
 -- Check if player controls a "The Phantom Knights" monster
@@ -32,13 +40,16 @@ end
 function s.spcon(e,c)
     if c==nil then return true end
     return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
-        and Duel.IsExistingMatchingCard(s.cfilter,c:GetControler(),LOCATION_MZONE,0,1,nil)
+        and Duel.IsExistingMatchingGroup(s.cfilter,c:GetControler(),LOCATION_MZONE,0,1,nil)
 end
 
--- Optional level increase on summon
-function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
-    local opt = Duel.SelectYesNo(tp,aux.Stringid(id,0))
-    if opt then
+-- Level increase prompt (after being Special Summoned by its own effect)
+function s.lvcon(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler():IsPreviousLocation(LOCATION_HAND)
+end
+function s.lvop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
         local e1=Effect.CreateEffect(c)
         e1:SetType(EFFECT_TYPE_SINGLE)
         e1:SetCode(EFFECT_UPDATE_LEVEL)
@@ -54,17 +65,19 @@ function s.setcost(e,tp,eg,ep,ev,re,r,rp,chk)
     aux.bfgcost(e,tp,eg,ep,ev,re,r,rp,1)
 end
 
--- Target 1 "Phantom Knights" Spell in your GY to Set
+-- Filter for Phantom Knights Spell
 function s.setfilter(c)
-    return c:IsType(TYPE_SPELL) and c:IsSetCard(0xdb) and c:IsSSetable()
+    return c:IsType(TYPE_SPELL) and c:IsSetCard(0x10db) and c:IsSSetable()
 end
 function s.settg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.setfilter(chkc) end
     if chk==0 then return Duel.IsExistingTarget(s.setfilter,tp,LOCATION_GRAVE,0,1,nil) end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
     local g=Duel.SelectTarget(tp,s.setfilter,tp,LOCATION_GRAVE,0,1,1,nil)
     Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,g,1,0,0)
 end
 
+-- Set the selected Spell
 function s.setop(e,tp,eg,ep,ev,re,r,rp)
     local tc=Duel.GetFirstTarget()
     if tc and tc:IsRelateToEffect(e) then
