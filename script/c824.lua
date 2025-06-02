f43cf217b3761818b331b89eacea16c1de24b367
Chips <aminekhoury0411@gmail.com>
2025-06-02 15:52:31 -0400

    Update c824.lua

--- script/c824.lua
+++ script/c824.lua
@@ -1,7 +1,6 @@
 --The Phantom Knights of Cursed Swords
 local s,id=GetID()
 function s.initial_effect(c)
-
     -- Effect 1: Discard 1 card; Set 1 "Phantom Knights" Spell from Deck
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
@@ -14,7 +13,7 @@ function s.initial_effect(c)
     e1:SetOperation(s.setop)
     c:RegisterEffect(e1)
 
-    -- Effect 2: Detached from DARK Xyz Monster to activate effect
+    -- Effect 2: Detached from DARK Xyz monster's effect → recover banished Phantom Knights Trap
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
     e2:SetCategory(CATEGORY_TOHAND)
@@ -27,7 +26,7 @@ function s.initial_effect(c)
     e2:SetOperation(s.recop)
     c:RegisterEffect(e2)
 
-    -- Effect 3: GY effect – banish this card, boost "The Phantom Knights" ATK
+    -- Effect 3: GY boost for "The Phantom Knights" monsters
     local e3=Effect.CreateEffect(c)
     e3:SetDescription(aux.Stringid(id,2))
     e3:SetType(EFFECT_TYPE_IGNITION)
@@ -38,40 +37,42 @@ function s.initial_effect(c)
     c:RegisterEffect(e3)
 end
 
--- Effect 1: Discard 1 card as cost
+-------------------------------
+-- Effect 1 - Set a Spell
+-------------------------------
 function s.setcost(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
     Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
 end
 
--- Set 1 "Phantom Knights" Spell from Deck
 function s.setfilter(c)
     return c:IsType(TYPE_SPELL) and c:IsSetCard(0xdb) and c:IsSSetable()
 end
+
 function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_DECK,0,1,nil) end
 end
+
 function s.setop(e,tp,eg,ep,ev,re,r,rp)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
     local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,1,1,nil)
-    if #g>0 then
-        Duel.SSet(tp,g:GetFirst())
-    end
+    if #g>0 then Duel.SSet(tp,g:GetFirst()) end
 end
 
--- Effect 2: Condition – detached from DARK Xyz to activate its effect
+-------------------------------
+-- Effect 2 - Recover Trap when detached
+-------------------------------
 function s.reccon(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
-    return c:IsReason(REASON_COST) and r&REASON_COST~=0
-        and c:IsReason(REASON_EFFECT) and r&REASON_EFFECT~=0
-        and c:IsPreviousLocation(LOCATION_OVERLAY)
-        and c:GetReasonCard() and c:GetReasonCard():IsAttribute(ATTRIBUTE_DARK)
+    return c:IsReason(REASON_COST) and c:IsPreviousLocation(LOCATION_OVERLAY)
+        and re and re:IsActivated() and re:IsMonsterEffect()
+        and re:GetHandler():IsAttribute(ATTRIBUTE_DARK)
 end
 
--- Target banished "Phantom Knights" Trap
 function s.trapfilter(c)
-    return c:IsSetCard(0xdb) and c:IsType(TYPE_TRAP) and c:IsFaceup() and c:IsAbleToHand()
+    return c:IsFaceup() and c:IsSetCard(0xdb) and c:IsType(TYPE_TRAP) and c:IsAbleToHand()
 end
+
 function s.rectg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
     if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_REMOVED) and s.trapfilter(chkc) end
     if chk==0 then return Duel.IsExistingTarget(s.trapfilter,tp,LOCATION_REMOVED,0,1,nil) end
@@ -79,6 +80,7 @@ function s.rectg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
     local g=Duel.SelectTarget(tp,s.trapfilter,tp,LOCATION_REMOVED,0,1,1,nil)
     Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
 end
+
 function s.recop(e,tp,eg,ep,ev,re,r,rp)
     local tc=Duel.GetFirstTarget()
     if tc and tc:IsRelateToEffect(e) then
@@ -87,10 +89,13 @@ function s.recop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
--- Effect 3: GY effect – boost all "The Phantom Knights" you control
+-------------------------------
+-- Effect 3 - ATK boost from GY
+-------------------------------
 function s.atkfilter(c)
     return c:IsFaceup() and c:IsSetCard(0x10db)
 end
+
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
     local g=Duel.GetMatchingGroup(s.atkfilter,tp,LOCATION_MZONE,0,nil)
     for tc in g:Iter() do
