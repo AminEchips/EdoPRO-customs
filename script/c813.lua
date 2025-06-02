2b3442bc4c16b60878b10980778732aeb50800e4
Chips <aminekhoury0411@gmail.com>
2025-06-01 23:03:57 -0400

    Update c813.lua

--- script/c813.lua
+++ script/c813.lua
@@ -30,15 +30,14 @@ function s.initial_effect(c)
     e3:SetOperation(s.atkop)
     c:RegisterEffect(e3)
 
-    -- Float effect when banished
+    -- Float when banished: revive an Xyz from GY and attach this card
     local e4=Effect.CreateEffect(c)
     e4:SetDescription(aux.Stringid(id,1))
     e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e4:SetCode(EVENT_REMOVE)
-    e4:SetProperty(EFFECT_FLAG_DELAY)
+    e4:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
     e4:SetCountLimit(1,id,EFFECT_COUNT_CODE_DUEL)
-    e4:SetCondition(s.recon)
     e4:SetTarget(s.retg)
     e4:SetOperation(s.reop)
     c:RegisterEffect(e4)
@@ -52,12 +51,12 @@ function s.matcon(e)
     return e:GetHandler():GetOverlayGroup():IsExists(s.matfilter,1,nil)
 end
 
--- ATK gain condition: during damage calc
+-- Battle damage condition
 function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
     return Duel.GetBattleDamage(tp)>0 or Duel.GetBattleDamage(1-tp)>0
 end
 
--- Cost: Send 1 Winged Beast from Extra Deck to GY
+-- Cost: send 1 Winged Beast from Extra Deck to GY
 function s.cfilter(c)
     return c:IsRace(RACE_WINGEDBEAST) and c:IsAbleToGraveAsCost() and c:GetAttack()>0
 end
@@ -66,11 +65,10 @@ function s.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
     local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_EXTRA,0,1,1,nil)
     e:SetLabel(g:GetFirst():GetAttack())
-    e:SetLabelObject(g:GetFirst())
     Duel.SendtoGrave(g,REASON_COST)
 end
 
--- Operation: Gain ATK and schedule banishment
+-- Operation: gain ATK and schedule banish
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     local atk=e:GetLabel()
@@ -82,61 +80,40 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
         e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE)
         c:RegisterEffect(e1)
 
-        -- Schedule banishment at Damage Step end
+        -- Schedule banish at Damage Step end
         local e2=Effect.CreateEffect(c)
         e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
         e2:SetCode(EVENT_DAMAGE_STEP_END)
-        e2:SetCountLimit(1)
-        e2:SetLabelObject(e:GetLabelObject())
-        e2:SetOperation(s.rmop)
+        e2:SetOperation(function(_,tp) Duel.Remove(c,POS_FACEUP,REASON_EFFECT) end)
         e2:SetReset(RESET_PHASE+PHASE_DAMAGE)
         Duel.RegisterEffect(e2,tp)
     end
 end
 
--- Banish self at end of Damage Step
-function s.rmop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetOwner()
-    if c:IsRelateToBattle() and c:IsControler(tp) then
-        Duel.Remove(c,POS_FACEUP,REASON_EFFECT)
-        c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1)
-        c:RegisterFlagEffect(id+1,RESET_EVENT+RESETS_STANDARD,0,1)
-        c:SetCardTarget(e:GetLabelObject())
-    end
-end
-
--- Float condition: self was banished by own effect
-function s.recon(e,tp,eg,ep,ev,re,r,rp)
-    return e:GetHandler():GetFlagEffect(id)>0 and e:GetHandler():GetFlagEffect(id+1)>0
-end
-
--- Float target
+-- Float Target: Any Xyz monster in GY
 function s.retg(e,tp,eg,ep,ev,re,r,rp,chk)
-    local c=e:GetHandler()
-    local tc=c:GetFirstCardTarget()
     if chk==0 then
-        return tc and tc:IsType(TYPE_XYZ)
-            and tc:IsAbleToExtra()
-            and Duel.GetLocationCountFromEx(tp,tp,nil,tc)>0
-            and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,tc:GetCode(),e,tp,c)
+        return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsType,TYPE_XYZ),tp,LOCATION_GRAVE,0,1,nil)
+            and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
     end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+    local g=Duel.SelectTarget(tp,aux.FaceupFilter(Card.IsType,TYPE_XYZ),tp,LOCATION_GRAVE,0,1,1,nil)
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 
--- Float summon operation
-function s.spfilter(c,code,e,tp,mat)
+-- Float Operation
+function s.spfilter(c,code,e,tp)
     return c:IsCode(code) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false)
-        and Duel.GetLocationCountFromEx(tp,tp,mat,c)>0
 end
 function s.reop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
-    local tc=c:GetFirstCardTarget()
+    local tc=Duel.GetFirstTarget()
     if not (c:IsRelateToEffect(e) and tc and tc:IsRelateToEffect(e)) then return end
     if Duel.SendtoDeck(tc,nil,SEQ_DECKTOP,REASON_EFFECT)>0 and tc:IsLocation(LOCATION_EXTRA) then
         Duel.ShuffleDeck(tp)
         Duel.BreakEffect()
         Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-        local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,tc:GetCode(),e,tp,c)
+        local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,tc:GetCode(),e,tp)
         local sc=g:GetFirst()
         if sc and Duel.SpecialSummon(sc,SUMMON_TYPE_XYZ,tp,tp,false,false,POS_FACEUP)>0 then
             Duel.Overlay(sc,Group.FromCards(c))
