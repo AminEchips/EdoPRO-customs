1fbf79baf95cad5bd87c5f7c473dd577e938ecc8
Chips <aminekhoury0411@gmail.com>
2025-06-03 07:48:48 -0400

    Create c827.lua

---
+++ script/c827.lua
@@ -0,0 +1,90 @@
+--The Phantom Knights of Starliege Cavalier
+local s,id=GetID()
+function s.initial_effect(c)
+    c:EnableReviveLimit()
+    -- Xyz Summon procedure
+    Xyz.AddProcedure(c,nil,3,2)
+
+    -- Effect 1: On Xyz Summon, revive 1 Rank 3 monster from GY, then attach this card to it
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+    e1:SetCondition(s.spcon1)
+    e1:SetTarget(s.sptg1)
+    e1:SetOperation(s.spop1)
+    c:RegisterEffect(e1)
+
+    -- Effect 2: When Xyz Monster is Special Summoned, revive another Rank 3 and attach this card
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e2:SetRange(LOCATION_GRAVE)
+    e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+    e2:SetCountLimit(1,id)
+    e2:SetCondition(s.spcon2)
+    e2:SetTarget(s.sptg2)
+    e2:SetOperation(s.spop2)
+    c:RegisterEffect(e2)
+end
+
+-- Filter for Rank 3 monsters
+function s.r3filter(c,e,tp)
+    return c:IsRank(3) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+
+-- e1: Check if summoned by Xyz
+function s.spcon1(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ)
+end
+function s.sptg1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.r3filter(chkc,e,tp) end
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.IsExistingTarget(s.r3filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectTarget(tp,s.r3filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function s.spop1(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local tc=Duel.GetFirstTarget()
+    if tc and tc:IsRelateToEffect(e) and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)>0 then
+        Duel.BreakEffect()
+        if c:IsRelateToEffect(e) then
+            Duel.Overlay(tc,Group.FromCards(c))
+        end
+    end
+end
+
+-- e2: When Xyz Monster is Summoned while this + another Rank 3 in GY
+function s.xyzcheck(c,tp)
+    return c:IsControler(tp) and c:IsType(TYPE_XYZ)
+end
+function s.spcon2(e,tp,eg,ep,ev,re,r,rp)
+    return eg:IsExists(s.xyzcheck,1,nil,tp)
+        and Duel.IsExistingMatchingCard(aux.FilterFaceup(Card.IsRank),tp,LOCATION_GRAVE,0,2,nil,3)
+        and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+end
+function s.r3filter2(c,e,tp)
+    return c:IsRank(3) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.r3filter2(chkc,e,tp) end
+    if chk==0 then return e:GetHandler():IsAbleToExtra()
+        and Duel.IsExistingTarget(s.r3filter2,tp,LOCATION_GRAVE,0,1,e:GetHandler(),e,tp) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectTarget(tp,s.r3filter2,tp,LOCATION_GRAVE,0,1,1,e:GetHandler(),e,tp)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function s.spop2(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local tc=Duel.GetFirstTarget()
+    if tc and tc:IsRelateToEffect(e) and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)>0 and c:IsRelateToEffect(e) then
+        Duel.BreakEffect()
+        Duel.Overlay(tc,Group.FromCards(c))
+    end
+end
