9fcda7c2d7422ceda742979af3d495ac3aa794e2
Chips <aminekhoury0411@gmail.com>
2025-06-03 12:10:43 -0400

    Update c831.lua

--- script/c831.lua
+++ script/c831.lua
@@ -5,10 +5,10 @@ function s.initial_effect(c)
     Xyz.AddProcedure(c,aux.FilterBoolFunction(Card.IsAttribute,ATTRIBUTE_DARK),5,2)
     c:EnableReviveLimit()
 
-    --Detach to activate 1 of 2 effects (Quick if has PK material)
+    --Choose effect after detaching material
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DRAW)
+    e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DRAW+CATEGORY_TODECK)
     e1:SetType(EFFECT_TYPE_IGNITION)
     e1:SetRange(LOCATION_MZONE)
     e1:SetCountLimit(1,id)
@@ -17,14 +17,14 @@ function s.initial_effect(c)
     e1:SetOperation(s.operation)
     c:RegisterEffect(e1)
 
-    --Quick effect clone if PK material
+    --Quick variant if PK material
     local e2=e1:Clone()
     e2:SetType(EFFECT_TYPE_QUICK_O)
     e2:SetCode(EVENT_FREE_CHAIN)
     e2:SetCondition(s.quickcon)
     c:RegisterEffect(e2)
 
-    --Floating effect
+    --Floating: On destruction, prevent a monster from attacking directly
     local e3=Effect.CreateEffect(c)
     e3:SetDescription(aux.Stringid(id,2))
     e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
@@ -35,24 +35,20 @@ function s.initial_effect(c)
     c:RegisterEffect(e3)
 end
 
---Detach 1
+--Detach 1 as cost
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
     e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
 end
 
---Choose effect
+--Determine possible options and handle targeting if needed
 function s.tgfilter(c)
     return c:IsSetCard(0x10db) and c:IsAbleToDeck()
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return true end
-end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
     local b1=Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_MZONE,1,nil)
     local b2=Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_GRAVE,0,1,nil) and Duel.IsPlayerCanDraw(tp,1)
-    if not (b1 or b2) then return end
+    if chk==0 then return b1 or b2 end
 
     local op=0
     if b1 and b2 then
@@ -64,48 +60,45 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
         Duel.Hint(HINT_OPSELECTED,1-tp,aux.Stringid(id,1))
         op=1
     end
+    e:SetLabel(op)
 
+    if op==1 then
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+        local g=Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+        Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
+        Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+    end
+end
+
+--Resolve based on chosen effect
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local op=e:GetLabel()
     if op==0 then
-        --Destroy + GY negate (non-targeting)
+        -- Destroy 1 opponent monster (non-targeting)
         local g=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,nil)
         if #g>0 then
             Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
             local sg=g:Select(tp,1,1,nil)
-            if #sg>0 and Duel.Destroy(sg,REASON_EFFECT)>0 then
-                local tc=sg:GetFirst()
-                local e1=Effect.CreateEffect(c)
-                e1:SetType(EFFECT_TYPE_SINGLE)
-                e1:SetCode(EFFECT_DISABLE)
-                e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-                tc:RegisterEffect(e1)
-                local e2=e1:Clone()
-                e2:SetCode(EFFECT_DISABLE_EFFECT)
-                tc:RegisterEffect(e2)
-            end
+            Duel.Destroy(sg,REASON_EFFECT)
         end
     else
-        --Target "Phantom Knights" in GY, shuffle + draw
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-        local g=Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_GRAVE,0,1,1,nil)
-        if #g>0 then
-            Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
-            Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
-            Duel.BreakEffect()
-            local tc=g:GetFirst()
-            if tc:IsRelateToEffect(e) then
-                Duel.SendtoDeck(tc,nil,SEQ_DECKBOTTOM,REASON_EFFECT)
+        -- Send target to bottom of Deck and draw 1
+        local tc=Duel.GetFirstTarget()
+        if tc and tc:IsRelateToEffect(e) then
+            if Duel.SendtoDeck(tc,nil,SEQ_DECKBOTTOM,REASON_EFFECT)~=0 then
                 Duel.Draw(tp,1,REASON_EFFECT)
             end
         end
     end
 end
 
---Check for PK material
+--Condition to upgrade to Quick Effect
 function s.quickcon(e,tp,eg,ep,ev,re,r,rp)
     return e:GetHandler():GetOverlayGroup():IsExists(Card.IsSetCard,1,nil,0x10db)
 end
 
---Floating effect (non-targeting)
+--Floating effect: non-targeting "can't attack directly"
 function s.discon(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     return c:IsPreviousLocation(LOCATION_MZONE) and c:IsSummonType(SUMMON_TYPE_XYZ)
@@ -113,6 +106,7 @@ end
 function s.disop(e,tp,eg,ep,ev,re,r,rp)
     local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
     if #g>0 then
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
         local tc=g:Select(tp,1,1,nil):GetFirst()
         if tc then
             local e1=Effect.CreateEffect(e:GetHandler())
