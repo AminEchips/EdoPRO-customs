3fda8052b1e4039de34e9d1e0dcb8deae53123ee
Chips <aminekhoury0411@gmail.com>
2025-06-03 08:17:13 -0400

    Update c829.lua

--- script/c829.lua
+++ script/c829.lua
@@ -12,23 +12,24 @@ function s.initial_effect(c)
     e1:SetType(EFFECT_TYPE_IGNITION)
     e1:SetRange(LOCATION_MZONE)
     e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-    e1:SetCountLimit(1)
+    e1:SetCountLimit(1,id)
     e1:SetCost(s.descost)
     e1:SetTarget(s.destg)
     e1:SetOperation(s.desop)
     c:RegisterEffect(e1)
 
-    --Battle effect: Retrieve and Set
+    --Battle: Retrieve and optionally Set
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
     e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e2:SetCode(EVENT_BATTLE_DESTROYING)
-    e2:SetCondition(aux.bdocon)
     e2:SetCountLimit(1,{id,1})
+    e2:SetCondition(aux.bdocon)
     e2:SetOperation(s.batop)
     c:RegisterEffect(e2)
 end
 
+-- First Effect: Destroy Spells/Traps
 function s.descost(e,tp,eg,ep,ev,re,r,rp,chk)
     local c=e:GetHandler()
     if chk==0 then return c:CheckRemoveOverlayCard(tp,1,REASON_COST) end
@@ -37,7 +38,7 @@ function s.descost(e,tp,eg,ep,ev,re,r,rp,chk)
     e:SetLabel(ct)
 end
 function s.destfilter(c)
-    return c:IsSpellTrap() and c:IsAbleToGrave()
+    return c:IsSpellTrap() and c:IsDestructable()
 end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
     local ct=e:GetLabel()
@@ -54,22 +55,34 @@ function s.desop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
-function s.banfilter(c)
+-- Second Effect: Retrieve and Set
+function s.banishedTrapFilter(c)
     return c:IsType(TYPE_TRAP) and c:IsAbleToHand()
 end
-function s.setfilter(c)
+function s.banishableSTFilter(c)
+    return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToRemove()
+end
+function s.settableTrapFilter(c)
     return c:IsType(TYPE_TRAP) and c:IsSSetable()
 end
 function s.batop(e,tp,eg,ep,ev,re,r,rp)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-    local g=Duel.SelectMatchingCard(tp,s.banfilter,tp,LOCATION_REMOVED,0,1,1,nil)
+    local g=Duel.SelectMatchingCard(tp,s.banishedTrapFilter,tp,LOCATION_REMOVED,0,1,1,nil)
     if #g>0 and Duel.SendtoHand(g,nil,REASON_EFFECT)>0 then
         Duel.ConfirmCards(1-tp,g)
-        Duel.BreakEffect()
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
-        local sg=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,1,nil)
-        if #sg>0 then
-            Duel.SSet(tp,sg)
+        -- Optional effect: Banish and Set
+        if Duel.IsExistingMatchingCard(s.banishableSTFilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,nil)
+            and Duel.IsExistingMatchingCard(s.settableTrapFilter,tp,LOCATION_GRAVE,0,1,nil)
+            and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+            local bg=Duel.SelectMatchingCard(tp,s.banishableSTFilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,1,nil)
+            if #bg>0 and Duel.Remove(bg,POS_FACEUP,REASON_EFFECT)>0 then
+                Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+                local sg=Duel.SelectMatchingCard(tp,s.settableTrapFilter,tp,LOCATION_GRAVE,0,1,1,nil)
+                if #sg>0 then
+                    Duel.SSet(tp,sg)
+                end
+            end
         end
     end
 end
