329d803b323324c41dbe1b2821b2db31c29f929b
Chips <aminekhoury0411@gmail.com>
2025-06-03 11:51:43 -0400

    Update c830.lua

--- script/c830.lua
+++ script/c830.lua
@@ -1,102 +1,97 @@
 --The Phantom Knights of Chosen Sword
 local s,id=GetID()
 function s.initial_effect(c)
-	-- Xyz Summon
-	Xyz.AddProcedure(c,nil,4,2)
-	c:EnableReviveLimit()
+    -- Xyz Summon
+    Xyz.AddProcedure(c,nil,4,2)
+    c:EnableReviveLimit()
 
-	-- Can be Xyz Summoned using Rank 3 DARK Xyz Monster with no materials
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_FIELD)
-	e0:SetCode(EFFECT_XYZ_LEVEL)
-	e0:SetProperty(EFFECT_FLAG_SET_AVAILABLE+EFFECT_FLAG_IGNORE_IMMUNE)
-	e0:SetRange(LOCATION_EXTRA)
-	e0:SetTargetRange(LOCATION_MZONE,0)
-	e0:SetValue(s.xyzlv)
-	c:RegisterEffect(e0)
+    -- Can be Xyz Summoned using Rank 3 DARK Xyz Monster with no materials
+    local e0=Effect.CreateEffect(c)
+    e0:SetType(EFFECT_TYPE_FIELD)
+    e0:SetCode(EFFECT_XYZ_LEVEL)
+    e0:SetProperty(EFFECT_FLAG_SET_AVAILABLE+EFFECT_FLAG_IGNORE_IMMUNE)
+    e0:SetRange(LOCATION_EXTRA)
+    e0:SetTargetRange(LOCATION_MZONE,0)
+    e0:SetValue(s.xyzlv)
+    c:RegisterEffect(e0)
 
-	-- Gain ATK for each face-up "Phantom Knights"
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_UPDATE_ATTACK)
-	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetValue(s.atkval)
-	c:RegisterEffect(e1)
+    -- Gain ATK for each face-up "Phantom Knights"
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_UPDATE_ATTACK)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetValue(s.atkval)
+    c:RegisterEffect(e1)
 
-	-- Optional bounce on banish of Trap
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetCategory(CATEGORY_TOHAND)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
-	e2:SetCode(EVENT_REMOVE)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1,id)
-	e2:SetCondition(s.thcon)
-	e2:SetTarget(s.thtg)
-	e2:SetOperation(s.thop)
-	c:RegisterEffect(e2)
+    -- Optional bounce on trap banish
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_TOHAND+CATEGORY_ATTACH)
+    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_REMOVE)
+    e2:SetProperty(EFFECT_FLAG_DELAY)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1,id)
+    e2:SetCondition(s.thcon)
+    e2:SetOperation(s.thop)
+    c:RegisterEffect(e2)
 
-	-- Protect from being destroyed
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
-	e3:SetCode(EFFECT_DESTROY_REPLACE)
-	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetTarget(s.reptg)
-	c:RegisterEffect(e3)
+    -- Destruction replacement
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+    e3:SetCode(EFFECT_DESTROY_REPLACE)
+    e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetTarget(s.reptg)
+    e3:SetOperation(s.repop)
+    c:RegisterEffect(e3)
 end
 
-s.listed_series={0xdb} -- Phantom Knights
+s.listed_series={0xdb}
 
 function s.xyzlv(e,c,rc)
-	if c:IsRank(3) and c:IsAttribute(ATTRIBUTE_DARK) and c:GetOverlayCount()==0 then
-		return 4
-	end
+    if c:IsRank(3) and c:IsAttribute(ATTRIBUTE_DARK) and c:GetOverlayCount()==0 then
+        return 0x40000 + 4 -- Treated as level 4 for Xyz
+    end
 end
 
 function s.atkval(e,c)
-	return Duel.GetMatchingGroupCount(Card.IsSetCard,c:GetControler(),LOCATION_ONFIELD,0,nil,0xdb)*200
+    return Duel.GetMatchingGroupCount(Card.IsSetCard,c:GetControler(),LOCATION_ONFIELD,0,nil,0xdb)*200
 end
 
 function s.cfilter(c,tp)
-	return c:IsType(TYPE_TRAP) and c:IsPreviousControler(tp)
+    return c:IsType(TYPE_TRAP) and c:IsPreviousControler(tp)
 end
 
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.cfilter,1,nil,tp)
-end
-
-function s.matfilter(c)
-	return c:IsType(TYPE_TRAP) and c:IsAbleToChangeControler()
-end
-
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_REMOVED) and chkc:IsControler(tp) and s.matfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.matfilter,tp,LOCATION_REMOVED,0,1,nil)
-		and Duel.IsExistingMatchingCard(Card.IsAbleToHand,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
-	local g=Duel.SelectTarget(tp,s.matfilter,tp,LOCATION_REMOVED,0,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_ATTACH,g,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,0,LOCATION_ONFIELD)
+    return eg:IsExists(s.cfilter,1,nil,tp)
 end
 
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local tc=Duel.GetFirstTarget()
-	if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) then
-		Duel.Overlay(c,Group.FromCards(tc))
-		local g=Duel.SelectMatchingCard(tp,Card.IsAbleToHand,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
-		if #g>0 then
-			Duel.HintSelection(g)
-			Duel.SendtoHand(g,nil,REASON_EFFECT)
-		end
-	end
+    local c=e:GetHandler()
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+    local g=Duel.SelectMatchingCard(tp,Card.IsType,tp,LOCATION_REMOVED,0,1,1,nil,TYPE_TRAP)
+    if #g>0 and c:IsRelateToEffect(e) then
+        Duel.Overlay(c,g)
+        if Duel.IsExistingMatchingCard(Card.IsAbleToHand,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
+            and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+            local sg=Duel.SelectMatchingCard(tp,Card.IsAbleToHand,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+            if #sg>0 then
+                Duel.HintSelection(sg)
+                Duel.SendtoHand(sg,nil,REASON_EFFECT)
+            end
+        end
+    end
 end
 
 function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return c:CheckRemoveOverlayCard(tp,1,REASON_EFFECT) end
-	return Duel.SelectYesNo(tp,aux.Stringid(id,1))
+    local c=e:GetHandler()
+    if chk==0 then return c:CheckRemoveOverlayCard(tp,1,REASON_EFFECT) end
+    return Duel.SelectYesNo(tp,aux.Stringid(id,1))
+end
+
+function s.repop(e,tp,eg,ep,ev,re,r,rp)
+    e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_EFFECT)
 end
