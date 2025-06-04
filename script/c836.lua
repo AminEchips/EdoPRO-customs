8885b184fa5d12ba7c609b02a057fdbb1ce19db4
Chips <aminekhoury0411@gmail.com>
2025-06-04 11:22:24 -0400

    Create c836.lua

---
+++ script/c836.lua
@@ -0,0 +1,85 @@
+--Phantom Knights' Rank-Up-Magic Around Burn
+local s,id=GetID()
+function s.initial_effect(c)
+    --Activation
+    local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E+TIMING_BATTLE_PHASE)
+    e1:SetCondition(s.condition)
+    e1:SetTarget(s.target)
+    e1:SetOperation(s.activate)
+    c:RegisterEffect(e1)
+
+    --Graveyard effect: Return banished Phantom Knights Spell/Trap
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_TOGRAVE)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetRange(LOCATION_GRAVE)
+    e2:SetCountLimit(1,{id,1})
+    e2:SetCost(aux.bfgcost)
+    e2:SetTarget(s.gytg)
+    e2:SetOperation(s.gyop)
+    c:RegisterEffect(e2)
+end
+
+-- Check for DARK Xyz Monster that has already attacked
+function s.filter(c)
+    return c:IsFaceup() and c:IsType(TYPE_XYZ) and c:IsAttribute(ATTRIBUTE_DARK) and c:GetBattledGroupCount()>0
+end
+function s.extrafil(c,e,tp)
+    return c:IsType(TYPE_XYZ) and c:IsAttribute(ATTRIBUTE_DARK) and (c:IsRank(5) or c:IsRank(6)) and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.IsBattlePhase()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then
+        return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil)
+            and Duel.IsExistingMatchingCard(s.extrafil,tp,LOCATION_EXTRA,0,1,nil,e,tp)
+    end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+    if not Duel.IsBattlePhase() then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+    local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+    local tc=g:GetFirst()
+    if not tc or not tc:IsControler(tp) or tc:IsImmuneToEffect(e) then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local rg=Duel.SelectMatchingCard(tp,function(c,e,tp)
+        return c:IsAttribute(ATTRIBUTE_DARK) and (c:IsRank(5) or c:IsRank(6))
+            and Duel.GetLocationCountFromEx(tp,tp,tc,c)>0 and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false)
+    end,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+    local sc=rg:GetFirst()
+    if sc then
+        local mg=tc:GetOverlayGroup()
+        if #mg>0 then
+            Duel.Overlay(sc,mg)
+        end
+        sc:SetMaterial(Group.FromCards(tc))
+        Duel.Overlay(sc,Group.FromCards(tc))
+        Duel.SpecialSummon(sc,SUMMON_TYPE_XYZ,tp,tp,false,false,POS_FACEUP)
+        sc:CompleteProcedure()
+    end
+end
+
+-- GY effect: return banished PK Spell/Trap
+function s.gyfilter(c)
+    return c:IsFaceup() and c:IsSetCard(0x10db) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToGrave()
+end
+function s.gytg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_REMOVED) and s.gyfilter(chkc) end
+    if chk==0 then return Duel.IsExistingTarget(s.gyfilter,tp,LOCATION_REMOVED,0,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+    local g=Duel.SelectTarget(tp,s.gyfilter,tp,LOCATION_REMOVED,0,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,1,0,0)
+end
+function s.gyop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc and tc:IsRelateToEffect(e) then
+        Duel.SendtoGrave(tc,REASON_EFFECT+REASON_RETURN)
+    end
+end
