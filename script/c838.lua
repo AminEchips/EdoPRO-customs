f0fede7b70f145322957ca7b605f773c28da40b2
Chips <aminekhoury0411@gmail.com>
2025-06-04 19:32:03 -0400

    Create c838.lua

---
+++ script/c838.lua
@@ -0,0 +1,86 @@
+--The Phantom Knights of Arise Lance
+local s,id=GetID()
+function s.initial_effect(c)
+    --Activate
+    local e0=Effect.CreateEffect(c)
+    e0:SetType(EFFECT_TYPE_ACTIVATE)
+    e0:SetCode(EVENT_FREE_CHAIN)
+    c:RegisterEffect(e0)
+
+    --Continuous Effect: PK monsters with Levels can't be destroyed by battle
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+    e1:SetRange(LOCATION_SZONE)
+    e1:SetTargetRange(LOCATION_MZONE,0)
+    e1:SetTarget(function(e,c) return c:IsSetCard(0x10db) and c:GetLevel()>0 end)
+    e1:SetValue(1)
+    c:RegisterEffect(e1)
+
+    --Set 1 PK Trap on activation (OPT)
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e2:SetProperty(EFFECT_FLAG_DELAY)
+    e2:SetCode(EVENT_ACTIVATE)
+    e2:SetCountLimit(1,{id,0})
+    e2:SetTarget(s.settg)
+    e2:SetOperation(s.setop)
+    c:RegisterEffect(e2)
+
+    --GY Quick Effect: Banish this; Target & SS 1 PK monster, banish when it leaves field
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e3:SetType(EFFECT_TYPE_QUICK_O)
+    e3:SetCode(EVENT_FREE_CHAIN)
+    e3:SetRange(LOCATION_GRAVE)
+    e3:SetCountLimit(1,{id,1})
+    e3:SetHintTiming(0,TIMING_MAIN_END+TIMING_BATTLE_END)
+    e3:SetCost(aux.bfgcost)
+    e3:SetTarget(s.sptg)
+    e3:SetOperation(s.spop)
+    c:RegisterEffect(e3)
+end
+
+-- Set a PK Trap directly from Deck
+function s.setfilter(c)
+    return c:IsSetCard(0xdb) and c:IsType(TYPE_TRAP) and not c:IsCode(id) and c:IsSSetable()
+end
+function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_DECK,0,1,nil) end
+end
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+    local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,1,1,nil)
+    if #g>0 then
+        Duel.SSet(tp,g:GetFirst())
+    end
+end
+
+-- GY Quick Effect: SS PK monster
+function s.spfilter(c,e,tp)
+    return c:IsSetCard(0x10db) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.spfilter(chkc,e,tp) end
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc and tc:IsRelateToEffect(e) then
+        if Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)>0 then
+            local e1=Effect.CreateEffect(e:GetHandler())
+            e1:SetType(EFFECT_TYPE_SINGLE)
+            e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+            e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+            e1:SetReset(RESET_EVENT+RESETS_REDIRECT)
+            e1:SetValue(LOCATION_REMOVED)
+            tc:RegisterEffect(e1,true)
+        end
+    end
+end
