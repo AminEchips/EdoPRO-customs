3c712b1e592e8e8777b76c34bdc59620d27fd69e
Chips <aminekhoury0411@gmail.com>
2025-06-04 23:29:18 -0400

    Create c840.lua

---
+++ script/c840.lua
@@ -0,0 +1,94 @@
+--The Phantom Knights of Misty Lancing
+local s,id=GetID()
+function s.initial_effect(c)
+    -- Activate: destroy 1 opponent's monster if you control a PK Xyz
+    local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_DESTROY+CATEGORY_REMOVE)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetCountLimit(1,{id,0})
+    e1:SetCondition(s.descon)
+    e1:SetTarget(s.destg)
+    e1:SetOperation(s.desop)
+    c:RegisterEffect(e1)
+
+    -- GY Effect: revive a banished PK Trap as monster if PK monster is banished
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_REMOVE)
+    e2:SetRange(LOCATION_GRAVE)
+    e2:SetCountLimit(1,{id,1})
+    e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+    e2:SetCondition(s.spcon)
+    e2:SetCost(aux.bfgcost)
+    e2:SetTarget(s.sptg)
+    e2:SetOperation(s.spop)
+    c:RegisterEffect(e2)
+end
+
+-- Check for PK Xyz
+function s.pkxyzfilter(c)
+    return c:IsFaceup() and c:IsSetCard(0x10db) and c:IsType(TYPE_XYZ)
+end
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.IsExistingMatchingCard(s.pkxyzfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+    local g=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_MZONE,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+    Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc and tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)>0 and tc:IsLocation(LOCATION_GRAVE) then
+        Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
+    end
+end
+
+-- GY effect condition: PK monster is banished
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+    return eg:IsExists(function(c) return c:IsSetCard(0x10db) and c:IsMonster() and c:IsPreviousLocation(LOCATION_MZONE+LOCATION_GRAVE) end,1,nil)
+end
+
+-- Get Level/Rank for later use
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return false end
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.IsExistingTarget(s.trapfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp) end
+
+    -- Store level/rank from banished PK monster for reference
+    local rc=eg:Filter(function(c) return c:IsSetCard(0x10db) and c:IsMonster() end,nil):GetFirst()
+    local lvl=rc:GetLevel()
+    if lvl==0 then lvl=rc:GetRank() end
+    e:SetLabel(lvl)
+
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectTarget(tp,s.trapfilter,tp,LOCATION_REMOVED,0,1,1,nil,e,tp)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+
+function s.trapfilter(c,e,tp)
+    return c:IsSetCard(0xdb) and c:IsType(TYPE_TRAP) and not c:IsCode(id)
+        and Duel.IsPlayerCanSpecialSummonMonster(tp,c:GetCode(),0,TYPE_NORMAL+TYPE_MONSTER,1800,0,e:GetLabel() or 1,RACE_WARRIOR,ATTRIBUTE_DARK)
+end
+
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    local lvl=e:GetLabel()
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 or not tc or not tc:IsRelateToEffect(e) then return end
+    if Duel.IsPlayerCanSpecialSummonMonster(tp,tc:GetCode(),0,TYPE_NORMAL+TYPE_MONSTER,1800,0,lvl,RACE_WARRIOR,ATTRIBUTE_DARK) then
+        tc:AddMonsterAttribute(TYPE_NORMAL+TYPE_MONSTER)
+        Duel.SpecialSummonStep(tc,0,tp,tp,true,false,POS_FACEUP)
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_CHANGE_LEVEL)
+        e1:SetValue(lvl)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+        tc:RegisterEffect(e1)
+        Duel.SpecialSummonComplete()
+    end
+end
