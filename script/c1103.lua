b4cdb478ed8189d1040a06dbf1a1b12588f60273
Chips <aminekhoury0411@gmail.com>
2025-06-11 19:52:17 -0400

    Create c1103.lua

---
+++ script/c1103.lua
@@ -0,0 +1,107 @@
+--Salamangreat Wyrm
+local s,id=GetID()
+function s.initial_effect(c)
+	s:EnableReviveLimit()
+	aux.EnableCheckReincarnation(c) -- Enables IsReincarnationSummoned() for all mechanics
+	s.listed_series={0x119}
+
+	-- Quick Effect: Special Summon from hand, destroy 1 you control, possibly destroy 1 opponent controls
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetHintTiming(0,TIMING_MAIN_END+TIMINGS_CHECK_MONSTER)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+
+	-- If sent from hand or field to GY: target opponent’s monster, return or reduce ATK
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_ATKCHANGE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e2:SetCountLimit(1,id+100)
+	e2:SetCondition(s.thcon)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+end
+
+-- ① Quick Summon Condition: You have any Salamangreat in GY
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(aux.FilterFaceup(Card.IsSetCard),tp,LOCATION_GRAVE,0,1,nil,0x119)
+end
+
+-- ① Target
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then
+		return c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+			and Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_ONFIELD,0,1,nil)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,tp,LOCATION_ONFIELD)
+end
+
+-- ① Operation
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_ONFIELD,0,1,1,nil)
+	if #g==0 then return end
+	local dc=g:GetFirst()
+	if Duel.Destroy(dc,REASON_EFFECT)>0 and dc:IsMonster() then
+		if (dc:IsSetCard(0x119) and dc:IsType(TYPE_LINK)) or dc:IsLevelAbove(5) then
+			local atk=dc:GetAttack()
+			local opp=Duel.GetMatchingGroup(s.dfilter,tp,0,LOCATION_MZONE,nil,atk)
+			if #opp>0 then
+				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+				local sg=opp:Select(tp,1,1,nil)
+				Duel.Destroy(sg,REASON_EFFECT)
+			end
+		end
+	end
+end
+
+function s.dfilter(c,atk)
+	return c:IsFaceup() and c:GetAttack()<atk
+end
+
+-- ② Condition: Sent from hand or field
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_HAND+LOCATION_ONFIELD)
+end
+
+-- ② Target
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
+end
+
+-- ② Operation
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if not tc or not tc:IsRelateToEffect(e) or not tc:IsFaceup() then return end
+	local has_reincarnated = Duel.IsExistingMatchingCard(aux.FilterFaceup(Card.IsReincarnationSummoned),tp,LOCATION_MZONE,0,1,nil)
+	if has_reincarnated then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+	else
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(-1000)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+	end
+end
