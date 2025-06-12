ae107a020e316ccc8663b07e9037a3fe64194d3b
Chips <aminekhoury0411@gmail.com>
2025-06-12 17:25:26 -0400

    Update c1107.lua

--- script/c1107.lua
+++ script/c1107.lua
@@ -4,33 +4,26 @@ function s.initial_effect(c)
 	c:EnableReviveLimit()
 	aux.EnableCheckReincarnation(c)
 
-	-- Fusion Materials: 2 "Salamangreat" monsters + 1 Fusion or Link monster
+	-- Fusion Materials: 2 Salamangreat + 1 Fusion/Link
 	Fusion.AddProcMix(c,true,true,
 		aux.FilterBoolFunction(Card.IsSetCard,0x119),
 		aux.FilterBoolFunction(Card.IsSetCard,0x119),
 		s.fmlinkfilter)
 
-	-- On Fusion Summon: Fusion Summon 1 FIRE Fusion Monster using field/GY (banished)
-	local params={
-		function(c) return c:IsType(TYPE_FUSION) and c:IsAttribute(ATTRIBUTE_FIRE) end, -- filter
-		nil, -- extra materials (none)
-		nil, -- stage2 function (none)
-		Fusion.BanishMaterial, -- banish as cost
-		nil -- extratarget (none)
-	}
+	-- Triggered Fusion Summon effect
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
-	e1:SetCondition(function(e,tp) return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION) end)
-	e1:SetTarget(Fusion.SummonEffTG(table.unpack(params)))
-	e1:SetOperation(Fusion.SummonEffOP(table.unpack(params)))
+	e1:SetCondition(s.fuscon)
+	e1:SetTarget(s.fustg)
+	e1:SetOperation(s.fusop)
 	e1:SetCountLimit(1,id)
 	c:RegisterEffect(e1)
 
-	-- Reincarnation effect: Special Summon 1 banished "Salamangreat" + add 1 Salamangreat Spell/Trap
+	-- Reincarnation: SS 1 banished + search 1 S/T
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND)
@@ -45,18 +38,57 @@ function s.initial_effect(c)
 end
 s.listed_series={0x119}
 
--- Fusion/Link monster filter
 function s.fmlinkfilter(c)
 	return c:IsType(TYPE_FUSION+TYPE_LINK)
 end
 
--- Reincarnation Summon condition
+-- Only activate on Fusion Summon
+function s.fuscon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION)
+end
+
+-- FIRE Fusion Monster filter
+function s.fusfilter(c,e,tp)
+	return c:IsType(TYPE_FUSION) and c:IsAttribute(ATTRIBUTE_FIRE)
+		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
+end
+function s.matfilter(c)
+	return c:IsCanBeFusionMaterial() and c:IsAbleToRemove()
+		and (c:IsLocation(LOCATION_MZONE) or c:IsLocation(LOCATION_GRAVE))
+end
+function s.fustg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local mg=Duel.GetMatchingGroup(s.matfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,nil)
+		return Duel.IsExistingMatchingCard(s.fusfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function s.fusop(e,tp,eg,ep,ev,re,r,rp)
+	local mg=Duel.GetMatchingGroup(s.matfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,nil)
+	local exg=Duel.GetMatchingGroup(s.fusfilter,tp,LOCATION_EXTRA,0,nil,e,tp)
+
+	if #exg==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local fsc=exg:Select(tp,1,1,nil):GetFirst()
+	if not fsc then return end
+
+	local matg=mg:Filter(Card.IsCanBeFusionMaterial,nil,fsc)
+	local chkf=tp
+	local mat=Duel.SelectFusionMaterial(tp,fsc,matg,nil,chkf)
+	if not mat or #mat==0 then return end
+
+	fsc:SetMaterial(mat)
+	Duel.Remove(mat,POS_FACEUP,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
+	Duel.SpecialSummon(fsc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
+	fsc:CompleteProcedure()
+end
+
+-- Reincarnation check
 function s.reinccon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return c:IsFaceup() and c:IsReincarnationSummoned()
 end
 
--- Reincarnation effect: Special Summon + Search
 function s.reincfilter(c,e,tp)
 	return c:IsSetCard(0x119) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
