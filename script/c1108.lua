cfadc3b68ea681ff4a844b0a5941b0b31c9e5e9f
Chips <aminekhoury0411@gmail.com>
2025-06-12 17:36:58 -0400

    Update c1108.lua

--- script/c1108.lua
+++ script/c1108.lua
@@ -4,14 +4,13 @@ function s.initial_effect(c)
 	c:EnableReviveLimit()
 	aux.EnableCheckReincarnation(c)
 
-	-- Synchro Summon
+	-- Synchro Summon procedure: 1 FIRE Tuner + 1+ non-Tuner FIRE monsters
 	Synchro.AddProcedure(c,
-		aux.FilterBoolFunction(Card.IsAttribute,ATTRIBUTE_FIRE),
-		1,1,
-		aux.FilterBoolFunction(Card.IsAttribute,ATTRIBUTE_FIRE),1,99)
+		aux.FilterBoolFunction(Card.IsAttribute,ATTRIBUTE_FIRE), 1, 1,
+		aux.FilterBoolFunction(Card.IsAttribute,ATTRIBUTE_FIRE), 1, 99)
 	c:EnableReviveLimit()
 
-	-- While Linked: becomes Level 5, can attack in DEF using DEF
+	-- While Linked: Becomes Level 5
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_CHANGE_LEVEL)
@@ -19,6 +18,7 @@ function s.initial_effect(c)
 	e1:SetValue(5)
 	c:RegisterEffect(e1)
 
+	-- While Linked: Can attack in Defense Position using DEF
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetCode(EFFECT_DEFENSE_ATTACK)
@@ -26,52 +26,44 @@ function s.initial_effect(c)
 	e2:SetValue(1)
 	c:RegisterEffect(e2)
 
+	-- On Battle Destroy: gain 2 more attacks and go to Attack Position
 	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetCode(EFFECT_DEFENSE_DAMAGE_CAL)
-	e3:SetCondition(s.linkcon)
-	e3:SetValue(1)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_BATTLE_DESTROYING)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCountLimit(1,id)
+	e3:SetCondition(aux.bdocon)
+	e3:SetOperation(s.atkop)
 	c:RegisterEffect(e3)
 
-	-- On battle destroy: switch to ATK and gain 2 more attacks
+	-- Reincarnation Synchro effect: change to DEF + gain 2800 DEF
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,0))
-	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e4:SetCode(EVENT_BATTLE_DESTROYING)
-	e4:SetProperty(EFFECT_FLAG_DELAY)
-	e4:SetCountLimit(1,id)
-	e4:SetCondition(aux.bdocon)
-	e4:SetOperation(s.atkop)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetCategory(CATEGORY_POSITION)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
+	e4:SetCountLimit(1,{id,1})
+	e4:SetCondition(s.reincon)
+	e4:SetTarget(s.reintg)
+	e4:SetOperation(s.reinop)
 	c:RegisterEffect(e4)
-
-	-- If Reincarnation Synchro Summoned: Quick DEF boost + position change
-	local e5=Effect.CreateEffect(c)
-	e5:SetDescription(aux.Stringid(id,1))
-	e5:SetCategory(CATEGORY_POSITION)
-	e5:SetType(EFFECT_TYPE_QUICK_O)
-	e5:SetCode(EVENT_FREE_CHAIN)
-	e5:SetRange(LOCATION_MZONE)
-	e5:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
-	e5:SetCountLimit(1,{id,1})
-	e5:SetCondition(s.reincon)
-	e5:SetTarget(s.reintg)
-	e5:SetOperation(s.reinop)
-	c:RegisterEffect(e5)
 end
 s.listed_series={0x119}
 
--- While Linked
+-- While Linked condition
 function s.linkcon(e)
 	local c=e:GetHandler()
-	local zone=c:GetLinkedZone()
-	return zone&c:GetLinkedZone()~=0 or c:IsLinked()
+	return c:IsLinked()
 end
 
--- On battle destroy: gain 2 more attacks
+-- Battle destroy effect: go to ATK + gain 2 attacks
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and c:IsFaceup() then
-		-- Change to Attack Position if not already
+		-- Change to Attack Position
 		if c:IsDefensePos() then
 			Duel.ChangePosition(c,POS_FACEUP_ATTACK)
 		end
@@ -85,23 +77,22 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- Reincarnation Synchro check
+-- Reincarnation Synchro Summon condition
 function s.reincon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return c:IsFaceup() and c:IsReincarnationSummoned() and c:IsAttackPos()
 end
 
+-- Reincarnation: change to DEF + gain 2800 DEF
 function s.reintg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return c:IsCanChangePosition() end
 	Duel.SetOperationInfo(0,CATEGORY_POSITION,c,1,0,0)
 end
-
 function s.reinop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
 	if Duel.ChangePosition(c,POS_FACEUP_DEFENSE)~=0 then
-		-- Gain 2800 DEF until opponent's End Phase
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_DEFENSE)
