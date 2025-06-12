9dc115c31334f2ee79e02a051c1391573ce177d1
Chips <aminekhoury0411@gmail.com>
2025-06-12 16:34:36 -0400

    Update c1106.lua

--- script/c1106.lua
+++ script/c1106.lua
@@ -1,122 +1,107 @@
---Salamangreat Violet Serpent
+--Salamangreat Exploder Calupoh
 local s,id=GetID()
 function s.initial_effect(c)
-    c:EnableReviveLimit()
-    aux.EnableCheckReincarnation(c)
+	c:EnableReviveLimit()
+	aux.EnableCheckReincarnation(c)
+	-- Fusion Summon or Tribute Summon from Extra Deck
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_FIELD)
+	e0:SetCode(EFFECT_SPSUMMON_PROC)
+	e0:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e0:SetRange(LOCATION_EXTRA)
+	e0:SetCondition(s.spcon)
+	e0:SetOperation(s.spop)
+	c:RegisterEffect(e0)
+	-- Fusion Material
+	Fusion.AddProcMixRep(c,true,true,s.matfilter,2,99)
 
-    -- Fusion materials: 2 Salamangreat + 1 Fusion/Link
-    Fusion.AddProcMix(c,true,true,
-        aux.FilterBoolFunction(Card.IsSetCard,0x119),
-        aux.FilterBoolFunction(Card.IsSetCard,0x119),
-        s.fmlinkfilter)
+	-- Search Fusion of Fire
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCondition(s.thcon)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
 
-    -- On Fusion Summon: Proper Fusion Summon 1 FIRE Fusion from field/GY (banished)
-    local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-    e1:SetCondition(s.fuscon)
-    e1:SetTarget(s.fustg)
-    e1:SetOperation(s.fusop)
-    e1:SetCountLimit(1,id)
-    c:RegisterEffect(e1)
-
-    -- Reincarnation effect: SS banished + add S/T
-    local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,1))
-    e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND)
-    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-    e2:SetCondition(s.reinccon)
-    e2:SetTarget(s.reinctg)
-    e2:SetOperation(s.reincop)
-    e2:SetCountLimit(1,{id,1})
-    c:RegisterEffect(e2)
+	-- Target cards opponent controls and lock effects
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_DISABLE)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.lockcon)
+	e2:SetTarget(s.locktg)
+	e2:SetOperation(s.lockop)
+	c:RegisterEffect(e2)
 end
 s.listed_series={0x119}
+s.listed_names={25800447,id} -- Fusion of Fire and itself
 
--- Fusion material filter: must be Fusion or Link
-function s.fmlinkfilter(c)
-    return c:IsType(TYPE_FUSION+TYPE_LINK)
+function s.matfilter(c)
+	return c:IsSetCard(0x119) or c:IsAttribute(ATTRIBUTE_FIRE)
 end
 
--- Condition: only if this was Fusion Summoned
-function s.fuscon(e,tp,eg,ep,ev,re,r,rp)
-    return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION)
+-- Special Summon procedure by tributing required monsters
+function s.spfilter(c)
+	return c:IsFaceup() and (c:IsSetCard(0x119) or c:IsAttribute(ATTRIBUTE_FIRE)) and c:IsReleasable()
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_MZONE,0,nil)
+	return #g>=2
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_MZONE,0,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	local sg=g:Select(tp,2,2,nil)
+	Duel.Release(sg,REASON_COST)
 end
 
--- Fusion Summon setup using FIRE + field/GY banish
-function s.fusfilter(c,e,tp)
-    return c:IsType(TYPE_FUSION) and c:IsAttribute(ATTRIBUTE_FIRE)
-        and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
-        and Duel.CheckFusionMaterial(tp,c,nil,nil,s.fusmatfilter,chkf)
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_SPECIAL)
+end
+function s.thfilter(c)
+	return c:IsCode(25800447) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
 end
 
-function s.fusmatfilter(c)
-    return c:IsAbleToRemoveAsCost() and (c:IsOnField() or c:IsLocation(LOCATION_GRAVE))
+function s.lockcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsFaceup() and c:IsReincarnationSummoned()
 end
-
-function s.fustg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then
-        local g=Duel.GetMatchingGroup(s.fusmatfilter,tp,LOCATION_ONFIELD+LOCATION_GRAVE,0,nil)
-        return Duel.IsExistingMatchingCard(s.fusfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp)
-            and #g>0
-    end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+function s.locktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local ct=#e:GetHandler():GetMaterial()
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,ct,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,#g,0,0)
 end
-
-function s.fusop(e,tp,eg,ep,ev,re,r,rp)
-    local chkf=tp
-    local mg=Duel.GetMatchingGroup(s.fusmatfilter,tp,LOCATION_ONFIELD+LOCATION_GRAVE,0,nil)
-    local sg=Duel.GetMatchingGroup(s.fusfilter,tp,LOCATION_EXTRA,0,nil,e,tp)
-
-    local tg=Duel.SelectFusionMaterial(tp,nil,mg,nil,chkf)
-    if #tg==0 then return end
-
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-    local sc=sg:Select(tp,1,1,nil):GetFirst()
-    if not sc then return end
-
-    local mat=Duel.SelectFusionMaterial(tp,sc,mg,nil,chkf)
-    if #mat==0 then return end
-
-    Duel.Remove(mat,POS_FACEUP,REASON_COST+REASON_MATERIAL)
-    Duel.SpecialSummon(sc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
-    sc:CompleteProcedure()
-end
-
--- Reincarnation Summon check
-function s.reinccon(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    return c:IsFaceup() and c:IsReincarnationSummoned()
-end
-
-function s.reincfilter(c,e,tp)
-    return c:IsSetCard(0x119) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-function s.stfilter(c)
-    return c:IsSetCard(0x119) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToHand()
-end
-
-function s.reinctg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then
-        return Duel.IsExistingMatchingCard(s.reincfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp)
-            and Duel.IsExistingMatchingCard(s.stfilter,tp,LOCATION_DECK,0,1,nil)
-    end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_REMOVED)
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
-end
-
-function s.reincop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-    local g1=Duel.SelectMatchingCard(tp,s.reincfilter,tp,LOCATION_REMOVED,0,1,1,nil,e,tp)
-    if #g1>0 and Duel.SpecialSummon(g1,0,tp,tp,false,false,POS_FACEUP)>0 then
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-        local g2=Duel.SelectMatchingCard(tp,s.stfilter,tp,LOCATION_DECK,0,1,1,nil)
-        if #g2>0 then
-            Duel.SendtoHand(g2,nil,REASON_EFFECT)
-            Duel.ConfirmCards(1-tp,g2)
-        end
-    end
+function s.lockop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetTargetCards(e)
+	for tc in g:Iter() do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CANNOT_TRIGGER)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+	end
 end
