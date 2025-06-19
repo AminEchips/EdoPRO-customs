d12132a22b52764a17a3a3c6addf8b200912e6e2
Chips <aminekhoury0411@gmail.com>
2025-06-19 00:24:22 -0400

    Update c1112.lua

--- script/c1112.lua
+++ script/c1112.lua
@@ -2,20 +2,23 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	-- Link Summon procedure: 3+ FIRE Effect monsters
 	Link.AddProcedure(c,aux.FilterBoolFunction(Card.IsAttribute,ATTRIBUTE_FIRE),3,99)
 
-	-- On Link Summon: activation lock + chain block
+	-- Effect ①: On Link Summon - opponent cannot activate effects of face-up cards this turn
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(0)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK) end)
-	e1:SetOperation(s.lockop)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCondition(s.actcon)
+	e1:SetOperation(s.actop)
+	e1:SetCountLimit(1,id)
 	c:RegisterEffect(e1)
 
-	-- Shuffle 1-3 Extra-type monsters from GY, gain ATK
+	-- Effect ②: Shuffle Extra Deck monsters from GY, gain ATK for each FIRE
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TODECK+CATEGORY_ATKCHANGE)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
@@ -24,27 +27,28 @@ function s.initial_effect(c)
 	e2:SetOperation(s.tdop)
 	c:RegisterEffect(e2)
 
-	-- Reincarnated: Shuffle 1 card on field into Deck (Quick Effect)
+	-- Effect ③: Reincarnated - shuffle 1 card on the field into the Deck (non-targeting)
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetDescription(aux.Stringid(id,2))
 	e3:SetCategory(CATEGORY_TODECK)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_QUICK_O)
 	e3:SetCode(EVENT_CHAINING)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetCountLimit(1,{id,2})
-	e3:SetCondition(function(e,tp,eg,ep,ev,re,r,rp)
-		return e:GetHandler():IsReincarnationSummoned()
-	end)
-	e3:SetTarget(s.qktg)
-	e3:SetOperation(s.qkop)
+	e3:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return e:GetHandler():IsReincarnationSummoned() end)
+	e3:SetOperation(s.reinop)
 	c:RegisterEffect(e3)
 end
 s.listed_series={0x119}
 
--- Lock opponent’s activations after Link Summon
-function s.lockop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local e1=Effect.CreateEffect(c)
+-- Effect ① Condition: Link Summon
+function s.actcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
+end
+
+-- Effect ① Operation: Prevent opponent’s face-up cards from activating this turn
+function s.actop(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
@@ -52,22 +56,13 @@ function s.lockop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetValue(s.aclimit)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
-
-	-- Chain block
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_CANNOT_ACTIVATE)
-	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e2:SetTargetRange(0,1)
-	e2:SetValue(function(e,re,tp) return re:IsHasType(EFFECT_TYPE_ACTIVATE) end)
-	e2:SetReset(RESET_CHAIN)
-	Duel.RegisterEffect(e2,tp)
 end
 function s.aclimit(e,re,tp)
-	return re:GetHandler():IsFaceup()
+	local rc=re:GetHandler()
+	return rc:IsFaceup()
 end
 
--- Shuffle up to 3 Extra-type monsters from GY
+-- Effect ②: Shuffle 1–3 Extra Deck-type monsters from GY, gain 500 ATK per FIRE
 function s.tdfilter(c)
 	return c:IsType(TYPE_RITUAL+TYPE_FUSION+TYPE_SYNCHRO+TYPE_XYZ+TYPE_LINK) and c:IsAbleToDeck()
 end
@@ -92,17 +87,12 @@ function s.tdop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- Reincarnated Quick Effect: shuffle 1 field card into Deck
-function s.qktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsOnField() and chkc:IsAbleToDeck() end
-	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToDeck,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+-- Effect ③: Reincarnated - shuffle 1 card (non-targeting)
+function s.reinop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsAbleToDeck,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+	if #g==0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local g=Duel.SelectTarget(tp,Card.IsAbleToDeck,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
-end
-function s.qkop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) then
-		Duel.SendtoDeck(tc,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
-	end
+	local sg=g:Select(tp,1,1,nil)
+	Duel.HintSelection(sg)
+	Duel.SendtoDeck(sg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
 end
