dc53ca123517ad854acd95102a6dd1574e6f0b7a
Chips <aminekhoury0411@gmail.com>
2025-06-20 22:14:22 -0400

    Update c1114.lua

--- script/c1114.lua
+++ script/c1114.lua
@@ -1,7 +1,8 @@
 --Salamangreat Cretan
+--Scripted by Meuh
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate: Search 1 "Salamangreat" Ritual Monster
+	--Activate and optionally search
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -9,9 +10,9 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 
-	--Send to GY to Ritual Summon 1 Ritual Monster
+	--Send to GY to Ritual Summon
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_SZONE)
@@ -21,13 +22,14 @@ function s.initial_effect(c)
 	e2:SetOperation(s.ritop)
 	c:RegisterEffect(e2)
 
-	--If "Salamangreat" Link is removed by opp, Special Summon Ritual ignoring conditions
+	--If Salamangreat Link leaves by opponent's effect, Special Summon Ritual
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetDescription(aux.Stringid(id,2))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e3:SetCode(EVENT_LEAVE_FIELD)
 	e3:SetRange(LOCATION_SZONE)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
 	e3:SetCountLimit(1,{id,1})
 	e3:SetCondition(s.spcon)
 	e3:SetTarget(s.sptg)
@@ -35,69 +37,81 @@ function s.initial_effect(c)
 	c:RegisterEffect(e3)
 end
 
---Search effect
+--Effect 1: Optional search
 function s.thfilter(c)
 	return c:IsSetCard(0x119) and c:IsType(TYPE_RITUAL) and c:IsAbleToHand()
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil)
-	if #g>0 then
+	if Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local sg=g:Select(tp,1,1,nil)
-		if #sg>0 then
-			Duel.SendtoHand(sg,nil,REASON_EFFECT)
-			Duel.ConfirmCards(1-tp,sg)
+		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+		if #g>0 then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
 		end
 	end
 end
 
---Ritual Summon
+--Effect 2: Ritual Summon
 function s.ritcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
 	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
 end
 function s.ritfilter(c,e,tp)
-	return c:IsSetCard(0x119) and c:IsType(TYPE_RITUAL) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,false,true)
+	return c:IsType(TYPE_RITUAL) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,false,true)
 end
-function s.mfilter(c)
-	return c:IsType(TYPE_MONSTER) and c:IsRace(RACE_CYBERSE) and c:IsAttribute(ATTRIBUTE_FIRE) and c:IsAbleToGrave()
+function s.matfilter(c)
+	return c:IsAttribute(ATTRIBUTE_FIRE) and (c:IsLocation(LOCATION_HAND) or c:IsFaceup())
 end
 function s.rittg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.ritfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
+function s.CheckRitualMaterial(g,level)
+	return g:GetSum(function(c)
+		if c:IsType(TYPE_LINK) then return c:GetLink()
+		else return c:GetLevel() end
+	end)>=level
+end
 function s.ritop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.SelectMatchingCard(tp,s.ritfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
-	local rc=g:GetFirst()
+	local g=Duel.GetMatchingGroup(s.ritfilter,tp,LOCATION_HAND,0,nil,e,tp)
+	if #g==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local rc=g:Select(tp,1,1,nil):GetFirst()
 	if not rc then return end
-	local lv=rc:GetLevel()
+	local mat=Duel.GetMatchingGroup(s.matfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,nil)
+	local l=rc:GetLevel()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-	local mat=Duel.SelectMatchingCard(tp,s.mfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,lv,nil)
-	if #mat>0 then
-		Duel.Release(mat,REASON_COST)
-		Duel.SpecialSummon(rc,SUMMON_TYPE_RITUAL,tp,tp,false,true,POS_FACEUP)
-		rc:CompleteProcedure()
-	end
+	aux.GCheckAdditional=aux.RitualCheckAdditional(rc,l,"Equal")
+	local selected=mat:SelectSubGroup(tp,function(g) return s.CheckRitualMaterial(g,l) end,false,1,#mat)
+	aux.GCheckAdditional=nil
+	if not selected then return end
+	rc:SetMaterial(selected)
+	Duel.Release(selected,REASON_EFFECT+REASON_MATERIAL+REASON_RITUAL)
+	Duel.BreakEffect()
+	Duel.SpecialSummon(rc,SUMMON_TYPE_RITUAL,tp,tp,false,true,POS_FACEUP)
+	rc:CompleteProcedure()
 end
 
---Float condition
+--Effect 3: Special Summon if Link leaves field by opponent
 function s.cfilter(c,tp)
-	return c:IsPreviousControler(tp) and c:IsPreviousLocation(LOCATION_MZONE)
-		and c:IsSetCard(0x119) and c:IsType(TYPE_LINK) and c:IsReason(REASON_EFFECT)
-end
-function s.spfilter(c,e,tp)
-	return c:IsSetCard(0x119) and c:IsType(TYPE_RITUAL) and c:IsCanBeSpecialSummoned(e,0,tp,true,true)
+	return c:IsSetCard(0x119) and c:IsType(TYPE_LINK) and c:IsPreviousControler(tp) and c:IsReason(REASON_EFFECT)
+		and c:GetReasonPlayer()~=tp and c:IsPreviousLocation(LOCATION_MZONE)
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return rp~=tp and eg:IsExists(s.cfilter,1,nil,tp)
+	return eg:IsExists(s.cfilter,1,nil,tp)
+end
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(0x119) and c:IsType(TYPE_RITUAL) and c:IsCanBeSpecialSummoned(e,0,tp,false,true)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_GRAVE)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,e,tp)
 	if #g>0 then
-		Duel.SpecialSummon(g,0,tp,tp,true,true,POS_FACEUP)
+		Duel.SpecialSummon(g,0,tp,tp,false,true,POS_FACEUP)
 	end
 end
