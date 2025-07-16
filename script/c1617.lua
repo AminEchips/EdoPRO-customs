9231834e79f077f723eba783299d5263295bc0fe
Chips <aminekhoury0411@gmail.com>
2025-07-16 18:44:27 -0400

    Update c1617.lua

--- script/c1617.lua
+++ script/c1617.lua
@@ -4,7 +4,7 @@ function s.initial_effect(c)
 	c:EnableReviveLimit()
 	Synchro.AddProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x42),1,1,Synchro.NonTuner(nil),1,99)
 
-	-- Double attack if Synchro Summoned using a "Nordic" Synchro
+	-- Double attack if used "Nordic" Synchro
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_EXTRA_ATTACK)
@@ -12,19 +12,19 @@ function s.initial_effect(c)
 	e1:SetValue(1)
 	c:RegisterEffect(e1)
 
-	-- Inflict damage + optional destroy all
+	-- Burn + optional destroy all
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_DAMAGE+CATEGORY_DESTROY)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_BATTLE_DESTROYING)
-	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_PLAYER_TARGET)
 	e2:SetCondition(aux.bdocon)
 	e2:SetTarget(s.damtg)
 	e2:SetOperation(s.damop)
 	c:RegisterEffect(e2)
 
-	-- Revive a different "Nordic" or "Aesir" monster
+	-- Revive another "Nordic" or "Aesir"
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -47,22 +47,26 @@ function s.atkcon(e)
 	return c:IsSummonType(SUMMON_TYPE_SYNCHRO) and c:GetMaterial():IsExists(s.matfilter,1,nil)
 end
 
--- Burn + optional destroy all
+-- Burn effect target
 function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local bc=e:GetHandler():GetBattleTarget()
-	if chk==0 then return bc and bc:IsMonster() end
-	local val=math.max(bc:GetAttack(),bc:GetDefense())
-	if val<0 then val=0 end
-	Duel.SetTargetParam(val)
-	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,val)
+	if not bc then return false end
+	local dam=math.max(bc:GetTextAttack(),bc:GetTextDefense())
+	if chk==0 then return dam>0 end
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(dam)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,dam)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,0,LOCATION_ONFIELD)
 end
+
+-- Burn and optional board wipe
 function s.damop(e,tp,eg,ep,ev,re,r,rp)
 	local bc=e:GetHandler():GetBattleTarget()
-	if not bc or not bc:IsRelateToBattle() then return end
-	local val=math.max(bc:GetAttack(),bc:GetDefense())
-	if val<0 then val=0 end
-	if Duel.Damage(1-tp,val,REASON_EFFECT)>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+	if not bc then return end
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	local dam=math.max(bc:GetTextAttack(),bc:GetTextDefense())
+	if dam<0 then dam=0 end
+	if Duel.Damage(p,dam,REASON_EFFECT)>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
 		local g=Duel.GetMatchingGroup(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
 		if #g>0 then
 			Duel.BreakEffect()
@@ -71,15 +75,17 @@ function s.damop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- GY condition
+-- Revive another Nordic/Aesir if destroyed
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsReason(REASON_DESTROY)
 end
 function s.spfilter(c,e,tp,id)
-	return (c:IsSetCard(0x42) or c:IsSetCard(0x4b)) and not c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return (c:IsSetCard(0x42) or c:IsSetCard(0x4b)) and not c:IsCode(id)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and s.spfilter(chkc,e,tp,id) end
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp)
+		and s.spfilter(chkc,e,tp,id) end
 	if chk==0 then return Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp,id) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp,id)
