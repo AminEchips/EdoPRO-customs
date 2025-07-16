bb40a490f131f2bd429a5f4f0f757f41c696ff82
Chips <aminekhoury0411@gmail.com>
2025-07-15 23:03:48 -0400

    Update c1600.lua

--- script/c1600.lua
+++ script/c1600.lua
@@ -1,16 +1,16 @@
 --Sif of the Nordic Ascendant
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Can be treated as a non-Tuner for Aesir Synchro
+    -- Can be treated as a non-Tuner for the Synchro Summon of an Aesir monster
     local e0=Effect.CreateEffect(c)
     e0:SetType(EFFECT_TYPE_SINGLE)
-    e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+    e0:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
     e0:SetCode(EFFECT_NONTUNER)
-    e0:SetCondition(s.ntcon)
-    e0:SetValue(1)
+    e0:SetRange(LOCATION_MZONE)
+    e0:SetValue(function(_,sc) return sc and sc:IsSetCard(0x4b) end) -- Aesir only
     c:RegisterEffect(e0)
 
-    -- Special Summon from hand if you control a "Nordic" or "Aesir" monster
+    -- Special Summon this card from the hand if you control a Nordic or Aesir monster
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -22,7 +22,7 @@ function s.initial_effect(c)
     e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
 
-    -- Banish from GY to Special Summon 1 "Nordic" from your hand
+    -- Banish from GY to Special Summon 1 "Nordic" monster from hand
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
     e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -35,23 +35,18 @@ function s.initial_effect(c)
     c:RegisterEffect(e2)
 end
 
--- Non-Tuner condition (for Aesir Synchro)
-function s.ntcon(e)
-    local sc=Duel.GetSynchroMaterial()
-    return sc and sc:IsSetCard(0x4b) -- Aesir
-end
-
--- Special Summon from hand condition
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-    return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
-end
+-- Check if you control a "Nordic" or "Aesir" monster
 function s.cfilter(c)
     return c:IsFaceup() and (c:IsSetCard(0x42) or c:IsSetCard(0x4b))
 end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
+end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local c=e:GetHandler()
     if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-        and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+        and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,LOCATION_HAND)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
@@ -60,18 +55,19 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
--- Banish from GY to summon 1 "Nordic" from hand
+-- Banish this card from GY as cost
 function s.gycost(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
     Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
+end
+-- Target 1 "Nordic" in hand to Special Summon
+function s.gyfilter(c,e,tp)
+    return c:IsSetCard(0x42) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.gytg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
         and Duel.IsExistingMatchingCard(s.gyfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
-end
-function s.gyfilter(c,e,tp)
-    return c:IsSetCard(0x42) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.gyop(e,tp,eg,ep,ev,re,r,rp)
     if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
