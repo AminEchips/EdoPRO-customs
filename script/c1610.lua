5a41d154b98b08b1f22229c968b0ef013c01b15f
Chips <aminekhoury0411@gmail.com>
2025-07-16 11:31:31 -0400

    Create c1610.lua

---
+++ script/c1610.lua
@@ -0,0 +1,88 @@
+--Skuld of the Nordic Ascendant
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Synchro Summon procedure
+	Synchro.AddProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x3042),1,1,Synchro.NonTuner(nil),1,99)
+	c:EnableReviveLimit()
+
+	-- Effect on Synchro Summon: shuffle 1 Nordic Ascendant from GY; summon 2 tokens
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCondition(s.tkcon)
+	e1:SetCost(s.tkcost)
+	e1:SetTarget(s.tktg)
+	e1:SetOperation(s.tkop)
+	c:RegisterEffect(e1)
+
+	-- Quick Effect from GY: shuffle all Spells/Traps if you control Odin
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TODECK)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(s.stcon)
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(s.sttg)
+	e2:SetOperation(s.stop)
+	c:RegisterEffect(e2)
+end
+s.listed_names={93483212}
+s.listed_series={0x3042} -- Nordic Ascendant
+
+-- e1 condition: must be Synchro Summoned
+function s.tkcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO)
+end
+
+-- Shuffle 1 "Nordic Ascendant" from GY into Deck as cost
+function s.costfilter(c)
+	return c:IsSetCard(0x3042) and c:IsAbleToDeckAsCost()
+end
+function s.tkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_COST)
+end
+
+-- Create 2 Valhalla Tokens (Level 2, LIGHT, Warrior)
+function s.tktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		return not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
+			and Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+			and Duel.IsPlayerCanSpecialSummonMonster(tp,,0,TYPES_TOKEN,1000,1000,2,RACE_WARRIOR,ATTRIBUTE_LIGHT)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,2,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,0)
+end
+function s.tkop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
+		or Duel.GetLocationCount(tp,LOCATION_MZONE)<2
+		or not Duel.IsPlayerCanSpecialSummonMonster(tp,40844553,0,TYPES_TOKEN,1000,1000,2,RACE_WARRIOR,ATTRIBUTE_LIGHT) then return end
+	for i=1,2 do
+		local token=Duel.CreateToken(tp,id+1)
+		Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+	end
+	Duel.SpecialSummonComplete()
+end
+
+-- GY Quick Effect: shuffle all Spells/Traps if you control Odin
+function s.stcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsCode,93483212),tp,LOCATION_MZONE,0,1,nil)
+end
+function s.sttg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(Card.IsSpellTrap,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+	if chk==0 then return #g>0 end
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,#g,0,0)
+end
+function s.stop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsSpellTrap,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+	if #g>0 then
+		Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+	end
+end
