69bf88909f1ca0bd9eacd08a8fcec1ddb76c21e8
Chips <aminekhoury0411@gmail.com>
2025-07-16 10:34:33 -0400

    Update c1607.lua

--- script/c1607.lua
+++ script/c1607.lua
@@ -1,7 +1,7 @@
 --Dvallin of the Nordic Alfar
 local s,id=GetID()
 function s.initial_effect(c)
-	-- Special Summon from hand + optionally banish from Deck
+	--Special Summon from hand
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -13,33 +13,33 @@ function s.initial_effect(c)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
 
-	-- Return banished Level 5+ monster and optionally destroy
+	--Return 1 banished Level 5+ monster to hand and optionally destroy
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_TODECK+CATEGORY_DESTROY)
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_DESTROY)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_REMOVE)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
 	e2:SetCountLimit(1,{id,1})
 	e2:SetCost(s.retcost)
+	e2:SetTarget(s.rettg)
 	e2:SetOperation(s.rmop)
 	c:RegisterEffect(e2)
+
 	local e3=e2:Clone()
-	e3:SetCode(EVENT_BE_MATERIAL)
-	e3:SetCondition(s.matcon)
+	e3:SetCode(EVENT_TO_GRAVE)
+	e3:SetCondition(s.grcon)
 	c:RegisterEffect(e3)
 end
 
--- Control no monsters or only "Nordic" monsters
-function s.cfilter(c)
-	return not c:IsSetCard(0x42)
-end
+-- Effect 1: Special Summon if you control only Nordic or no monsters
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0
-		or not Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
+	local g=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
+	return #g==0 or g:FilterCount(function(c) return c:IsSetCard(0x42) end,nil)==#g
 end
-
--- Effect 1: Special Summon + optional banish from Deck
+function s.spfilter(c)
+	return c:IsSetCard(0x42) and c:IsAbleToRemove()
+end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
@@ -48,58 +48,57 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
-		local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,LOCATION_DECK,0,nil)
-		if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
+		-- Optional: Banish 1 Nordic card from Deck
+		if Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil)
+			and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-			local rg=g:Select(tp,1,1,nil)
-			Duel.Remove(rg,POS_FACEUP,REASON_EFFECT)
+			local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil)
+			if #g>0 then
+				Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+			end
 		end
 	end
 end
 
--- Effect 2 & 3 condition if used as Synchro material
-function s.matcon(e,tp,eg,ep,ev,re,r,rp)
-	local rc=e:GetHandler():GetReasonCard()
-	return r & REASON_SYNCHRO ~= 0 and rc:IsType(TYPE_SYNCHRO)
-end
-
--- Cost: select target banished Level 5+ monster
+-- Effect 2/3 Cost: target 1 banished Level 5+ monster (must target as cost)
 function s.retfilter(c)
-	return c:IsFaceup() and c:IsLevelAbove(5) and (c:IsAbleToHand() or c:IsAbleToExtra())
+	return c:IsFaceup() and c:IsLevelAbove(5) and c:IsAbleToHand()
 end
 function s.retcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.retfilter,tp,LOCATION_REMOVED,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	local g=Duel.SelectMatchingCard(tp,s.retfilter,tp,LOCATION_REMOVED,0,1,1,nil)
-	if #g>0 then
-		e:SetLabelObject(g:GetFirst())
-	end
-	return true
+	if chk==0 then return Duel.IsExistingTarget(s.retfilter,tp,LOCATION_REMOVED,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.SelectTarget(tp,s.retfilter,tp,LOCATION_REMOVED,0,1,1,nil)
+	e:SetLabelObject(g:GetFirst())
 end
-
--- Operation: return target to hand/Extra + optional destruction
+function s.rettg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tc=e:GetLabelObject()
+	if chk==0 then return tc and tc:IsAbleToHand() end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,tc,1,0,0)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_DESTROY,nil,1,1-tp,LOCATION_MZONE)
+end
+function s.grcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return r&(REASON_MATERIAL|REASON_SYNCHRO)==(REASON_MATERIAL|REASON_SYNCHRO)
+end
 function s.rmop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=e:GetLabelObject()
 	if not tc or not tc:IsRelateToEffect(e) then return end
 	local lv=tc:GetLevel()
-	local loc=tc:IsAbleToExtra() and LOCATION_EXTRA or LOCATION_HAND
-	if loc==LOCATION_EXTRA then
-		Duel.SendtoDeck(tc,nil,SEQ_DECKTOP,REASON_EFFECT)
-	else
-		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+	if Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 then
 		Duel.ConfirmCards(1-tp,tc)
 	end
 
-	-- Destroy opponent's monster with same Level if used as Synchro material
-	local rc=e:GetHandler():GetReasonCard()
-	if rc and rc:IsType(TYPE_SYNCHRO) and e:GetHandler():IsReason(REASON_MATERIAL) then
-		local dg=Duel.GetMatchingGroup(function(c) return c:IsFaceup() and c:IsLevel(lv) end,tp,0,LOCATION_MZONE,nil)
-		if #dg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-			local sg=dg:Select(tp,1,1,nil)
-			Duel.Destroy(sg,REASON_EFFECT)
+	-- Destroy opponent's monster with same Level, only if banished as cost for Synchro effect
+	if e:GetHandler():IsReason(REASON_MATERIAL) then
+		local rc=e:GetHandler():GetReasonCard()
+		if rc and rc:IsType(TYPE_SYNCHRO) then
+			local g=Duel.GetMatchingGroup(function(c) return c:IsFaceup() and c:IsLevel(lv) end,tp,0,LOCATION_MZONE,nil)
+			if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
+				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+				local sg=g:Select(tp,1,1,nil)
+				Duel.Destroy(sg,REASON_EFFECT)
+			end
 		end
 	end
 end
