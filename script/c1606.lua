5684d6672bd11fe96021c9e249467c10f5c908ff
Chips <aminekhoury0411@gmail.com>
2025-07-16 10:08:37 -0400

    Create c1606.lua

---
+++ script/c1606.lua
@@ -0,0 +1,82 @@
+--Laufeyson of the Nordic Alfar
+local s,id=GetID()
+function s.initial_effect(c)
+    -- Special Summon itself from hand or GY by targeting a Normal Summoned Level 5 or lower "Nordic"
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetRange(LOCATION_HAND+LOCATION_GRAVE)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e1:SetCountLimit(1,id)
+    e1:SetTarget(s.sptg)
+    e1:SetOperation(s.spop)
+    c:RegisterEffect(e1)
+
+    -- If banished: send 1 "Nordic" card from Deck to GY
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_TOGRAVE)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e2:SetProperty(EFFECT_FLAG_DELAY)
+    e2:SetCode(EVENT_REMOVE)
+    e2:SetCountLimit(1,{id,1})
+    e2:SetTarget(s.tgtg)
+    e2:SetOperation(s.tgop)
+    c:RegisterEffect(e2)
+end
+
+-- FILTERS
+function s.spfilter(c)
+    return c:IsFaceup() and c:IsSetCard(0x42) and c:IsLevelBelow(5) and c:IsSummonType(SUMMON_TYPE_NORMAL)
+end
+
+-- e1: Target function
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.spfilter(chkc) end
+    local c=e:GetHandler()
+    if chk==0 then
+        return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+            and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+            and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_MZONE,0,1,nil)
+    end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+    Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_MZONE,0,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+
+-- e1: Special Summon + Level adjustment
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local tc=Duel.GetFirstTarget()
+    if c:IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and tc and tc:IsRelateToEffect(e)
+        and c:IsCanBeSpecialSummoned(e,0,tp,false,false) then
+        if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 and c:IsFaceup() and tc:IsFaceup() then
+            local lv=tc:GetLevel()
+            if lv>0 then
+                local e1=Effect.CreateEffect(c)
+                e1:SetType(EFFECT_TYPE_SINGLE)
+                e1:SetCode(EFFECT_UPDATE_LEVEL)
+                e1:SetValue(-lv)
+                e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+                c:RegisterEffect(e1)
+            end
+        end
+    end
+end
+
+-- e2: Banish Trigger – Foolish 1 Nordic
+function s.tgfilter(c)
+    return c:IsSetCard(0x42) and c:IsAbleToGrave()
+end
+function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+end
+function s.tgop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+    local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
+    if #g>0 then
+        Duel.SendtoGrave(g,REASON_EFFECT)
+    end
+end
