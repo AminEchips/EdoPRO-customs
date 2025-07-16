c692456f5db3ee2f72ad5da33dd3a23474ae00fc
Chips <aminekhoury0411@gmail.com>
2025-07-16 19:13:31 -0400

    Update c1618.lua

--- script/c1618.lua
+++ script/c1618.lua
@@ -1,97 +1,81 @@
 --Hela, Grim of the Aesir
 local s,id=GetID()
 function s.initial_effect(c)
-	--Synchro Summon procedure
-	Synchro.AddProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x42),1,1,aux.NonTuner(nil),2,99)
+	--Synchro Summon
+	Synchro.AddProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x42),1,1,Synchro.NonTuner(nil),2,99)
 	c:EnableReviveLimit()
 
-	--Cannot Tribute during opponent's turn
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_FIELD)
-	e0:SetCode(EFFECT_CANNOT_RELEASE)
-	e0:SetRange(LOCATION_MZONE)
-	e0:SetTargetRange(0,LOCATION_MZONE)
-	e0:SetCondition(function(e) return Duel.GetTurnPlayer()~=e:GetHandlerPlayer() end)
-	e0:SetTarget(aux.TRUE)
-	c:RegisterEffect(e0)
-	local e0b=e0:Clone()
-	e0b:SetCode(EFFECT_CANNOT_RELEASE_SUM)
-	c:RegisterEffect(e0b)
-
-	--Quick: Reduce ATK to 0 or send to GY if Spellcaster used
+	--ATK to 0 or send to GY (Quick Effect)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_TOGRAVE)
+	e1:SetCategory(CATEGORY_TOGRAVE)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1,id)
-	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
+	e1:SetHintTiming(0,TIMING_MAIN_END)
 	e1:SetCondition(function(e,tp) return Duel.IsMainPhase() end)
 	e1:SetOperation(s.atkop)
 	c:RegisterEffect(e1)
 
-	--Revive itself by banishing 1 other Aesir
+	--Special Summon from GY by banishing 1 other Aesir
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_GRAVE)
 	e2:SetCountLimit(1,{id,1})
-	e2:SetCondition(function(e) return Duel.GetTurnPlayer()==e:GetHandlerPlayer() end)
+	e2:SetCondition(function(e,tp) return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end)
 	e2:SetCost(s.spcost)
 	e2:SetTarget(s.sptg)
 	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
 end
-s.listed_series={0x4b} -- Aesir
-s.listed_names={id}
+s.listed_series={0x42,0x4b}
 
---e1 operation
+--e1: ATK to 0 or send to GY instead
 function s.atkfilter(c)
 	return c:IsFaceup() and c:IsMonster()
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local g=Duel.GetMatchingGroup(s.atkfilter,tp,0,LOCATION_MZONE,nil)
-	if #g==0 then return end
+	if not Duel.IsExistingMatchingCard(s.atkfilter,tp,0,LOCATION_MZONE,1,nil) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local tc=g:Select(tp,1,1,nil):GetFirst()
+	local g=Duel.SelectMatchingCard(tp,s.atkfilter,tp,0,LOCATION_MZONE,1,1,nil)
+	local tc=g:GetFirst()
 	if not tc then return end
-	local b1=not c:IsSummonType(SUMMON_TYPE_SYNCHRO) or not c:GetMaterial():IsExists(Card.IsRace,1,nil,RACE_SPELLCASTER)
-	local b2=c:IsSummonType(SUMMON_TYPE_SYNCHRO) and c:GetMaterial():IsExists(Card.IsRace,1,nil,RACE_SPELLCASTER) and tc:IsAbleToGrave()
-	local op=1
-	if b1 and not b2 then op=1
-	elseif not b1 and b2 then op=2
-	elseif b1 and b2 then
-		op=Duel.SelectOption(tp,aux.Stringid(id,2),aux.Stringid(id,3))+1
+
+	if c:IsSummonType(SUMMON_TYPE_SYNCHRO) and c:GetMaterial():IsExists(Card.IsRace,1,nil,RACE_SPELLCASTER) then
+		local opt=Duel.SelectOption(tp,aux.Stringid(id,2),aux.Stringid(id,3)) -- 0 = reduce ATK, 1 = send
+		if opt==1 and tc:IsAbleToGrave() then
+			Duel.SendtoGrave(tc,REASON_EFFECT)
+			return
+		end
 	end
-	if op==1 then
+
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
 		e1:SetValue(0)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e1)
-	elseif op==2 then
-		Duel.SendtoGrave(tc,REASON_EFFECT)
 	end
 end
 
---e2 cost
+--e2: Special Summon from GY
 function s.spcfilter(c)
-	return c:IsSetCard(0x4b) and c:IsAbleToRemove() and not c:IsCode(id)
+	return c:IsSetCard(0x4b) and c:IsMonster() and c:IsAbleToRemoveAsCost()
 end
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.spcfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.spcfilter,tp,LOCATION_ONFIELD+LOCATION_GRAVE,0,1,e:GetHandler()) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,s.spcfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.spcfilter,tp,LOCATION_ONFIELD+LOCATION_GRAVE,0,1,1,e:GetHandler())
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+	if chk==0 then return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
