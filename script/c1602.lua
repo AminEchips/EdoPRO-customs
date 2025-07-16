6bacf94065bdb5cd1fe9c4fb307f7f608bcc76cc
Chips <aminekhoury0411@gmail.com>
2025-07-15 23:26:14 -0400

    Update c1602.lua

--- script/c1602.lua
+++ script/c1602.lua
@@ -1,7 +1,7 @@
 --Fafnir of the Nordic Beasts
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Special Summon from hand when a Nordic or Aesir monster is sent to your GY
+    -- e1/e2: Special Summon from hand when a Nordic or Aesir monster is sent to your GY or destroyed
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -14,26 +14,28 @@ function s.initial_effect(c)
     e1:SetTarget(s.sptg)
     e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
-
-    -- On Normal or Special Summon: Search a Nordic monster (except itself)
-    local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,1))
-    e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e2:SetCode(EVENT_SUMMON_SUCCESS)
-    e2:SetProperty(EFFECT_FLAG_DELAY)
-    e2:SetTarget(s.thtg)
-    e2:SetOperation(s.thop)
+    local e2=e1:Clone()
+    e2:SetCode(EVENT_DESTROYED)
     c:RegisterEffect(e2)
-    local e3=e2:Clone()
-    e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+
+    -- e3/e4: Search a Nordic monster (on Normal or Special Summon)
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e3:SetCode(EVENT_SUMMON_SUCCESS)
+    e3:SetProperty(EFFECT_FLAG_DELAY)
+    e3:SetTarget(s.thtg)
+    e3:SetOperation(s.thop)
     c:RegisterEffect(e3)
+    local e4=e3:Clone()
+    e4:SetCode(EVENT_SPSUMMON_SUCCESS)
+    c:RegisterEffect(e4)
 end
 
--- e1 condition: a "Nordic" or "Aesir" monster (except this) sent to your GY from anywhere
+-- Special Summon condition: if any other "Nordic" or "Aesir" monster was sent to your GY
 function s.cfilter(c,tp)
     return c:IsType(TYPE_MONSTER)
-        and c:IsLocation(LOCATION_GRAVE)
         and c:IsPreviousControler(tp)
         and (c:IsSetCard(0x42) or c:IsSetCard(0x4b))
         and not c:IsCode(id)
@@ -54,9 +56,10 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
--- e2: Search a "Nordic" monster from Deck to hand on Summon
+-- Search effect: add a "Nordic" monster from Deck to hand
 function s.thfilter(c)
-    return c:IsSetCard(0x42) and c:IsType(TYPE_MONSTER) and not c:IsCode(id) and c:IsAbleToHand()
+    return c:IsSetCard(0x42) and c:IsType(TYPE_MONSTER)
+        and not c:IsCode(id) and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
