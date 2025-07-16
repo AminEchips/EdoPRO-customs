c0472205b63c0344687e06fcba17c16c15f23c3b
Chips <aminekhoury0411@gmail.com>
2025-07-16 13:00:05 -0400

    Create c1613.lua

---
+++ script/c1613.lua
@@ -0,0 +1,121 @@
+--Sleipnir of the Nordic Beasts
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Synchro procedure
+	Synchro.AddProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x42),1,1,Synchro.NonTuner(nil),1,99)
+	c:EnableReviveLimit()
+
+	-- 1. Battle protection if Synchro Summoned with Nordic Beast Tuner
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetCondition(s.indcon)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+
+	-- 2. Quick Effect: Redirect attack and negate if you control an "Aesir"
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_DISABLE)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.atkcon)
+	e2:SetOperation(s.atkop)
+	c:RegisterEffect(e2)
+
+	-- 3. On destruction, equip to "Odin" monster
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_EQUIP)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_DESTROYED)
+	e3:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e3:SetCondition(s.eqcon)
+	e3:SetTarget(s.eqtg)
+	e3:SetOperation(s.eqop)
+	c:RegisterEffect(e3)
+end
+
+-- Odin IDs (used for explicit name "Odin")
+local odin_ids = {
+	[93483212]=true, [1621]=true, [1647]=true
+}
+
+-- Check Synchro Summon with Nordic Beast Tuner
+function s.indcon(e)
+	local c=e:GetHandler()
+	local mg=c:GetMaterial()
+	return c:IsSummonType(SUMMON_TYPE_SYNCHRO)
+		and mg:IsExists(function(mc) return mc:IsSetCard(0x6042) and mc:IsType(TYPE_TUNER) end,1,nil)
+end
+
+-- Aesir control check
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	local at=Duel.GetAttacker()
+	return at and at:IsControler(1-tp) and Duel.IsExistingMatchingCard(Card.IsSetCard,tp,LOCATION_MZONE,0,1,nil,0x4b)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local at=Duel.GetAttacker()
+	if not c:IsRelateToEffect(e) or not at or at:IsImmuneToEffect(e) then return end
+	-- Change attack target
+	Duel.ChangeAttackTarget(c)
+	-- Negate effects of attacking monster
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_DISABLE)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	at:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_DISABLE_EFFECT)
+	at:RegisterEffect(e2)
+end
+
+-- Equip to Odin monster upon destruction
+function s.eqcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsFaceup()
+end
+function s.odinfilter(c,tp)
+	return c:IsFaceup() and odin_ids[c:GetCode()] and c:IsControler(tp)
+end
+function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.odinfilter(chkc,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.odinfilter,tp,LOCATION_MZONE,0,1,nil,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	local g=Duel.SelectTarget(tp,s.odinfilter,tp,LOCATION_MZONE,0,1,1,nil,tp)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,g,1,0,0)
+end
+function s.eqop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if not tc or not c:IsRelateToEffect(e) or not Duel.Equip(tp,c,tc) then return end
+	-- Cannot equip to anything else
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_EQUIP_LIMIT)
+	e1:SetProperty(EFFECT_FLAG_COPY_INHERIT+EFFECT_FLAG_OWNER_RELATE)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	e1:SetValue(function(e,c) return c==tc end)
+	c:RegisterEffect(e1)
+	-- Battle protection clause while equipped
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_REMOVE)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e2:SetTarget(s.battlelock)
+	e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+	e2:SetCondition(s.battlecon)
+	c:RegisterEffect(e2)
+end
+
+function s.battlecon(e)
+	local tc=e:GetHandler():GetEquipTarget()
+	return tc and Duel.GetAttacker()==tc and Duel.GetAttackTarget()
+end
+function s.battlelock(e,c)
+	return c==Duel.GetAttacker() or c==Duel.GetAttackTarget()
+end
