9ec88999696fd9f916c376f3c32f6cbd1efb19f0
Chips <aminekhoury0411@gmail.com>
2025-07-17 13:34:48 -0400

    Update c1621.lua

--- script/c1621.lua
+++ script/c1621.lua
@@ -5,105 +5,116 @@ function s.initial_effect(c)
 	Synchro.AddProcedure(c,s.tfilter,1,1,Synchro.NonTuner(nil),1,99)
 	c:EnableReviveLimit()
 
-	--Aesir monsters unaffected by opponent's Spells/Traps
+	--Make Aesir DIVINE and immune to opponent's Spell/Trap effects
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_IMMUNE_EFFECT)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetTargetRange(LOCATION_MZONE,0)
-	e1:SetTarget(s.immfilter_spelltrap)
-	e1:SetValue(s.efilter_spelltrap)
+	e1:SetTarget(s.immfilter)
+	e1:SetValue(s.efilter)
 	c:RegisterEffect(e1)
 
-	--Quick Effect: Banish 1 opponent monster, draw 1, protect from monster effects
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetCategory(CATEGORY_REMOVE+CATEGORY_DRAW)
-	e2:SetType(EFFECT_TYPE_QUICK_O)
-	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_ADD_ATTRIBUTE)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1)
-	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
-	e2:SetTarget(s.target)
-	e2:SetOperation(s.operation)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(s.immfilter)
+	e2:SetValue(ATTRIBUTE_DIVINE)
 	c:RegisterEffect(e2)
 
-	--Float if sent to GY
+	--Banish + Draw + Protect current monsters from monster effects
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,1))
-	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e3:SetCode(EVENT_TO_GRAVE)
-	e3:SetProperty(EFFECT_FLAG_DELAY)
-	e3:SetCountLimit(1,id)
-	e3:SetCondition(s.spcon_gy)
-	e3:SetCost(s.spcost_gy)
-	e3:SetTarget(s.sptg)
-	e3:SetOperation(s.spop)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_REMOVE+CATEGORY_DRAW)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1)
+	e3:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
+	e3:SetTarget(s.bdtg)
+	e3:SetOperation(s.bdop)
 	c:RegisterEffect(e3)
 
-	--Float if banished by opponent
+	--Float if sent to GY
 	local e4=Effect.CreateEffect(c)
 	e4:SetDescription(aux.Stringid(id,1))
 	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e4:SetCode(EVENT_REMOVE)
+	e4:SetCode(EVENT_TO_GRAVE)
 	e4:SetProperty(EFFECT_FLAG_DELAY)
-	e4:SetCountLimit(1,id+100)
-	e4:SetCondition(s.spcon_rm)
+	e4:SetCountLimit(1,id)
+	e4:SetCondition(s.spcon_gy)
+	e4:SetCost(s.spcost_gy)
 	e4:SetTarget(s.sptg)
 	e4:SetOperation(s.spop)
 	c:RegisterEffect(e4)
+
+	--Float if banished by opponent
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,1))
+	e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e5:SetCode(EVENT_REMOVE)
+	e5:SetProperty(EFFECT_FLAG_DELAY)
+	e5:SetCountLimit(1,id+100)
+	e5:SetCondition(s.spcon_rm)
+	e5:SetTarget(s.sptg)
+	e5:SetOperation(s.spop)
+	c:RegisterEffect(e5)
 end
 s.listed_series={0x4b}
-s.listed_names={93483212}
+s.listed_names={93483212} -- Odin, Father of the Aesir
 
--- Synchro Material must include a Nordic Ascendant Tuner
+-- Synchro Material: 1 "Nordic Ascendant" Tuner
 function s.tfilter(c,scard,sumtype,tp)
 	return c:IsSetCard(0x3042,scard,sumtype,tp) or c:IsHasEffect(EFFECT_SYNSUB_NORDIC)
 end
 
--- Aesir monsters on field unaffected by opponent's Spell/Trap effects
-function s.immfilter_spelltrap(e,c)
+-- Immunity target: face-up Aesir
+function s.immfilter(e,c)
 	return c:IsFaceup() and c:IsSetCard(0x4b)
 end
-function s.efilter_spelltrap(e,te)
-	return te:IsActivated() and te:IsSpellTrapEffect() and te:GetOwnerPlayer()~=e:GetHandlerPlayer()
+
+-- Immune to ALL opponent's Spell/Trap effects (continuous and activated)
+function s.efilter(e,te)
+	local c=e:GetHandler()
+	local tc=te:GetOwner()
+	return te:IsActiveType(TYPE_SPELL+TYPE_TRAP) and te:GetOwnerPlayer()~=e:GetHandlerPlayer()
 end
 
--- Banish 1 opponent monster, draw 1, Aesir unaffected by monster effects
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+-- Banish + Draw + Protect current monsters from monster effects
+function s.bdtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,0,LOCATION_MZONE,1,nil)
 		and Duel.IsPlayerCanDraw(tp,1) end
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,1-tp,LOCATION_MZONE)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.bdop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,0,LOCATION_MZONE,1,1,nil)
-	local tc=g:GetFirst()
-	if tc and Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)~=0 then
-		if Duel.Draw(tp,1,REASON_EFFECT)~=0 then
-			-- Aesir monsters are unaffected by monster effects this turn
-			local e1=Effect.CreateEffect(e:GetHandler())
-			e1:SetType(EFFECT_TYPE_FIELD)
-			e1:SetCode(EFFECT_IMMUNE_EFFECT)
-			e1:SetTargetRange(LOCATION_MZONE,0)
-			e1:SetTarget(s.immfilter_monster)
-			e1:SetValue(s.efilter_monster)
-			e1:SetReset(RESET_PHASE+PHASE_END)
-			Duel.RegisterEffect(e1,tp)
-		end
+	local g1=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,0,LOCATION_MZONE,1,1,nil)
+	if #g1==0 then return end
+	if Duel.Remove(g1,POS_FACEUP,REASON_EFFECT)==0 then return end
+	if Duel.Draw(tp,1,REASON_EFFECT)==0 then return end
+
+	local g2=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
+	if #g2==0 then return end
+	for tc in g2:Iter() do
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_IMMUNE_EFFECT)
+		e1:SetValue(function(e,te) return te:IsActiveType(TYPE_MONSTER)
+			and te:GetOwnerPlayer()~=e:GetHandlerPlayer()
+			and te:GetHandler()~=e:GetHandler()
+			and te:GetHandler()~=c end)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
 	end
 end
-function s.immfilter_monster(e,c)
-	return c:IsFaceup() and c:IsSetCard(0x4b)
-end
-function s.efilter_monster(e,te)
-	return te:IsActivated() and te:IsMonsterEffect() and te:GetOwnerPlayer()~=e:GetHandlerPlayer()
-end
 
--- Float from GY
+-- Float from GY: triggers, banish as cost
 function s.spcon_gy(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return rp~=tp and c:IsReason(REASON_EFFECT+REASON_DESTROY) and c:IsPreviousControler(tp)
@@ -114,12 +125,12 @@ function s.spcost_gy(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Remove(c,POS_FACEUP,REASON_COST)
 end
 
--- Float from banish
+-- Float from banish: just condition
 function s.spcon_rm(e,tp,eg,ep,ev,re,r,rp)
 	return rp~=tp
 end
 
--- Shared float target and operation
+-- Shared target and operation
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA|LOCATION_GRAVE,0,1,nil,e,tp) end
@@ -129,7 +140,6 @@ function s.spfilter(c,e,tp)
 	return c:IsCode(93483212) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_SYNCHRO,tp,false,false)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_EXTRA|LOCATION_GRAVE,0,1,1,nil,e,tp)
 	local sc=g:GetFirst()
