fd9e72071a31bee57708fe4108cbfdb1db232382
Chips <aminekhoury0411@gmail.com>
2025-07-17 10:09:00 -0400

    Update c1619.lua

--- script/c1619.lua
+++ script/c1619.lua
@@ -3,18 +3,9 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	-- Custom Synchro Summon procedure: 1 Nordic Beast Tuner + 1+ "Thor, Lord of the Aesir"
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_FIELD)
-	e0:SetCode(EFFECT_SPSUMMON_PROC)
-	e0:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-	e0:SetRange(LOCATION_EXTRA)
-	e0:SetCondition(s.syncon)
-	e0:SetTarget(s.syntg)
-	e0:SetOperation(s.synop)
-	e0:SetValue(SUMMON_TYPE_SYNCHRO)
-	c:RegisterEffect(e0)
-	
+	-- 1 "Nordic Beast" Tuner + 1+ non-Tuners (must include Thor manually)
+	Synchro.AddProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x6042),1,1,Synchro.NonTuner(nil),1,99)
+
 	--Add 1 Aesir or Nordic Relic Spell/Trap from Deck or GY
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
@@ -56,28 +47,6 @@ end
 s.listed_series={0x6042,0x5042,0x4b} -- Nordic Beast, Nordic Relic, Aesir
 s.listed_names={30604579} -- Thor, Lord of the Aesir
 
--- Manual Synchro Summon logic
-function s.synfilter1(c) -- Tuner: Nordic Beast
-	return c:IsSetCard(0x6042) and c:IsType(TYPE_TUNER)
-end
-function s.synfilter2(c) -- Non-Tuner: Thor, Lord of the Aesir
-	return c:IsCode(30604579)
-end
-function s.syncon(e,c)
-	if c==nil then return true end
-	local tp=c:GetControler()
-	local mg=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
-	return Duel.CheckSynchroMaterial(c,s.synfilter1,s.synfilter2,1,99,mg)
-end
-function s.syntg(e,tp,eg,ep,ev,re,r,rp,c,smat,mg)
-	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
-	return Duel.SelectSynchroMaterial(tp,c,s.synfilter1,s.synfilter2,1,99,g)
-end
-function s.synop(e,tp,eg,ep,ev,re,r,rp,c,smat,mg,sg)
-	c:SetMaterial(sg)
-	Duel.SendtoGrave(sg,REASON_MATERIAL+REASON_SYNCHRO)
-end
-
 -- On Synchro Summon: add Aesir or Nordic Relic S/T
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO)
@@ -97,7 +66,7 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- Negate + optional destroy all opponent monsters
+-- Negate and optionally destroy all opponent monsters
 function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(aux.NegateAnyFilter,tp,0,LOCATION_MZONE,1,nil) end
 end
@@ -126,7 +95,7 @@ function s.negop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- Leave field due to opponent: banish this, revive "Thor, Lord of the Aesir"
+-- When leaves field due to opponent: banish self, Special Summon 1 "Thor, Lord of the Aesir"
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return c:IsSummonType(SUMMON_TYPE_SYNCHRO) and rp==1-tp and c:IsPreviousControler(tp)
