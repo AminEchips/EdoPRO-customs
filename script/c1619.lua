5f78e23046211f10158c80d1a2c2694122cfaee0
Chips <aminekhoury0411@gmail.com>
2025-07-17 09:49:39 -0400

    Update c1619.lua

--- script/c1619.lua
+++ script/c1619.lua
@@ -3,20 +3,10 @@ local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
 	--Synchro Summon procedure
-	Synchro.AddProcedure(c,
-		aux.FilterBoolFunction(Card.IsSetCard,0x6042),1,1,
-		aux.FilterBoolFunction(Card.IsCode,30604579),1,99)
-	c:SetSPSummonOnce(id)
-
-	--Must be Synchro Summoned first
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_SINGLE)
-	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e0:SetValue(aux.synlimit)
-	c:RegisterEffect(e0)
-
-	--Add 1 "Aesir" or "Nordic Relic" Spell/Trap
+	Synchro.AddProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x6042),1,1,s.matfilter,1,99)
+	c:EnableReviveLimit()
+	
+	--Add 1 "Aesir" or "Nordic Relic" from Deck or GY
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
@@ -29,101 +19,104 @@ function s.initial_effect(c)
 	e1:SetOperation(s.thop)
 	c:RegisterEffect(e1)
 
-	--Quick Effect: negate effect of 1 Effect Monster, then optionally destroy all opponent's monsters
+	--Negate and destroy
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_DISABLE+CATEGORY_DESTROY)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetHintTiming(0,TIMING_MAIN_END+TIMING_BATTLE_END)
 	e2:SetCountLimit(1,{id,1})
-	e2:SetCondition(function(e,tp) return Duel.IsMainPhase() or Duel.IsBattlePhase() end)
-	e2:SetTarget(s.negtg)
+	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
+	e2:SetCondition(function(e,tp) return Duel.IsMainPhase() end)
 	e2:SetOperation(s.negop)
 	c:RegisterEffect(e2)
 
-	--If this Synchro Summoned card leaves the field by opponent's card: SS "Thor, Lord of the Aesir"
+	--Revive Thor, Lord of the Aesir
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,2))
-	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e3:SetProperty(EFFECT_FLAG_DELAY)
 	e3:SetCode(EVENT_LEAVE_FIELD)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
 	e3:SetCountLimit(1,{id,2})
-	e3:SetCondition(s.spcon)
-	e3:SetTarget(s.sptg)
-	e3:SetOperation(s.spop)
+	e3:SetCondition(s.rvcon)
+	e3:SetTarget(s.rvtg)
+	e3:SetOperation(s.rvop)
 	c:RegisterEffect(e3)
 end
-s.listed_names={30604579}
+s.listed_names={30604579} -- Thor, Lord of the Aesir
 s.listed_series={0x4b,0x5042} -- Aesir, Nordic Relic
 
+--Material filter
+function s.matfilter(c,sc,sumtype,tp)
+	return c:IsCode(30604579)
+end
+
+--Search
 function s.thfilter(c)
 	return (c:IsSetCard(0x4b) or c:IsSetCard(0x5042)) and c:IsSpellTrap() and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK|LOCATION_GRAVE,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK|LOCATION_GRAVE)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.thfilter),tp,LOCATION_DECK|LOCATION_GRAVE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
 	if #g>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
 	end
 end
 
-function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(aux.AND(Card.IsFaceup,Card.IsType,Card.IsDisabled),tp,0,LOCATION_MZONE,1,nil,TYPE_EFFECT) end
+--Negate and destroy
+function s.negfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_EFFECT) and not c:IsDisabled()
 end
 function s.negop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISABLE)
-	local tc=Duel.SelectMatchingCard(tp,aux.AND(Card.IsFaceup,Card.IsType,Card.IsDisabled),tp,0,LOCATION_MZONE,1,1,nil,TYPE_EFFECT):GetFirst()
-	if not tc or not tc:IsRelateToEffect(e) then return end
-
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_DISABLE)
-	e1:SetReset(RESETS_STANDARD)
-	tc:RegisterEffect(e1)
-	local e2=e1:Clone()
-	e2:SetCode(EFFECT_DISABLE_EFFECT)
-	tc:RegisterEffect(e2)
-
-	--Optional destroy all opponent's monsters
-	if Duel.IsExistingMatchingCard(Card.IsMonster,tp,0,LOCATION_MZONE,1,nil)
-		and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
-		local g=Duel.GetMatchingGroup(Card.IsMonster,tp,0,LOCATION_MZONE,nil)
-		Duel.Destroy(g,REASON_EFFECT)
+	local g=Duel.GetMatchingGroup(s.negfilter,tp,0,LOCATION_MZONE,nil)
+	if #g==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local tc=g:Select(tp,1,1,nil):GetFirst()
+	if tc and not tc:IsDisabled() and tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		local c=e:GetHandler()
+		--Negate its effects
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		tc:RegisterEffect(e2)
+		if Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
+			local dg=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,nil)
+			if #dg>0 then
+				Duel.Destroy(dg,REASON_EFFECT)
+			end
+		end
 	end
 end
 
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+--Revive Thor
+function s.rvcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return c:IsSummonType(SUMMON_TYPE_SYNCHRO)
-		and rp==1-tp and c:IsPreviousControler(tp) and c:IsPreviousPosition(POS_FACEUP)
+	return c:IsPreviousControler(tp) and c:IsPreviousPosition(POS_FACEUP) and rp==1-tp
 end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-			and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA|LOCATION_GRAVE,0,1,nil,e,tp)
-	end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA|LOCATION_GRAVE)
+function s.rvtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_GRAVE+LOCATION_EXTRA,0,1,nil,30604579)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
 end
-function s.spfilter(c,e,tp)
-	return c:IsCode(30604579) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_SYNCHRO,tp,false,false)
-end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
+function s.rvop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e) then return end
-	if not c:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED) then
+	if c:IsRelateToEffect(e) and not c:IsLocation(LOCATION_REMOVED) then
 		Duel.Remove(c,POS_FACEUP,REASON_EFFECT)
 	end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local sc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_EXTRA|LOCATION_GRAVE,0,1,1,nil,e,tp):GetFirst()
-	if sc and Duel.SpecialSummon(sc,SUMMON_TYPE_SYNCHRO,tp,tp,false,false,POS_FACEUP)>0 then
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	local g=Duel.SelectMatchingCard(tp,Card.IsCode,tp,LOCATION_GRAVE+LOCATION_EXTRA,0,1,1,nil,30604579)
+	local sc=g:GetFirst()
+	if sc then
+		Duel.SpecialSummon(sc,SUMMON_TYPE_SYNCHRO,tp,tp,false,false,POS_FACEUP)
 		sc:CompleteProcedure()
 	end
 end
