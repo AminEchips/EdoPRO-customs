06de88542036ff5f0e240c149df1bdf3e674d140
Chips <aminekhoury0411@gmail.com>
2025-07-17 12:01:11 -0400

    Update c1607.lua

--- script/c1607.lua
+++ script/c1607.lua
@@ -77,9 +77,10 @@ end
 
 -- Target for return
 function s.thfilter(c)
-	return c:IsFaceup() and c:IsLevelAbove(5) and c:IsAbleToHand()
+	return (c:IsFaceup() and c:IsLevelAbove(5) and c:IsAbleToHand())
 		or (c:IsLocation(LOCATION_REMOVED) and c:IsFaceup() and c:IsLevelAbove(5) and c:IsAbleToExtra())
 end
+
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_REMOVED) and s.thfilter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(s.thfilter,tp,LOCATION_REMOVED,0,1,nil) end
