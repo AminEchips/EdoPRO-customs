fc9dff6b389b4275edf68fb8d9482eb13db034c2
Chips <aminekhoury0411@gmail.com>
2025-07-17 11:15:21 -0400

    Update c1620.lua

--- script/c1620.lua
+++ script/c1620.lua
@@ -1,35 +1,25 @@
---Loki, Trickster Lord of the Aesir
---Custom script by Amine
+-- Loki, Trickster Lord of the Aesir
 local s,id=GetID()
 function s.initial_effect(c)
-	-- Synchro Summon procedure (1 "Nordic Alfar" Tuner or substitute + 1+ non-Tuners)
-	Synchro.AddProcedure(c,s.tfilter,1,1,Synchro.NonTuner(nil),1,99)
+	-- Synchro Summon
+	Synchro.AddProcedure(c,s.tfilter,1,1,Synchro.NonTuner(nil),2,99)
 	c:EnableReviveLimit()
-
-	-- Must first be Synchro Summoned
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_SINGLE)
-	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e0:SetValue(aux.synlimit)
-	c:RegisterEffect(e0)
-
-	-- On Synchro Summon: Banish any number of "Nordic" in GY → set that many "Nordic Relic" with different names
+	-- Set Nordic Relic on summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_REMOVE+CATEGORY_SEARCH+CATEGORY_TOHAND+CATEGORY_POSITION)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetCondition(s.setcon)
+	e1:SetCost(s.setcost)
 	e1:SetTarget(s.settg)
 	e1:SetOperation(s.setop)
 	c:RegisterEffect(e1)
-
-	-- Negate Spell/Trap on resolution and inflict damage
+	-- Negate spell/trap effect and burn
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_NEGATE+CATEGORY_DAMAGE)
+	e2:SetCategory(CATEGORY_DISABLE+CATEGORY_DAMAGE)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_CHAIN_SOLVING)
 	e2:SetRange(LOCATION_MZONE)
@@ -38,118 +28,102 @@ function s.initial_effect(c)
 	e2:SetTarget(s.negtg)
 	e2:SetOperation(s.negop)
 	c:RegisterEffect(e2)
-
-	-- Float: If removed by opponent, revive "Loki, Lord of the Aesir"
+	-- Float (banish as cost)
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,2))
-	e3:SetCategory(CATEGORY_REMOVE+CATEGORY_SPECIAL_SUMMON)
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e3:SetCode(EVENT_LEAVE_FIELD)
+	e3:SetCode(EVENT_TO_GRAVE)
 	e3:SetProperty(EFFECT_FLAG_DELAY)
 	e3:SetCondition(s.spcon)
+	e3:SetCost(s.spcost)
 	e3:SetTarget(s.sptg)
 	e3:SetOperation(s.spop)
 	c:RegisterEffect(e3)
+	local e4=e3:Clone()
+	e4:SetCode(EVENT_REMOVE)
+	e4:SetCondition(s.spcon2)
+	e4:SetCost(nil)
+	c:RegisterEffect(e4)
 end
-
-s.listed_series={0xa042,0x5042,0x4b} -- Nordic Alfar, Nordic Relic, Aesir
-s.listed_names={67098114} -- Loki, Lord of the Aesir
-
--- Nordic Alfar Tuner (with substitute)
+s.listed_series={0x42,0x4b,0x5042}
 function s.tfilter(c,scard,sumtype,tp)
 	return c:IsSetCard(0xa042,scard,sumtype,tp) or c:IsHasEffect(EFFECT_SYNSUB_NORDIC)
 end
-
--- EFFECT 1: Set "Nordic Relic" by banishing "Nordic" monsters
 function s.setcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO)
 end
-function s.setfilter1(c)
-	return c:IsSetCard(0x42) and c:IsMonster() and c:IsAbleToRemoveAsCost()
+function s.cfilter(c)
+	return c:IsSetCard(0x42) and c:IsAbleToRemoveAsCost()
 end
-function s.setfilter2(c,tp)
-	return c:IsSetCard(0x5042) and c:IsSpellTrap() and c:IsSSetable() and Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+function s.setcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_GRAVE,0,nil)
+	if chk==0 then return #g>0 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local rg=g:SelectSubGroup(tp,aux.dncheck,false,1,g:GetClassCount(Card.GetCode))
+	if #rg==0 then return false end
+	e:SetLabel(#rg)
+	Duel.Remove(rg,POS_FACEUP,REASON_COST)
+end
+function s.setfilter(c)
+	return c:IsSetCard(0x5042) and c:IsSSetable()
 end
 function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local ct=Duel.GetMatchingGroupCount(s.setfilter2,tp,LOCATION_DECK,0,nil,tp)
-	if chk==0 then return ct>0 and Duel.IsExistingMatchingCard(s.setfilter1,tp,LOCATION_GRAVE,0,1,nil) end
+	if chk==0 then return Duel.GetMatchingGroupCount(s.setfilter,tp,LOCATION_DECK,0,nil)>=e:GetLabel() end
 end
 function s.setop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(s.setfilter1,tp,LOCATION_GRAVE,0,nil)
-	if #g==0 then return end
-	local maxct=math.min(#g,Duel.GetMatchingGroupCount(aux.NecroValleyFilter(s.setfilter2),tp,LOCATION_DECK,0,nil,tp))
-	if maxct==0 then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local rg=g:Select(tp,1,maxct,nil)
-	if Duel.Remove(rg,POS_FACEUP,REASON_COST)==0 then return end
-	local ct=#rg
-	local sg=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.setfilter2),tp,LOCATION_DECK,0,nil,tp)
-	local setg=Group.CreateGroup()
-	for i=1,ct do
-		if #sg==0 then break end
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
-		local sc=sg:Select(tp,1,1,nil):GetFirst()
-		sg:Remove(Card.IsCode,nil,sc:GetCode())
-		setg:AddCard(sc)
-	end
-	if #setg>0 then
-		for tc in aux.Next(setg) do
+	local ct=e:GetLabel()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g=Duel.GetMatchingGroup(s.setfilter,tp,LOCATION_DECK,0,nil):SelectSubGroup(tp,aux.dncheck,false,ct,ct)
+	if #g>0 then
+		for tc in aux.Next(g) do
 			Duel.SSet(tp,tc)
-			-- Allow activation this turn
 			local e1=Effect.CreateEffect(e:GetHandler())
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
-			e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
 			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 			tc:RegisterEffect(e1)
-			local e2=e1:Clone()
-			e2:SetCode(EFFECT_QP_ACT_IN_SET_TURN)
-			tc:RegisterEffect(e2)
 		end
 	end
 end
-
--- EFFECT 2: Negate Spell/Trap + burn
 function s.negcon(e,tp,eg,ep,ev,re,r,rp)
-	return re:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.IsChainDisablable(ev)
+	return rp==1-tp and re:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.IsChainDisablable(ev)
 end
 function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,800)
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,0)
 end
 function s.negop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.NegateActivation(ev) then
+	if Duel.NegateEffect(ev) then
 		local ct=Duel.GetFieldGroupCount(tp,LOCATION_SZONE,LOCATION_SZONE)
-		if ct>0 then
-			Duel.Damage(1-tp,ct*800,REASON_EFFECT)
-		end
+		Duel.Damage(1-tp,ct*800,REASON_EFFECT)
 	end
 end
-
--- EFFECT 3: Float to "Loki, Lord of the Aesir"
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return c:IsSummonType(SUMMON_TYPE_SYNCHRO) and rp==1-tp and c:IsPreviousControler(tp)
+	return rp==1-tp and c:IsReason(REASON_EFFECT) and c:IsPreviousLocation(LOCATION_ONFIELD)
+end
+function s.spcon2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsReason(REASON_EFFECT) and c:IsPreviousLocation(LOCATION_ONFIELD)
+end
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
+	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
 end
 function s.spfilter(c,e,tp)
 	return c:IsCode(67098114) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_SYNCHRO,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then
-		return c:IsAbleToRemove() and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE+LOCATION_EXTRA,0,1,nil,e,tp)
-	end
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,c,1,tp,LOCATION_MZONE)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE+LOCATION_EXTRA)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA|LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA|LOCATION_GRAVE)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e) then return end
-	if Duel.Remove(c,POS_FACEUP,REASON_EFFECT)==0 then return end
-	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE+LOCATION_EXTRA,0,1,1,nil,e,tp)
-	if #g>0 then
-		local tc=g:GetFirst()
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_EXTRA|LOCATION_GRAVE,0,1,1,nil,e,tp)
+	local tc=g:GetFirst()
+	if tc then
 		Duel.SpecialSummon(tc,SUMMON_TYPE_SYNCHRO,tp,tp,false,false,POS_FACEUP)
 		tc:CompleteProcedure()
 	end
