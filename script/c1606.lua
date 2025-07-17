ff7217ddb84ebb492a7281e3db987a6db259fc21
Chips <aminekhoury0411@gmail.com>
2025-07-17 11:46:55 -0400

    Update c1606.lua

--- script/c1606.lua
+++ script/c1606.lua
@@ -13,9 +13,9 @@ function s.initial_effect(c)
     e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
 
-    -- If banished: send 1 "Nordic" card from Deck to GY
+    -- e3: If banished → send 1 Nordic or Aesir from Extra Deck to GY
     local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetDescription(aux.Stringid(id,2))
     e2:SetCategory(CATEGORY_TOGRAVE)
     e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e2:SetProperty(EFFECT_FLAG_DELAY)
@@ -65,18 +65,18 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
--- e2: Banish Trigger – Foolish 1 Nordic
+-- e3: If banished → send 1 Nordic or Aesir from Extra Deck to GY
 function s.tgfilter(c)
-    return c:IsSetCard(0x42) and c:IsAbleToGrave()
+    return (c:IsSetCard(0x42) or c:IsSetCard(0x4b))
+        and c:IsType(TYPE_MONSTER) and c:IsAbleToGrave()
 end
 function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_DECK,0,1,nil) end
-    Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_EXTRA,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_EXTRA)
 end
 function s.tgop(e,tp,eg,ep,ev,re,r,rp)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-    local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
+    local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_EXTRA,0,1,1,nil)
     if #g>0 then
         Duel.SendtoGrave(g,REASON_EFFECT)
     end
-end
