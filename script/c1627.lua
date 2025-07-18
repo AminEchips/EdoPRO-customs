80ce18c4585e43b7365c5f9fa763048f595820ed
Chips <aminekhoury0411@gmail.com>
2025-07-18 15:31:59 -0400

    Update c1627.lua

--- script/c1627.lua
+++ script/c1627.lua
@@ -43,16 +43,13 @@ function s.tkcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.tkfilter,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
 	local g=Duel.SelectMatchingCard(tp,s.tkfilter,tp,LOCATION_GRAVE,0,1,1,nil)
-	local tc=g:GetFirst()
-	if tc then
-		e:SetLabel(tc:GetLevel(), tc:GetAttack(), tc:GetDefense())
-		Duel.Remove(tc,POS_FACEUP,REASON_COST)
-	end
+	e:SetLabelObject(g:GetFirst())
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
 function s.tktg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local lv,atk,def = e:GetLabel()
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,1648,0,TYPES_TOKEN,atk,def,lv,RACE_ZOMBIE,ATTRIBUTE_DARK) end
+	local tc=e:GetLabelObject()
+	if chk==0 then return tc and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
+			and Duel.IsPlayerCanSpecialSummonMonster(tp,1648,0,TYPES_TOKEN|TYPE_TUNER,math.max(0,tc:GetTextAttack()),math.max(0,tc:GetTextDefense()),tc:GetOriginalLevel(),tc:GetOriginalRace(),tc:GetOriginalAttribute()) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0)
 end
 function s.tkop(e,tp,eg,ep,ev,re,r,rp)
@@ -60,33 +57,21 @@ function s.tkop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=e:GetLabelObject()
 	if not tc then return end
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	if not Duel.IsPlayerCanSpecialSummonMonster(tp,1648,0,TYPES_TOKEN|TYPE_TUNER,tc:GetAttack(),tc:GetDefense(),tc:GetLevel(),RACE_ZOMBIE,ATTRIBUTE_DARK) then return end
+	if not Duel.IsPlayerCanSpecialSummonMonster(tp,1648,0,TYPES_TOKEN|TYPE_TUNER,math.max(0,tc:GetTextAttack()),math.max(0,tc:GetTextDefense()),tc:GetOriginalLevel(),tc:GetOriginalRace(),tc:GetOriginalAttribute()) then return end
 	local token=Duel.CreateToken(tp,1648)
-	-- Set token's stats
+	token:Attack(math.max(0,tc:GetTextAttack()))
+	token:Defense(math.max(0,tc:GetTextDefense()))
+	token:Level(tc:GetOriginalLevel())
+	token:Race(tc:GetOriginalRace())
+	token:Attribute(tc:GetOriginalAttribute())
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_CHANGE_LEVEL)
-	e1:SetValue(tc:GetLevel())
+	e1:SetCode(EFFECT_SYNSUB_NORDIC)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 	token:RegisterEffect(e1,true)
-	local e2=e1:Clone()
-	e2:SetCode(EFFECT_SET_BASE_ATTACK)
-	e2:SetValue(tc:GetAttack())
-	token:RegisterEffect(e2,true)
-	local e3=e1:Clone()
-	e3:SetCode(EFFECT_SET_BASE_DEFENSE)
-	e3:SetValue(tc:GetDefense())
-	token:RegisterEffect(e3,true)
-	-- Nordic Tuner substitute
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_SINGLE)
-	e4:SetCode(EFFECT_SYNSUB_NORDIC)
-	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e4:SetReset(RESET_EVENT+RESETS_STANDARD)
-	token:RegisterEffect(e4,true)
 	Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)
 end
-
 
 -- 2nd Effect helpers
 function s.revcost(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -98,15 +83,15 @@ function s.revfilter(c,e,tp)
 end
 function s.revtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_REMOVED) and s.revfilter(chkc,e,tp) end
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(s.revfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.revfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectTarget(tp,s.revfilter,tp,LOCATION_REMOVED,0,1,1,nil,e,tp)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,tp,LOCATION_REMOVED)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
 function s.revop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) then
-		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	if tc and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)>0 then
+		-- No additional effect
 	end
 end
