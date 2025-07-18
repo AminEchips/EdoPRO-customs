7c70265472bac60ba9b5fff3f6093f03095bbd86
Chips <aminekhoury0411@gmail.com>
2025-07-18 15:11:39 -0400

    Create c1627.lua

---
+++ script/c1627.lua
@@ -0,0 +1,93 @@
+--Niflheim, Peak of Mist
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e0)
+
+	--1st Effect: Banish 1 "Nordic" or "Aesir" from GY, summon token
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_FZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetCost(s.tkcost)
+	e1:SetTarget(s.tktg)
+	e1:SetOperation(s.tkop)
+	c:RegisterEffect(e1)
+
+	--2nd Effect: If any monster is banished, pay 1000 LP, target and summon Aesir
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_REMOVE)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCost(s.revcost)
+	e2:SetTarget(s.revtg)
+	e2:SetOperation(s.revop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={0x42,0x4b}
+
+-- 1st Effect helpers
+function s.tkfilter(c)
+	return c:IsMonster() and (c:IsSetCard(0x42) or c:IsSetCard(0x4b)) and c:IsAbleToRemoveAsCost()
+end
+function s.tkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tkfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,s.tkfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	e:SetLabelObject(g:GetFirst())
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+function s.tktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,1648,0,TYPES_TOKEN,0,0,e:GetLabelObject():GetLevel(),RACE_ZOMBIE,ATTRIBUTE_DARK) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0)
+end
+function s.tkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=e:GetLabelObject()
+	if not tc then return end
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	if not Duel.IsPlayerCanSpecialSummonMonster(tp,1648,0,TYPES_TOKEN,tc:GetAttack(),tc:GetDefense(),tc:GetLevel(),RACE_ZOMBIE,ATTRIBUTE_DARK) then return end
+	local token=Duel.CreateToken(tp,1648)
+	if Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)>0 then
+		-- Substitute for Nordic Tuner
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SYNSUB_NORDIC)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		token:RegisterEffect(e1,true)
+	end
+end
+
+-- 2nd Effect helpers
+function s.revcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,1000) end
+	Duel.PayLPCost(tp,1000)
+end
+function s.revfilter(c,e,tp)
+	return c:IsSetCard(0x4b) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.revtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_REMOVED) and s.revfilter(chkc,e,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.revfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.revfilter,tp,LOCATION_REMOVED,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function s.revop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)>0 then
+		-- No additional effect
+	end
+end
