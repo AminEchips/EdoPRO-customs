2a0c3dd692c5cc47b7563392191df0d559724992
Chips <aminekhoury0411@gmail.com>
2025-07-18 14:49:59 -0400

    Update c1625.lua

--- script/c1625.lua
+++ script/c1625.lua
@@ -1,5 +1,4 @@
---Midgard, Home of the Mortals
---Scripted by Meuh
+""--Midgard, Home of the Mortals
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -7,19 +6,18 @@ function s.initial_effect(c)
 	e0:SetType(EFFECT_TYPE_ACTIVATE)
 	e0:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e0)
-
-	--Ignition shuffle and draw (usable by each player on their own turn)
+	--1st Effect: Turn player can shuffle 1 monster into Deck, draw 1
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_FZONE)
-	e1:SetCountLimit(1,{id,0})
-	e1:SetTarget(s.drtg)
-	e1:SetOperation(s.drop)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.shufflecon)
+	e1:SetTarget(s.shuffletg)
+	e1:SetOperation(s.shuffleop)
 	c:RegisterEffect(e1)
-
-	--Trigger on non-Synchro "Nordic" leaving field due to opponent's effect
+	--2nd Effect: If non-Synchro "Nordic" leaves field by battle or opponent effect
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -27,66 +25,64 @@ function s.initial_effect(c)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_LEAVE_FIELD)
 	e2:SetRange(LOCATION_FZONE)
-	e2:SetCountLimit(1,{id,1})
+	e2:SetCountLimit(1,id)
 	e2:SetCondition(s.spcon)
 	e2:SetTarget(s.sptg)
 	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
 end
 s.listed_series={0x42}
-
-function s.dfilter(c)
-	return c:IsMonster() and c:IsAbleToDeck() and c:IsLocation(LOCATION_MZONE)
+-- 1st Effect: Shuffle into Deck and draw
+function s.shufflecon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp
 end
-function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.dfilter,tp,LOCATION_MZONE,0,1,nil) end
+function s.shufffilter(c)
+	return c:IsAbleToDeck() and c:IsMonster() and c:IsLocation(LOCATION_MZONE)
+end
+function s.shuffletg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.shufffilter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_MZONE)
-	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,1,tp,1)
 end
-function s.drop(e,tp,eg,ep,ev,re,r,rp)
+function s.shuffleop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local g=Duel.SelectMatchingCard(tp,s.dfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.shufffilter,tp,LOCATION_MZONE,0,1,1,nil)
 	if #g>0 and Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)>0 then
-		Duel.BreakEffect()
 		Duel.Draw(tp,1,REASON_EFFECT)
 	end
 end
 
+-- 2nd Effect: Special Summon when non-Synchro "Nordic" leaves field
 function s.cfilter(c,tp)
-	return c:IsPreviousControler(tp) and c:IsSetCard(0x42) and not c:IsType(TYPE_SYNCHRO)
-		and c:IsReason(REASON_EFFECT) and rp~=tp
+	return c:IsPreviousControler(tp) and c:IsPreviousSetCard(0x42) and not c:IsPreviousType(TYPE_SYNCHRO)
+		and c:IsPreviousLocation(LOCATION_MZONE)
+		and (c:IsReason(REASON_EFFECT) and c:GetReasonPlayer()==1-tp or c:IsReason(REASON_BATTLE))
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(s.cfilter,1,nil,tp)
 end
-
-function s.spfilter(c,e,tp,code)
-	return c:IsSetCard(0x42) and not c:IsCode(code)
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(0x42) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and (not c:IsLocation(LOCATION_EXTRA) or Duel.GetLocationCountFromEx(tp)>0)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=eg:Filter(s.cfilter,nil,tp)
-	if chk==0 then
-		return g:GetFirst() and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK+LOCATION_EXTRA,0,1,nil,e,tp,g:GetFirst():GetCode())
-	end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK+LOCATION_EXTRA,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_EXTRA)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	local code=eg:Filter(s.cfilter,nil,tp):GetFirst():GetCode()
-	if not code then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK+LOCATION_EXTRA,0,1,1,nil,e,tp,code)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK+LOCATION_EXTRA,0,1,1,nil,e,tp)
 	local tc=g:GetFirst()
-	if tc and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)>0
-		and tc:IsLocation(LOCATION_MZONE) and tc:IsSummonLocation(LOCATION_EXTRA) then
-		-- Negate its effects if from Extra Deck
+	if tc and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)>0 and tc:IsLocation(LOCATION_MZONE) and tc:IsLocation(LOCATION_EXTRA) then
+		-- Negate its effects this turn
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_DISABLE)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e1)
-		local e2=e1:Clone()
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetType(EFFECT_TYPE_SINGLE)
 		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e2)
 	end
 end
