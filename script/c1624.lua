eb2abf4f8d7a223b68a6a3d6f94b8d574732f07f
Chips <aminekhoury0411@gmail.com>
2025-07-18 00:06:30 -0400

    Update c1624.lua

--- script/c1624.lua
+++ script/c1624.lua
@@ -7,7 +7,7 @@ function s.initial_effect(c)
 	e0:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e0)
 
-	--Effect 1: Trigger when Aesir is Special Summoned from GY
+	--Effect 1: Trigger when Aesir is Special Summoned from GY → apply continuous battle effect
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(0)
@@ -16,11 +16,10 @@ function s.initial_effect(c)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetRange(LOCATION_FZONE)
 	e1:SetCondition(s.trigcon)
-	e1:SetTarget(s.trigtg)
 	e1:SetOperation(s.trigop)
 	c:RegisterEffect(e1)
 
-	--Effect 2: On leaving field, summon Tuners from hand and Synchro Summon
+	--Effect 2: On leave field, summon Tuners from hand and allow repeated Synchro Summons
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EVENT_LEAVE_FIELD)
@@ -29,29 +28,26 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 
---===== EFFECT 1: Trigger if Aesir is revived from GY =====--
+--=== Effect 1: Trigger on Aesir revived ===--
 function s.trigfilter(c,tp)
 	return c:IsSetCard(0x4b) and c:IsPreviousLocation(LOCATION_GRAVE) and c:GetSummonPlayer()==tp
 end
 function s.trigcon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(s.trigfilter,1,nil,tp)
 end
-function s.trigtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	return true
-end
 function s.trigop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local e1=Effect.CreateEffect(c)
+	-- Register a temporary continuous effect
+	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
-	e1:SetLabel(Duel.GetTurnCount())
 	e1:SetCondition(function(e,tp,eg,ep,ev,re,r,rp)
 		local a=Duel.GetAttacker()
-		return a:IsSetCard(0x4b) and a:IsControler(tp)
+		return a and a:IsControler(tp) and a:IsSetCard(0x4b)
 	end)
 	e1:SetOperation(function(e,tp,eg,ep,ev,re,r,rp)
 		local at=Duel.GetAttackTarget()
-		if at and at:IsAttackPos() and at:IsRelateToBattle() then
+		if at and at:IsRelateToBattle() and at:IsAttackPos() then
+			Duel.Hint(HINT_CARD,0,id)
 			Duel.ChangePosition(at,POS_FACEUP_DEFENSE)
 		end
 	end)
@@ -59,7 +55,7 @@ function s.trigop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.RegisterEffect(e1,tp)
 end
 
---===== EFFECT 2: Summon Tuners from hand + repeat Synchro =====--
+--=== Effect 2: Summon Tuners and loop Synchro ===--
 function s.leaveop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 
@@ -69,6 +65,7 @@ function s.leaveop(e,tp,eg,ep,ev,re,r,rp)
 	if #g==0 then return end
 	Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 
+	-- Allow repeat Synchro
 	while true do
 		local mg=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
 		local syncs=Duel.GetMatchingGroup(Card.IsSynchroSummonable,tp,LOCATION_EXTRA,0,nil,nil,mg)
