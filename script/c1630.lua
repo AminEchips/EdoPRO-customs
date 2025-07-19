21d259f0165d91fc7ef5af1082b5596963ebfecd
Chips <aminekhoury0411@gmail.com>
2025-07-19 18:29:51 -0400

    Update c1630.lua

--- script/c1630.lua
+++ script/c1630.lua
@@ -2,55 +2,51 @@
 --Scripted by Meuh
 local s,id=GetID()
 function s.initial_effect(c)
-	
-	--Activate
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_ACTIVATE)
-	e0:SetCode(EVENT_FREE_CHAIN)
-	c:RegisterEffect(e0)
-	
 	--End Phase: Tribute or take damage
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e1:SetCode(EVENT_PHASE+PHASE_END)
-	e1:SetProperty(EFFECT_FLAG_BOTH_SIDE)
 	e1:SetRange(LOCATION_SZONE)
 	e1:SetCountLimit(1)
 	e1:SetOperation(s.endop)
 	c:RegisterEffect(e1)
 
-	--If destroyed by a monster effect
+	--Store phase when about to leave field
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetCategory(CATEGORY_CONTROL)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_DESTROYED)
-	e2:SetCondition(s.ctrlcon)
-	e2:SetOperation(s.ctrlop)
+	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e2:SetCode(EVENT_LEAVE_FIELD_P)
+	e2:SetOperation(function(e,tp,eg,ep,ev,re,r,rp) e:SetLabel(Duel.GetCurrentPhase()) end)
 	c:RegisterEffect(e2)
+
+	--If destroyed by monster effect
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_CONTROL)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_DESTROYED)
+	e3:SetCondition(s.ctrlcon)
+	e3:SetOperation(s.ctrlop)
+	c:RegisterEffect(e3)
 end
 
--- END PHASE EFFECT
-
--- Filter monsters that declared attack this turn and are still valid
+--Filter monsters that attacked this turn
 function s.attackedfilter(c)
 	return c:GetAttackAnnouncedCount()>0 and c:IsAttackable() and c:IsReleasable()
 end
 
+--End Phase: Tribute or take damage
 function s.endop(e,tp,eg,ep,ev,re,r,rp)
 	local p=Duel.GetTurnPlayer()
 	local g=Duel.GetMatchingGroup(s.attackedfilter,p,LOCATION_MZONE,0,nil)
 	if #g==0 then return end
 
-	-- Get highest ATK value
 	local maxatk=0
 	for tc in g:Iter() do
 		if tc:GetAttack()>maxatk then maxatk=tc:GetAttack() end
 	end
 	local highest=g:Filter(Card.IsAttackAbove,nil,maxatk)
-	Duel.Hint(HINT_MESSAGE,p,aux.Stringid(id,1)) -- Prompt player
+	Duel.Hint(HINT_MESSAGE,p,aux.Stringid(id,1))
 	if Duel.SelectYesNo(p,aux.Stringid(id,2)) then
-		-- Choose 1 monster among highest ATK attackers
 		Duel.Hint(HINT_SELECTMSG,p,HINTMSG_RELEASE)
 		local sg=highest:Select(p,1,1,nil)
 		if #sg>0 then
@@ -58,19 +54,18 @@ function s.endop(e,tp,eg,ep,ev,re,r,rp)
 			return
 		end
 	end
-	-- Didn't tribute → take damage
 	local dmgmon=highest:GetFirst()
 	if dmgmon then
 		Duel.Damage(p,dmgmon:GetAttack(),REASON_EFFECT)
 	end
 end
 
--- DESTRUCTION CONDITION
+--Check if destroyed by monster effect
 function s.ctrlcon(e,tp,eg,ep,ev,re,r,rp)
 	return r&REASON_EFFECT~=0 and re and re:IsActivated() and re:GetHandler():IsMonster()
 end
 
--- CONTROL MONSTER ON FLOAT
+--Take control on destruction
 function s.ctrlop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	local g=Duel.GetMatchingGroup(Card.IsControlerCanBeChanged,tp,0,LOCATION_MZONE,nil)
@@ -78,8 +73,9 @@ function s.ctrlop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
 	local tc=g:Select(tp,1,1,nil):GetFirst()
 	if tc and Duel.GetControl(tc,tp)~=0 then
-		if Duel.GetCurrentPhase()~=PHASE_BATTLE then
-			-- Apply restrictions only if not destroyed during BP
+		-- Only apply restrictions if this wasn't destroyed during Battle Phase
+		local wasBattlePhase = e:GetLabel()==PHASE_BATTLE
+		if not wasBattlePhase then
 			local e1=Effect.CreateEffect(e:GetHandler())
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_DISABLE)
