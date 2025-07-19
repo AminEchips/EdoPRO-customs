7fb18f33cc729d178714bac4593b1e5861b526ab
Chips <aminekhoury0411@gmail.com>
2025-07-19 13:33:41 -0400

    Update c1630.lua

--- script/c1630.lua
+++ script/c1630.lua
@@ -2,6 +2,12 @@
 --Scripted by Meuh
 local s,id=GetID()
 function s.initial_effect(c)
+	--Activate
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e0)
+	
 	--End Phase effect
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
