3264b780ebaf21ef3bfb930986e5100775bd6953
Chips <aminekhoury0411@gmail.com>
2025-07-19 18:06:45 -0400

    Update c1630.lua

--- script/c1630.lua
+++ script/c1630.lua
@@ -2,13 +2,7 @@
 --Scripted by Meuh
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_ACTIVATE)
-	e0:SetCode(EVENT_FREE_CHAIN)
-	c:RegisterEffect(e0)
-	
-	--End Phase effect
+	--End Phase: Tribute or take damage
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e1:SetCode(EVENT_PHASE+PHASE_END)
@@ -17,7 +11,7 @@ function s.initial_effect(c)
 	e1:SetOperation(s.endop)
 	c:RegisterEffect(e1)
 
-	--If destroyed by monster effect
+	--If destroyed by a monster effect
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_CONTROL)
@@ -28,37 +22,47 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 
---Effect 1: End Phase tribute or damage
+-- END PHASE EFFECT
+
+-- Filter monsters that declared attack this turn and are still valid
 function s.attackedfilter(c)
-	return c:GetAttackAnnouncedCount()>0
+	return c:GetAttackAnnouncedCount()>0 and c:IsAttackable() and c:IsReleasable()
 end
+
 function s.endop(e,tp,eg,ep,ev,re,r,rp)
 	local p=Duel.GetTurnPlayer()
 	local g=Duel.GetMatchingGroup(s.attackedfilter,p,LOCATION_MZONE,0,nil)
 	if #g==0 then return end
-	--Find highest ATK
-	local maxatk=g:GetFirst():GetAttack()
+
+	-- Get highest ATK value
+	local maxatk=0
 	for tc in g:Iter() do
 		if tc:GetAttack()>maxatk then maxatk=tc:GetAttack() end
 	end
 	local highest=g:Filter(Card.IsAttackAbove,nil,maxatk)
-	if #highest>1 then
+	Duel.Hint(HINT_MESSAGE,p,aux.Stringid(id,1)) -- Prompt player
+	if Duel.SelectYesNo(p,aux.Stringid(id,2)) then
+		-- Choose 1 monster among highest ATK attackers
 		Duel.Hint(HINT_SELECTMSG,p,HINTMSG_RELEASE)
-		highest=highest:Select(p,1,1,nil)
+		local sg=highest:Select(p,1,1,nil)
+		if #sg>0 then
+			Duel.Release(sg,REASON_EFFECT)
+			return
+		end
 	end
-	local tc=highest:GetFirst()
-	if tc then
-		if Duel.Release(tc,REASON_EFFECT)>0 then return end
-		-- If tribute failed, inflict damage
-		Duel.Damage(p,tc:GetAttack(),REASON_EFFECT)
+	-- Didn't tribute → take damage
+	local dmgmon=highest:GetFirst()
+	if dmgmon then
+		Duel.Damage(p,dmgmon:GetAttack(),REASON_EFFECT)
 	end
 end
 
---Effect 2: Control 1 opponent's monster
+-- DESTRUCTION CONDITION
 function s.ctrlcon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
 	return r&REASON_EFFECT~=0 and re and re:IsActivated() and re:GetHandler():IsMonster()
 end
+
+-- CONTROL MONSTER ON FLOAT
 function s.ctrlop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	local g=Duel.GetMatchingGroup(Card.IsControlerCanBeChanged,tp,0,LOCATION_MZONE,nil)
@@ -66,22 +70,21 @@ function s.ctrlop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
 	local tc=g:Select(tp,1,1,nil):GetFirst()
 	if tc and Duel.GetControl(tc,tp)~=0 then
-		-- Apply restrictions
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_DISABLE)
-		e1:SetCondition(function(e) return not Duel.GetCurrentPhase()~=PHASE_BATTLE end)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		tc:RegisterEffect(e1)
-		local e2=e1:Clone()
-		e2:SetCode(EFFECT_DISABLE_EFFECT)
-		e2:SetCondition(function(e) return not Duel.GetCurrentPhase()~=PHASE_BATTLE end)
-		tc:RegisterEffect(e2)
-		local e3=Effect.CreateEffect(e:GetHandler())
-		e3:SetType(EFFECT_TYPE_SINGLE)
-		e3:SetCode(EFFECT_CANNOT_ATTACK)
-		e3:SetCondition(function(e) return not Duel.GetCurrentPhase()~=PHASE_BATTLE end)
-		e3:SetReset(RESET_EVENT+RESETS_STANDARD)
-		tc:RegisterEffect(e3)
+		if Duel.GetCurrentPhase()~=PHASE_BATTLE then
+			-- Apply restrictions only if not destroyed during BP
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_DISABLE)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e1)
+			local e2=e1:Clone()
+			e2:SetCode(EFFECT_DISABLE_EFFECT)
+			tc:RegisterEffect(e2)
+			local e3=Effect.CreateEffect(e:GetHandler())
+			e3:SetType(EFFECT_TYPE_SINGLE)
+			e3:SetCode(EFFECT_CANNOT_ATTACK)
+			e3:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e3)
+		end
 	end
 end
