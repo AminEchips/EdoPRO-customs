03e38381dde98350e4ea72c15d0ad9f369d677e7
Chips <aminekhoury0411@gmail.com>
2025-07-20 19:06:24 -0400

    Update c1631.lua

--- script/c1631.lua
+++ script/c1631.lua
@@ -2,16 +2,15 @@
 --Scripted by Meuh
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate this card
+	--Activate
 	local e0=Effect.CreateEffect(c)
 	e0:SetType(EFFECT_TYPE_ACTIVATE)
 	e0:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e0)
 
-	--Ignition effect: choose 1 of 2 options
+	--Choose 1 of 2 effects
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_SZONE)
 	e1:SetCountLimit(1,id)
@@ -20,7 +19,6 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 
---Check options
 function s.spfilter(c,e,tp)
 	return c:IsSetCard(0xa042) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 		and (c:IsLocation(LOCATION_HAND) or c:IsFaceup())
@@ -32,45 +30,46 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local b1=Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_REMOVED,0,1,nil,e,tp)
 	local b2=Duel.IsExistingMatchingCard(s.chfilter,tp,LOCATION_MZONE,0,1,nil)
 	if chk==0 then return b1 or b2 end
+
 	local op=0
 	if b1 and b2 then
 		op=Duel.SelectOption(tp,aux.Stringid(id,1),aux.Stringid(id,2))
 	elseif b1 then
-		op=Duel.SelectOption(tp,aux.Stringid(id,1))
+		op=0
+		Duel.SelectOption(tp,aux.Stringid(id,1))
 	else
-		op=Duel.SelectOption(tp,aux.Stringid(id,2))+1
+		op=1
+		Duel.SelectOption(tp,aux.Stringid(id,2))
 	end
 	e:SetLabel(op)
 	if op==0 then
-		e:SetCategory(CATEGORY_SPECIAL_SUMMON)
 		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_REMOVED)
-	else
-		e:SetCategory(0)
 	end
 end
 
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local op=e:GetLabel()
 	if op==0 then
-		-- Special Summon "Nordic Alfar"
+		-- Special Summon 1 "Nordic Alfar" from hand or banished
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND+LOCATION_REMOVED,0,1,1,nil,e,tp)
 		if #g>0 then
 			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 		end
 	else
-		-- Transform "Nordic Ascendant"
+		-- 1 "Nordic Ascendant" becomes "Nordic Alfar" and DARK
 		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,2))
 		local tc=Duel.SelectMatchingCard(tp,s.chfilter,tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
 		if tc and tc:IsFaceup() and tc:IsRelateToEffect(e) then
-			-- Gain "Nordic Alfar" archetype
+			-- Add "Nordic Alfar" archetype
 			local e1=Effect.CreateEffect(e:GetHandler())
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_ADD_SETCODE)
 			e1:SetValue(0xa042)
 			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 			tc:RegisterEffect(e1)
-			-- Become DARK
+
+			-- Change Attribute to DARK
 			local e2=Effect.CreateEffect(e:GetHandler())
 			e2:SetType(EFFECT_TYPE_SINGLE)
 			e2:SetCode(EFFECT_CHANGE_ATTRIBUTE)
