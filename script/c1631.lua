49f31bff024e0f8fca8af284e0246f67c0ea7abd
Chips <aminekhoury0411@gmail.com>
2025-07-20 19:13:10 -0400

    Update c1631.lua

--- script/c1631.lua
+++ script/c1631.lua
@@ -1,85 +1,96 @@
---Svartalfheim, Home of the Dark Elves
+--Jotunheim, Home of the Frost Giants
 --Scripted by Meuh
 local s,id=GetID()
 function s.initial_effect(c)
-	-- Activate
+	--Activate as Field Spell
 	local e0=Effect.CreateEffect(c)
 	e0:SetType(EFFECT_TYPE_ACTIVATE)
 	e0:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e0)
 
-	-- Ignition: Choose 1 of 2 effects
+	--Main effect (once per turn)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
 	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetRange(LOCATION_SZONE)
+	e1:SetRange(LOCATION_FZONE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCountLimit(1,id)
 	e1:SetTarget(s.target)
-	e1:SetOperation(s.operation)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
 
--- Filters
-function s.spfilter(c,e,tp)
-	return c:IsSetCard(0xa042) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) 
-		and (c:IsLocation(LOCATION_HAND) or (c:IsFaceup() and c:IsLocation(LOCATION_REMOVED)))
-end
-function s.ascfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0x3042)
+-- Filter: Field Spells in GY
+function s.fieldspellfilter(c)
+	return c:IsType(TYPE_FIELD) and c:IsAbleToHand()
 end
 
--- Target
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local b1=Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_REMOVED,0,1,nil,e,tp)
-	local b2=Duel.IsExistingMatchingCard(s.ascfilter,tp,LOCATION_MZONE,0,1,nil)
-	if chk==0 then return b1 or b2 end
-	local op=0
-	if b1 and b2 then
-		op=Duel.SelectOption(tp,aux.Stringid(id,1),aux.Stringid(id,2))
-	elseif b1 then
-		op=Duel.SelectOption(tp,aux.Stringid(id,1))
-	else
-		op=Duel.SelectOption(tp,aux.Stringid(id,2))+1
-	end
-	e:SetLabel(op)
-	if op==0 then
-		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_REMOVED)
-	end
+-- Filter: Valid monsters (no 0 ATK/DEF, face-up)
+function s.validmonster(c,tp)
+	return c:IsFaceup() and c:GetAttack()>0 and c:GetDefense()>0
 end
 
--- Operation
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local op=e:GetLabel()
-	if op==0 then
-		-- Special Summon "Nordic Alfar"
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND+LOCATION_REMOVED,0,1,1,nil,e,tp)
-		if #g>0 then
-			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	local g=Duel.GetMatchingGroup(s.validmonster,tp,LOCATION_MZONE,LOCATION_MZONE,nil,tp)
+	if chk==0 then
+		return Duel.IsExistingMatchingCard(s.fieldspellfilter,tp,LOCATION_GRAVE,0,1,nil)
+			and g:GetCount()>=2
+	end
+
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local tg=Duel.SelectMatchingCard(tp,s.fieldspellfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,tg,1,tp,LOCATION_GRAVE)
+
+	-- Select up to 5 monsters including at least 2 from your field
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local sg=g:Select(tp,2,5,nil)
+	if sg:FilterCount(Card.IsControler,nil,tp)<2 then return end
+	Duel.SetTargetCard(sg)
+end
+
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+
+	-- Add Field Spell from GY
+	local g=Duel.GetMatchingGroup(s.fieldspellfilter,tp,LOCATION_GRAVE,0,nil)
+	if #g>=1 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local tg=g:Select(tp,1,1,nil)
+		if #tg>0 then
+			Duel.SendtoHand(tg,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,tg)
 		end
-	else
-		-- Modify "Nordic Ascendant"
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-		local tc=Duel.SelectMatchingCard(tp,s.ascfilter,tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
-		if not tc or not tc:IsRelateToEffect(e) or not tc:IsFaceup() then return end
+	end
 
-		-- Add Nordic Alfar archetype
-		local e1=Effect.CreateEffect(e:GetHandler())
+	-- Get targeted monsters
+	local tg=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	if not tg or #tg<2 or tg:FilterCount(Card.IsControler,nil,tp)<2 then return end
+	tg=tg:Filter(Card.IsRelateToEffect,nil,e)
+
+	local sum_atk,sum_def,count=0,0,#tg
+	for tc in tg:Iter() do
+		sum_atk = sum_atk + tc:GetAttack()
+		sum_def = sum_def + tc:GetDefense()
+	end
+	if count==0 then return end
+
+	local avg_atk=math.floor(sum_atk/count)
+	local avg_def=math.floor(sum_def/count)
+
+	for tc in tg:Iter() do
+		-- Set ATK
+		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_ADD_SETCODE)
-		e1:SetValue(0xa042)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetValue(avg_atk)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e1)
-
-		-- Become DARK Attribute
-		local e2=Effect.CreateEffect(e:GetHandler())
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_CHANGE_ATTRIBUTE)
-		e2:SetValue(ATTRIBUTE_DARK)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		-- Set DEF
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_SET_DEFENSE_FINAL)
+		e2:SetValue(avg_def)
 		tc:RegisterEffect(e2)
-
-		-- Optional: for debug visual
-		tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
 	end
 end
