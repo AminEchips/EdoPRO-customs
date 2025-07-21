a26619803c11c6d13c098d1cde83e3ec476e2975
Chips <aminekhoury0411@gmail.com>
2025-07-21 15:13:53 -0400

    Update c1635.lua

--- script/c1635.lua
+++ script/c1635.lua
@@ -1,14 +1,15 @@
 --Nordic Relic Guanjefear
---Scripted by Meuh
+--Scripted by Meuh, corrected with Kuibelt logic
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate, select 1 "Aesir" monster at resolution
+	--Activate: select 1 "Aesir" monster at resolution
 	local e0=Effect.CreateEffect(c)
 	e0:SetType(EFFECT_TYPE_ACTIVATE)
 	e0:SetCode(EVENT_FREE_CHAIN)
 	e0:SetOperation(s.actop)
 	c:RegisterEffect(e0)
-	--Selected monster cannot be destroyed by battle
+
+	--The selected monster cannot be destroyed by battle
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
@@ -17,7 +18,8 @@ function s.initial_effect(c)
 	e1:SetTarget(s.battletg)
 	e1:SetValue(1)
 	c:RegisterEffect(e1)
-	--Face-up "Nordic Relic" cards cannot be destroyed by effects
+
+	--While you control that monster: "Nordic Relic" cards cannot be destroyed
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
@@ -26,6 +28,7 @@ function s.initial_effect(c)
 	e2:SetTarget(s.relictg)
 	e2:SetValue(1)
 	c:RegisterEffect(e2)
+
 	--You take no battle damage from battles involving "Nordic" monsters
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD)
@@ -35,7 +38,8 @@ function s.initial_effect(c)
 	e3:SetTarget(s.nordictg)
 	e3:SetValue(1)
 	c:RegisterEffect(e3)
-	--If opponent's monster attacks your Aesir or Nordic monster, it cannot attack next turn
+
+	--Opponent's monster cannot attack during their next turn
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e4:SetCode(EVENT_ATTACK_ANNOUNCE)
@@ -43,19 +47,20 @@ function s.initial_effect(c)
 	e4:SetCondition(s.atkcon)
 	e4:SetOperation(s.atkop)
 	c:RegisterEffect(e4)
-	--Draw 1 if your "Aesir" monster attacks (once per turn)
+
+	--Draw 1 card if your "Aesir" monster attacks (once per turn)
 	local e5=Effect.CreateEffect(c)
 	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e5:SetCode(EVENT_ATTACK_ANNOUNCE)
 	e5:SetRange(LOCATION_SZONE)
-	e5:SetCountLimit(1,{id,1})
+	e5:SetCountLimit(1,{id,1}) -- Only this effect is once per turn
 	e5:SetCondition(s.drcon)
 	e5:SetTarget(s.drtg)
 	e5:SetOperation(s.drop)
 	c:RegisterEffect(e5)
 end
 
--- At resolution, select 1 Aesir monster to grant protection
+-- Choose 1 "Aesir" monster at resolution
 function s.aesirfilter(c)
 	return c:IsFaceup() and c:IsSetCard(0x4b)
 end
@@ -69,22 +74,22 @@ function s.actop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- Effect 1: Protection applies to selected monster
+-- Targeted "Aesir" monster gains battle protection
 function s.battletg(e,c)
 	return e:GetHandler():IsHasCardTarget(c)
 end
 
--- Effect 2: Face-up "Nordic Relic" cards cannot be destroyed
+-- Protect face-up "Nordic Relic" cards
 function s.relictg(e,c)
 	return c:IsFaceup() and c:IsSetCard(0x5042)
 end
 
--- Effect 3: No battle damage from battles involving "Nordic" monsters
+-- No battle damage from battles involving "Nordic" monsters
 function s.nordictg(e,c)
 	return c:IsSetCard(0x42)
 end
 
--- Effect 4: Opponent’s monster cannot attack next turn
+-- Opponent's monster cannot attack during their next turn
 function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
 	local d=Duel.GetAttackTarget()
 	return d and d:IsControler(tp) and d:IsFaceup() and (d:IsSetCard(0x42) or d:IsSetCard(0x4b))
@@ -92,22 +97,16 @@ end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local a=Duel.GetAttacker()
 	if not a:IsRelateToBattle() or not a:IsFaceup() then return end
-	local c=e:GetHandler()
-	
-	-- Store turn ID to check later
-	local turn_id=Duel.GetTurnCount()
-	local e1=Effect.CreateEffect(c)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(3206) -- Optional hint for the client
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_CANNOT_ATTACK)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_IGNORE_IMMUNE)
-	e1:SetCondition(function(e)
-		return Duel.GetTurnCount()~=turn_id
-	end)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_OPPO_TURN)
+	e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+	e1:SetReset(RESETS_STANDARD_PHASE_END, Duel.IsTurnPlayer(tp) and 2 or 1)
 	a:RegisterEffect(e1)
 end
 
--- Effect 5: Draw if your Aesir monster attacks
+-- Draw 1 card if your "Aesir" monster attacks
 function s.drcon(e,tp,eg,ep,ev,re,r,rp)
 	local a=Duel.GetAttacker()
 	return a:IsControler(tp) and a:IsSetCard(0x4b)
