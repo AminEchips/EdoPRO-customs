77db77f708f4666cc3a7431d77fb4b28a8e4bd1c
Chips <aminekhoury0411@gmail.com>
2025-07-21 21:13:00 -0400

    Update c1636.lua

--- script/c1636.lua
+++ script/c1636.lua
@@ -1,42 +1,44 @@
 --Nordic Relic Mjollnir
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate: Equip to 1 "Aesir" monster
+	--Activate and equip
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_EQUIP)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCost(aux.RemainFieldCost)
 	e1:SetTarget(s.eqtg)
 	e1:SetOperation(s.eqop)
 	c:RegisterEffect(e1)
 
-	--Equip limit: only to "Aesir" monster
+	--Equip Limit: only to "Aesir"
 	local e1x=Effect.CreateEffect(c)
 	e1x:SetType(EFFECT_TYPE_SINGLE)
 	e1x:SetCode(EFFECT_EQUIP_LIMIT)
 	e1x:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e1x:SetValue(function(e,c) return c:IsSetCard(0x4b) end)
+	e1x:SetReset(RESET_EVENT|RESETS_STANDARD)
 	c:RegisterEffect(e1x)
 
-	--This card cannot be destroyed by card effects
+	--Equipped monster cannot be targeted by opponent
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e2:SetRange(LOCATION_SZONE)
-	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-	e2:SetValue(1)
+	e2:SetType(EFFECT_TYPE_EQUIP)
+	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e2:SetValue(aux.tgoval)
 	c:RegisterEffect(e2)
 
-	--Equipped monster cannot be targeted by opponent's card effects
+	--This card cannot be destroyed by card effects
 	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_EQUIP)
-	e3:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
-	e3:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-	e3:SetValue(aux.tgoval)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e3:SetValue(1)
 	c:RegisterEffect(e3)
 
-	--Once per turn: banish up to 3 cards from opponent's GY (if Level 10+)
+	--Banish up to 3 cards from opponent's GY (non-targeting)
 	local e4=Effect.CreateEffect(c)
 	e4:SetDescription(aux.Stringid(id,0))
 	e4:SetType(EFFECT_TYPE_IGNITION)
@@ -46,7 +48,7 @@ function s.initial_effect(c)
 	e4:SetOperation(s.rmop)
 	c:RegisterEffect(e4)
 
-	--GY effect: recover 1 other "Nordic Relic" if you control a Level 10 Aesir
+	--GY effect: Add 1 "Nordic Relic" from GY to hand (except itself)
 	local e5=Effect.CreateEffect(c)
 	e5:SetDescription(aux.Stringid(id,1))
 	e5:SetCategory(CATEGORY_TOHAND)
@@ -61,34 +63,38 @@ function s.initial_effect(c)
 	c:RegisterEffect(e5)
 end
 
---Equip to "Aesir"
+--Equip filter
 function s.eqfilter(c)
 	return c:IsFaceup() and c:IsSetCard(0x4b)
 end
 function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.eqfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.eqfilter,tp,LOCATION_MZONE,0,1,nil) end
+	if chk==0 then return e:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.IsExistingTarget(s.eqfilter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-	local g=Duel.SelectTarget(tp,s.eqfilter,tp,LOCATION_MZONE,0,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_EQUIP,g,1,0,0)
+	Duel.SelectTarget(tp,s.eqfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
 end
 function s.eqop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) or not c:IsLocation(LOCATION_SZONE) or c:IsStatus(STATUS_LEAVE_CONFIRMED) then return end
 	local tc=Duel.GetFirstTarget()
-	if not c:IsRelateToEffect(e) or not tc or not tc:IsRelateToEffect(e) or not tc:IsFaceup() then return end
-	Duel.Equip(tp,c,tc)
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsControler(tp) then
+		Duel.Equip(tp,c,tc)
+	else
+		c:CancelToGrave(false)
+	end
 end
 
---Check if equipped to Level 10 or higher monster
+--Banish up to 3 cards from opponent's GY
 function s.rmcon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler():GetEquipTarget()
-	return c and c:IsLevelAbove(10)
+	local ec=e:GetHandler():GetEquipTarget()
+	return ec and ec:IsLevelAbove(10)
 end
 function s.rmop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_GRAVE,nil)
+	local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0,LOCATION_GRAVE,nil)
 	if #g==0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local sg=g:Select(tp,1,3,nil)
+	local sg=g:Select(tp,1,math.min(3,#g),nil)
 	Duel.Remove(sg,POS_FACEUP,REASON_EFFECT)
 end
 
