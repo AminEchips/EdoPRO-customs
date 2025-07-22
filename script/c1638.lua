19d91aec2ff6fffb8241a00f9051add400bbbd19
Chips <aminekhoury0411@gmail.com>
2025-07-21 21:40:48 -0400

    Update c1638.lua

--- script/c1638.lua
+++ script/c1638.lua
@@ -1,13 +1,7 @@
 --Ragnarock of the Aesir Realm
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_ACTIVATE)
-	e0:SetCode(EVENT_FREE_CHAIN)
-	c:RegisterEffect(e0)
-
-	--Effect 1: Draw 1 card when Aesir is Synchro Summoned or from GY
+	--Draw when Aesir is Synchro Summoned or revived from GY
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DRAW)
@@ -21,13 +15,13 @@ function s.initial_effect(c)
 	e1:SetOperation(s.drop)
 	c:RegisterEffect(e1)
 
-	--Effect 2: If Nordic/Aesir Synchro destroyed, revive 2 Nordics
+	--Special Summon 2 Nordic monsters from GY if Nordic/Aesir Synchro is destroyed
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_DESTROYED)
-	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_CARD_TARGET)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetCountLimit(1,{id,1})
 	e2:SetCondition(s.spcon)
@@ -36,13 +30,13 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 
---Check if a summoned Aesir came from Extra Deck or GY
-function s.cfilter(c,tp)
-	return c:IsFaceup() and c:IsSetCard(0x4b) and c:IsType(TYPE_SYNCHRO)
-		and (c:IsPreviousLocation(LOCATION_EXTRA) or c:IsPreviousLocation(LOCATION_GRAVE)) and c:IsSummonPlayer(tp)
+-- e1: Draw
+function s.cfilter1(c,tp)
+	return c:IsSummonPlayer(tp) and c:IsFaceup() and c:IsType(TYPE_SYNCHRO) and c:IsSetCard(0x4b)
+		and (c:IsSummonType(SUMMON_TYPE_SYNCHRO) or (c:IsPreviousLocation(LOCATION_GRAVE)))
 end
 function s.drcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.cfilter,1,nil,tp)
+	return eg:IsExists(s.cfilter1,1,nil,tp)
 end
 function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
@@ -55,22 +49,23 @@ function s.drop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Draw(p,d,REASON_EFFECT)
 end
 
---Check destroyed Nordic or Aesir Synchros
+-- e2: Revival
 function s.desfilter(c,tp)
-	return c:IsPreviousControler(tp)
+	return c:IsReason(REASON_BATTLE+REASON_EFFECT)
 		and c:IsPreviousLocation(LOCATION_MZONE)
-		and c:IsReason(REASON_BATTLE+REASON_EFFECT)
+		and c:IsPreviousControler(tp)
 		and c:IsType(TYPE_SYNCHRO)
+		and c:IsFaceup()
 		and (c:IsSetCard(0x42) or c:IsSetCard(0x4b))
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(s.desfilter,1,nil,tp)
 end
 function s.spfilter(c,e,tp)
-	return c:IsSetCard(0x42) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
+	return c:IsSetCard(0x42) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_DEFENSE)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.spfilter(chkc,e,tp) end
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.spfilter(chkc,e,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>1
 		and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE,0,2,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
@@ -79,14 +74,8 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetTargetCards(e)
-	if #g==0 then return end
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if ft<=0 then return end
-	if #g>ft then
-		g=g:Select(tp,ft,ft,nil)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<#g then return end
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_DEFENSE)
 	end
-	for tc in aux.Next(g) do
-		Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
-	end
-	Duel.SpecialSummonComplete()
 end
