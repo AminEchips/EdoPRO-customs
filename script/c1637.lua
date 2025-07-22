c2ca59dcf6e4b72241495294495884672cd424b2
Chips <aminekhoury0411@gmail.com>
2025-07-21 21:26:40 -0400

    Update c1637.lua

--- script/c1637.lua
+++ script/c1637.lua
@@ -7,43 +7,42 @@ function s.initial_effect(c)
 	e0:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e0)
 
-	--Destroy 1 Level 12 Aesir monster you control, destroy opponent's Attack Position monsters, then optionally manipulate top card
+	--Quick: Destroy Level 12 Aesir, clear opponent field, manipulate top card
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_TOHAND+CATEGORY_TOGRAVE+CATEGORY_REMOVE)
-	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
 	e1:SetRange(LOCATION_SZONE)
 	e1:SetCountLimit(1,id)
 	e1:SetTarget(s.destg)
 	e1:SetOperation(s.desop)
 	c:RegisterEffect(e1)
 
-	--Send this face-up card to the GY; 1 Thor monster you control gains 2000 ATK
+	--Quick: Send this card to GY; select 1 Thor monster, it gains 2000 ATK permanently
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_ATKCHANGE)
-	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetCountLimit(1,{id,1})
-	e2:SetTarget(s.boosttg)
+	e2:SetCondition(function(e) return Duel.IsExistingMatchingCard(s.thorfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil) end)
 	e2:SetOperation(s.boostop)
 	c:RegisterEffect(e2)
 end
 s.listed_names={id,30604579,1619}
 
---Filter for Level 12 Aesir monsters
+-- Filter for Level 12 Aesir
 function s.aesirfilter(c)
 	return c:IsFaceup() and c:IsLevel(12) and c:IsSetCard(0x4b) and c:IsDestructable()
 end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		return Duel.IsExistingMatchingCard(s.aesirfilter,tp,LOCATION_MZONE,0,1,nil)
-	end
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,tp,LOCATION_MZONE)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.aesirfilter,tp,LOCATION_MZONE,0,1,nil) end
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectMatchingCard(tp,s.aesirfilter,tp,LOCATION_MZONE,0,1,1,nil)
 	if #g==0 then return end
 	if Duel.Destroy(g,REASON_EFFECT)==0 then return end
@@ -57,6 +56,7 @@ function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.ConfirmDecktop(tp,1)
 	local tc=Duel.GetDecktopGroup(tp,1):GetFirst()
 	if not tc then return end
+
 	local opt=Duel.SelectOption(tp,aux.Stringid(id,2),aux.Stringid(id,3),aux.Stringid(id,4))
 	if opt==0 then
 		Duel.DisableShuffleCheck()
@@ -69,26 +69,22 @@ function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
---Filter for Thor monsters
+-- Thor filter
 function s.thorfilter(c)
 	return c:IsFaceup() and (c:IsCode(30604579) or c:IsCode(1619))
-end
-function s.boosttg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.thorfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.thorfilter,tp,LOCATION_MZONE,0,1,nil) and e:GetHandler():IsAbleToGrave() end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,s.thorfilter,tp,LOCATION_MZONE,0,1,1,nil)
 end
 function s.boostop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) or Duel.SendtoGrave(c,REASON_EFFECT)==0 then return end
-	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+	local g=Duel.GetMatchingGroup(s.thorfilter,tp,LOCATION_MZONE,0,nil)
+	if #g==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local tg=g:Select(tp,1,1,nil):GetFirst()
+	if tg then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
 		e1:SetValue(2000)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		tc:RegisterEffect(e1)
+		tg:RegisterEffect(e1)
 	end
 end
