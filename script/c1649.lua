ecf12d89792eb850e569d0a9c7feb2f6cd6a2b74
Chips <aminekhoury0411@gmail.com>
2025-07-23 11:32:07 -0400

    Create c1649.lua

---
+++ script/c1649.lua
@@ -0,0 +1,122 @@
+--Vidar, the Aesir Might of Valhalla
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Synchro Summon procedure
+	Synchro.AddProcedure(c,s.tfilter,1,1,aux.FilterBoolFunction(Card.IsType,TYPE_SYNCHRO),1,99)
+	c:EnableReviveLimit()
+
+	-- Battle position + DEF to 0 (non-targeting)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	e1:SetCondition(s.poscon)
+	e1:SetOperation(s.posop)
+	c:RegisterEffect(e1)
+
+	-- Target and banish 1 monster
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_REMOVE)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetTarget(s.rmtg)
+	e2:SetOperation(s.rmop)
+	c:RegisterEffect(e2)
+
+	-- Leave field: revive 2 targets
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_LEAVE_FIELD)
+	e3:SetCondition(s.spcon)
+	e3:SetTarget(s.sptg)
+	e3:SetOperation(s.spop)
+	c:RegisterEffect(e3)
+end
+s.listed_series={0x42}
+
+-- Must be "Nordic" Tuner + 1+ non-Tuner Synchro Monsters
+function s.tfilter(c,scard,sumtype,tp)
+	return c:IsSetCard(0x42,scard,sumtype,tp) and c:IsType(TYPE_TUNER)
+end
+
+-- Battle condition: vs Special Summoned monster
+function s.poscon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local bc=c:GetBattleTarget()
+	return bc and bc:IsSummonType(SUMMON_TYPE_SPECIAL)
+end
+function s.posop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local bc=c:GetBattleTarget()
+	if bc and bc:IsRelateToBattle() then
+		Duel.ChangePosition(bc,POS_FACEUP_DEFENSE,POS_FACEUP_ATTACK,POS_FACEUP_ATTACK,POS_FACEUP_ATTACK)
+		-- DEF becomes 0 permanently
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_DEFENSE_FINAL)
+		e1:SetValue(0)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		bc:RegisterEffect(e1)
+	end
+end
+
+-- Target and banish
+function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsAbleToRemove() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToRemove,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToRemove,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
+end
+function s.rmop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
+		e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1,tc:GetOriginalCode()) -- store for ref
+	end
+end
+
+-- Leave field condition (must be face-up when it leaves)
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousPosition(POS_FACEUP)
+end
+
+-- Target 1 previously banished monster and 1 Synchro in GY (not itself)
+function s.spfilter1(c,e,tp)
+	return c:IsFaceup() and c:IsAbleToReturn() and c:IsLocation(LOCATION_REMOVED)
+end
+function s.spfilter2(c,e,tp,id)
+	return c:IsType(TYPE_SYNCHRO) and not c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	if chkc then return false end
+	if chk==0 then
+		return Duel.IsExistingTarget(s.spfilter1,tp,LOCATION_REMOVED,LOCATION_REMOVED,1,nil,e,tp)
+			and Duel.IsExistingTarget(function(tc) return s.spfilter2(tc,e,tp,c:GetCode()) end,tp,LOCATION_GRAVE,0,1,nil)
+	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g1=Duel.SelectTarget(tp,s.spfilter1,tp,LOCATION_REMOVED,LOCATION_REMOVED,1,1,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g2=Duel.SelectTarget(tp,function(tc) return s.spfilter2(tc,e,tp,c:GetCode()) end,tp,LOCATION_GRAVE,0,1,1,nil)
+	g1:Merge(g2)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g2,1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetTargetCards(e)
+	local summon=Group.CreateGroup()
+	for tc in g:Iter() do
+		if tc:IsLocation(LOCATION_GRAVE) and tc:IsControler(tp) then
+			summon:AddCard(tc)
+		else
+			Duel.SpecialSummonStep(tc,0,tc:GetOwner(),tc:GetOwner(),false,false,POS_FACEUP)
+		end
+	end
+	Duel.SpecialSummon(summon,0,tp,tp,false,false,POS_FACEUP)
+end
