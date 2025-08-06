242b0eb228df1825d3aad3c21db4edd7c5acc63e
Chips <aminekhoury0411@gmail.com>
2025-08-06 13:15:32 -0400

    Create c139.lua

---
+++ script/c139.lua
@@ -0,0 +1,92 @@
+--Performapal Nightmare Knight
+local s,id=GetID()
+function s.initial_effect(c)
+	--(1) If sent to GY: Add 1 "Rank-Up-Magic" card
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_TO_GRAVE)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.thcon)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+
+	--(2) Quick Effect from GY: Inflict 1000 damage to players who took battle damage
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_DAMAGE)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCost(aux.bfgcost) -- banish self
+	e2:SetCondition(s.condition)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+
+	--Global battle damage tracker
+	aux.GlobalCheck(s,function()
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_BATTLE_DAMAGE)
+		ge1:SetOperation(s.gop)
+		ge1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		Duel.RegisterEffect(ge1,0)
+	end)
+end
+
+s.listed_series={0x95}
+
+--(1) Add Rank-Up-Magic from Deck
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsReason(REASON_EFFECT+REASON_BATTLE)
+end
+function s.thfilter(c)
+	return c:IsSetCard(0x95) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+
+--(2) Inflict 1000 damage to player(s) who took battle damage
+function s.gop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.RegisterFlagEffect(ep,id,RESET_PHASE+PHASE_END,0,1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentPhase()>=PHASE_BATTLE_START and Duel.GetCurrentPhase()<=PHASE_BATTLE and Duel.IsTurnPlayer(tp)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetFlagEffect(tp,id)~=0 or Duel.GetFlagEffect(1-tp,id)~=0 end
+	local dep=nil
+	if Duel.GetFlagEffect(tp,id)~=0 and Duel.GetFlagEffect(1-tp,id)~=0 then
+		dep=PLAYER_ALL
+	elseif Duel.GetFlagEffect(tp,id)~=0 then
+		dep=tp
+	else
+		dep=1-tp
+	end
+	Duel.SetTargetPlayer(dep)
+	Duel.SetTargetParam(1000)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,dep,1000)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	if p~=PLAYER_ALL then
+		Duel.Damage(p,d,REASON_EFFECT)
+	else
+		Duel.Damage(tp,d,REASON_EFFECT,true)
+		Duel.Damage(1-tp,d,REASON_EFFECT,true)
+		Duel.RDComplete()
+	end
+end
