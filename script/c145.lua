24828c212813faa6884f720517956fd6b2c90501
Chips <aminekhoury0411@gmail.com>
2025-08-15 22:34:02 -0400

    Update c145.lua

--- script/c145.lua
+++ script/c145.lua
@@ -17,7 +17,7 @@ function s.initial_effect(c)
 	e1:SetTarget(s.dmtg)
 	e1:SetOperation(s.dmop)
 	c:RegisterEffect(e1)
-	--(2) If sent to GY: add to hand or Special Summon a Performapal / "Magician" Pendulum / Odd-Eyes from your GY (except itself)
+	--(2) If sent to GY: add to hand or Special Summon (NON-TARGETING)
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,2))
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
@@ -29,11 +29,11 @@ function s.initial_effect(c)
 	e2:SetOperation(s.gyop)
 	c:RegisterEffect(e2)
 end
--- list for script helpers
+-- list
 s.listed_series={0x99,0x9f,0x98} -- Odd-Eyes, Performapal, "Magician"
 s.listed_names={id}
 
--- "Magician" Pendulum in your Extra Deck (face-up) required
+-- "Magician" Pendulum in your face-up Extra Deck
 function s.magpend(c)
 	return c:IsFaceup() and c:IsSetCard(0x98) and c:IsType(TYPE_PENDULUM)
 end
@@ -45,12 +45,10 @@ function s.dmtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetTargetPlayer(1-tp)
 	Duel.SetTargetParam(500)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,500)
-	-- draw info is optional & checked on resolution
 end
 function s.dmop(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	if Duel.Damage(p,d,REASON_EFFECT)>0 then
-		-- can draw if you control an Xyz Monster
 		local canDraw=Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_MZONE,0,1,nil,TYPE_XYZ)
 			and Duel.IsPlayerCanDraw(tp,1)
 		if canDraw and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
@@ -60,28 +58,28 @@ function s.dmop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- GY target filter
+-- ====== GY effect (non-targeting) ======
 function s.gyfilter(c,e,tp)
 	if c:IsCode(id) then return false end
 	local okset = c:IsSetCard(0x9f) or (c:IsSetCard(0x98) and c:IsType(TYPE_PENDULUM)) or c:IsSetCard(0x99)
 	return okset and (c:IsAbleToHand() or c:IsCanBeSpecialSummoned(e,0,tp,false,false))
 end
-function s.gytg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.gyfilter(chkc,e,tp) end
+function s.gytg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.gyfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	local g=Duel.SelectMatchingCard(tp,s.gyfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+	-- No target selected here; select at resolution
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
 function s.gyop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if not (tc and tc:IsRelateToEffect(e)) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_OPERATECARD)
+	local g=Duel.SelectMatchingCard(tp,s.gyfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	local tc=g:GetFirst()
+	if not tc then return end
 	local canSS=tc:IsCanBeSpecialSummoned(e,0,tp,false,false)
 	local canTH=tc:IsAbleToHand()
-	local op=nil
+	local op
 	if canSS and canTH then
-		op=Duel.SelectOption(tp,aux.Stringid(id,3),aux.Stringid(id,4)) -- 0: add, 1: SS
+		op=Duel.SelectOption(tp,aux.Stringid(id,3),aux.Stringid(id,4)) -- 0:add, 1:SS
 	elseif canTH then
 		op=0
 	elseif canSS then
@@ -90,8 +88,7 @@ function s.gyop(e,tp,eg,ep,ev,re,r,rp)
 		return
 	end
 	if op==1 then
-		if Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)<=0 and tc:IsAbleToHand() and tc:IsRelateToEffect(e) then
-			-- fallback: add to hand if SS failed and possible
+		if Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)<=0 and tc:IsRelateToEffect(e) and tc:IsAbleToHand() then
 			Duel.SendtoHand(tc,nil,REASON_EFFECT)
 			Duel.ConfirmCards(1-tp,tc)
 		end
