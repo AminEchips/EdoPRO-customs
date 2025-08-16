be8f172a73f022786b61da5481f2b6da0d9cc3df
Chips <aminekhoury0411@gmail.com>
2025-08-15 22:23:59 -0400

    Create c145.lua

---
+++ script/c145.lua
@@ -0,0 +1,103 @@
+--Odd-Eyes Cometvolly Dragon
+local s,id=GetID()
+function s.initial_effect(c)
+	--Synchro Summon
+	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTuner(nil),1,99)
+	c:EnableReviveLimit()
+	--(1) Quick Effect: 500 damage, then optional draw if you control an Xyz
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DAMAGE+CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E+TIMING_MAIN_END)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.dmcon)
+	e1:SetTarget(s.dmtg)
+	e1:SetOperation(s.dmop)
+	c:RegisterEffect(e1)
+	--(2) If sent to GY: add to hand or Special Summon a Performapal / "Magician" Pendulum / Odd-Eyes from your GY (except itself)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,2))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e2:SetCountLimit(1,id+100)
+	e2:SetTarget(s.gytg)
+	e2:SetOperation(s.gyop)
+	c:RegisterEffect(e2)
+end
+-- list for script helpers
+s.listed_series={0x99,0x9f,0x98} -- Odd-Eyes, Performapal, "Magician"
+s.listed_names={id}
+
+-- "Magician" Pendulum in your Extra Deck (face-up) required
+function s.magpend(c)
+	return c:IsFaceup() and c:IsSetCard(0x98) and c:IsType(TYPE_PENDULUM)
+end
+function s.dmcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.magpend,tp,LOCATION_EXTRA,0,1,nil)
+end
+function s.dmtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(500)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,500)
+	-- draw info is optional & checked on resolution
+end
+function s.dmop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	if Duel.Damage(p,d,REASON_EFFECT)>0 then
+		-- can draw if you control an Xyz Monster
+		local canDraw=Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_MZONE,0,1,nil,TYPE_XYZ)
+			and Duel.IsPlayerCanDraw(tp,1)
+		if canDraw and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+			Duel.BreakEffect()
+			Duel.Draw(tp,1,REASON_EFFECT)
+		end
+	end
+end
+
+-- GY target filter
+function s.gyfilter(c,e,tp)
+	if c:IsCode(id) then return false end
+	local okset = c:IsSetCard(0x9f) or (c:IsSetCard(0x98) and c:IsType(TYPE_PENDULUM)) or c:IsSetCard(0x99)
+	return okset and (c:IsAbleToHand() or c:IsCanBeSpecialSummoned(e,0,tp,false,false))
+end
+function s.gytg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.gyfilter(chkc,e,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.gyfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectTarget(tp,s.gyfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function s.gyop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if not (tc and tc:IsRelateToEffect(e)) then return end
+	local canSS=tc:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	local canTH=tc:IsAbleToHand()
+	local op=nil
+	if canSS and canTH then
+		op=Duel.SelectOption(tp,aux.Stringid(id,3),aux.Stringid(id,4)) -- 0: add, 1: SS
+	elseif canTH then
+		op=0
+	elseif canSS then
+		op=1
+	else
+		return
+	end
+	if op==1 then
+		if Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)<=0 and tc:IsAbleToHand() and tc:IsRelateToEffect(e) then
+			-- fallback: add to hand if SS failed and possible
+			Duel.SendtoHand(tc,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,tc)
+		end
+	else
+		if Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 then
+			Duel.ConfirmCards(1-tp,tc)
+		end
+	end
+end
