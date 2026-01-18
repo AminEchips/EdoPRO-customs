14f290e54d2348dc9e5d7da9b201eea36a02ebdd
Chips <aminekhoury0411@gmail.com>
2026-01-18 12:35:31 -0500

    Update c110.lua

--- script/c110.lua
+++ script/c110.lua
@@ -1,147 +1,151 @@
---Performapal Spectral Magician
+--Supreme King Gate Reality
 local s,id=GetID()
+s.listed_names={13331639}
+s.listed_series={0x10f8, 0x20f8, 0x10f2, 0x1046, 0x2017, 0x2073}
+
 function s.initial_effect(c)
-	--Pendulum Summon
 	Pendulum.AddProcedure(c)
 
-	-- Always treated as "Predaplant"
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_SINGLE)
-	e0:SetCode(EFFECT_ADD_SETCODE)
-	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e0:SetValue(0x10f3) -- Predaplant
-	c:RegisterEffect(e0)
-
-	-- Pendulum Effect: Optional when DARK Pendulum declares attack
+	--Pendulum Effect 1: Z-ARC battle indestructible once per turn
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_ATKCHANGE)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
 	e1:SetRange(LOCATION_PZONE)
+	e1:SetTargetRange(LOCATION_MZONE,0)
 	e1:SetCountLimit(1)
-	e1:SetCondition(s.atkoptcon)
-	e1:SetTarget(s.atkoptg)
-	e1:SetOperation(s.atkopop)
+	e1:SetValue(1)
+	e1:SetTarget(s.indtg)
 	c:RegisterEffect(e1)
 
-	-- Monster Effect: from hand, destroy 1 DARK monster you control or 1 card in your PZone; choose 1 effect
+	--Pendulum Effect 2: If Z-ARC leaves field, revive a banished Dragon
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
-	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetRange(LOCATION_HAND)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_LEAVE_FIELD)
+	e2:SetRange(LOCATION_PZONE)
 	e2:SetCountLimit(1,id)
-	e2:SetTarget(s.hdtg)
-	e2:SetOperation(s.hdop)
+	e2:SetCondition(s.spcon)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
+
+	--Quick Effect: Special Summon from hand, then optionally send Dragon ED monster to GY
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOGRAVE)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_CHAINING)
+	e3:SetRange(LOCATION_HAND)
+	e3:SetCountLimit(1,id+100)
+	e3:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_CHAIN_END)
+	e3:SetCondition(s.hspcon)
+	e3:SetTarget(s.hsptg)
+	e3:SetOperation(s.hspop)
+	c:RegisterEffect(e3)
+
+	--End Phase: Place Supreme King Gate into Pendulum Zone
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,2))
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_PHASE+PHASE_END)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1,id+200)
+	e4:SetTarget(s.pztg)
+	e4:SetOperation(s.pzop)
+	c:RegisterEffect(e4)
+
+	--If destroyed: place itself in Pendulum Zone
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e5:SetCode(EVENT_DESTROYED)
+	e5:SetProperty(EFFECT_FLAG_DELAY)
+	e5:SetCondition(function(e) return e:GetHandler():IsPreviousLocation(LOCATION_MZONE) end)
+	e5:SetTarget(s.pztg2)
+	e5:SetOperation(s.pzop2)
+	c:RegisterEffect(e5)
 end
 
--- PENDULUM EFFECT (optional on attack declare)
-function s.atkoptcon(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetAttacker()
-	local otherpz=Duel.GetFieldCard(tp,LOCATION_PZONE,1)
-	return tc and tc:IsControler(tp)
-		and tc:IsAttribute(ATTRIBUTE_DARK) and tc:IsType(TYPE_PENDULUM)
-		and otherpz and otherpz:IsAttribute(ATTRIBUTE_DARK)
+-- Z-ARC battle indestructible
+function s.indtg(e,c)
+	return c:IsCode(13331639)
 end
-function s.atkoptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsDestructable() end
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,e:GetHandler(),1,0,0)
+
+-- Z-ARC leaves field
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(Card.IsCode,1,nil,13331639)
 end
-function s.atkopop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local tc=Duel.GetAttacker()
-	if not c:IsRelateToEffect(e) or Duel.Destroy(c,REASON_EFFECT)==0 then return end
-	if tc:IsRelateToBattle() and tc:IsFaceup() and tc:IsControler(tp) then
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetValue(1200)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		tc:RegisterEffect(e1)
+function s.spfilter(c,e,tp)
+	return c:IsRace(RACE_DRAGON) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_REMOVED)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_REMOVED,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
 
--- MONSTER EFFECT
-function s.hdtgfilter(c,tp)
-	return (c:IsFaceup() and c:IsControler(tp) and c:IsLocation(LOCATION_MZONE) and c:IsAttribute(ATTRIBUTE_DARK))
-		or (c:IsControler(tp) and c:IsLocation(LOCATION_PZONE))
+-- Quick Effect: summon self + optionally send ED monster
+function s.hspcon(e,tp,eg,ep,ev,re,r,rp)
+	return rp==1-tp and re~=nil
 end
-
-function s.banfilter(c,e,tp)
-	return c:IsFaceup() and c:IsType(TYPE_PENDULUM) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-		and (c:IsSetCard(0x9f) or c:IsSetCard(0x99) or c:IsSetCard(0x98)) -- Performapal / Odd-Eyes / Magician
-end
-
-function s.hdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and s.hdtgfilter(chkc,tp) end
-	if chk==0 then
-		return Duel.IsExistingTarget(s.hdtgfilter,tp,LOCATION_MZONE+LOCATION_PZONE,0,1,nil,tp)
-	end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,s.hdtgfilter,tp,LOCATION_MZONE+LOCATION_PZONE,0,1,1,nil,tp)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
-	-- (we don't pre-declare special summon targets because the effect branches)
-end
-
-function s.hdop(e,tp,eg,ep,ev,re,r,rp)
+function s.hsptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	local tc=Duel.GetFirstTarget()
-	if not tc or not tc:IsRelateToEffect(e) then return end
-	if Duel.Destroy(tc,REASON_EFFECT)==0 then return end
+	if chk==0 then return c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+function s.hspfilter(c)
+	return c:IsAbleToGrave()
+		and (c:IsSetCard(0x10f2) or c:IsSetCard(0x1046) or c:IsSetCard(0x2017) or c:IsSetCard(0x2073))
+end
+function s.hspop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
-
-	-- Build which main options are available
-	local canA=true -- "SS this OR place in PZONE" (we'll check the sub-choices after selecting A)
-	local canB=Duel.IsExistingMatchingCard(s.banfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp)
-	local opts={}
-	local map={} -- maps displayed index -> main option id (1=A,2=B)
-	if canA then
-		opts[#opts+1]=aux.Stringid(id,2)
-		map[#opts]=1
-	end
-	if canB then
-		opts[#opts+1]=aux.Stringid(id,3)
-		map[#opts]=2
-	end
-	if #opts==0 then return end
-
-	local sel=Duel.SelectOption(tp,table.unpack(opts))
-	local choice=map[sel+1]
-
-	if choice==1 then
-		-- After destroying, either Special Summon this card OR place it in the Pendulum Zone
-		local canSS=Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-		local canPZ=Duel.CheckPendulumZones(tp) and not c:IsForbidden()
-
-		if not canSS and not canPZ then return end
-
-		local sub=0
-		if canSS and canPZ then
-			sub=Duel.SelectOption(tp,aux.Stringid(id,4),aux.Stringid(id,5)) -- SS / place in PZONE
-		elseif canSS then
-			sub=0
-		else
-			sub=1
-		end
-
-		if sub==0 then
-			Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
-		else
-			Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
-		end
-
-	else
-		-- Special Summon 1 of your banished Performapal/Odd-Eyes/Magician Pendulum Monsters
-		if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local g=Duel.SelectMatchingCard(tp,s.banfilter,tp,LOCATION_REMOVED,0,1,1,nil,e,tp)
-		local sc=g:GetFirst()
-		if sc then
-			Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)
+	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
+		if Duel.IsExistingMatchingCard(s.hspfilter,tp,LOCATION_EXTRA,0,1,nil)
+			and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+			local g=Duel.SelectMatchingCard(tp,s.hspfilter,tp,LOCATION_EXTRA,0,1,1,nil)
+			if #g>0 then
+				Duel.SendtoGrave(g,REASON_EFFECT)
+			end
 		end
 	end
 end
 
+-- End Phase place SK Gate to Pend Zone
+function s.pzfilter(c)
+	return c:IsSetCard(0x10f8) and c:IsType(TYPE_PENDULUM) and not c:IsForbidden()
+end
+function s.pztg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		return (Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1))
+			and Duel.IsExistingMatchingCard(s.pzfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE+LOCATION_EXTRA,0,1,nil)
+	end
+end
+function s.pzop(e,tp,eg,ep,ev,re,r,rp)
+	if not (Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1)) then return end
+	local g=Duel.SelectMatchingCard(tp,s.pzfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE+LOCATION_EXTRA,0,1,1,nil)
+	if #g>0 then
+		Duel.MoveToField(g:GetFirst(),tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+	end
+end
+
+-- Float to Pend Zone
+function s.pztg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		return Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1)
+	end
+end
+function s.pzop2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	if Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1) then
+		Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+	end
+end
